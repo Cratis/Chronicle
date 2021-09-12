2a20afe7445916130daaebae226fd1846050f12a
Einar Ingebrigtsen <einari@me.com>
2021-09-12 19:25:33 +0200

    Scaffolded new Web project for event workbench

---
+++ Source/Workbench/Events/Web/App.tsx
@@ -0,0 +1,10 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export const App = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
---
+++ Source/Workbench/Events/Web/webpack.config.js
@@ -0,0 +1,18 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+const path = require('path');
+
+const webpack = require('@cratis/webpack/frontend');
+module.exports = (env, argv) => {
+    const isWebDevServer = (process.env.WEBPACK_DEV_SERVER || false) === 'true' ? true : false;
+    const basePath = isWebDevServer ? '/' : './';
+    return webpack(env, argv, basePath, config => {
+        config.output.path = path.resolve(process.cwd(), '..', 'build');
+        config.devServer.port = 9000;
+        config.devServer.proxy = {
+            '/graphql': 'http://localhost:5000',
+            '/api': 'http://localhost:5000'
+        }
+    }, 'Cratis Events Workbench');
+};
