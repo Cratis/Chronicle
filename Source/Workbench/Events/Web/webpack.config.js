64f1bbfef29edff0114b12cf2eaaf324d9b7ffea
Einar Ingebrigtsen <einari@me.com>
2021-09-24 13:50:34 +0200

    Making it possible to override the base path

--- Source/Workbench/Events/Web/webpack.config.js
+++ Source/Workbench/Events/Web/webpack.config.js
@@ -6,9 +6,8 @@ const path = require('path');
 const webpack = require('@cratis/webpack/frontend');
 module.exports = (env, argv) => {
     const isWebDevServer = (process.env.WEBPACK_DEV_SERVER || false) === 'true' ? true : false;
-    const basePath = isWebDevServer ? '/' : './';
+    const basePath = isWebDevServer ? '/' : process.env.base_path || './';
     return webpack(env, argv, basePath, config => {
-        config.output.path = path.resolve(process.cwd(), '..', 'build');
         config.devServer.port = 9000;
         config.devServer.proxy = {
             '/graphql': 'http://localhost:5000',
