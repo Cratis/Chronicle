233716bbe70a9850e8401c326e0b8e2d9c50a3a4
Einar Ingebrigtsen <einari@me.com>
2021-09-27 11:26:43 +0200

    Fixing basePath for devServer

--- Source/Workbench/Events/Web/webpack.config.js
+++ Source/Workbench/Events/Web/webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const webpack = require('@cratis/webpack/frontend');
 module.exports = (env, argv) => {
     const isWebDevServer = (process.env.WEBPACK_DEV_SERVER || false) === 'true' ? true : false;
-    const basePath = isWebDevServer ? '/' : process.env.base_path || './';
+    const basePath = isWebDevServer ? '/' : process.env.base_path || '';
     return webpack(env, argv, basePath, config => {
         config.devServer.port = 9000;
         config.devServer.proxy = {
