50c530cfcbc150ecb849d3032391579796f53308
Einar Ingebrigtsen <einari@me.com>
2021-09-28 16:14:17 +0200

    Separating out the chart selector

--- Source/Workbench/Events/Web/EventLogs.tsx
+++ Source/Workbench/Events/Web/EventLogs.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { useRef, useEffect } from 'react';
-import * as echarts from 'echarts';
+
+
 import {
     CommandBar,
     DetailsList,
@@ -17,85 +17,8 @@ import { default as styles } from './EventLogs.module.scss';
 import { useDataFrom } from './useDataFrom';
 import { EventLogInformation } from './EventLogInformation';
 import { EventType } from './EventType';
+import { EventChartSelector } from './EventChartSelector';
 
-type EChartsOption = echarts.EChartsOption;
-
-const dataCount = 42;
-const data = generateData(dataCount);
-
-const option: EChartsOption = {
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'shadow'
-        }
-    },
-    grid: {
-        bottom: 90
-    },
-    dataZoom: [
-        {
-            type: 'inside'
-        },
-        {
-            type: 'slider'
-        }
-    ],
-    xAxis: {
-        data: data.categoryData,
-        silent: false,
-        splitLine: {
-            show: false
-        },
-        splitArea: {
-            show: false
-        }
-    },
-    yAxis: {
-        splitArea: {
-            show: false
-        }
-    },
-    series: [
-        {
-            type: 'line',
-            data: data.valueData,
-            smooth: true,
-            large: true
-        } as any
-    ]
-};
-
-function generateData(count: number) {
-    let baseValue = Math.random() * 2;
-    let time = +new Date(2011, 0, 1);
-    let smallBaseValue: number;
-
-    function next(idx: number) {
-        smallBaseValue =
-            idx % 30 === 0
-                ? Math.random() * 10
-                : smallBaseValue + Math.random() * 5 - 2;
-        baseValue += Math.random() * 5 - 2;
-        return Math.max(0, Math.round(baseValue + smallBaseValue) + 30);
-    }
-
-    const categoryData: any[] = [];
-    const valueData: any[] = [];
-
-    for (let i = 0; i < count; i++) {
-        categoryData.push(
-            echarts.format.formatTime('yyyy-MM-dd\nhh:mm:ss', time, false)
-        );
-        valueData.push(next(i).toFixed(2));
-        time += 1000;
-    }
-
-    return {
-        categoryData: categoryData,
-        valueData: valueData
-    };
-}
 
 const eventListColumns: IColumn[] = [
 
@@ -122,7 +45,7 @@ const eventListColumns: IColumn[] = [
 
 // https://echarts.apache.org/examples/en/
 export const EventLogs = () => {
-    const chartContainer = useRef<HTMLDivElement>(null);
+
     const [eventLogs, refreshEventLogs] = useDataFrom<ICommandBarItemProps>('/api/events/store/logs', (_: EventLogInformation) => {
         return {
             key: _.id,
@@ -138,23 +61,6 @@ export const EventLogs = () => {
 
     const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
 
-    const getChart = () => echarts.getInstanceByDom(chartContainer.current!);
-
-    useEffect(() => {
-        if (chartContainer.current) {
-            const chart = echarts.init(chartContainer.current);
-            chart.setOption(option);
-            chart.resize();
-        }
-    }, []);
-
-    useEffect(() => {
-        const listener = () => {
-            getChart().resize();
-        };
-        window.addEventListener('resize', listener);
-        return () => window.removeEventListener('resize', listener);
-    }, []);
 
 
     const commandBarItems: ICommandBarItemProps[] = [
@@ -183,11 +89,11 @@ export const EventLogs = () => {
             text: 'Reset Zoom',
             iconProps: { iconName: 'ZoomToFit' },
             onClick: () => {
-                getChart().dispatchAction({
-                    type: 'dataZoom',
-                    start: 0,
-                    end: 100
-                });
+                // getChart().dispatchAction({
+                //     type: 'dataZoom',
+                //     start: 0,
+                //     end: 100
+                // });
             }
         }
     ];
@@ -216,7 +122,7 @@ export const EventLogs = () => {
             <div className={styles.commandBar}>
                 <CommandBar items={commandBarItems} />
             </div>
-            <div className={styles.eventSamplesContainer} ref={chartContainer} />
+            <EventChartSelector/>
             <div className={styles.eventList}>
                 <DetailsList
                     columns={eventListColumns}
