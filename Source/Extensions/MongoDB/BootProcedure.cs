b8aded0f2a0c039a4d11904d2b1679c4e9923bf2
Einar Ingebrigtsen <einari@me.com>
2021-10-01 10:13:09 +0200

    Using BootProcedure instead of abusing Autofac modules

--- Source/Extensions/MongoDB/BootProcedure.cs
+++ Source/Extensions/MongoDB/BootProcedure.cs
@@ -1,16 +1,17 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Autofac;
+using Cratis.Boot;
 
 namespace Cratis.Extensions.MongoDB
 {
     /// <summary>
-    /// Represents a <see cref="Module"/> for setting up defaults and bindings for MongoDB
+    /// Represents a <see cref="IPerformBootProcedure"/> for setting up defaults and bindings for MongoDB
     /// </summary>
-    public class MongoDBModule : Module
+    public class BootProcedure : IPerformBootProcedure
     {
-        protected override void Load(ContainerBuilder builder)
+        /// <inheritdoc/>
+        public void Perform()
         {
             MongoDBDefaults.Initialize();
         }
