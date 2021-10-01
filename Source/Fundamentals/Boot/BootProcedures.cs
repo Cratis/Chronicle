35015a4087988962e3888f1e0d1781e7e119d196
Einar Ingebrigtsen <einari@me.com>
2021-10-01 10:12:27 +0200

    Introducing boot procedures

---
+++ Source/Fundamentals/Boot/BootProcedures.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Collections;
+using Cratis.Types;
+
+namespace Cratis.Boot
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IBootProcedures"/>.
+    /// </summary>
+    public class BootProcedures : IBootProcedures
+    {
+        readonly IInstancesOf<IPerformBootProcedure> _procedures;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="BootProcedures"/>.
+        /// </summary>
+        /// <param name="procedures"><see cref="IInstancesOf{T}"/> <see cref="IPerformBootProcedure"/>.</param>
+        public BootProcedures(IInstancesOf<IPerformBootProcedure> procedures) => _procedures = procedures;
+
+        /// <inheritdoc/>
+        public void Perform() => _procedures.ForEach(_ => _.Perform());
+    }
+}
---
+++ Source/Fundamentals/Boot/IPerformBootProcedure.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Boot
+{
+    /// <summary>
+    /// Marker interface that can be used for a boot procedure that will be called automatically during startup.
+    /// </summary>
+    public interface IPerformBootProcedure
+    {
+        /// <summary>
+        /// The method that will be called.
+        /// </summary>
+        void Perform();
+    }
+}
