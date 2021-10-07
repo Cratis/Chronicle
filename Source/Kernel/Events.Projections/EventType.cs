3ee5137ad79edeb1b1cb9141cc4b94a7858cab02
Einar Ingebrigtsen <einari@me.com>
2021-10-07 19:28:18 +0200

    Adding implicit operators for converting from underlying type to concept type

--- Source/Kernel/Events.Projections/EventType.cs
+++ Source/Kernel/Events.Projections/EventType.cs
@@ -9,5 +9,13 @@ namespace Cratis.Events.Projections
     /// Represents the unique identifier of the type of an event.
     /// </summary>
     /// <param name="Value">The value.</param>
-    public record EventType(string Value) : ConceptAs<string>(Value);
+    public record EventType(string Value) : ConceptAs<string>(Value)
+    {
+        /// <summary>
+        /// Implicitly convert from <see cref="string"/> to <see cref="EventType"/>.
+        /// </summary>
+        /// <param name="Value">Value to convert from.</param>
+        /// <returns>A converted <see cref="EventType"/>.</returns>;
+        public static implicit operator EventType(string Value) => new (Value);
+    }
 }
