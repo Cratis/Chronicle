4bd37d4d74d8b1c9e099c5e0e22442332f14377d
Einar Ingebrigtsen <einari@me.com>
2021-10-07 19:33:41 +0200

    Adding spec for when there aren't any changes

---
+++ Specifications/Kernel/Events.Projections/for_Changeset/when_applying_properties_that_does_not_cause_changes.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+
+namespace Cratis.Events.Projections
+{
+    public class when_applying_properties_that_does_not_cause_changes : Specification
+    {
+        Changeset   changeset;
+        ExpandoObject initial_state;
+        Event @event;
+        IEnumerable<PropertyMapper> property_mappers;
+
+        void Establish()
+        {
+            initial_state = new();
+
+            ((dynamic)initial_state).Integer = 42;
+            ((dynamic)initial_state).String = "Forty Two";
+            dynamic nested = ((dynamic)initial_state).Nested = new ExpandoObject();
+            nested.Integer = 43;
+            nested.String = "Forty Three";
+
+            property_mappers = new PropertyMapper[]
+            {
+                (_, target) => ((dynamic)target).Integer = 42,
+                (_, target) => ((dynamic)target).String = "Forty Two",
+                (_, target) => ((dynamic)target).Nested.Integer = 43,
+                (_, target) => ((dynamic)target).Nested.String = "Forty Three",
+            };
+
+            @event = new Event(0, "some event", DateTimeOffset.UtcNow, string.Empty, new object());
+
+            changeset = new Changeset(@event, initial_state);
+        }
+
+        void Because() => changeset.ApplyProperties(property_mappers);
+
+        [Fact] void should_not_have_any_changes() => changeset.Changes.Count().ShouldEqual(0);
+    }
+}
