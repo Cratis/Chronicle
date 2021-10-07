5fb3c3c3fc3b418515390367c731c17ddda2362f
Einar Ingebrigtsen <einari@me.com>
2021-10-07 19:28:18 +0200

    Adding implicit operators for converting from underlying type to concept type

--- Source/Kernel/Events.Projections/EventLogSequenceNumber.cs
+++ Source/Kernel/Events.Projections/EventLogSequenceNumber.cs
@@ -9,5 +9,13 @@ namespace Cratis.Events.Projections
     /// Represents the sequence number within an event log for an event.
     /// </summary>
     /// <param name="Value">The sequence number</param>
-    public record EventLogSequenceNumber(uint Value): ConceptAs<uint>(Value);
+    public record EventLogSequenceNumber(uint Value): ConceptAs<uint>(Value)
+    {
+        /// <summary>
+        /// Implicitly convert from <see cref="uint"/> to <see cref="EventLogSequenceNumber"/>.
+        /// </summary>
+        /// <param name="Value">Value to convert from.</param>
+        /// <returns>A converted <see cref="EventLogSequenceNumber"/>.</returns>;
+        public static implicit operator EventLogSequenceNumber(uint Value) => new (Value);
+    }
 }
--- Source/Kernel/Events.Projections/EventSourceId.cs
+++ Source/Kernel/Events.Projections/EventSourceId.cs
@@ -9,5 +9,13 @@ namespace Cratis.Events.Projections
     /// Represents the unique identifier of an instance of an event source.
     /// </summary>
     /// <param name="Value">The value.</param>
-    public record EventSourceId(string Value) : ConceptAs<string>(Value);
+    public record EventSourceId(string Value) : ConceptAs<string>(Value)
+    {
+        /// <summary>
+        /// Implicitly convert from <see cref="string"/> to <see cref="EventSourceId"/>.
+        /// </summary>
+        /// <param name="Value">Value to convert from.</param>
+        /// <returns>A converted <see cref="EventSourceId"/>.</returns>;
+        public static implicit operator EventSourceId(string Value) => new (Value);
+    }
 }
