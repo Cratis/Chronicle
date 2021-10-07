b5e2b337390a57437f21962bfebfa95ad7c78934
Einar Ingebrigtsen <einari@me.com>
2021-10-07 19:52:46 +0200

    Renaming to pipeline + moving responsibilities for storage

--- Source/Kernel/Events.Projections/IProjectionPipeline.cs
+++ Source/Kernel/Events.Projections/IProjectionPipeline.cs
@@ -6,13 +6,24 @@ namespace Cratis.Events.Projections
     /// <summary>
     /// Defines a system that can coordinate the effort around projections.
     /// </summary>
-    public interface IProjectionCoordinator
+    public interface IProjectionPipeline
     {
+        /// <summary>
+        /// Gets the <see cref="IProjectionEventProvider"/> used in the pipeline.
+        /// </summary>
+        IProjectionEventProvider    EventProvider { get; }
+
         /// <summary>
         /// Provides the projection with a new <see cref="Event"/>.
         /// </summary>
         /// <param name="event"><see cref="Event"/> to provide.</param>
         /// <return>Async Task containing <see cref="Changeset"/> as result.</return>
         Task<Changeset> OnNext(Event @event);
+
+        /// <summary>
+        /// Adds a <see cref="IProjectionStorage"/> for storing results.
+        /// </summary>
+        /// <param name="storage"><see cref="IProjectionStorage"/> to use.</param>
+        void StoreIn(IProjectionStorage storage);
     }
 }
