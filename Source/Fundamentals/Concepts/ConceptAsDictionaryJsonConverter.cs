06b4ddb70234ff32391fbd8c1a451d7e8f781039
Einar Ingebrigtsen <einari@me.com>
2021-10-08 16:20:09 +0200

    Up and running with the simple property mapping scenario end to end

---
+++ Source/Fundamentals/Concepts/ConceptAsDictionaryJsonConverter.cs
@@ -0,0 +1,94 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections;
+using Cratis.Reflection;
+using Newtonsoft.Json;
+
+namespace Cratis.Concepts
+{
+    /// <summary>
+    /// Represents a <see cref="JsonConverter"/> that can serialize and deserialize a <see cref="IDictionary{TKey, TValue}">dictionary</see> of <see cref="ConceptAs{T}"/>
+    /// </summary>
+    public class ConceptAsDictionaryJsonConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            if (objectType.HasInterface(typeof(IDictionary<,>)) && objectType.GetType().IsGenericType)
+            {
+                var keyType = objectType.GetType().GetGenericArguments()[0].GetType().BaseType!;
+                if (keyType.GetType().IsGenericType)
+                {
+                    var genericArgumentType = keyType.GetType().GetGenericArguments()[0];
+                    return typeof(ConceptAs<>).MakeGenericType(genericArgumentType).GetType().IsAssignableFrom(keyType);
+                }
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return default;
+            }
+
+            var keyType = objectType.GetType().GetGenericArguments()[0]!;
+            var keyValueType = keyType
+                .GetType()!
+                .BaseType!
+                .GetType()
+                .GetGenericArguments()[0];
+
+            var valueType = objectType.GetType().GetGenericArguments()[1];
+            var intermediateDictionaryType = typeof(Dictionary<,>).MakeGenericType(typeof(string), valueType);
+            var intermediateDictionary = (Activator.CreateInstance(intermediateDictionaryType) as IDictionary)!;
+            serializer.Populate(reader, intermediateDictionary);
+
+            var valueProperty = keyType.GetType().GetProperty("Value")!;
+            var finalDictionary = (Activator.CreateInstance(objectType) as IDictionary)!;
+            foreach (DictionaryEntry pair in intermediateDictionary)
+            {
+                object value;
+                if (keyValueType == typeof(Guid))
+                {
+                    value = Guid.Parse(pair.Key.ToString()!);
+                }
+                else
+                {
+                    value = Convert.ChangeType(pair.Key, keyValueType, null);
+                }
+
+                var key = Activator.CreateInstance(keyType)!;
+                valueProperty.SetValue(key, value, null);
+                finalDictionary.Add(key, pair.Value);
+            }
+            return finalDictionary;
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var dictionary = value as IDictionary;
+            if( dictionary == default) return;
+
+            var objectType = dictionary.GetType();
+            var keyType = objectType.GetType().GetGenericArguments()[0];
+            var valueType = objectType.GetType().GetGenericArguments()[1];
+            var intermediateDictionaryType = typeof(Dictionary<,>).MakeGenericType(typeof(string), valueType);
+            var intermediateDictionary = (Activator.CreateInstance(intermediateDictionaryType) as IDictionary)!;
+            var valueProperty = keyType.GetType().GetProperty("Value")!;
+
+            foreach (DictionaryEntry pair in dictionary)
+            {
+                var keyValue = valueProperty.GetValue(pair.Key, null)!.ToString()!;
+                intermediateDictionary[keyValue] = pair.Value;
+            }
+
+            writer.WriteValue(intermediateDictionary);
+        }
+    }
+}
---
+++ Source/Kernel/Events.Projections/Json/JsonProjectionParser.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+using System.Linq.Expressions;
+using Cratis.Concepts;
+using Newtonsoft.Json;
+
+namespace Cratis.Events.Projections.Json
+{
+    public class FromDefinition : Dictionary<string, string> { }
+
+    public record ProjectionDefinition(ProjectionId Identifier, IDictionary<EventType, FromDefinition> From);
+
+    /// <summary>
+    /// Represents a parser for JSON definition of a <see cref="IProjection"/>.
+    /// </summary>
+    public class JsonProjectionParser
+    {
+        /// <summary>
+        /// Parse a JSON string definition and produce a <see cref="IProjection"/>.
+        /// </summary>
+        /// <param name="json">JSON to parse.</param>
+        /// <returns><see cref="IProjection"/> instance.</returns>
+        public IProjection Parse(string json)
+        {
+            var eventsForProjection = new List<EventType>();
+            var definition = JsonConvert.DeserializeObject<ProjectionDefinition>(json,
+                new ConceptAsJsonConverter(),
+                new ConceptAsDictionaryJsonConverter())!;
+
+            eventsForProjection.AddRange(definition.From.Keys);
+
+            var eventParameter = Expression.Parameter(typeof(Event));
+            var targetParameter = Expression.Parameter(typeof(ExpandoObject));
+            var contentAccessor = Expression.Property(eventParameter, typeof(Event), "Content");
+            var itemProperty = typeof(IDictionary<string, object>).GetProperty("Item")!;
+
+            var projection = new Projection(definition.Identifier, eventsForProjection);
+            foreach (var (eventType, definitions) in definition.From)
+            {
+                var propertyMappers = new List<PropertyMapper>();
+                foreach (var (target, source) in definitions)
+                {
+                    var propertyMapperExpression = Expression.Assign(
+                        Expression.Property(targetParameter, itemProperty, Expression.Constant(target)),
+                        Expression.Property(
+                            contentAccessor,
+                            itemProperty,
+                            Expression.Constant(source)));
+
+                    propertyMappers.Add(Expression.Lambda<PropertyMapper>(propertyMapperExpression, new[] {
+                        eventParameter,
+                        targetParameter
+                    }).Compile());
+                }
+
+                projection.Event.From(eventType).Project(propertyMappers);
+            }
+
+            return projection;
+        }
+    }
+}
