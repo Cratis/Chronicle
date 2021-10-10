a1005dd36d879f8ac1ec6daa66eed2c599b3c6c4
Einar Ingebrigtsen <einari@me.com>
2021-10-10 09:07:16 +0200

    Speccing out the pipeline with key resolvers

--- Specifications/Kernel/Events.Projections/for_ProjectionPipeline/when_events_are_provided_and_we_have_two_storage_providers.cs
+++ Specifications/Kernel/Events.Projections/for_ProjectionPipeline/when_events_are_provided_and_we_have_two_storage_providers.cs
@@ -8,11 +8,13 @@ namespace Cratis.Events.Projections.for_ProjectionPipeline
 {
     public class when_events_are_provided_and_we_have_two_storage_providers : given.a_pipeline
     {
+        static Guid key = Guid.NewGuid();
         ISubject<Event>   subject;
         Mock<IProjectionStorage>    first_storage;
         Mock<IProjectionStorage>    second_storage;
         ExpandoObject initial_state;
         Event @event;
+        Changeset changeset;
 
         void Establish()
         {
@@ -26,12 +28,18 @@ namespace Cratis.Events.Projections.for_ProjectionPipeline
             pipeline.StoreIn(first_storage.Object);
             pipeline.StoreIn(second_storage.Object);
             pipeline.Start();
+            projection.Setup(_ => _.GetKeyResolverFor(IsAny<EventType>())).Returns((_) => key);
             @event = new Event(0, "8ccc9ac5-50bc-4791-97ca-8a03bc4a6ccf", DateTimeOffset.UtcNow, "14dea6ef-bc08-4fb3-86a3-21dedee157fd", new ExpandoObject());
+            changeset = new Changeset(projection.Object, @event, initial_state);
+            projection.Setup(_ => _.OnNext(@event, initial_state)).Returns(changeset);
         }
 
         void Because() => subject.OnNext(@event);
 
-        [Fact] void should_call_projection_for_first_storage() => projection.Verify(_ => _.OnNext(@event, initial_state), Once());
-        [Fact] void should_call_projection_for_second_storage() => projection.Verify(_ => _.OnNext(@event, initial_state), Once());
+        [Fact] void should_find_model_using_key_for_first_storage() => first_storage.Verify(_ => _.FindOrDefault(key), Once());
+        [Fact] void should_find_model_using_key_for_second_storage() => second_storage.Verify(_ => _.FindOrDefault(key), Once());
+        [Fact] void should_call_projection_for_both_storages() => projection.Verify(_ => _.OnNext(@event, initial_state), Exactly(2));
+        [Fact] void should_apply_changes_for_first_storage() => first_storage.Verify(_ => _.ApplyChanges(key, changeset), Once());
+        [Fact] void should_apply_changes_for_second_storage() => second_storage.Verify(_ => _.ApplyChanges(key, changeset), Once());
     }
 }
