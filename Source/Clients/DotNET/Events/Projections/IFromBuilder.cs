95785acf712befc06dda2ba97247e3c6c6fe7dfa
Einar Ingebrigtsen <einari@me.com>
2021-10-13 10:12:31 +0200

    Adding type safety for properties when mapping

--- Source/Clients/DotNET/Events/Projections/IFromBuilder.cs
+++ Source/Clients/DotNET/Events/Projections/IFromBuilder.cs
@@ -15,17 +15,19 @@ namespace Cratis.Events.Projections
         /// <summary>
         /// Define what key to use. This is optional, if not set - it will default to using the event source identifier on the event.
         /// </summary>
+        /// <typeparam name="TProperty">Type of the property.</typeparam>
         /// <param name="keyAccessor">Accessor for the property to use.</param>
         /// <returns>Builder continuation</returns>
-        IFromBuilder<TModel, TEvent> UsingKey(Expression<Func<TEvent, object>> keyAccessor);
+        IFromBuilder<TModel, TEvent> UsingKey<TProperty>(Expression<Func<TEvent, TProperty>> keyAccessor);
 
         /// <summary>
         /// Set the target property on the model to the content of the source property on an event.
         /// </summary>
+        /// <typeparam name="TProperty">Type of the property.</typeparam>
         /// <param name="modelPropertyAccessor">Model property accessor for defining the target property.</param>
         /// <param name="eventPropertyAccessor">Event property accessor for defining the source property.</param>
         /// <returns>Builder continuation</returns>
-        IFromBuilder<TModel, TEvent> Set(Expression<Func<TModel, object>> modelPropertyAccessor, Expression<Func<TEvent, object>> eventPropertyAccessor);
+        IFromBuilder<TModel, TEvent> Set<TProperty>(Expression<Func<TModel, TProperty>> modelPropertyAccessor, Expression<Func<TEvent, TProperty>> eventPropertyAccessor);
 
         /// <summary>
         /// Build <see cref="FromDefinition"/> from the builder.
