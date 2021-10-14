61f514165a11bf1b82ffc33c49b3dfa7ec1814eb
Einar Ingebrigtsen <einari@me.com>
2021-10-14 11:08:43 +0200

    Type guards are causing issues - can't see that we need it

--- Source/Node/Fundamentals/PrimitiveTypeMap.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-export interface PrimitiveTypeMap {
-    string: string;
-    number: number;
-    boolean: boolean;
-    bigint: bigint
-}
