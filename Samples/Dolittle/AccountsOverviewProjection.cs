c2b8394975ed9283b64cb18e125dc1556984bc0f
Einar Ingebrigtsen <einari@me.com>
2021-11-03 15:44:15 +0100

    "Tasting" the children APIs

---
+++ Samples/Dolittle/AccountsOverviewProjection.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Events.Projections;
+
+namespace Sample
+{
+    [Projection("8fdaaf0d-1291-47b7-b661-2eeba340a520")]
+    public class AccountsOverviewProjection : IProjectionFor<AccountsOverview>
+    {
+        public void Define(IProjectionBuilderFor<AccountsOverview> builder)
+        {
+            builder
+                .Children(_ => _.DebitAccounts, _ => _
+                    .IdentifiedBy(_ => _.Id)
+                    .From<DebitAccountOpened>(_ => _
+                        .UsingKey(_ => _.Owner)
+                        .Set(_ => _.Name).To(_ => _.Name)));
+        }
+    }
+}
