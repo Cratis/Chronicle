61cddd9e4c91f5eb78287b8c85163a62cc9851b4
Einar Ingebrigtsen <einari@me.com>
2021-11-07 18:08:34 +0100

    Started on the workbench for Projections

--- Source/Workbench/Events/Web/Programmatic/Projections.tsx
+++ Source/Workbench/Events/Web/Programmatic/Projections.tsx
@@ -1,45 +1,104 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { useDataFrom } from '../useDataFrom';
+import { useMemo, useState } from 'react';
+
 import {
-    IconButton,
-    Stack,
-    Pivot,
-    PivotItem,
-    IPivotItemProps
+    CommandBar,
+    DetailsList,
+    IColumn,
+    ICommandBarItemProps,
+    IDetailsListStyles,
+    Selection,
+    SelectionMode,
+    Stack
 } from '@fluentui/react';
 
-function pivotItemHeaderRenderer(
-    link?: IPivotItemProps,
-    defaultRenderer?: (link?: IPivotItemProps) => JSX.Element | null,
-): JSX.Element | null {
-    if (!link || !defaultRenderer) {
-        return null;
+const gridStyles: Partial<IDetailsListStyles> = {
+    root: {
+        height: '100%',
+        overflowX: 'scroll',
+        selectors: {
+            '& [role=grid]': {
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'start'
+            },
+        },
+    },
+    headerWrapper: {
+        flex: '0 0 auto',
+    },
+    contentWrapper: {
+        flex: '1 1 auto',
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        height: '300px'
+    },
+};
+
+const columns: IColumn[] = [
+    {
+        key: 'name',
+        name: 'Name',
+        fieldName: 'name',
+        minWidth: 200
+    },
+    {
+        key: 'position',
+        name: 'Position',
+        fieldName: 'position',
+        minWidth: 200
     }
 
-    return (
-        <span style={{ flex: '0 1 100%' }}>
-            <IconButton iconProps={{ iconName: 'StatusCircleErrorX' }} title="Close query" onClick={() => alert('hello world')} />
-            {defaultRenderer({ ...link, itemIcon: undefined })}
-        </span>
-    );
-}
+];
 
 
 export const Projections = () => {
+    const [projections, refreshProjections] = useDataFrom('/api/events/projections');
+    const commandBarItems: ICommandBarItemProps[] = [];
+    const [selected, setSelected] = useState(undefined);
+
+
+    const selection = useMemo(
+        () => new Selection({
+            selectionMode: SelectionMode.single,
+            onSelectionChanged: () => {
+                const selected = selection.getSelection();
+                if (selected.length === 1) {
+                    setSelected(selected[0]);
+                } else {
+                    setSelected(undefined);
+                }
+            },
+            items: projections
+        }), [projections]);
+
+    if (selected) {
+        commandBarItems.push({
+            key: 'rewind',
+            name: 'Rewind',
+            iconProps: { iconName: 'Rewind' }
+        });
+    }
+
     return (
-        <>
-            <Stack>
-                <Stack.Item disableShrink>
-                    <Stack horizontal style={{ textAlign: 'center' }}>
-                        <Pivot linkFormat="links">
-                            <PivotItem key="5c5af4ee-282a-456c-a53d-e3dee158a3be" headerText="Untitled" onRenderItemLink={pivotItemHeaderRenderer} />
-                            <PivotItem key="b7a5f0a3-82d3-4170-a1e7-36034d763008" headerText="Good old query" onRenderItemLink={pivotItemHeaderRenderer} />
-                        </Pivot>
-                        <IconButton iconProps={{ iconName: 'Add' }} title="Add query" />
-                    </Stack>
-                </Stack.Item>
-            </Stack>
-        </>
+        <Stack style={{ height: '100%' }}>
+            <Stack.Item>
+                <CommandBar items={commandBarItems} />
+            </Stack.Item>
+            <Stack.Item grow={1}>
+                <DetailsList
+                    items={projections}
+                    columns={columns}
+                    selection={selection}
+                    styles={gridStyles} />
+
+
+            </Stack.Item>
+
+        </Stack>
     );
-}
+};
