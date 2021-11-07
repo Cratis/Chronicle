2551fee00f48d473fb85ca1106646562da7495ea
Einar Ingebrigtsen <einari@me.com>
2021-11-07 08:52:15 +0100

    Removing unused using

--- Source/Kernel/Events.Projections/Json/JsonProjectionParser.cs
+++ Source/Kernel/Events.Projections/Json/JsonProjectionParser.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Dynamic;
 using Cratis.Concepts;
 using Cratis.Events.Projections.Expressions;
 using Newtonsoft.Json;
