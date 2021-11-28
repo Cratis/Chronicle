7a5c15d49e25bd4f93014a73cac0d57f7efeafe1
Einar Ingebrigtsen <einari@me.com>
2021-11-28 09:48:13 +0100

    Updated proxies

--- Source/Workbench/Events/Web/API/events/projections/Rewind.ts
+++ Source/Workbench/Events/Web/API/events/projections/Rewind.ts
@@ -2,8 +2,9 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
+import { Command } from '@aksio/frontend/commands';
 
-export type EventLogInformation = {
-    id: string;
-    name: string;
-};
+export class Rewind extends Command {
+    readonly route: string = '/api/events/projections/rewind/{projectionId}';
+
+}
