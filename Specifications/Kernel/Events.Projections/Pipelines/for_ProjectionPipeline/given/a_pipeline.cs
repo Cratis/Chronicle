2706c43cdbae859cb4f77653acd00b328d1c57a8
Einar Ingebrigtsen <einari@me.com>
2021-12-09 15:58:11 +0100

    Adding specs for public API

--- Specifications/Kernel/Events.Projections/Pipelines/for_ProjectionPipeline/given/a_pipeline.cs
+++ Specifications/Kernel/Events.Projections/Pipelines/for_ProjectionPipeline/given/a_pipeline.cs
@@ -8,12 +8,18 @@ namespace Cratis.Events.Projections.Pipelines.for_ProjectionPipeline.given
     public class a_pipeline : all_dependencies
     {
         protected ProjectionPipeline pipeline;
+        protected List<ProjectionState> states;
 
-        void Establish() => pipeline = new(
-            projection.Object,
-            event_provider.Object,
-            pipeline_handler.Object,
-            jobs.Object,
-            Mock.Of<ILogger<ProjectionPipeline>>());
+        void Establish()
+        {
+            states = new();
+            pipeline = new(
+                projection.Object,
+                event_provider.Object,
+                pipeline_handler.Object,
+                jobs.Object,
+                Mock.Of<ILogger<ProjectionPipeline>>());
+            pipeline.State.Subscribe(_ => states.Add(_));
+        }
     }
 }
