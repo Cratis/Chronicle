052ec23e70f6b9a83781548a889766204126b6e6
Einar Ingebrigtsen <einari@me.com>
2021-12-09 15:58:11 +0100

    Adding specs for public API

--- Specifications/Kernel/Events.Projections/Pipelines/for_ProjectionPipeline/when_resuming_paused_job.cs
+++ Specifications/Kernel/Events.Projections/Pipelines/for_ProjectionPipeline/when_resuming_paused_job.cs
@@ -3,22 +3,25 @@
 
 namespace Cratis.Events.Projections.Pipelines.for_ProjectionPipeline
 {
-    public class when_starting : given.a_pipeline
+    public class when_resuming_paused_job : given.a_pipeline
     {
         List<IProjectionPipelineJob> started_jobs;
         Mock<IProjectionPipelineJob> job;
 
-        void Establish()
+        async Task Establish()
         {
             started_jobs = new();
             job = new();
             pipeline.Jobs.Added.Subscribe(_ => started_jobs.Add(_));
+            await pipeline.Start();
+            await pipeline.Pause();
+            states.Clear();
             jobs.Setup(_ => _.Catchup(pipeline)).Returns(new[] { job.Object });
         }
 
-        async Task Because() => await pipeline.Start();
+        async Task Because() => await pipeline.Resume();
 
-        [Fact] void should_have_been_through_registering_catchup_and_active_state() => states.ShouldContainOnly(ProjectionState.Registering, ProjectionState.CatchingUp, ProjectionState.Active);
+        [Fact] void should_have_been_through_registering_catchup_and_active_state() => states.ShouldContainOnly(ProjectionState.CatchingUp, ProjectionState.Active);
         [Fact] void should_add_catchup_job() => started_jobs.ShouldContainOnly(job.Object);
         [Fact] void should_start_catchup_job() => job.Verify(_ => _.Run(), Once());
         [Fact] void should_not_have_active_jobs_after_all_done() => pipeline.Jobs.ShouldBeEmpty();
---
+++ Specifications/Kernel/Events.Projections/Pipelines/for_ProjectionPipeline/when_suspending.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Events.Projections.Pipelines.for_ProjectionPipeline
+{
+    public class when_suspending : given.a_pipeline
+    {
+        Mock<IProjectionPipelineJob> job;
+        TaskCompletionSource tcs;
+        void Establish()
+        {
+            job = new();
+            tcs = new();
+
+            job.Setup(_ => _.Run()).Returns(tcs.Task);
+            jobs.Setup(_ => _.Catchup(pipeline)).Returns(new[] { job.Object });
+            pipeline.Start();
+            states.Clear();
+        }
+
+        void Destroy() => tcs.SetResult();
+
+        async Task Because() => await pipeline.Suspend(string.Empty);
+
+        [Fact] void should_have_been_through_suspended_state() => states.ShouldContainOnly(ProjectionState.Suspended);
+        [Fact] void should_stop_the_running_job() => job.Verify(_ => _.Stop(), Once());
+    }
+
+}
