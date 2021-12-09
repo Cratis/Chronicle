2de9b1f8135197b9203a63ac9ac6a8c552fe01a7
Einar Ingebrigtsen <einari@me.com>
2021-12-09 19:58:25 +0100

    Removing unused log messages

--- Source/Kernel/Events.Projections/Pipelines/ProjectionPipelineLogMessages.cs
+++ Source/Kernel/Events.Projections/Pipelines/ProjectionPipelineLogMessages.cs
@@ -25,10 +25,7 @@ namespace Cratis.Events.Projections.Pipelines
         [LoggerMessage(4, LogLevel.Debug, "Rewinding projection '{Projection}' for configuration '{Configuration}'")]
         internal static partial void RewindingForConfiguration(this ILogger logger, ProjectionId projection, ProjectionResultStoreConfigurationId configuration);
 
-        [LoggerMessage(5, LogLevel.Error, "Error starting projection '{Projection}'")]
-        internal static partial void ErrorStartingProviding(this ILogger logger, ProjectionId projection, Exception exception);
-
-        [LoggerMessage(6, LogLevel.Warning, "Projection '{Projection}' is being suspended with the reason '{reason}'")]
+        [LoggerMessage(5, LogLevel.Warning, "Projection '{Projection}' is being suspended with the reason '{reason}'")]
         internal static partial void Suspended(this ILogger logger, ProjectionId projection, string reason);
     }
 }
