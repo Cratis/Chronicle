973f12611cea58a47e839309465710f6a6074efd
Einar Ingebrigtsen <einari@me.com>
2021-12-12 18:11:19 +0100

    Adding metadata to events and event compensation

--- Source/Kernel/Events.Store.MongoDB/Event.cs
+++ Source/Kernel/Events.Store.MongoDB/Event.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Execution;
 using MongoDB.Bson;
 
 #nullable disable
@@ -11,10 +12,22 @@ namespace Cratis.Events.Store.MongoDB
     /// Represents the document representation of a stored event.
     /// </summary>
     /// <param name="SequenceNumber">The sequence number of the event - the primary key.</param>
+    /// <param name="CorrelationId">The unique identifier used to correlation.</param>
+    /// <param name="CausationId">The unique identifier of the cause.</param>
+    /// <param name="CausedBy">Who or what caused the event.</param>
     /// <param name="Type">The <see cref="EventTypeId">type identifier</see> of the event</param>
     /// <param name="Occurred">The time the event occurred.</param>
     /// <param name="EventSourceId">The <see cref="EventSourceId"/> for the event.</param>
     /// <param name="Content">The content per event type generation.</param>
     /// <param name="Compensations">Any compensations for the event.</param>
-    public record Event(EventLogSequenceNumber SequenceNumber, EventTypeId Type, DateTimeOffset Occurred, EventSourceId EventSourceId, Dictionary<EventGeneration, BsonDocument> Content, IEnumerable<EventCompensation> Compensations);
+    public record Event(
+        EventLogSequenceNumber SequenceNumber,
+        CorrelationId CorrelationId,
+        CausationId CausationId,
+        CausedBy CausedBy,
+        EventTypeId Type,
+        DateTimeOffset Occurred,
+        EventSourceId EventSourceId,
+        Dictionary<EventGeneration, BsonDocument> Content,
+        IEnumerable<EventCompensation> Compensations);
 }
