10786a1488e7ff1c97dad857c1740f8c8ebbd809
Einar Ingebrigtsen <einari@me.com>
2021-12-13 08:43:09 +0100

    Moving changeset specs to the right place

--- Specifications/Fundamentals/Changes/for_Changeset/when_applying_properties_that_does_not_cause_changes.cs
+++ Specifications/Fundamentals/Changes/for_Changeset/when_applying_properties_that_does_not_cause_changes.cs
@@ -2,23 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Dynamic;
-using Cratis.Changes;
 using Cratis.Properties;
 
-namespace Cratis.Events.Projections
+namespace Cratis.Changes
 {
     public class when_applying_properties_that_does_not_cause_changes : Specification
     {
-        Changeset<Event, ExpandoObject> changeset;
+        Changeset<ExpandoObject, ExpandoObject> changeset;
         ExpandoObject initial_state;
-        Event @event;
-        IEnumerable<PropertyMapper<Event, ExpandoObject>> property_mappers;
-        Mock<IProjection> projection;
+        ExpandoObject source;
+        IEnumerable<PropertyMapper<ExpandoObject, ExpandoObject>> property_mappers;
 
         void Establish()
         {
             initial_state = new();
-            projection = new();
 
             ((dynamic)initial_state).Integer = 42;
             ((dynamic)initial_state).String = "Forty Two";
@@ -26,7 +23,7 @@ namespace Cratis.Events.Projections
             nested.Integer = 43;
             nested.String = "Forty Three";
 
-            property_mappers = new PropertyMapper<Event, ExpandoObject>[]
+            property_mappers = new PropertyMapper<ExpandoObject, ExpandoObject>[]
             {
                 (_, target) => ((dynamic)target).Integer = 42,
                 (_, target) => ((dynamic)target).String = "Forty Two",
@@ -34,9 +31,9 @@ namespace Cratis.Events.Projections
                 (_, target) => ((dynamic)target).Nested.String = "Forty Three",
             };
 
-            @event = new Event(0, "some event", DateTimeOffset.UtcNow, string.Empty, new ExpandoObject());
+            source = new ExpandoObject();
 
-            changeset = new(@event, initial_state);
+            changeset = new(source, initial_state);
         }
 
         void Because() => changeset.ApplyProperties(property_mappers);
