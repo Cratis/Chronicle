fd107783eb9eb1ed70e7ee63e30444aef6932dbf
Einar Ingebrigtsen <einari@me.com>
2021-12-13 13:05:44 +0100

    Specs for projecting

---
+++ Specifications/Kernel/Events.Projections/for_ProjectionExtensions/when_projecting_to_root_property.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+using Cratis.Properties;
+
+namespace Cratis.Events.Projections.for_ProjectionExtensions.when_applying_from_filter
+{
+    public class when_projecting_to_root_property : given.an_observable_and_event_setup
+    {
+        IEnumerable<PropertyMapper<Event, ExpandoObject>> property_mappers = Array.Empty<PropertyMapper<Event, ExpandoObject>>();
+
+        void Establish() => observable.Project("", "Id", _ => 42, property_mappers);
+
+        void Because() => observable.OnNext(event_context);
+
+        [Fact] void should_set_properties_on_changeset() => changeset.Verify(_ => _.SetProperties(property_mappers), Once());
+    }
+}
