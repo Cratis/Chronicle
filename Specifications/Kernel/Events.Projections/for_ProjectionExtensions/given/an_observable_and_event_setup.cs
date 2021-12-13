030f4e170247a1c23d70f33f9e27555bfda1570f
Einar Ingebrigtsen <einari@me.com>
2021-12-13 10:33:36 +0100

    Adding basic specs for child handling

---
+++ Specifications/Kernel/Events.Projections/for_ProjectionExtensions/given/an_observable_and_event_setup.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+using System.Reactive.Subjects;
+using Cratis.Changes;
+
+namespace Cratis.Events.Projections.for_ProjectionExtensions.given
+{
+    public class an_observable_and_event_setup : Specification
+    {
+        protected Subject<EventContext> observable;
+        protected List<EventContext> received = new();
+        protected EventContext event_context;
+        protected Event @event;
+        protected Changeset<Event, ExpandoObject> changeset;
+
+        void Establish()
+        {
+            observable = new();
+
+            @event = new(
+                    1,
+                    new EventType("98949b84-ee1b-48d5-a486-c5adf0332028", 1),
+                    DateTimeOffset.UtcNow,
+                    "14b33c19-1311-4825-93f9-bedab9e7d5ee",
+                    new ExpandoObject());
+            changeset = new(@event, new ExpandoObject());
+
+            event_context = new(@event, changeset);
+        }
+    }
+}
