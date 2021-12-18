eaec8f88575dbca8a8de742f8a4c301d611ed4f2
Einar Ingebrigtsen <einari@me.com>
2021-12-18 08:54:45 +0100

    Getting ready to add the compliance metadata onto the schema

--- Source/Fundamentals/Schemas/JsonSchemaGenerator.cs
+++ Source/Fundamentals/Schemas/JsonSchemaGenerator.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Compliance;
 using Cratis.Execution;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -21,7 +22,8 @@ namespace Cratis.Schemas
         /// <summary>
         /// Initializes a new instance of the <see cref="JsonSchemaGenerator"/> class.
         /// </summary>
-        public JsonSchemaGenerator()
+        /// <param name="metadataResolver"><see cref="IComplianceMetadataResolver"/> for resolving metadata.</param>
+        public JsonSchemaGenerator(IComplianceMetadataResolver metadataResolver)
         {
             var settings = new JsonSchemaGeneratorSettings
             {
@@ -32,7 +34,8 @@ namespace Cratis.Schemas
                 },
                 ReflectionService = new ReflectionService(),
             };
-            settings.SchemaProcessors.Add(new ComplianceMetadataSchemaProcessor());
+            settings.SchemaProcessors.Add(new ComplianceMetadataSchemaProcessor(metadataResolver));
+            settings.SchemaProcessors.Add(new TypeFormatSchemaProcessor());
             _generator = new NJsonSchemaGenerator(settings);
         }
 
