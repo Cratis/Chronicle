3548368eb778862137abc5d8b8cf4bf0c65549ea
Einar Ingebrigtsen <einari@me.com>
2021-12-20 11:30:32 +0100

    Adding specs to confirm metadata on types and properties

---
+++ Specifications/Fundamentals/Schemas/for_ComplianceMetadataSchemaProcessor/TypeWithProperties.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+
+namespace Cratis.Schemas.for_ComplianceMetadataSchemaProcessor
+{
+    public class TypeWithProperties
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+
+        public static PropertyInfo FirstProperty = typeof(TypeWithProperties).GetProperty(nameof(First), BindingFlags.Public | BindingFlags.Instance);
+        public static PropertyInfo SecondProperty = typeof(TypeWithProperties).GetProperty(nameof(Second), BindingFlags.Public | BindingFlags.Instance);
+    }
+}
