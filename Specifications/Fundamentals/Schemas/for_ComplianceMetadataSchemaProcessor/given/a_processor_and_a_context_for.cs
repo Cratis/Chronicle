30cd1335d91481fae2a2abf088d3ab96d1914183
Einar Ingebrigtsen <einari@me.com>
2021-12-20 10:50:21 +0100

    Started speccing out schema processor for compliance

---
+++ Specifications/Fundamentals/Schemas/for_ComplianceMetadataSchemaProcessor/given/a_processor_and_a_context_for.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Compliance;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NJsonSchema.Generation;
+using NJsonSchemaGenerator = NJsonSchema.Generation.JsonSchemaGenerator;
+
+namespace Cratis.Schemas.for_ComplianceMetadataSchemaProcessor.given
+{
+    public class a_processor_and_a_context_for<T> : Specification
+        where T : new()
+    {
+        protected Mock<IComplianceMetadataResolver> resolver;
+        protected ComplianceMetadataSchemaProcessor processor;
+        protected SchemaProcessorContext context;
+
+        void Establish()
+        {
+            resolver = new();
+            processor = new(resolver.Object);
+            var settings = new JsonSchemaGeneratorSettings
+            {
+                SerializerSettings = new()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    DateParseHandling = DateParseHandling.DateTimeOffset
+                }
+            };
+
+            var generator = new NJsonSchemaGenerator(settings);
+            var schema = generator.Generate(typeof(T));
+
+            var instance = new T();
+            context = new(
+                typeof(T),
+                schema,
+                new JsonSchemaResolver(instance, settings),
+                generator,
+                settings
+            );
+        }
+    }
+}
