8952ba7ff16b19a6acb830d45b6bb72791f374be
Einar Ingebrigtsen <einari@me.com>
2021-12-20 12:36:21 +0100

    Including generated log API proxies

---
+++ Source/Workbench/Events/Web/API/events/store/log/Event.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+
+export type Event = {
+    sequence: number;
+    name: string;
+    occurred: string;
+    content: string;
+};
