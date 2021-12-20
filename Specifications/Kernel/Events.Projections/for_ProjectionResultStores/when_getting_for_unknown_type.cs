63b93db5e004fa2f2c9455b1b0a51dcfb7c1f2f7
Einar Ingebrigtsen <einari@me.com>
2021-12-20 09:03:32 +0100

    Removing using statement for Cratis.Common - it is now global

--- Specifications/Kernel/Events.Projections/for_ProjectionResultStores/when_getting_for_unknown_type.cs
+++ Specifications/Kernel/Events.Projections/for_ProjectionResultStores/when_getting_for_unknown_type.cs
@@ -1,4 +1,5 @@
-using Cratis.Common;
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Cratis.Events.Projections.for_ProjectionResultStores
 {
