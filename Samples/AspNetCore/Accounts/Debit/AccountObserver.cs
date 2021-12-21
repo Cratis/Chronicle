13fa1d5d3c4bd20b8fcc1adf8951bb0927d9e062
Einar Ingebrigtsen <einari@me.com>
2021-12-21 09:00:53 +0100

    Started looking into observers

---
+++ Samples/AspNetCore/Accounts/Debit/AccountObserver.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Events.Observation;
+
+namespace Sample.Accounts.Debit
+{
+    // [Observer("4680f4dc-5731-4fde-9b3c-a0f59b7713d9")]
+    // public class AccountObserver
+    // {
+    //     public Task DebitAccountOpened(DebitAccountOpened @event)
+    //     {
+    //         return Task.CompletedTask;
+    //     }
+    // }
+}
