7636f3b2b56b50b005740f0cb6feb4bf0f2d9638
Einar Ingebrigtsen <einari@me.com>
2021-12-21 18:42:54 +0100

    Introducing a serializer for events

---
+++ Source/Clients/DotNET/Events/IEventSerializer.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Events
+{
+    /// <summary>
+    /// Defines a serializer of events.
+    /// </summary>
+    public interface IEventSerializer
+    {
+        /// <summary>
+        /// Serialize an event to JSON.
+        /// </summary>
+        /// <param name="event">The event instance to serialize.</param>
+        /// <returns>Serialized JSON.</returns>
+        string  Serialize(object @event);
+
+        /// <summary>
+        /// Deserialize a JSON representation of an event to a specific type.
+        /// </summary>
+        /// <param name="type">Type to deserialize to.</param>
+        /// <param name="json">JSON to deserialize.</param>
+        /// <returns>Deserialized instance.</returns>
+        object Deserialize(Type type, string json);
+    }
+}
