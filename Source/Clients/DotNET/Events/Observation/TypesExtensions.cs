8704561fcb740451d9ff167055bf6a7729d35afe
Einar Ingebrigtsen <einari@me.com>
2021-12-21 18:43:07 +0100

    Adding extension method for getting observers from types

---
+++ Source/Clients/DotNET/Events/Observation/TypesExtensions.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Reflection;
+using Cratis.Types;
+
+namespace Cratis.Events.Observation
+{
+    /// <summary>
+    /// Extension methods for working with observers and type discovery.
+    /// </summary>
+    public static class TypesExtensions
+    {
+        /// <summary>
+        /// Find all observers.
+        /// </summary>
+        /// <param name="types"><see cref="ITypes"/> to extend</param>
+        /// <returns>Collection of types that are observers.</returns>
+        public static IEnumerable<Type> AllObservers(this ITypes types) => types.All.Where(_ => _.HasAttribute<ObserverAttribute>()).ToArray();
+    }
+}
