061896e2c5c1d3588e22530a4270ca3cca62c934
Einar Ingebrigtsen <einari@me.com>
2021-12-22 12:19:44 +0100

    Specced out

---
+++ Specifications/Kernel/Compliance/for_JsonComplianceManager/given/a_type_with_one_property.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json.Linq;
+using NJsonSchema;
+using NJsonSchema.Generation;
+
+namespace Cratis.Compliance.for_JsonComplianceManager.given
+{
+    public class a_type_with_one_property : Specification
+    {
+        protected JsonSchema schema;
+        protected JObject input;
+
+        void Establish()
+        {
+            var instance = new { Something = 42 };
+            var settings = new JsonSchemaGeneratorSettings();
+            var generator = new JsonSchemaGenerator(settings);
+            schema = generator.Generate(instance.GetType());
+            input = JObject.FromObject(instance);
+        }
+    }
+}
