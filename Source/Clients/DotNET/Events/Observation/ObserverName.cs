ebdb35c99679ea246d857e695a89ef89a9388c4a
Einar Ingebrigtsen <einari@me.com>
2021-12-22 19:40:49 +0100

    Adding name for observer + some crude error handling for now

---
+++ Source/Clients/DotNET/Events/Observation/ObserverName.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Concepts;
+
+namespace Cratis.Events.Observation
+{
+    /// <summary>
+    /// Concept that represents the name of an observer.
+    /// </summary>
+    /// <param name="Value">Actual value.</param>
+    public record ObserverName(string Value) : ConceptAs<string>(Value)
+    {
+        /// <summary>
+        /// Implicitly convert from a string to <see cref="ObserverId"/>.
+        /// </summary>
+        /// <param name="id">String  to convert from.</param>
+        public static implicit operator ObserverName(string id) => new(id);
+    }
+}
