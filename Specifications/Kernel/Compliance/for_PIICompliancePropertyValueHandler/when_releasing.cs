d2e413fb2b3eb109fc5e0b54758b8d370e1b9393
Einar Ingebrigtsen <einari@me.com>
2021-12-22 12:43:37 +0100

    Adding specs for PII value handler

---
+++ Specifications/Kernel/Compliance/for_PIICompliancePropertyValueHandler/when_releasing.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Cratis.Compliance.for_PIICompliancePropertyValueHandler
+{
+    public class when_releasing : given.a_property_handler
+    {
+        JToken input;
+        JToken result;
+        string decrypted_string = "Hello";
+        byte[] decrypted_bytes;
+
+        void Establish()
+        {
+            input = JToken.FromObject(Convert.ToBase64String(Encoding.UTF8.GetBytes("42")));
+            decrypted_bytes = Encoding.UTF8.GetBytes(decrypted_string);
+            encryption.Setup(_ => _.Decrypt(IsAny<byte[]>(), key)).Returns(decrypted_bytes);
+        }
+
+        async Task Because() => result = await handler.Release(identifier, input);
+
+        [Fact] void should_return_encrypted_string() => result.ToString().ShouldEqual(decrypted_string);
+    }
+}
