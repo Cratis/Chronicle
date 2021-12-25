7e21631cd8b8f0bfb58127e6aba50ea5c9130070
Einar Ingebrigtsen <einari@me.com>
2021-12-25 17:52:44 +0100

    Cleanup

--- Source/Kernel/Events.Store/EventLog.cs
+++ Source/Kernel/Events.Store/EventLog.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Execution;
 using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Providers.Streams.Common;
