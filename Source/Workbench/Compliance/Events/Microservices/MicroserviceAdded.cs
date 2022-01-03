6b9124df1580e8dd0ff8bc584aca55433d01c7d7
Einar Ingebrigtsen <einari@me.com>
2022-01-03 10:40:13 +0100

    Adding the MicroserviceAdded event

---
+++ Source/Workbench/Compliance/Events/Microservices/MicroserviceAdded.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Workbench.Compliance.Events.Microservices
+{
+    [EventType("431919dd-a0b0-4360-9299-31eed7dc811e")]
+    public record MicroserviceAdded(string Name);
+}
