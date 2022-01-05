56a5bc9886928f68071e1ba0f7331bd097459a23
Einar Ingebrigtsen <einari@me.com>
2022-01-05 14:41:33 +0100

    Adding country

--- Source/Workbench/Compliance/Events/GDPR/AddressRegisteredForPerson.cs
+++ Source/Workbench/Compliance/Events/GDPR/AddressRegisteredForPerson.cs
@@ -6,5 +6,5 @@ using Cratis.Compliance.Concepts.PersonalInformation;
 namespace Cratis.Compliance.Events
 {
     [EventType("fc5a389b-efd9-4606-9532-86c08cb10946")]
-    public record AddressRegisteredForPerson(Address Address, City City, PostalCode PostalCode);
+    public record AddressRegisteredForPerson(Address Address, City City, PostalCode PostalCode, Country Country);
 }
