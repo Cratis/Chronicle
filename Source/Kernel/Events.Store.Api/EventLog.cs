d0770fd746d8d5fd0e6771d53c14bbe50c1fc35e
Einar Ingebrigtsen <einari@me.com>
2022-01-05 14:40:15 +0100

    Adding API for appending events to the event log

--- Source/Kernel/Events.Store.Api/EventLog.cs
+++ Source/Kernel/Events.Store.Api/EventLog.cs
@@ -1,22 +1,49 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text;
+using Cratis.Events.Store.Grains;
+using Cratis.Execution;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Orleans;
 
 namespace Cratis.Events.Store.Api
 {
-    [Route("/api/events/store/log")]
+    [Route("/api/events/store/eventlog")]
     public class EventLog : Controller
     {
+        readonly IGrainFactory _grainFactory;
+        readonly IExecutionContextManager _executionContextManager;
         readonly ILogger<EventLog> _logger;
 
-        public EventLog(ILogger<EventLog> logger)
+        public EventLog(
+            IGrainFactory grainFactory,
+            IExecutionContextManager executionContextManager,
+            ILogger<EventLog> logger)
         {
+            _grainFactory = grainFactory;
+            _executionContextManager = executionContextManager;
             _logger = logger;
         }
 
-        [HttpGet("{eventLogId}/{eventSourceId}")]
+        [HttpPost("{eventSourceId}/{eventTypeId}/{eventGeneration}")]
+        public async Task Append(
+            [FromRoute] EventSourceId eventSourceId,
+            [FromRoute] EventTypeId eventTypeId,
+            [FromRoute] EventGeneration eventGeneration)
+        {
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var content = await reader.ReadToEndAsync();
+
+            var eventLog = _grainFactory.GetGrain<IEventLog>(EventLogId.Default, keyExtension: _executionContextManager.Current.TenantId.ToString());
+            await eventLog.Append(
+                eventSourceId,
+                new EventType(eventTypeId, eventGeneration),
+                content);
+        }
+
+        [HttpGet("{eventSourceId}")]
         public Task FindFor(
             [FromRoute] EventLogId eventLogId,
             [FromRoute] EventSourceId eventSourceId)
