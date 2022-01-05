99efef3202c960192e3dc284121afd3796b2b2ad
Einar Ingebrigtsen <einari@me.com>
2022-01-05 14:41:46 +0100

    Adding person and people projection

---
+++ Source/Workbench/Compliance/Read/GDPR/Person.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Compliance.Concepts.PersonalInformation;
+
+namespace Cratis.Compliance.Read.GDPR
+{
+    public record Person(
+        SocialSecurityNumber SocialSecurityNumber,
+        FirstName FirstName,
+        LastName LastName,
+        Address Address,
+        City City,
+        PostalCode PostalCode,
+        Country Country);
+}
