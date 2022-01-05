aed1e163d79d89e4275c7a32c199082d5f71e5b9
Einar Ingebrigtsen <einari@me.com>
2022-01-05 12:47:05 +0100

    Formalized concepts and events for now

---
+++ Source/Workbench/Compliance/Concepts/PersonalInformation/PersonalInformationType.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Concepts;
+
+namespace Cratis.Compliance.Concepts.PersonalInformation
+{
+    public record PersonalInformationType(string Value) : ConceptAs<string>(Value);
+}
