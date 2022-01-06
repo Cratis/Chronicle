b3a0800de0c0d4f3a10757bde63d8f4accc368cf
Einar Ingebrigtsen <einari@me.com>
2022-01-06 14:27:23 +0100

    Proxy generated

---
+++ Source/Workbench/Compliance/Web/API/compliance/gdpr/people/Person.ts
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { SocialSecurityNumber } from './SocialSecurityNumber';
+import { FirstName } from './FirstName';
+import { LastName } from './LastName';
+import { Address } from './Address';
+import { City } from './City';
+import { PostalCode } from './PostalCode';
+import { Country } from './Country';
+import { PersonalInformation } from './PersonalInformation';
+
+export type Person = {
+    socialSecurityNumber: SocialSecurityNumber;
+    firstName: FirstName;
+    lastName: LastName;
+    address: Address;
+    city: City;
+    postalCode: PostalCode;
+    country: Country;
+    personalInformation: PersonalInformation[];
+};
