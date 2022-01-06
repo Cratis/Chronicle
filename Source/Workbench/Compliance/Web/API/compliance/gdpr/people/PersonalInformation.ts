4b935cbf45ce58cc2b1cecfe417b83b324ab89dd
Einar Ingebrigtsen <einari@me.com>
2022-01-06 14:27:23 +0100

    Proxy generated

---
+++ Source/Workbench/Compliance/Web/API/compliance/gdpr/people/PersonalInformation.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { PersonalInformationValue } from './PersonalInformationValue';
+
+export type PersonalInformation = {
+    type: string;
+    value: PersonalInformationValue;
+};
