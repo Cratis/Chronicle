e4d5ec19f2ecf6401c54c41c3858fc634f179d2e
Einar Ingebrigtsen <einari@me.com>
2022-01-07 10:30:59 +0100

    Fixing how we register projections. Run projections when registering.
    Already running projections are suspended and tasks cancelled.

---
+++ Source/Kernel/Events.Projections/ProjectionLogMessages.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace Cratis.Events.Projections
+{
+    public static partial class ProjectionLogMessages
+    {
+        [LoggerMessage(0, LogLevel.Debug, "Running projection pipeline with identifier '{Identifier}' - name '{Name}'")]
+        internal static partial void Running(this ILogger logger, ProjectionId identifier, ProjectionName name);
+
+        [LoggerMessage(1, LogLevel.Debug, "Stopping the running og projection pipeline with identifier '{Identifier}' - name '{Name}'")]
+        internal static partial void StopRunning(this ILogger logger, ProjectionId identifier, ProjectionName name);
+    }
+}
