79806a7119d9b6c12b308c7b148349cc894c0829
Einar Ingebrigtsen <einari@me.com>
2022-01-08 07:50:27 +0100

    Read Models Configuration

---
+++ Source/Kernel/Configuration.Grains/Configurations.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Orleans;
+
+namespace Cratis.Configuration.Grains
+{
+    /// <summary>
+    /// Defines a system for working with configurations.
+    /// </summary>
+    public class Configurations : Grain, IConfigurations
+    {
+        readonly Storage _storage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Configurations"/> class.
+        /// </summary>
+        /// <param name="storage"><see cref="Storage"/> configuration.</param>
+        public Configurations(Storage storage)
+        {
+            _storage = storage;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Storage"/> configuration.
+        /// </summary>
+        /// <returns><see cref="Storage"/> configuration instance.</returns>
+        public Task<Storage> GetStorage() => Task.FromResult(_storage);
+    }
+}
