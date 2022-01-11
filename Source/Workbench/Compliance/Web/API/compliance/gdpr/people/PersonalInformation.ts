a1db4350e394472117f5b969479b0ff7e3af40e5
Einar Ingebrigtsen <einari@me.com>
2022-01-11 09:12:28 +0100

    New version of ProxyGenerator - removing proxies not needed

--- Source/Workbench/Compliance/Web/API/compliance/gdpr/people/PersonalInformation.ts
+++ Source/Workbench/Compliance/Web/API/compliance/gdpr/people/PersonalInformation.ts
@@ -2,10 +2,9 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
-import { PersonalInformationValue } from './PersonalInformationValue';
 
 export type PersonalInformation = {
     identifier: string;
     type: string;
-    value: PersonalInformationValue;
+    value: string;
 };
