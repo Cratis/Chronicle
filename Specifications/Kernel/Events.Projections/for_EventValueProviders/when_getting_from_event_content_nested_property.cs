0a8232f26df5c92d8612396977ae8c78d21e7c82
Einar Ingebrigtsen <einari@me.com>
2022-01-11 11:24:29 +0100

    Fixing broken specs

--- Specifications/Kernel/Events.Projections/for_EventValueProviders/when_getting_from_event_content_nested_property.cs
+++ Specifications/Kernel/Events.Projections/for_EventValueProviders/when_getting_from_event_content_nested_property.cs
@@ -4,7 +4,7 @@
 using System.Dynamic;
 using Cratis.Properties;
 
-namespace Cratis.Events.Projections.for_PropertyMappers
+namespace Cratis.Events.Projections.for_EventValueProviders
 {
     public class when_getting_from_event_content_nested_property : Specification
     {
@@ -15,8 +15,8 @@ namespace Cratis.Events.Projections.for_PropertyMappers
         void Establish()
         {
             dynamic content = new ExpandoObject();
-            content.Nested = new ExpandoObject();
-            content.Nested.SourceString = "Forty two";
+            content.nested = new ExpandoObject();
+            content.nested.sourceString = "Forty two";
             @event = new Event(0, new  EventType("02405794-91e7-4e4f-8ad1-f043070ca297", 1), DateTimeOffset.UtcNow, "2f005aaf-2f4e-4a47-92ea-63687ef74bd4", content);
             value_provider = EventValueProviders.FromEventContent("Nested.SourceString");
         }
