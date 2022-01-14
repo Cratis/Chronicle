b73b7affd27d9bf0eb39503e78650bb826446b0a
Einar Ingebrigtsen <einari@me.com>
2022-01-14 12:54:43 +0100

    API proxies

---
+++ Source/Workbench/API/events/store/log/Count.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { QueryFor, QueryResult, useQuery, PerformQuery } from '@aksio/frontend/queries';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/events/store/log/{eventLogId}/count');
+
+export class Count extends QueryFor<number> {
+    readonly route: string = '/api/events/store/log/{eventLogId}/count';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+    readonly defaultValue: number = {} as any;
+    readonly requiresArguments: boolean = false;
+
+    static use(): [QueryResult<number>, PerformQuery] {
+        return useQuery<number, Count>(Count);
+    }
+}
