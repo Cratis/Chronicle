57c07c1727e59641bbb50e0d58d9f9744bae11a3
Einar Ingebrigtsen <einari@me.com>
2022-01-14 13:22:40 +0100

    Removing desktop app for now

--- Source/Tooling/Desktop/for_RemoveMeSpec/when_actual_spec_added.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-describe('when actual spec added', () => {
-    it('should be removed', () => true.should.be.true);
-});
