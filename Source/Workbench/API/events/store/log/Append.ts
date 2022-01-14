18aea86f4ff8020d3ab9277e759bb666b58277de
Einar Ingebrigtsen <einari@me.com>
2022-01-14 12:54:43 +0100

    API proxies

---
+++ Source/Workbench/API/events/store/log/Append.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Command } from '@aksio/frontend/commands';
+
+export class Append extends Command {
+    readonly route: string = '/api/events/store/log/{eventSourceId}/{eventTypeId}/{eventGeneration}';
+
+    isSpecified!: any;
+}
