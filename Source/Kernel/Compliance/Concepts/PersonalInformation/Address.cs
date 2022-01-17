ea04d9bf6b7fdfd4150861ba979a385ce305ebbb
Einar Ingebrigtsen <einari@me.com>
2022-01-17 13:30:25 +0100

    Fixing using statements after GDPR move

--- Source/Kernel/Compliance/Concepts/PersonalInformation/Address.cs
+++ Source/Kernel/Compliance/Concepts/PersonalInformation/Address.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Compliance.GDPR;
+
 namespace Cratis.Compliance.Concepts.PersonalInformation
 {
     public record Address(string Value) : PIIConceptAs<string>(Value);
