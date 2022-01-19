4790221eeee8c1076850f3f6b2c3ac20c187ad9c
Einar Ingebrigtsen <einari@me.com>
2022-01-19 14:52:45 +0100

    Copyright header

--- Source/Node/WebPack/frontend/devServer.js
+++ Source/Node/WebPack/frontend/devServer.js
@@ -1,4 +1,4 @@
-// Copyright (c) Cratis. All rights reserved.
+// Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 const fileTypes = /\.(js|json|css|html|png|jpg|jpeg|gif)$/;
