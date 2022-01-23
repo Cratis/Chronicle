9057add70561d373164aae87c65995ff4bc906bf
Einar Ingebrigtsen <einari@me.com>
2022-01-23 18:28:10 +0100

    Switching to System.Text.Json

--- Source/Fundamentals/Schemas/JsonSchemaGenerator.cs
+++ Source/Fundamentals/Schemas/JsonSchemaGenerator.cs
@@ -1,10 +1,9 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json;
 using Aksio.Cratis.Compliance;
 using Aksio.Cratis.Execution;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using NJsonSchema;
 using NJsonSchema.Generation;
 using NJsonSchemaGenerator = NJsonSchema.Generation.JsonSchemaGenerator;
@@ -32,10 +31,9 @@ namespace Aksio.Cratis.Schemas
         {
             var settings = new JsonSchemaGeneratorSettings
             {
-                SerializerSettings = new()
+                SerializerOptions = new JsonSerializerOptions
                 {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                    DateParseHandling = DateParseHandling.DateTimeOffset
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 },
                 ReflectionService = new ReflectionService(),
             };
