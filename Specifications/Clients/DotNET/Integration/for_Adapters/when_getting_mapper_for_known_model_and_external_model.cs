a82486b195f9d030cf349a8f4e6868983a9b322e
Einar Ingebrigtsen <einari@me.com>
2022-01-24 13:39:41 +0100

    Fixing specs

--- Specifications/Clients/DotNET/Integration/for_Adapters/when_getting_mapper_for_known_model_and_external_model.cs
+++ Specifications/Clients/DotNET/Integration/for_Adapters/when_getting_mapper_for_known_model_and_external_model.cs
@@ -7,14 +7,7 @@ namespace Aksio.Cratis.Integration.for_Adapters
 {
     public class when_getting_mapper_for_known_model_and_external_model : given.one_adapter
     {
-        Mock<IMapper> mapper;
         IMapper result;
-
-        void Establish()
-        {
-            mapper = new();
-            mapper_factory.Setup(_ => _.CreateFor(adapter.Object)).Returns(mapper.Object);
-        }
 
         void Because() => result = adapters.GetMapperFor<Model, ExternalModel>();
 
--- Specifications/Clients/DotNET/Integration/for_Adapters/when_getting_projection_for_known_model_and_external_model.cs
+++ Specifications/Clients/DotNET/Integration/for_Adapters/when_getting_projection_for_known_model_and_external_model.cs
@@ -5,14 +5,7 @@ namespace Aksio.Cratis.Integration.for_Adapters
 {
     public class when_getting_projection_for_known_model_and_external_model : given.one_adapter
     {
-        Mock<IAdapterProjectionFor<Model>> adapter_projection;
         IAdapterProjectionFor<Model> result;
-
-        void Establish()
-        {
-            adapter_projection = new();
-            projection_factory.Setup(_ => _.CreateFor(adapter.Object)).Returns(Task.FromResult(adapter_projection.Object));
-        }
 
         void Because() => result = adapters.GetProjectionFor<Model, ExternalModel>();
 
