336ae19209d9595c53c2b6e91e7782e8c88f9b45
Einar Ingebrigtsen <einari@me.com>
2022-01-24 09:23:22 +0100

    Scaffolded grain for Projection

---
+++ Source/Kernel/Projections/Grains.Interfaces/IProjection.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json.Nodes;
+using Orleans;
+
+namespace Aksio.Cratis.Events.Projections.Grains
+{
+    /// <summary>
+    /// Defines a projection.
+    /// </summary>
+    public interface IProjection : IGrainWithGuidKey
+    {
+        /// <summary>
+        /// Get a model instance by its <see cref="EventSourceId">identifier</see>.
+        /// </summary>
+        /// <param name="eventSourceId"><see cref="EventSourceId"/> to get for.</param>
+        /// <returns>A projected model in the form of a <see cref="JsonObject"/>.</returns>
+        Task<JsonObject> GetModelInstanceById(EventSourceId eventSourceId);
+    }
+}
