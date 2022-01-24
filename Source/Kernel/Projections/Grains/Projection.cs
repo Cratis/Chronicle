a32bb639cd7098b30ed2d5d28b0e87c1d383cd78
Einar Ingebrigtsen <einari@me.com>
2022-01-24 11:34:26 +0100

    Core implementation of running through events from storage

--- Source/Kernel/Projections/Grains/Projection.cs
+++ Source/Kernel/Projections/Grains/Projection.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Dynamic;
 using System.Text.Json.Nodes;
+using Aksio.Cratis.Changes;
 using Aksio.Cratis.Events.Projections.Definitions;
+using Aksio.Cratis.Events.Store;
 using Orleans;
 using EngineProjection = Aksio.Cratis.Events.Projections.IProjection;
 
@@ -15,6 +18,7 @@ namespace Aksio.Cratis.Events.Projections.Grains
     {
         readonly IProjectionDefinitions _projectionDefinitions;
         readonly IProjectionFactory _projectionFactory;
+        readonly IEventLogStorageProvider _eventLogStorageProvider;
         EngineProjection? _projection;
 
         /// <summary>
@@ -22,12 +26,15 @@ namespace Aksio.Cratis.Events.Projections.Grains
         /// </summary>
         /// <param name="projectionDefinitions"><see cref="IProjectionDefinitions"/>.</param>
         /// <param name="projectionFactory"><see cref="IProjectionFactory"/> for creating engine projections.</param>
+        /// <param name="eventLogStorageProvider"><see cref="IEventLogStorageProvider"/> for getting events from storage.</param>
         public Projection(
             IProjectionDefinitions projectionDefinitions,
-            IProjectionFactory projectionFactory)
+            IProjectionFactory projectionFactory,
+            IEventLogStorageProvider eventLogStorageProvider)
         {
             _projectionDefinitions = projectionDefinitions;
             _projectionFactory = projectionFactory;
+            _eventLogStorageProvider = eventLogStorageProvider;
         }
 
         /// <inheritdoc/>
@@ -39,10 +46,29 @@ namespace Aksio.Cratis.Events.Projections.Grains
         }
 
         /// <inheritdoc/>
-        public Task<JsonObject> GetModelInstanceById(EventSourceId eventSourceId)
+        public async Task<JsonObject> GetModelInstanceById(EventSourceId eventSourceId)
         {
-            Console.WriteLine(_projection?.Name ?? string.Empty);
-            return Task.FromResult(new JsonObject());
+            if (_projection is null)
+            {
+                return new JsonObject();
+            }
+            var cursor = await _eventLogStorageProvider.GetFromSequenceNumber(EventLogSequenceNumber.First, eventSourceId, _projection.EventTypes);
+            var initialState = new ExpandoObject();
+            while (await cursor.MoveNext())
+            {
+                if (!cursor.Current.Any())
+                {
+                    break;
+                }
+
+                foreach (var @event in cursor.Current)
+                {
+                    var changeSet = new Changeset<AppendedEvent, ExpandoObject>(@event, initialState);
+                    _projection.OnNext(@event, changeSet);
+                }
+            }
+
+            return new JsonObject();
         }
     }
 }
