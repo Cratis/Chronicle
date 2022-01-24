153cae39261949edf6692c54c123dab5da05bf9b
Einar Ingebrigtsen <einari@me.com>
2022-01-24 14:43:43 +0100

    Getting rid of [Projection] attribute - putting identifier on the interface

--- Source/Clients/DotNET/Events/Projections/ProjectionsRegistrar.cs
+++ Source/Clients/DotNET/Events/Projections/ProjectionsRegistrar.cs
@@ -18,10 +18,10 @@ namespace Aksio.Cratis.Events.Projections
     {
         static class ProjectionDefinitionCreator<TModel>
         {
-            public static ProjectionDefinition CreateAndDefine(Type type, ProjectionId identifier, IEventTypes eventTypes, IJsonSchemaGenerator schemaGenerator)
+            public static ProjectionDefinition CreateAndDefine(Type type, IEventTypes eventTypes, IJsonSchemaGenerator schemaGenerator)
             {
                 var instance = (Activator.CreateInstance(type) as IProjectionFor<TModel>)!;
-                var builder = new ProjectionBuilderFor<TModel>(identifier, eventTypes, schemaGenerator);
+                var builder = new ProjectionBuilderFor<TModel>(instance.Identifier, eventTypes, schemaGenerator);
                 instance.Define(builder);
                 return builder.Build();
             }
@@ -60,14 +60,13 @@ namespace Aksio.Cratis.Events.Projections
         /// <returns>Collection of <see cref="ProjectionDefinition"/>.</returns>
         public static IEnumerable<ProjectionDefinition> FindAllProjectionDefinitions(IEventTypes eventTypes, ITypes types, IJsonSchemaGenerator schemaGenerator) =>
             types.All
-                    .Where(_ => _.HasAttribute<ProjectionAttribute>() && _.HasInterface(typeof(IProjectionFor<>)))
+                    .Where(_ => _.HasInterface(typeof(IProjectionFor<>)))
                     .Select(_ =>
                     {
-                        var projection = _.GetCustomAttribute<ProjectionAttribute>()!;
                         var modelType = _.GetInterface(typeof(IProjectionFor<>).Name)!.GetGenericArguments()[0]!;
                         var creatorType = typeof(ProjectionDefinitionCreator<>).MakeGenericType(modelType);
                         var method = creatorType.GetMethod(nameof(ProjectionDefinitionCreator<object>.CreateAndDefine), BindingFlags.Public | BindingFlags.Static)!;
-                        return (method.Invoke(null, new object[] { _, projection.Identifier, eventTypes, schemaGenerator }) as ProjectionDefinition)!;
+                        return (method.Invoke(null, new object[] { _, eventTypes, schemaGenerator }) as ProjectionDefinition)!;
                     }).ToArray();
 
         /// <inheritdoc/>
