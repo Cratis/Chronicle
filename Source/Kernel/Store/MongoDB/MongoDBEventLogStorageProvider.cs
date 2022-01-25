1b2d7f7f381153a6f04abbfb0f05c2445d1602a2
Einar Ingebrigtsen <einari@me.com>
2022-01-25 08:34:04 +0100

    Fixing deadlock issue. We'd like to revisit this: #118

--- Source/Kernel/Store/MongoDB/MongoDBEventLogStorageProvider.cs
+++ Source/Kernel/Store/MongoDB/MongoDBEventLogStorageProvider.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Compliance;
-using Aksio.Cratis.DependencyInversion;
 using Aksio.Cratis.Events.Schemas;
 using MongoDB.Driver;
 
@@ -13,18 +12,18 @@ namespace Aksio.Cratis.Events.Store.MongoDB
     /// </summary>
     public class MongoDBEventLogStorageProvider : IEventLogStorageProvider
     {
-        readonly ProviderFor<IEventStoreDatabase> _eventStoreDatabase;
+        readonly IEventStoreDatabase _eventStoreDatabase;
         readonly ISchemaStore _schemaStore;
         readonly IJsonComplianceManager _jsonComplianceManager;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MongoDBEventLogStorageProvider"/> class.
         /// </summary>
-        /// <param name="eventStoreDatabase"><see cref="ProviderFor{T}"/> for <see cref="IEventStoreDatabase"/> to use.</param>
+        /// <param name="eventStoreDatabase"><see cref="IEventStoreDatabase"/> to use.</param>
         /// <param name="schemaStore"><see cref="ISchemaStore"/> for event schemas.</param>
         /// <param name="jsonComplianceManager"><see cref="IJsonComplianceManager"/> for handling compliance on events.</param>
         public MongoDBEventLogStorageProvider(
-            ProviderFor<IEventStoreDatabase> eventStoreDatabase,
+            IEventStoreDatabase eventStoreDatabase,
             ISchemaStore schemaStore,
             IJsonComplianceManager jsonComplianceManager)
         {
@@ -34,15 +33,15 @@ namespace Aksio.Cratis.Events.Store.MongoDB
         }
 
         /// <inheritdoc/>
-        public async Task<IEventCursor> GetFromSequenceNumber(
+        public Task<IEventCursor> GetFromSequenceNumber(
             EventLogSequenceNumber sequenceNumber,
             EventSourceId? eventSourceId = null,
             IEnumerable<EventType>? eventTypes = null)
         {
-            var collection = _eventStoreDatabase().GetEventLogCollectionFor(EventLogId.Default);
+            var collection = _eventStoreDatabase.GetEventLogCollectionFor(EventLogId.Default);
             var filters = new List<FilterDefinition<Event>>
             {
-                Builders<Event>.Filter.Gte(_ => _.SequenceNumber, sequenceNumber)
+                Builders<Event>.Filter.Gte(_ => _.SequenceNumber, sequenceNumber.Value)
             };
 
             if (eventSourceId?.IsSpecified == true)
@@ -56,8 +55,9 @@ namespace Aksio.Cratis.Events.Store.MongoDB
             }
 
             var filter = Builders<Event>.Filter.And(filters.ToArray());
-            var cursor = await collection.FindAsync(filter);
-            return new EventCursor(_schemaStore, _jsonComplianceManager, cursor);
+            #pragma warning disable CA1849, MA0042 // Allow this blocking call - we get deadlocks when using FindAsync() / ToCursorAsync()
+            var cursor = collection.Find(filter).ToCursor();
+            return Task.FromResult<IEventCursor>(new EventCursor(_schemaStore, _jsonComplianceManager, cursor));
         }
     }
 }
