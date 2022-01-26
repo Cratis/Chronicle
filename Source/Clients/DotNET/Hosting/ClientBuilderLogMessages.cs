bfbca9fe6b943b636cc3cb7bfb0ef2d5c966af01
Einar Ingebrigtsen <einari@me.com>
2022-01-26 10:09:29 +0100

    Adding logging to startup + improving startup performance by initializing Orleans lazily

---
+++ Source/Clients/DotNET/Hosting/ClientBuilderLogMessages.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace Aksio.Cratis.Hosting
+{
+    /// <summary>
+    /// Holds log messages for <see cref="ClientBuilder"/>.
+    /// </summary>
+    public static partial class ClientBuilderLogMessages
+    {
+        [LoggerMessage(0, LogLevel.Information, "Configuring Cratis client")]
+        internal static partial void Configuring(this ILogger logger);
+
+        [LoggerMessage(1, LogLevel.Information, "Configuring services")]
+        internal static partial void ConfiguringServices(this ILogger logger);
+
+        [LoggerMessage(2, LogLevel.Information, "Configuring compliance")]
+        internal static partial void ConfiguringCompliance(this ILogger logger);
+
+        [LoggerMessage(3, LogLevel.Information, "Configuring Kernel connection")]
+        internal static partial void ConfiguringKernelConnection(this ILogger logger);
+
+        [LoggerMessage(4, LogLevel.Information, "Connecting to Kernel")]
+        internal static partial void ConnectingToKernel(this ILogger logger);
+
+        [LoggerMessage(5, LogLevel.Information, "Connected to Kernel")]
+        internal static partial void ConnectedToKernel(this ILogger logger);
+    }
+}
