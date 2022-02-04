3e6340fe62fd92daeb954e7ffe917612c82785c6
Einar Ingebrigtsen <einari@me.com>
2022-02-04 13:55:38 +0100

    Adding support for combining property paths easily (operator +)

---
+++ Specifications/Fundamentals/Properties/for_PropertyPath/when_adding_a_property_to_an_empty_property.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Properties.for_PropertyPath
+{
+    public class when_adding_a_property_to_an_empty_property : Specification
+    {
+        const string left = "";
+        const string right = "Something";
+
+        PropertyPath result;
+
+        void Because() => result = new PropertyPath(left) + right;
+
+        [Fact] void should_hold_only_property_added_on() => result.Path.ShouldEqual(right);
+    }
+}
