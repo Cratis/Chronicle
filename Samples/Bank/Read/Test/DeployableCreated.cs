1fce7db0ef32c5c6ed7f7623c01adb0ac98e7d51
Einar Ingebrigtsen <einari@me.com>
2022-02-04 13:57:48 +0100

    Children of children test setup

---
+++ Samples/Bank/Read/Test/DeployableCreated.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Concepts.Applications;
+using ApplicationId = Concepts.Applications.ApplicationId;
+
+namespace Events.Applications
+{
+    [EventType("12e6d722-1e91-4802-86db-00ff3d61d798")]
+    public record DeployableCreated(MicroserviceId MicroserviceId, DeployableName Name);
+}
