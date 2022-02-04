55b32e77d95db9e4dced7d80be29169677afcce4
Einar Ingebrigtsen <einari@me.com>
2022-02-04 13:57:48 +0100

    Children of children test setup

---
+++ Samples/Bank/Read/Test/ApplicationRemoved.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Concepts.Applications;
+
+namespace Events.Applications
+{
+    [EventType("e0f22346-a1f2-485c-b314-5b5f97e7a950")]
+    public record ApplicationRemoved();
+}
