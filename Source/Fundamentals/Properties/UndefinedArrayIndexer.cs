07550c2473e433125f58d8ee2f427461763a9fe6
Einar Ingebrigtsen <einari@me.com>
2022-02-07 11:08:37 +0100

    Adding support for array indexers and complex structures for typed objects

--- Source/Fundamentals/Properties/UndefinedArrayIndexer.cs
+++ Source/Fundamentals/Properties/UndefinedArrayIndexer.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Aksio.Cratis.Properties;
-
-namespace Aksio.Cratis.Dynamic
+namespace Aksio.Cratis.Properties
 {
     /// <summary>
     /// Exception that gets thrown when an array indexer is not defined within a <see cref="PropertyPath"/>.
