2a99c65e3bdd02be378c809dc2010d5ac1a10fa3
Einar Ingebrigtsen <einari@me.com>
2022-02-09 11:57:57 +0100

    New proxy

--- Samples/Bank/Web/API/accounts/debit/OpenDebitAccount.ts
+++ Samples/Bank/Web/API/accounts/debit/OpenDebitAccount.ts
@@ -3,9 +3,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Command } from '@aksio/cratis-applications-frontend/commands';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/accounts/debit');
 
 export class OpenDebitAccount extends Command {
     readonly route: string = '/api/accounts/debit';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+
+    get requestArguments(): string[] {
+        return [
+        ];
+    }
 
     accountId!: string;
     name!: string;
