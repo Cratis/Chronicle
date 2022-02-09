3a1566f9347635ae8f9999004e41b919c875cc11
Einar Ingebrigtsen <einari@me.com>
2022-02-09 11:36:59 +0100

    New proxies

--- Samples/Bank/Web/API/accounts/debit/CloseDebitAccount.ts
+++ Samples/Bank/Web/API/accounts/debit/CloseDebitAccount.ts
@@ -3,9 +3,19 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Command } from '@aksio/cratis-applications-frontend/commands';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/accounts/debit/{{accountId}}/close');
 
 export class CloseDebitAccount extends Command {
-    readonly route: string = '/api/accounts/debit/close';
+    readonly route: string = '/api/accounts/debit/{{accountId}}/close';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+
+    get requestArguments(): string[] {
+        return [
+            'accountId',
+        ];
+    }
 
     accountId!: string;
 }
