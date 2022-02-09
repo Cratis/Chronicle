dcb73529ad530febf23f8a500402a5ab3536cf13
Einar Ingebrigtsen <einari@me.com>
2022-02-09 12:45:59 +0100

    New proxies

--- Source/Workbench/API/compliance/gdpr/pii/CreateAndRegisterKeyFor.ts
+++ Source/Workbench/API/compliance/gdpr/pii/CreateAndRegisterKeyFor.ts
@@ -3,9 +3,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Command } from '@aksio/cratis-applications-frontend/commands';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/compliance/gdpr/pii');
 
 export class CreateAndRegisterKeyFor extends Command {
     readonly route: string = '/api/compliance/gdpr/pii';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+
+    get requestArguments(): string[] {
+        return [
+        ];
+    }
 
     identifier!: string;
 }
--- Source/Workbench/API/compliance/gdpr/pii/DeletePIIForPerson.ts
+++ Source/Workbench/API/compliance/gdpr/pii/DeletePIIForPerson.ts
@@ -3,9 +3,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Command } from '@aksio/cratis-applications-frontend/commands';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/compliance/gdpr/pii/delete');
 
 export class DeletePIIForPerson extends Command {
     readonly route: string = '/api/compliance/gdpr/pii/delete';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+
+    get requestArguments(): string[] {
+        return [
+        ];
+    }
 
     personId!: string;
 }
