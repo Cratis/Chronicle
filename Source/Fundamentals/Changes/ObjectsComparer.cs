e524b0109343dd05b99ec7bd27e0ea564be90fa2
Einar Ingebrigtsen <einari@me.com>
2022-02-11 16:04:01 +0100

    Simple non recursive for regular objects

--- Source/Fundamentals/Changes/ObjectsComparer.cs
+++ Source/Fundamentals/Changes/ObjectsComparer.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Aksio.Cratis.Properties;
+
 namespace Aksio.Cratis.Changes
 {
     /// <summary>
@@ -11,8 +13,25 @@ namespace Aksio.Cratis.Changes
         /// <inheritdoc/>
         public bool Compare<TTarget>(TTarget left, TTarget right, out IEnumerable<PropertyDifference<TTarget>> differences)
         {
-            differences = Array.Empty<PropertyDifference<TTarget>>();
-            return false;
+            var allDifferences = new List<PropertyDifference<TTarget>>();
+            differences = allDifferences;
+
+            var currentPropertyPath = new PropertyPath(string.Empty);
+
+            foreach (var property in typeof(TTarget).GetProperties())
+            {
+                var leftValue = property.GetValue(left);
+                var rightValue = property.GetValue(right);
+
+                var propertyPath = currentPropertyPath + property.Name;
+
+                if (!leftValue!.Equals(rightValue))
+                {
+                    allDifferences.Add(new PropertyDifference<TTarget>(propertyPath, left, right));
+                }
+            }
+
+            return allDifferences.Count > 0;
         }
     }
 }
---
+++ Specifications/Fundamentals/Changes/for_ObjectsComparer/when_comparing_object_with_top_level_changes.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Strings;
+
+namespace Aksio.Cratis.Changes.for_ObjectsComparer
+{
+    public class when_comparing_object_with_top_level_changes : given.an_object_comparer
+    {
+        record TheType(string StringValue, int IntValue);
+
+        TheType left;
+        TheType right;
+
+        bool result;
+
+        IEnumerable<PropertyDifference<TheType>> differences;
+
+        void Establish()
+        {
+            left = new TheType("FortyTwo", 42);
+            right = new TheType("FortyThree", 43);
+        }
+
+        void Because() => result = comparer.Compare(left, right, out differences);
+
+        [Fact] void should_not_be_considered_equal() => result.ShouldBeTrue();
+        [Fact] void should_have_first_difference_be_the_first_property() => differences.ToArray()[0].PropertyPath.Path.ShouldEqual(nameof(TheType.StringValue).ToCamelCase());
+        [Fact] void should_have_second_difference_be_the_second_property() => differences.ToArray()[1].PropertyPath.Path.ShouldEqual(nameof(TheType.IntValue).ToCamelCase());
+        [Fact] void should_hold_original_value_for_first_property() => differences.ToArray()[0].Original.ShouldEqual(left.StringValue);
+        [Fact] void should_hold_changed_value_for_first_property() => differences.ToArray()[0].Changed.ShouldEqual(right.StringValue);
+        [Fact] void should_hold_original_value_for_second_property() => differences.ToArray()[1].Original.ShouldEqual(left.IntValue);
+        [Fact] void should_hold_changed_value_for_second_property() => differences.ToArray()[1].Changed.ShouldEqual(right.IntValue);
+    }
+}
