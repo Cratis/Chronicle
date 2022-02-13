2919fa54d93962ce8c9d0c7323879bfaceaa2c8a
Einar Ingebrigtsen <einari@me.com>
2022-02-13 11:09:07 +0100

    Changing to using PropertyPath consistently + Adding support for higher level definition of properties

---
+++ Specifications/Fundamentals/Reflection/for_ExpressionExtensions/when_getting_property_path_from_expression.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq.Expressions;
+using Aksio.Cratis.Properties;
+
+namespace Aksio.Cratis.Reflection.for_ExpressionExtensions
+{
+    public class when_getting_property_path_from_expression : Specification
+    {
+        record TheType(int SomeInteger);
+
+        Expression expression;
+        PropertyPath result;
+        PropertyPath expected = new(nameof(TheType.SomeInteger));
+
+        void Establish() => expression = (TheType instance) => instance.SomeInteger;
+
+        void Because() => result = expression.GetPropertyPath();
+
+        [Fact] void should_produce_correct_path() => result.Path.ShouldEqual(expected.Path);
+    }
+}
