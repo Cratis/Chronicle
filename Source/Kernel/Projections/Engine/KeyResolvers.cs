4bf75b6bd6168a127d68623ff552f1b768c629fa
Einar Ingebrigtsen <einari@me.com>
2022-02-19 08:57:36 +0100

    Refactoring for clarity

--- Source/Kernel/Projections/Engine/KeyResolvers.cs
+++ Source/Kernel/Projections/Engine/KeyResolvers.cs
@@ -14,49 +14,46 @@ namespace Aksio.Cratis.Events.Projections
         /// <summary>
         /// Create a <see cref="KeyResolver"/> that provides the event source id from an event.
         /// </summary>
-        /// <param name="projection"><see cref="IProjection"/> to start at.</param>
-        /// <param name="identifiedByProperty">The property that identifies the key on the child object.</param>
-        /// <param name="childrenKey">Key that identifies the child from the event.</param>
         /// <returns>A new <see cref="KeyResolver"/>.</returns>
-        public static KeyResolver FromEventSourceId(IProjection projection, PropertyPath identifiedByProperty, PropertyPath? childrenKey = default)
-        {
-            return (IProjectionEventProvider eventProvider, AppendedEvent @event) =>
-            {
-                if (childrenKey is null)
-                {
-                    return Task.FromResult(new Key(EventValueProviders.FromEventSourceId(@event), ArrayIndexers.NoIndexers))!;
-                }
-
-                var key = EventValueProviders.FromEventSourceId(@event);
-                var childKey = EventValueProviders.FromEventContent(childrenKey)(@event);
-                var arrayIndexers = new List<ArrayIndexer>
-                {
-                    new(projection.ChildrenPropertyPath, identifiedByProperty, childKey)
-                };
-                return Task.FromResult(new Key(key, new ArrayIndexers(arrayIndexers)))!;
-            };
-        }
+        public static KeyResolver FromEventSourceId() => (IProjectionEventProvider eventProvider, AppendedEvent @event) => Task.FromResult(new Key(EventValueProviders.FromEventSourceId(@event), ArrayIndexers.NoIndexers))!;
 
         /// <summary>
         /// Create a <see cref="KeyResolver"/> that provides a value from the event content.
         /// </summary>
-        /// <param name="sourceProperty">Source property.</param>
+        /// <param name="projection"><see cref="IProjection"/> to start at.</param>
+        /// <param name="keyProperty">Property that holds the key on the event.</param>
+        /// <param name="identifiedByProperty">The property that identifies the key on the child object.</param>
         /// <returns>A new <see cref="KeyResolver"/>.</returns>
-        public static KeyResolver FromEventContent(PropertyPath sourceProperty) => (IProjectionEventProvider _, AppendedEvent @event) => Task.FromResult(new Key(EventValueProviders.FromEventContent(sourceProperty)(@event), ArrayIndexers.NoIndexers));
+        public static KeyResolver FromEventContent(IProjection projection, PropertyPath keyProperty, PropertyPath identifiedByProperty)
+        {
+            return (IProjectionEventProvider eventProvider, AppendedEvent @event) =>
+            {
+                var key = EventValueProviders.FromEventContent(keyProperty)(@event);
+                if (!projection.HasParent)
+                {
+                    return Task.FromResult(new Key(key, ArrayIndexers.NoIndexers))!;
+                }
+
+                var arrayIndexers = new List<ArrayIndexer>();
+                var parentKey = EventValueProviders.FromEventSourceId(@event);
+                arrayIndexers.Add(new(projection.ChildrenPropertyPath, identifiedByProperty, key));
+                return Task.FromResult(new Key(parentKey, new ArrayIndexers(arrayIndexers)))!;
+            };
+        }
 
         /// <summary>
         /// Create a <see cref="KeyResolver"/> that provides a key value hierarchically upwards in Child->Parent relationships.
         /// </summary>
         /// <param name="projection"><see cref="IProjection"/> to start at.</param>
-        /// <param name="sourceProperty">The property that represents the parent key.</param>
+        /// <param name="parentKeyProperty">The property that represents the parent key.</param>
         /// <param name="identifiedByProperty">The property that identifies the key on the child object.</param>
         /// <returns>A new <see cref="KeyResolver"/>.</returns>
-        public static KeyResolver FromParentHierarchy(IProjection projection, PropertyPath sourceProperty, PropertyPath identifiedByProperty)
+        public static KeyResolver FromParentHierarchy(IProjection projection, PropertyPath parentKeyProperty, PropertyPath identifiedByProperty)
         {
             return async (IProjectionEventProvider eventProvider, AppendedEvent @event) =>
             {
                 var arrayIndexers = new List<ArrayIndexer>();
-                var parentKey = EventValueProviders.FromEventContent(sourceProperty)(@event);
+                var parentKey = EventValueProviders.FromEventContent(parentKeyProperty)(@event);
                 var currentProjection = projection;
                 while (currentProjection.HasParent)
                 {
@@ -68,7 +65,7 @@ namespace Aksio.Cratis.Events.Projections
 
                     if (!currentProjection.ChildrenPropertyPath.IsRoot)
                     {
-                        arrayIndexers.Add(new(currentProjection.ChildrenPropertyPath, sourceProperty, parentKey));
+                        arrayIndexers.Add(new(currentProjection.ChildrenPropertyPath, parentKeyProperty, parentKey));
                     }
                     var firstEvent = currentProjection.EventTypes.First()!;
                     var parentEvent = await eventProvider.GetLastInstanceFor(firstEvent.Id, parentKey.ToString()!);
