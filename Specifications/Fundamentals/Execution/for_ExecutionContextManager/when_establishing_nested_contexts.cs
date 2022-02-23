4323c4a60e45a4ce7e6264617efc3ce0e9ff0088
Einar Ingebrigtsen <einari@me.com>
2022-02-23 13:38:51 +0100

    Addming MicroserviceId as part of the execution context

--- Specifications/Fundamentals/Execution/for_ExecutionContextManager/when_establishing_nested_contexts.cs
+++ Specifications/Fundamentals/Execution/for_ExecutionContextManager/when_establishing_nested_contexts.cs
@@ -6,13 +6,20 @@ namespace Aksio.Cratis.Execution;
 public class when_establishing_nested_contexts : Specification
 {
     ExecutionContextManager manager;
+
+    MicroserviceId first_microservice_id;
     TenantId first_tenant_id;
     CorrelationId first_correlation_id;
+
+    MicroserviceId second_microservice_id;
     TenantId second_tenant_id;
     CorrelationId second_correlation_id;
+
+    MicroserviceId root_microservice_id;
     TenantId root_tenant_id;
     CorrelationId root_correlation_id;
 
+    MicroserviceId actual_first_microservice_id;
     TenantId actual_first_tenant_id;
     CorrelationId actual_first_correlation_id;
 
@@ -21,21 +28,25 @@ public class when_establishing_nested_contexts : Specification
         // Since the specification runner is using IAsyncLifetime - it will be in a different async context.
         // Use default behavior, since we need to have control over the async context.
         manager = new();
+        root_microservice_id = Guid.NewGuid();
         root_tenant_id = Guid.NewGuid();
         root_correlation_id = Guid.NewGuid().ToString();
+        first_microservice_id = Guid.NewGuid();
         first_tenant_id = Guid.NewGuid();
         first_correlation_id = Guid.NewGuid().ToString();
+        second_microservice_id = Guid.NewGuid();
         second_tenant_id = Guid.NewGuid();
         second_correlation_id = Guid.NewGuid().ToString();
-        manager.Establish(root_tenant_id, root_correlation_id);
+        manager.Establish(root_microservice_id, root_tenant_id, root_correlation_id);
     }
 
     void Because()
     {
         Task.Run(() =>
         {
-            manager.Establish(first_tenant_id, first_correlation_id);
-            Task.Run(() => manager.Establish(second_tenant_id, second_correlation_id)).Wait();
+            manager.Establish(first_microservice_id, first_tenant_id, first_correlation_id);
+            Task.Run(() => manager.Establish(second_microservice_id, second_tenant_id, second_correlation_id)).Wait();
+            actual_first_microservice_id = manager.Current.MicroserviceId;
             actual_first_tenant_id = manager.Current.TenantId;
             actual_first_correlation_id = manager.Current.CorrelationId;
         }).Wait();
