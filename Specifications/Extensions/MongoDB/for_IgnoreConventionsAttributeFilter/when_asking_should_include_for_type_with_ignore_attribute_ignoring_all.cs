7f0de4b389a3790baac383af40d243862ad1d434
Einar Ingebrigtsen <einari@me.com>
2022-02-24 19:28:35 +0100

    Adding support for easier ignoring model conventions + naming

---
+++ Specifications/Extensions/MongoDB/for_IgnoreConventionsAttributeFilter/when_asking_should_include_for_type_with_ignore_attribute_ignoring_all.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace Aksio.Cratis.Extensions.MongoDB.for_IgnoreConventionsAttributeFilter
+{
+    public class when_asking_should_include_for_type_with_ignore_attribute_ignoring_all : Specification
+    {
+        [IgnoreConventions]
+        record TheType();
+
+        IgnoreConventionsAttributeFilter filter;
+
+        bool result;
+
+        void Establish() => filter = new();
+
+        void Because() => result = filter.ShouldInclude("SomePack", Mock.Of<IConventionPack>(), typeof(TheType));
+
+        [Fact] void should_not_include_it() => result.ShouldBeFalse();
+    }
+}
