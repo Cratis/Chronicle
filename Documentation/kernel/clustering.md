cd73c4891685eb1e60f8b98b58e4f9bc7c7be09a
Einar Ingebrigtsen <einari@me.com>
2022-02-25 15:43:07 +0100

    Started on Cluster config for host

---
+++ Documentation/kernel/clustering.md
@@ -0,0 +1,11 @@
+# Clustering
+
+## ADO .NET
+
+https://www.devart.com/dotconnect/connection-strings.html
+https://dotnet.github.io/orleans/docs/host/configuration_guide/configuring_ADO.NET_providers.html
+
+SQL Scripts:
+https://github.com/dotnet/orleans/tree/main/src/AdoNet
+https://github.com/dotnet/orleans/tree/main/src/AdoNet/Shared
+https://github.com/dotnet/orleans/tree/main/src/AdoNet/Orleans.Clustering.AdoNet
--- Source/Kernel/Server/Program.cs
+++ Source/Kernel/Server/Program.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Net;
 using Aksio.Cratis.Compliance;
 using Aksio.Cratis.Compliance.MongoDB;
 using Aksio.Cratis.Events.Projections;
@@ -13,7 +12,6 @@ using Aksio.Cratis.Events.Schemas.MongoDB;
 using Aksio.Cratis.Events.Store;
 using Aksio.Cratis.Events.Store.MongoDB;
 using Orleans;
-using Orleans.Configuration;
 using Orleans.Hosting;
 using Serilog;
 
@@ -34,7 +32,7 @@ namespace Aksio.Cratis.Server
              Host.CreateDefaultBuilder(args)
                 .UseAksio(_ => _.InSilo())
                 .UseOrleans(_ => _
-                    .UseLocalhostClustering()
+                    .UseCluster()
                     .ConfigureServices(_ => _
                         .AddSingleton<IProjectionPositions, MongoDBProjectionPositions>()
                         .AddSingleton<IChangesetStorage, MongoDBChangesetStorage>()
@@ -44,13 +42,6 @@ namespace Aksio.Cratis.Server
                         .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>()
                         .AddSingleton<IProjectionPipelineDefinitionsStorage, MongoDBProjectionPipelineDefinitionsStorage>()
                         .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>())
-                    .Configure<EndpointOptions>(options =>
-                    {
-                        options.SiloPort = 11111;
-                        options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 11111);
-                        options.GatewayPort = 30000;
-                        options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 30000);
-                    })
                     .AddConnectedClientsTracking()
                     .AddEventLogStream()
                     .UseMongoDBReminderService()
