aa1ce5788fe5273a0f27ad328bd0c9d08c7baeb3
Einar Ingebrigtsen <einari@me.com>
2022-03-19 09:01:34 +0100

    Fixing styles

--- Source/Kernel/Server/Program.cs
+++ Source/Kernel/Server/Program.cs
@@ -19,72 +19,71 @@ using Serilog;
 
 #pragma warning disable SA1600
 
-namespace Aksio.Cratis.Server
+namespace Aksio.Cratis.Server;
+
+public static class Program
 {
-    public static class Program
+    public static Task Main(string[] args)
     {
-        public static Task Main(string[] args)
-        {
-            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptions;
+        AppDomain.CurrentDomain.UnhandledException += UnhandledExceptions;
 
-            return CreateHostBuilder(args).RunConsoleAsync();
-        }
+        return CreateHostBuilder(args).RunConsoleAsync();
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-             Host.CreateDefaultBuilder(args)
-                .UseAksio(_ => _.InSilo())
-                .UseOrleans(_ => _
-                    .UseLocalhostClustering()
-                    .ConfigureServices(_ => _
-                        .AddSingleton<IProjectionPositions, MongoDBProjectionPositions>()
-                        .AddSingleton<IChangesetStorage, MongoDBChangesetStorage>()
-                        .AddSingleton<IEncryptionKeyStore>(sp => new CacheEncryptionKeyStore(sp.GetService<MongoDBEncryptionKeyStore>()!))
-                        .AddSingleton<ISchemaStore, MongoDBSchemaStore>()
-                        .AddSingleton<IEventLogStorageProvider, MongoDBEventLogStorageProvider>()
-                        .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>()
-                        .AddSingleton<IProjectionPipelineDefinitionsStorage, MongoDBProjectionPipelineDefinitionsStorage>()
-                        .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>())
-                    .Configure<EndpointOptions>(options =>
-                    {
-                        options.SiloPort = 11111;
-                        options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 11111);
-                        options.GatewayPort = 30000;
-                        options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 30000);
-                    })
-                    .AddConnectedClientsTracking()
-                    .AddEventLogStream()
-                    .UseMongoDBReminderService()
-                    .AddSimpleMessageStreamProvider("observer-handlers", cs => cs.Configure(o => o.FireAndForgetDelivery = false))
-                    .AddExecutionContext())
-                .ConfigureWebHostDefaults(_ => _
-                    .UseStartup<Startup>());
-
-        static void UnhandledExceptions(object sender, UnhandledExceptionEventArgs args)
-        {
-            if (args.ExceptionObject is Exception exception)
-            {
-                Log.Logger?.Error(exception, "Unhandled exception");
-                Log.CloseAndFlush();
-                Console.WriteLine("************ BEGIN UNHANDLED EXCEPTION ************");
-                PrintExceptionInfo(exception);
-
-                while (exception.InnerException != null)
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
+            .UseAksio(, _ => _.InSilo())
+            .UseOrleans(_ => _
+                .UseLocalhostClustering()
+                .ConfigureServices(_ => _
+                    .AddSingleton<IProjectionPositions, MongoDBProjectionPositions>()
+                    .AddSingleton<IChangesetStorage, MongoDBChangesetStorage>()
+                    .AddSingleton<IEncryptionKeyStore>(sp => new CacheEncryptionKeyStore(sp.GetService<MongoDBEncryptionKeyStore>()!))
+                    .AddSingleton<ISchemaStore, MongoDBSchemaStore>()
+                    .AddSingleton<IEventLogStorageProvider, MongoDBEventLogStorageProvider>()
+                    .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>()
+                    .AddSingleton<IProjectionPipelineDefinitionsStorage, MongoDBProjectionPipelineDefinitionsStorage>()
+                    .AddSingleton<IProjectionDefinitionsStorage, MongoDBProjectionDefinitionsStorage>())
+                .Configure<EndpointOptions>(options =>
                 {
-                    Console.WriteLine("\n------------ BEGIN INNER EXCEPTION ------------");
-                    PrintExceptionInfo(exception.InnerException);
-                    exception = exception.InnerException;
-                    Console.WriteLine("------------ END INNER EXCEPTION ------------\n");
-                }
+                    options.SiloPort = 11111;
+                    options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 11111);
+                    options.GatewayPort = 30000;
+                    options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 30000);
+                })
+                .AddConnectedClientsTracking()
+                .AddEventLogStream()
+                .UseMongoDBReminderService()
+                .AddSimpleMessageStreamProvider("observer-handlers", cs => cs.Configure(o => o.FireAndForgetDelivery = false))
+                .AddExecutionContext())
+            .ConfigureWebHostDefaults(_ => _
+                .UseStartup<Startup>());
 
-                Console.WriteLine("************ END UNHANDLED EXCEPTION ************ ");
-            }
-        }
-
-        static void PrintExceptionInfo(Exception exception)
+    static void UnhandledExceptions(object sender, UnhandledExceptionEventArgs args)
+    {
+        if (args.ExceptionObject is Exception exception)
         {
-            Console.WriteLine($"Exception type: {exception.GetType().FullName}");
-            Console.WriteLine($"Exception message: {exception.Message}");
-            Console.WriteLine($"Stack Trace: {exception.StackTrace}");
+            Log.Logger?.Error(exception, "Unhandled exception");
+            Log.CloseAndFlush();
+            Console.WriteLine("************ BEGIN UNHANDLED EXCEPTION ************");
+            PrintExceptionInfo(exception);
+
+            while (exception.InnerException != null)
+            {
+                Console.WriteLine("\n------------ BEGIN INNER EXCEPTION ------------");
+                PrintExceptionInfo(exception.InnerException);
+                exception = exception.InnerException;
+                Console.WriteLine("------------ END INNER EXCEPTION ------------\n");
+            }
+
+            Console.WriteLine("************ END UNHANDLED EXCEPTION ************ ");
         }
+    }
+
+    static void PrintExceptionInfo(Exception exception)
+    {
+        Console.WriteLine($"Exception type: {exception.GetType().FullName}");
+        Console.WriteLine($"Exception message: {exception.Message}");
+        Console.WriteLine($"Stack Trace: {exception.StackTrace}");
     }
 }
