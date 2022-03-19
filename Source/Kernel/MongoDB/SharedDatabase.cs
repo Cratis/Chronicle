5df6bd8fe59e5ae70137d06f46b1f59a41bdee4c
Einar Ingebrigtsen <einari@me.com>
2022-03-19 09:38:23 +0100

    Be execution context aware

--- Source/Kernel/MongoDB/SharedDatabase.cs
+++ Source/Kernel/MongoDB/SharedDatabase.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Configuration;
+using Aksio.Cratis.Execution;
 using Aksio.Cratis.Extensions.MongoDB;
 using MongoDB.Driver;
 
@@ -12,30 +13,51 @@ namespace Aksio.Cratis.MongoDB;
 /// </summary>
 public class SharedDatabase : ISharedDatabase
 {
-    readonly IMongoDatabase _database;
+    readonly Dictionary<MicroserviceId, IMongoDatabase> _databases = new();
+    readonly IExecutionContextManager _executionContextManager;
+    readonly IMongoDBClientFactory _clientFactory;
+    readonly Storage _configuration;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SharedDatabase"/> class.
     /// </summary>
-    /// <param name="executionContext">The current <see cref="ExecutionContext"/>.</param>
+    /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for getting current execution context.</param>
     /// <param name="clientFactory"><see cref="IMongoDBClientFactory"/> for working with MongoDB.</param>
     /// <param name="configuration"><see cref="Storage"/> configuration.</param>
-    public SharedDatabase(ExecutionContext executionContext, IMongoDBClientFactory clientFactory, Storage configuration)
+    public SharedDatabase(
+        IExecutionContextManager executionContextManager,
+        IMongoDBClientFactory clientFactory,
+        Storage configuration)
     {
-        var storageForMicroservice = configuration[executionContext.MicroserviceId];
-        var url = new MongoUrl(storageForMicroservice.Get(WellKnownStorageTypes.EventStore).Shared.ToString());
-        var client = clientFactory.Create(url);
-        _database = client.GetDatabase(url.DatabaseName);
+        _executionContextManager = executionContextManager;
+        _clientFactory = clientFactory;
+        _configuration = configuration;
     }
 
     /// <inheritdoc/>
     public IMongoCollection<T> GetCollection<T>(string? collectionName = null)
     {
+        var database = GetDatabase();
         if (collectionName == null)
         {
-            return _database.GetCollection<T>();
+            return database.GetCollection<T>();
         }
 
-        return _database.GetCollection<T>(collectionName);
+        return database.GetCollection<T>(collectionName);
+    }
+
+    IMongoDatabase GetDatabase()
+    {
+        var executionContext = _executionContextManager.Current;
+        var microserviceId = executionContext.MicroserviceId;
+        if (_databases.ContainsKey(microserviceId))
+        {
+            return _databases[microserviceId];
+        }
+
+        var storageForMicroservice = _configuration[executionContext.MicroserviceId];
+        var url = new MongoUrl(storageForMicroservice.Get(WellKnownStorageTypes.EventStore).Shared.ToString());
+        var client = _clientFactory.Create(url);
+        return _databases[microserviceId] = client.GetDatabase(url.DatabaseName);
     }
 }
