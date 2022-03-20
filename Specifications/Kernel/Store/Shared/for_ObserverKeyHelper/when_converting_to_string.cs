f8efc58bc945910983296ab251570ff1363289f2
Einar Ingebrigtsen <einari@me.com>
2022-03-20 10:02:59 +0100

    Adding specs for observer key

---
+++ Specifications/Kernel/Store/Shared/for_ObserverKeyHelper/when_converting_to_string.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Events.Store.Observation.for_ObserverKey;
+
+public class when_converting_to_string : Specification
+{
+    ObserverKey input;
+    string result;
+
+    void Establish() => input = new(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+    void Because() => result = input.ToString();
+
+    [Fact] void should_combine_correctly() => result.ShouldEqual($"{input.MicroserviceId}+{input.TenantId}+{input.EventSequenceId}");
+}
