d55e3d15d8ed74df9c6f2a1218de9afcb37d9b74
Einar Ingebrigtsen <einari@me.com>
2022-03-28 20:01:50 +0200

    Fleshing out how the command trackker will work

--- Source/ApplicationModel/Frontend/commands/CommandResult.ts
+++ Source/ApplicationModel/Frontend/commands/CommandResult.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import { ICommandResult } from './ICommandResult';
+
 /**
  * Represents the result from executing a {@link ICommand}.
  */
-export class CommandResult {
+export class CommandResult implements ICommandResult {
     private _ok: boolean;
 
     /**
@@ -15,9 +17,7 @@ export class CommandResult {
         this._ok = response.ok;
     }
 
-    /**
-     * Gets whether execution is successful or not.
-     */
+    /** @inheritdoc */
     get isSuccess() {
         return this._ok;
     }
