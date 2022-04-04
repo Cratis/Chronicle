faab9827510e071159e374346c1ab3e255f25288
Einar Ingebrigtsen <einari@me.com>
2022-04-04 15:11:09 +0200

    Adding system as a valid thing that can be causing changes

--- Source/Fundamentals/Execution/CausedBy.cs
+++ Source/Fundamentals/Execution/CausedBy.cs
@@ -12,6 +12,17 @@ namespace Aksio.Cratis.Execution;
 public record CausedBy(Guid Value) : ConceptAs<Guid>(Value)
 {
     /// <summary>
+    /// Used for when the system has caused it.
+    /// </summary>
+    public static readonly CausedBy System = new(Guid.Empty);
+
+    /// <summary>
+    /// Implicitly convert from <see cref="string"/> representation of a <see cref="Guid"/> to <see cref="CausedBy"/>.
+    /// </summary>
+    /// <param name="value"><see cref="string"/> to convert from.</param>
+    public static implicit operator CausedBy(string value) => new(Guid.Parse(value));
+
+    /// <summary>
     /// Implicitly convert from <see cref="Guid"/> to <see cref="CausedBy"/>.
     /// </summary>
     /// <param name="value"><see cref="Guid"/> to convert from.</param>
