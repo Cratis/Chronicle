e4d6eb2ea7af88afd39131dba68ac2c2ec9ecbf5
Einar Ingebrigtsen <einari@me.com>
2022-04-07 09:35:19 +0200

    Template fixed for dotnet style

--- Source/ApplicationModel/Tooling/Templates/templates/aspnet/Integration/AccountHolders/KontoEier.cs
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Integration/AccountHolders/KontoEier.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Integration.AccountHolders
-{
-    public record KontoEier(string Fnr, string Fornavn, string Etternavn, DateTime FodselsDato, string Adresse, string By, string PostNr, string Land);
-}
+namespace Integration.AccountHolders;
+
+public record KontoEier(string Fnr, string Fornavn, string Etternavn, DateTime FodselsDato, string Adresse, string By, string PostNr, string Land);
