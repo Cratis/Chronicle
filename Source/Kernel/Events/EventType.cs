71b4549d79842e68c727857ca48e00cb133d5405
Einar Ingebrigtsen <einari@me.com>
2022-04-07 14:14:48 +0200

    Making event type convertable to and from string

--- Source/Kernel/Events/EventType.cs
+++ Source/Kernel/Events/EventType.cs
@@ -10,9 +10,36 @@ namespace Aksio.Cratis.Events;
 /// <param name="Generation"><see cref="EventGeneration">Generation</see> of the event.</param>
 public record EventType(EventTypeId Id, EventGeneration Generation)
 {
+    /// <summary>
+    /// Implicitly convert from <see cref="EventType"/> to string.
+    /// </summary>
+    /// <param name="type">EventType to convert from.</param>
+    public static implicit operator string(EventType type) => type.ToString();
+
+    /// <summary>
+    /// Implicitly convert from string to <see cref="EventType"/>.
+    /// </summary>
+    /// <param name="value">String representation.</param>
+    public static implicit operator EventType(string value) => Parse(value);
+
     /// <inheritdoc/>
     public override string ToString()
     {
-        return $"({Id.Value} - {Generation.Value})";
+        return $"{Id.Value}+{Generation.Value}";
+    }
+
+    /// <summary>
+    /// Parse from a string representation of event type to actual type.
+    /// </summary>
+    /// <param name="input">String representation.</param>
+    /// <returns>Parsed <see cref="EventType"/>.</returns>
+    /// <remarks>
+    /// The expected format is guid+generation.
+    /// Ex: aa7faa25-afc1-48d1-8558-716581c0e916+1.
+    /// </remarks>
+    public static EventType Parse(string input)
+    {
+        var segments = input.Split('+');
+        return new(segments[0], uint.Parse(segments[1]));
     }
 }
