8cb3e25e22771fbeb99f066ee65a5226b5960c9e
Einar Ingebrigtsen <einari@me.com>
2022-04-13 15:46:49 +0200

    Created a plan for what we need

--- Source/Kernel/Store/Grains/Connections/ConnectedClients.cs
+++ Source/Kernel/Store/Grains/Connections/ConnectedClients.cs
@@ -4,6 +4,7 @@
 using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 using Orleans;
+using Orleans.Runtime;
 
 namespace Aksio.Cratis.Events.Store.Grains.Connections;
 
@@ -13,16 +14,26 @@ namespace Aksio.Cratis.Events.Store.Grains.Connections;
 public class ConnectedClients : Grain, IConnectedClients
 {
     readonly ConcurrentDictionary<string, List<IConnectedClientObserver>> _observers = new();
+    readonly ISiloStatusOracle _siloStatusOracle;
     readonly ILogger<ConnectedClients> _logger;
     string _lastConnectedClient = string.Empty;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ConnectedClients"/> class.
     /// </summary>
+    /// <param name="siloStatusOracle"><see cref="ISiloStatusOracle"/> for details on currently running silo.</param>
     /// <param name="logger"><see cref="ILogger"/> for logging.</param>
-    public ConnectedClients(ILogger<ConnectedClients> logger)
+    public ConnectedClients(ISiloStatusOracle siloStatusOracle, ILogger<ConnectedClients> logger)
     {
+        _siloStatusOracle = siloStatusOracle;
         _logger = logger;
+
+        // TODO: (the plan)
+        // - Clients connecting are connected for the silo they are connecting to
+        // - If silo goes down, during OnActivateAsync() we will get members of the cluster and remove all connected clients for silos that are no longer with us
+        // - Subscribe to silo changes (SubscribeToSiloStatusEvents)
+        // - Update GetLastConnectedClientConnectionId() for connected clients to be for specific silo - client needs to know which silo it is connected to.
+        Console.WriteLine(_siloStatusOracle);
     }
 
     /// <inheritdoc/>
