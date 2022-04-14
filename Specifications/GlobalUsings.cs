2c0e36f16ed21602e8da0021f8463a54ebda7310
Einar Ingebrigtsen <einari@me.com>
2022-04-14 08:32:39 +0200

    Adding out ExecutionContext as preferred type

--- Specifications/GlobalUsings.cs
+++ Specifications/GlobalUsings.cs
@@ -4,6 +4,7 @@
 global using Aksio.Cratis.Common;
 global using Aksio.Cratis.Common.Grains;
 global using Aksio.Specifications;
+global using ExecutionContext = Aksio.Cratis.Execution.ExecutionContext;
 global using Moq;
 global using Xunit;
 global using static Moq.It;
