bb58d93054a7b3b97b83661d1a10718bfef06385
Einar Ingebrigtsen <einari@me.com>
2022-04-21 15:55:44 +0200

    Added support for rewinding

--- Source/Kernel/Store/Api/Observation/Observers.cs
+++ Source/Kernel/Store/Api/Observation/Observers.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Applications.Queries;
+using Aksio.Cratis.Events.Store.Grains.Observation;
 using Aksio.Cratis.Execution;
 using Microsoft.AspNetCore.Mvc;
+using Orleans;
 
 namespace Aksio.Cratis.Events.Store.Observation.Api;
 
@@ -14,19 +16,40 @@ namespace Aksio.Cratis.Events.Store.Observation.Api;
 public class Observers : Controller
 {
     readonly IObserversState _observersState;
+    readonly IGrainFactory _grainFactory;
     readonly IExecutionContextManager _executionContextManager;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Observers"/> class.
     /// </summary>
     /// <param name="observersState"><see cref="IObserversState"/> for working with the state of observers.</param>
+    /// <param name="grainFactory"><see cref="IGrainFactory"/> for getting grains.</param>
     /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for working with the execution context.</param>
     public Observers(
         IObserversState observersState,
+        IGrainFactory grainFactory,
         IExecutionContextManager executionContextManager)
     {
         _observersState = observersState;
+        _grainFactory = grainFactory;
         _executionContextManager = executionContextManager;
+    }
+
+    /// <summary>
+    /// Rewind a specific observer for a microservice and tenant.
+    /// </summary>
+    /// <param name="observerId"><see cref="ObserverId"/> to rewind.</param>
+    /// <param name="microserviceId"><see cref="MicroserviceId"/> the observer is for.</param>
+    /// <param name="tenantId"><see cref="TenantId"/> the observer is for.</param>
+    /// <returns>Awaitable task.</returns>
+    [HttpPost("{observerId}/rewind")]
+    public async Task Rewind(
+        [FromRoute] ObserverId observerId,
+        [FromQuery] MicroserviceId microserviceId,
+        [FromQuery] TenantId tenantId)
+    {
+        var observer = _grainFactory.GetGrain<IObserver>(observerId, new ObserverKey(microserviceId, tenantId, EventSequenceId.Log));
+        await observer.Rewind();
     }
 
     /// <summary>
