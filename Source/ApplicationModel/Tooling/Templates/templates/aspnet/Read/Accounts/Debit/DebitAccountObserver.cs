2b10675c2ac827885dabd787536933cc3b88f839
Einar Ingebrigtsen <einari@me.com>
2022-04-24 09:30:42 +0200

    New Guid

--- Source/ApplicationModel/Tooling/Templates/templates/aspnet/Read/Accounts/Debit/DebitAccountObserver.cs
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Read/Accounts/Debit/DebitAccountObserver.cs
@@ -5,7 +5,7 @@ using Events.Accounts.Debit;
 
 namespace Read.Accounts.Debit;
 
-[Observer("b195adfb-e743-4457-b295-5637368436e3")]
+[Observer("b195adfb-e743-4457-b295-5637368436e4")]
 public class DebitAccountObserver
 {
     public Task Opened(DebitAccountOpened @event, EventContext context)
