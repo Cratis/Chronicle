4ecba5d392248d6e63c92347f0668304b15a8b05
Einar Ingebrigtsen <einari@me.com>
2022-05-03 13:14:17 +0200

    Splitting out for reusing the base information

--- Samples/Bank/Integration.Specs/AccountHolders/for_AccountHolderDetailsAdapter/when_importing_for_the_first_time.cs
+++ Samples/Bank/Integration.Specs/AccountHolders/for_AccountHolderDetailsAdapter/when_importing_for_the_first_time.cs
@@ -1,27 +1,26 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Aksio.Cratis.Specifications.Integration;
 using Events.AccountHolders;
 
 namespace Integration.AccountHolders.for_AccountHolderDetailsAdapter;
 
-public class when_importing_for_the_first_time : AdapterSpecificationFor<AccountHolder, KontoEier>
+public class when_importing_for_the_first_time : given.person_information
 {
     KontoEier object_to_import = new(
-        "12345678901",
-        "Bør",
-        "Børsen",
-        new DateTime(1873, 3, 17),
-        "Langgata 1",
-        "Oslo",
-        "0103",
-        "Norge");
+        social_security_number,
+        first_name,
+        last_name,
+        birth_date,
+        address,
+        city,
+        postal_code,
+        country);
 
     protected override IAdapterFor<AccountHolder, KontoEier> CreateAdapter() => new AccountHolderDetailsAdapter();
 
     void Because() => context.Import(object_to_import);
 
-    [Fact] void should_append_account_holder_registered() => context.ShouldAppendEvents(new AccountHolderRegistered(object_to_import.Fornavn, object_to_import.Etternavn, object_to_import.FodselsDato));
-    [Fact] void should_append_account_holder_address_changed() => context.ShouldAppendEvents(new AccountHolderAddressChanged(object_to_import.Adresse, object_to_import.By, object_to_import.PostNr, object_to_import.Land));
+    [Fact] void should_append_account_holder_registered() => context.ShouldAppendEvents(new AccountHolderRegistered(first_name, last_name, birth_date));
+    [Fact] void should_append_account_holder_address_changed() => context.ShouldAppendEvents(new AccountHolderAddressChanged(address, city, postal_code, country));
 }
