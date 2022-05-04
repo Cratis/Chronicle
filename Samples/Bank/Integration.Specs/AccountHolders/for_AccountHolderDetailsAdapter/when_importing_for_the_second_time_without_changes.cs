bad8f1b0e28a2c1fac2a8a1b08e200130a50cc5a
Einar Ingebrigtsen <einari@me.com>
2022-05-04 13:47:18 +0200

    Tidying up specs + adding more scenarios as samples

---
+++ Samples/Bank/Integration.Specs/AccountHolders/for_AccountHolderDetailsAdapter/when_importing_for_the_second_time_without_changes.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Events.AccountHolders;
+
+namespace Integration.AccountHolders.for_AccountHolderDetailsAdapter;
+
+public class when_importing_for_the_second_time_without_changes : given.object_ready_for_import
+{
+    void Establish()
+    {
+        context.EventLog.Append(social_security_number, new AccountHolderRegistered(first_name, last_name, birth_date));
+        context.EventLog.Append(social_security_number, new AccountHolderAddressChanged(address, city, postal_code, country));
+    }
+
+    void Because() => context.Import(object_to_import);
+
+    [Fact] void should_not_append_any_events() => context.ShouldNotAppendEvents();
+}
