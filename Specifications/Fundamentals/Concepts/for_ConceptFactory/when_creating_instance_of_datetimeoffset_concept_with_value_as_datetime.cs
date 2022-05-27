8d5506a6888840af31da68c8d56a2ce1899e388e
Einar Ingebrigtsen <einari@me.com>
2022-05-27 08:49:42 +0200

    Fixing a problem regarding parsing of DateTimeOffset
    Typeconversion via Convert.ChangeType() is not supported
    for string -> DateTimeOffset om BCL.

---
+++ Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_of_datetimeoffset_concept_with_value_as_datetime.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Concepts.for_ConceptFactory;
+
+public class when_creating_instance_of_datetimeoffset_concept_with_value_as_datetime : Specification
+{
+    DateTimeOffsetConcept result;
+    DateTimeOffset now;
+
+    void Establish() => now = DateTimeOffset.Now;
+
+    void Because() => result = ConceptFactory.CreateConceptInstance(typeof(DateTimeOffsetConcept), now) as DateTimeOffsetConcept;
+
+    [Fact] void should_be_the_value_of_the_datetime() => result.Value.ShouldEqual(now);
+}
