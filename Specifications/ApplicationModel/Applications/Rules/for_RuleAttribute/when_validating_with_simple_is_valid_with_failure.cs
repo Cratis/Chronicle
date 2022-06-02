19783a180d579f040a876f50758ab618b389b6b0
Einar Ingebrigtsen <einari@me.com>
2022-06-02 10:35:48 +0200

    Adding specs for RuleAttribute

---
+++ Specifications/ApplicationModel/Applications/Rules/for_RuleAttribute/when_validating_with_simple_is_valid_with_failure.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Aksio.Cratis.Applications.Rules.for_Rules.for_RuleAttribute;
+
+public class when_validating_with_simple_is_valid_with_failure : given.a_validation_context
+{
+    const int value_to_validate = 42;
+    MyRuleAttribute rule;
+    ValidationResult result;
+
+    void Establish()
+    {
+        rule = new();
+        rule.simple_is_valid_return = false;
+    }
+
+    void Because()
+    {
+        try
+        {
+            rule.Validate(value_to_validate, validation_context);
+        }
+        catch (ValidationException ex)
+        {
+            result = ex.ValidationResult;
+        }
+    }
+
+    [Fact] void should_not_call_complex_is_valid() => rule.complex_is_valid_called.ShouldBeFalse();
+    [Fact] void should_return_validation_result_with_error_message() => result.ErrorMessage.ShouldEqual(MyRuleAttribute.error_message);
+}
