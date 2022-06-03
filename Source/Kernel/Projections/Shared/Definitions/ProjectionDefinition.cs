cfe51caaadb7f10c3e6d31d04bf022fefde315b2
Einar Ingebrigtsen <einari@me.com>
2022-06-03 13:21:32 +0200

    Removing empty line in XML doc

--- Source/Kernel/Projections/Shared/Definitions/ProjectionDefinition.cs
+++ Source/Kernel/Projections/Shared/Definitions/ProjectionDefinition.cs
@@ -13,7 +13,6 @@ namespace Aksio.Cratis.Events.Projections.Definitions;
 /// <param name="Model">The target <see cref="ModelDefinition"/>.</param>
 /// <param name="IsRewindable">Whether or not the projection is rewindable.</param>
 /// <param name="From">All the <see cref="FromDefinition"/> for <see cref="EventType">event types</see>.</param>
-///
 /// <param name="Children">All the <see cref="ChildrenDefinition"/> for properties on model.</param>
 /// <param name="RemovedWith">The definition of what removes a child, if any.</param>
 public record ProjectionDefinition(
