52952827c455826e3f1b6ba579e309b5aa852273
Einar Ingebrigtsen <einari@me.com>
2022-06-03 13:14:41 +0200

    Test for inbox observer

---
+++ Samples/Bank/Public/Accounts/Debit/AccountBalanceObserver.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Events.Public.Accounts.Debit;
+
+namespace Public.Accounts.Debit;
+
+[Observer("2b110250-319d-4246-8b30-7a191b513e33", inbox: true)]
+public class AccountBalanceObserver
+{
+    public Task Balance(AccountBalance @event, EventContext context)
+    {
+        Console.WriteLine($"Balance for {context.EventSourceId} : {@event.Amount}");
+        return Task.CompletedTask;
+    }
+}
