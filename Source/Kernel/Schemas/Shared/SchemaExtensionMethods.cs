8f7120c5eb45efcad067d09a2f789fdf78393989
Einar Ingebrigtsen <einari@me.com>
2022-06-04 10:28:12 +0200

    Adding event type to schema. Used when projecting to other events.

--- Source/Kernel/Schemas/Shared/SchemaExtensionMethods.cs
+++ Source/Kernel/Schemas/Shared/SchemaExtensionMethods.cs
@@ -10,9 +10,44 @@ namespace Aksio.Cratis.Events.Schemas;
 /// </summary>
 public static class SchemaExtensionMethods
 {
+    const string EventTypeExtension = "eventType";
     const string IsPublicExtension = "isPublic";
     const string DisplayNameExtension = "displayName";
     const string GenerationExtension = "generation";
+
+    /// <summary>
+    /// Set the event type extension in the schema.
+    /// </summary>
+    /// <param name="schema"><see cref="JsonSchema"/> to use.</param>
+    /// <param name="eventType">EventType to set.</param>
+    public static void SetEventType(this JsonSchema schema, EventType eventType)
+    {
+        schema.EnsureExtensionData();
+        schema.ExtensionData[EventTypeExtension] = $"{eventType.Id}+{eventType.Generation}";
+    }
+
+    /// <summary>
+    /// Get the event type extension from the schema.
+    /// </summary>
+    /// <param name="schema"><see cref="JsonSchema"/> to get from.</param>
+    /// <returns>True if event type is set, false if not.</returns>
+    public static bool HasEventType(this JsonSchema schema) => schema.ExtensionData?[EventTypeExtension] is not null;
+
+    /// <summary>
+    /// Get the event type extension from the schema.
+    /// </summary>
+    /// <param name="schema"><see cref="JsonSchema"/> to get from.</param>
+    /// <returns>The event type. If there is no event type in the schema, it will return an unknown event type.</returns>
+    public static EventType GetEventType(this JsonSchema schema)
+    {
+        if (schema.HasEventType())
+        {
+            var elements = schema.ExtensionData![EventTypeExtension]!.ToString()!.Split('+');
+            return new(Guid.Parse(elements[0]), uint.Parse(elements[1]));
+        }
+
+        return new(EventTypeId.Unknown, EventGeneration.First);
+    }
 
     /// <summary>
     /// Set the isPublic extension in the schema.
