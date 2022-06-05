06f12cd8153102a3bc3440d2ef740f51c48532d9
Einar Ingebrigtsen <einari@me.com>
2022-06-05 11:48:14 +0200

    Adding a second microservice to the bank setup

---
+++ Samples/BalanceNotifier/GlobalUsings.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma warning disable CS8019
+
+global using Aksio.Cratis.Applications.Commands;
+global using Aksio.Cratis.Applications.Queries;
+global using Aksio.Cratis.Applications.Queries.MongoDB;
+global using Aksio.Cratis.Applications.Rules;
+global using Aksio.Cratis.Concepts;
+global using Aksio.Cratis.Events;
+global using Aksio.Cratis.Events.Observation;
+global using Aksio.Cratis.Events.Projections;
+global using Aksio.Cratis.Events.Store;
+global using Aksio.Cratis.Integration;
+global using Aksio.Cratis.Models;
+global using AutoMapper;
+global using FluentValidation;
+global using Microsoft.AspNetCore.Mvc;
+global using MongoDB.Driver;
