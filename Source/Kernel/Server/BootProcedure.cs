1a28148b57b9d779df30aa24a1836f20f81f903a
Einar Ingebrigtsen <einari@me.com>
2022-06-07 17:58:27 +0200

    Explicitly populating schema store with existing schemas

--- Source/Kernel/Server/BootProcedure.cs
+++ Source/Kernel/Server/BootProcedure.cs
@@ -3,27 +3,38 @@
 
 using Aksio.Cratis.Boot;
 using Aksio.Cratis.Configuration;
+using Aksio.Cratis.Events.Projections.Grains;
+using Aksio.Cratis.Events.Schemas;
+using Aksio.Cratis.Execution;
 using Orleans;
 
-namespace Aksio.Cratis.Events.Projections.Grains;
+namespace Aksio.Cratis.Server;
 
 /// <summary>
 /// Represents a <see cref="IPerformBootProcedure"/> for the event store.
 /// </summary>
 public class BootProcedure : IPerformBootProcedure
 {
+    readonly IServiceProvider _serviceProvider;
+    readonly IExecutionContextManager _executionContextManager;
     readonly IGrainFactory _grainFactory;
     readonly Microservices _microservices;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="BootProcedure"/> class.
     /// </summary>
+    /// <param name="serviceProvider"><see cref="IServiceProvider"/> for getting services.</param>
+    /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for working with the execution context.</param>
     /// <param name="grainFactory"><see cref="IGrainFactory"/> for getting grains.</param>
     /// <param name="microservices"><see cref="Microservices"/> configuration.</param>
     public BootProcedure(
+        IServiceProvider serviceProvider,
+        IExecutionContextManager executionContextManager,
         IGrainFactory grainFactory,
         Microservices microservices)
     {
+        _serviceProvider = serviceProvider;
+        _executionContextManager = executionContextManager;
         _grainFactory = grainFactory;
         _microservices = microservices;
     }
@@ -33,6 +44,10 @@ public class BootProcedure : IPerformBootProcedure
     {
         foreach (var microserviceId in _microservices.GetMicroserviceIds())
         {
+            _executionContextManager.Establish(microserviceId);
+            var schemaStore = _serviceProvider.GetService<ISchemaStore>()!;
+            schemaStore.Populate().Wait();
+
             var projections = _grainFactory.GetGrain<IProjections>(microserviceId);
             projections.Rehydrate().Wait();
         }
