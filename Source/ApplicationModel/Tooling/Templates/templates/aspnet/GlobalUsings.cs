73c366643283d749881da673b9e828e5c95b81c9
Einar Ingebrigtsen <einari@me.com>
2022-06-07 17:27:08 +0200

    Updated template

--- Source/ApplicationModel/Tooling/Templates/templates/aspnet/GlobalUsings.cs
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/GlobalUsings.cs
@@ -6,6 +6,7 @@
 global using Aksio.Cratis.Applications.Commands;
 global using Aksio.Cratis.Applications.Queries;
 global using Aksio.Cratis.Applications.Queries.MongoDB;
+global using Aksio.Cratis.Applications.Rules;
 global using Aksio.Cratis.Concepts;
 global using Aksio.Cratis.Events;
 global using Aksio.Cratis.Events.Observation;
@@ -14,5 +15,6 @@ global using Aksio.Cratis.Events.Store;
 global using Aksio.Cratis.Integration;
 global using Aksio.Cratis.Models;
 global using AutoMapper;
+global using FluentValidation;
 global using Microsoft.AspNetCore.Mvc;
 global using MongoDB.Driver;
