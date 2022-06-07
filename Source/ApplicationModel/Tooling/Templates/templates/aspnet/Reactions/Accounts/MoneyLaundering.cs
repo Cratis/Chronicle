598d72b278b9bc23744d3299982772edd6f94304
Einar Ingebrigtsen <einari@me.com>
2022-06-07 17:27:08 +0200

    Updated template

---
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Reactions/Accounts/MoneyLaundering.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Events.Accounts.Debit;
+
+namespace Reactions.Accounts;
+
+#pragma warning disable IDE0060
+
+[Observer("6f71990b-8a96-4bf0-92ad-76f31cd7819f")]
+public class MoneyLaundering
+{
+    readonly IImmediateProjections _immediateProjections;
+    readonly IEventLog _eventLog;
+
+    public MoneyLaundering(IImmediateProjections immediateProjections, IEventLog eventLog)
+    {
+        _immediateProjections = immediateProjections;
+        _eventLog = eventLog;
+    }
+
+    public async Task AccountOpened(DebitAccountOpened @event, EventContext context)
+    {
+        var count = await _immediateProjections.GetInstanceById<AccountsCounter>(context.EventSourceId);
+        if (count.Count > 42)
+        {
+            await _eventLog.Append(Guid.Empty.ToString(), new PossibleMoneyLaunderingDetected(@event.Owner, context.EventSourceId));
+        }
+    }
+}
