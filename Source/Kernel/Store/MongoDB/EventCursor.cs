b015c25767356b5b6ab602cc61629b7ef7fb9664
Einar Ingebrigtsen <einari@me.com>
2022-06-15 09:49:58 +0200

    Due to deadlocks, we have to do MoveNext() and not MoveNextAsync()

--- Source/Kernel/Store/MongoDB/EventCursor.cs
+++ Source/Kernel/Store/MongoDB/EventCursor.cs
@@ -5,13 +5,13 @@ using MongoDB.Driver;
 
 namespace Aksio.Cratis.Events.Store.MongoDB;
 
+#pragma warning disable CA1849, MA0042 // Allow this blocking call - we get deadlocks when using MongoDB MoveNextAsync()
+
 /// <summary>
 /// Represents an implementation of <see cref="IEventCursor"/> for handling events from event sequence.
 /// </summary>
 public class EventCursor : IEventCursor
 {
-    readonly EventSequenceNumber _sequenceNumber;
-    readonly IEnumerable<EventType> _eventTypes;
     readonly IEventConverter _converter;
     readonly IAsyncCursor<Event>? _innerCursor;
 
@@ -21,18 +21,12 @@ public class EventCursor : IEventCursor
     /// <summary>
     /// Initializes a new instance of the <see cref="EventCursor"/> class.
     /// </summary>
-    /// <param name="sequenceNumber">Blah.</param>
-    /// <param name="eventTypes">Blasdah.</param>
     /// <param name="converter"><see cref="IEventConverter"/> to convert event types.</param>
     /// <param name="innerCursor">The underlying MongoDB cursor.</param>
     public EventCursor(
-        EventSequenceNumber sequenceNumber,
-        IEnumerable<EventType> eventTypes,
         IEventConverter converter,
         IAsyncCursor<Event>? innerCursor)
     {
-        _sequenceNumber = sequenceNumber;
-        _eventTypes = eventTypes;
         _converter = converter;
         _innerCursor = innerCursor;
     }
@@ -40,20 +34,17 @@ public class EventCursor : IEventCursor
     /// <inheritdoc/>
     public async Task<bool> MoveNext()
     {
-        Console.WriteLine($"MoveNext: {_sequenceNumber}:{string.Join(", ", _eventTypes.Select(_ => _.ToString()))}");
         if (_innerCursor is null) return false;
-        var result = await _innerCursor.MoveNextAsync();
+
+        var result = _innerCursor.MoveNext();
         if (_innerCursor.Current is not null)
         {
-            Console.WriteLine($"{_sequenceNumber}:{string.Join(", ", _eventTypes.Select(_ => _.ToString()))} - {_innerCursor.Current.Count()} - {_innerCursor.Current.First().SequenceNumber}");
             Current = await Task.WhenAll(_innerCursor.Current.Select(@event => _converter.ToAppendedEvent(@event)));
         }
         else
         {
-            Console.WriteLine($"MoveNext: {_sequenceNumber}:{string.Join(", ", _eventTypes.Select(_ => _.ToString()))} - nothing");
             Current = Array.Empty<AppendedEvent>();
         }
-        Console.WriteLine($"MoveNext: {_sequenceNumber}:{string.Join(", ", _eventTypes.Select(_ => _.ToString()))} - result: {result}");
         return result;
     }
 
