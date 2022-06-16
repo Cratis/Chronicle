4f6c7033bd44c4912395aa02d86c9f97d38da62b
Einar Ingebrigtsen <einari@me.com>
2022-06-16 21:17:33 +0200

    Fixing specs to follow implementation

--- Specifications/Kernel/Store/Shared/EventSequences/Caching/for_EventSequenceCache/when_getting_from_sequence_number/and_sequence_number_intersects_cache_range.cs
+++ Specifications/Kernel/Store/Shared/EventSequences/Caching/for_EventSequenceCache/when_getting_from_sequence_number/and_sequence_number_intersects_cache_range.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Aksio.Cratis.Events.Store.EventSequences.Caching.for_EventSequenceCache.when_getting_from_Sequence_number;
+namespace Aksio.Cratis.Events.Store.EventSequences.Caching.for_EventSequenceCache.when_getting_from_sequence_number;
 
 public class and_sequence_number_intersects_cache_range : given.a_cache_with_a_set_of_events
 {
