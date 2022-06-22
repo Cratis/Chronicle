f120e7b5d136c6e9e4fca95ad291cb64477593e0
Einar Ingebrigtsen <einari@me.com>
2022-06-22 13:52:21 +0200

    Concept for Address

---
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Web/API/AccountHolders/Address.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+
+export type Address = {
+    addressLine: string;
+    postalCode: string;
+    city: string;
+    country: string;
+};
