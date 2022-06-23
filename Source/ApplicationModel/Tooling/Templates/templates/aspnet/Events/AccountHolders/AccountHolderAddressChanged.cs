70faa252394c8848a569eac630226a5880dc30b9
Einar Ingebrigtsen <einari@me.com>
2022-06-23 14:19:53 +0200

    Fixing sample to work with complex types for integration

--- Source/ApplicationModel/Tooling/Templates/templates/aspnet/Events/AccountHolders/AccountHolderAddressChanged.cs
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Events/AccountHolders/AccountHolderAddressChanged.cs
@@ -4,4 +4,4 @@
 namespace Events.AccountHolders;
 
 [EventType("ccb6afd1-e47e-45e0-8d37-6d2a8d071344")]
-public record AccountHolderAddressChanged(string Address, string City, string PostalCode, string Country);
+public record AccountHolderAddressChanged(string AddressLine, string City, string PostalCode, string Country);
