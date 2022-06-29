5f2ef0feb19229c599221adb0b46a4aed27c1eae
Einar Ingebrigtsen <einari@me.com>
2022-06-29 18:31:12 +0200

    Adding support for filtering on properties becoming null
    Fixes #373

--- Specifications/Clients/DotNET/Integration/for_ImportBuilderExtensions/when_filtering_on_properties_becoming_null_and_next_value_matches_one_property.cs
+++ Specifications/Clients/DotNET/Integration/for_ImportBuilderExtensions/when_filtering_on_properties_becoming_null_and_next_value_matches_one_property.cs
@@ -3,17 +3,17 @@
 
 namespace Aksio.Cratis.Integration.for_ImportBuilderExtensions;
 
-public class when_filtering_on_properties_and_next_value_does_not_match_the_property : given.a_change_on_one_property
+public class when_filtering_on_properties_becoming_null_and_next_value_matches_one_property : given.a_change_on_one_property_that_becomes_null
 {
     ImportContext<Model, ExternalModel> result;
 
     void Establish()
     {
-        context = import_builder.WithProperties(_ => _.SomeInteger);
+        context = import_builder.WithPropertiesBecomingNull(_ => _.SomeString);
         context.Subscribe(_ => result = _);
     }
 
     void Because() => subject.OnNext(new ImportContext<Model, ExternalModel>(changeset, events_to_append));
 
-    [Fact] void should_not_filter_through_the_context() => result.ShouldBeNull();
+    [Fact] void should_filter_through_the_context() => result.ShouldNotBeNull();
 }
