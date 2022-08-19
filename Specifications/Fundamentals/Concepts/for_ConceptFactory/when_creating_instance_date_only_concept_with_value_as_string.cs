ac7b16878e79dfbad376dba91f5e6d8f6a5eb8e0
Einar Ingebrigtsen <einari@me.com>
2022-08-19 10:24:20 +0200

    Adding support for DateOnly & TimeOnly concepts

---
+++ Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_date_only_concept_with_value_as_string.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Concepts.for_ConceptFactory;
+
+public class when_creating_instance_date_only_concept_with_value_as_string : Specification
+{
+    DateOnlyConcept result;
+    DateOnly input;
+    string input_as_string;
+
+    void Establish()
+    {
+        input = DateOnly.FromDateTime(DateTime.UtcNow);
+        input_as_string = input.ToString("O");
+    }
+
+    void Because() => result = ConceptFactory.CreateConceptInstance(typeof(DateOnlyConcept), input) as DateOnlyConcept;
+
+    [Fact] void should_be_the_value_of_the_date_only() => result.Value.ShouldEqual(input);
+}
