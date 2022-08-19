9a08a4a8f2d8b61515f4750b2c93ada5494022cc
Einar Ingebrigtsen <einari@me.com>
2022-08-19 12:10:49 +0200

    Adding sample for testing DateOnly all the way through

---
+++ Samples/Banking/Bank/Web/API/accounts/debit/moneylaundry/AllPotentialMoneyLaundryCases.ts
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { QueryFor, QueryResult, useQuery, PerformQuery } from '@aksio/cratis-applications-frontend/queries';
+import { PotentialMoneyLaundryCase } from './PotentialMoneyLaundryCase';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/accounts/debit/moneylaundry');
+
+export class AllPotentialMoneyLaundryCases extends QueryFor<PotentialMoneyLaundryCase[]> {
+    readonly route: string = '/api/accounts/debit/moneylaundry';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+    readonly defaultValue: PotentialMoneyLaundryCase[] = [];
+
+    get requestArguments(): string[] {
+        return [
+        ];
+    }
+
+    static use(): [QueryResult<PotentialMoneyLaundryCase[]>, PerformQuery] {
+        return useQuery<PotentialMoneyLaundryCase[], AllPotentialMoneyLaundryCases>(AllPotentialMoneyLaundryCases);
+    }
+}
