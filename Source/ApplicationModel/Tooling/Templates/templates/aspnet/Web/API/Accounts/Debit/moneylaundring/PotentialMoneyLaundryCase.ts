60f68537eb89299babaa1549e750f8046b3a5c66
Einar Ingebrigtsen <einari@me.com>
2022-08-25 11:04:18 +0200

    Regen of templaet

---
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Web/API/Accounts/Debit/moneylaundring/PotentialMoneyLaundryCase.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { DateOnly } from './DateOnly';
+
+export type PotentialMoneyLaundryCase = {
+    personId: string;
+    accountId: string;
+    lastOccurrence: DateOnly;
+};
