c187610d5cb2765d4530271eb390f9337f8abc3d
Einar Ingebrigtsen <einari@me.com>
2022-09-13 12:55:09 +0200

    Fixing broken specs after API change

--- Specifications/Clients/DotNET/Integration/for_ImportOperations/when_applying_external_model_with_one_property_changed_for_public_event.cs
+++ Specifications/Clients/DotNET/Integration/for_ImportOperations/when_applying_external_model_with_one_property_changed_for_public_event.cs
@@ -18,7 +18,7 @@ public class when_applying_external_model_with_one_property_changed_for_public_e
 
         event_outbox
             .Setup(_ => _.Append(IsAny<EventSourceId>(), IsAny<object>(), null))
-            .Callback((EventSourceId _, object @event) => event_appended_to_outbox = (@event as SomePublicEvent)!);
+            .Callback((EventSourceId _, object @event, DateTimeOffset? __) => event_appended_to_outbox = (@event as SomePublicEvent)!);
     }
 
     async Task Because() => await operations.Apply(incoming);
