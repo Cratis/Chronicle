fc71533ae615a8445ef51e039567d8512765563d
Einar Ingebrigtsen <einari@me.com>
2022-09-14 12:52:20 +0200

    Command changed for nested testing

--- Source/ApplicationModel/Tooling/Templates/templates/aspnet/Domain/Accounts/Debit/AccountDetails.cs
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Domain/Accounts/Debit/AccountDetails.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.ComponentModel.DataAnnotations;
+using Concepts;
 using Concepts.Accounts;
 
 namespace Domain.Accounts.Debit;
 
-public record OpenDebitAccount(
-    [Required] AccountId AccountId,
-    AccountDetails Details);
+public record AccountDetails(AccountName Name, PersonId Owner, CardEnabledOnAccount IncludeCard);
