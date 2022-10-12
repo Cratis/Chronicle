cf425e196052ab4bc1b92ac1f2ae6ff7ee1b3141
Einar Ingebrigtsen <einari@me.com>
2022-10-12 16:31:33 +0200

    Expanding @field() decorator to take derivatives and making the proxy generator output this instead

--- Source/Node/Fundamentals/derivedTypeDecorator.ts
+++ Source/Node/Fundamentals/derivedTypeDecorator.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export function derivedType(identifier) {
+import 'reflect-metadata';
+import { DerivedType } from './DerivedType';
+
+export function derivedType(identifier: string) {
     return function (target: any) {
-        debugger;
+        DerivedType.set(target, identifier);
     };
 }
