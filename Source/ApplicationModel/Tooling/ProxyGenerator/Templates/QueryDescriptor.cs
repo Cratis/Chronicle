fa055110d18805328c1ca78974922f3fe86f32be
Einar Ingebrigtsen <einari@me.com>
2022-10-12 19:11:11 +0200

    Adding the constructor to the descriptor

--- Source/ApplicationModel/Tooling/ProxyGenerator/Templates/QueryDescriptor.cs
+++ Source/ApplicationModel/Tooling/ProxyGenerator/Templates/QueryDescriptor.cs
@@ -9,7 +9,8 @@ namespace Aksio.Cratis.Applications.ProxyGenerator.Templates;
 /// <param name="Route">API route for the command.</param>
 /// <param name="Name">Name of the command.</param>
 /// <param name="Model">Type of model the query is for.</param>
+/// <param name="Constructor">The JavaScript constructor for the model type.</param>
 /// <param name="IsEnumerable">Whether or not the result is an enumerable or not.</param>
 /// <param name="Imports">Additional import statements.</param>
 /// <param name="Arguments">Arguments for the query.</param>
-public record QueryDescriptor(string Route, string Name, string Model, bool IsEnumerable, IEnumerable<ImportStatement> Imports, IEnumerable<RequestArgumentDescriptor> Arguments);
+public record QueryDescriptor(string Route, string Name, string Model, string Constructor, bool IsEnumerable, IEnumerable<ImportStatement> Imports, IEnumerable<RequestArgumentDescriptor> Arguments);
