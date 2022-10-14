9eb4f1daf5b2cc9a758b90a22d445dca9ca0aea0
Einar Ingebrigtsen <einari@me.com>
2022-10-14 10:09:10 +0200

    Generated

--- Source/Workbench/API/events/store/observers/RecoveringFailedObserverPartition.ts
+++ Source/Workbench/API/events/store/observers/RecoveringFailedObserverPartition.ts
@@ -6,10 +6,13 @@ import { field } from '@aksio/cratis-fundamentals';
 
 
 export class RecoveringFailedObserverPartition {
+
     @field(String)
     eventSourceId!: string;
+
     @field(Number)
     sequenceNumber!: number;
+
     @field(Date)
     startedRecoveryAt!: Date;
 }
