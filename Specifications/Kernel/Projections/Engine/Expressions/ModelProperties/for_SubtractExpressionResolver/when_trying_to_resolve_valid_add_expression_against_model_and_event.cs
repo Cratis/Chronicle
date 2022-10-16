8ecb33edc1f090e75a0f8520b08c78fa2c1afdb9
Einar Ingebrigtsen <einari@me.com>
2022-10-16 17:56:29 +0200

    Changed after API change

---
+++ Specifications/Kernel/Projections/Engine/Expressions/ModelProperties/for_SubtractExpressionResolver/when_trying_to_resolve_valid_add_expression_against_model_and_event.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+using System.Text.Json.Nodes;
+using Aksio.Cratis.Events.Store;
+using Aksio.Cratis.Properties;
+
+namespace Aksio.Cratis.Events.Projections.Expressions.ModelProperties.for_SubtractExpressionResolver;
+
+public class when_trying_to_resolve_valid_add_expression_against_model_and_event : Specification
+{
+    Mock<IEventValueProviderExpressionResolvers> event_value_resolvers;
+    SubtractExpressionResolver resolver;
+    AppendedEvent @event;
+    ExpandoObject target;
+
+    void Establish()
+    {
+        target = new();
+        dynamic targetAsDynamic = target;
+        targetAsDynamic.targetProperty = 42d;
+        var content = new JsonObject
+        {
+            ["sourceProperty"] = 2d
+        };
+        @event = new(
+            new(0,
+            new("02405794-91e7-4e4f-8ad1-f043070ca297", 1)),
+            new("2f005aaf-2f4e-4a47-92ea-63687ef74bd4", 0, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow, "123b8935-a1a4-410d-aace-e340d48f0aa0", "41f18595-4748-4b01-88f7-4c0d0907aa90", "50308963-d8b5-4b6e-97c7-e2486e8237e1", "bfb7fd4a-1822-4937-a6d1-52464a173f84"),
+            content);
+
+        event_value_resolvers = new();
+        resolver = new(event_value_resolvers.Object);
+    }
+
+    void Because() => resolver.Resolve("targetProperty", "$subtract(sourceProperty)")(@event, target, ArrayIndexers.NoIndexers);
+
+    [Fact] void should_resolve_to_a_propertymapper_that_can_subtract_from_the_property() => ((double)((dynamic)target).targetProperty).ShouldEqual(40d);
+}
