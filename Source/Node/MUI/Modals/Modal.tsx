ab163012bb2fe90e149869d8054ddcb9d8bc24ea
Einar Ingebrigtsen <einari@me.com>
2022-10-19 07:52:48 +0200

    Introducing MUI package with Modal support for now

---
+++ Source/Node/MUI/Modals/Modal.tsx
@@ -0,0 +1,70 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { CloseModal, ModalClosed, ModalResult } from './useModal';
+
+
+export enum ModalButtons {
+    None = 1,
+    OkCancel,
+    YesNo
+}
+
+export interface IModalRenderProps {
+    buttons: ModalButtons;
+    title?: string | JSX.Element;
+    content?: string | JSX.Element;
+    minWidth?: string | number;
+    closeModal?: CloseModal;
+    modalClosed?: ModalClosed<any>;
+}
+
+
+export const Modal = (props: IModalRenderProps) => {
+    const success = () => {
+        props.closeModal?.(ModalResult.Success);
+    };
+
+    const dismiss = () => {
+        props.closeModal?.(ModalResult.Dismissed);
+    };
+
+    return (
+        <Dialog
+            open={true}
+            onClose={dismiss}>
+
+            <DialogTitle>
+                {props.title}
+            </DialogTitle>
+
+            <DialogContent>
+                {props.content}
+            </DialogContent>
+
+            {(() => {
+                switch (props.buttons) {
+                    case ModalButtons.OkCancel:
+                        return (
+                            <DialogActions>
+                                <Button onClick={success}>Ok</Button>
+                                <Button onClick={dismiss}>Cancel</Button>
+                            </DialogActions>
+                        );
+
+                    case ModalButtons.YesNo:
+                        return (
+                            <DialogActions>
+                                <Button onClick={success}>Yes</Button>
+                                <Button onClick={dismiss}>No</Button>
+                            </DialogActions>
+                        );
+
+                    default:
+                        return <></>;
+                }
+            })()}
+        </Dialog>
+    );
+};
