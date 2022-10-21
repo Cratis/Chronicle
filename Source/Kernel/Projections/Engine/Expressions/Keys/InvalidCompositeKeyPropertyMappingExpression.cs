aca1ada7bd5312f187db0a6483231a6c90040803
Einar Ingebrigtsen <einari@me.com>
2022-10-21 14:38:55 +0200

    Fixing static code analysis errors

--- Source/Kernel/Projections/Engine/Expressions/Keys/InvalidCompositeKeyPropertyMappingExpression.cs
+++ Source/Kernel/Projections/Engine/Expressions/Keys/InvalidCompositeKeyPropertyMappingExpression.cs
@@ -6,7 +6,7 @@ using Aksio.Cratis.Properties;
 namespace Aksio.Cratis.Events.Projections.Expressions.Keys;
 
 /// <summary>
-/// Exception that is thrown when a composite key property mapping expression is invalid.
+/// Exception that gets thrown when a composite key property mapping expression is invalid.
 /// </summary>
 public class InvalidCompositeKeyPropertyMappingExpression : Exception
 {
@@ -15,7 +15,7 @@ public class InvalidCompositeKeyPropertyMappingExpression : Exception
     /// </summary>
     /// <param name="projectionId">Identifier of the projection.</param>
     /// <param name="identifiedByProperty">The property used as identifier for the key.</param>
-    /// <param name="expression">Key/value expression</param>
+    /// <param name="expression">Key/value expression.</param>
     public InvalidCompositeKeyPropertyMappingExpression(ProjectionId projectionId, PropertyPath identifiedByProperty, string expression) : base($"Expression '{expression}' in projection '{projectionId}' for property '{identifiedByProperty}' is invalid. Expecting a key/value of property=expression")
     {
     }
