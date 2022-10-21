e4d4b37ef19dedb6da6b89b03ad62a108d5fe5a9
Einar Ingebrigtsen <einari@me.com>
2022-10-21 14:38:58 +0200

    Template

---
+++ Source/ApplicationModel/Tooling/Templates/templates/aspnet/Web/API/Accounts/Debit/DebitAccountsPerDayKey.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { field } from '@aksio/cratis-fundamentals';
+
+
+export class DebitAccountsPerDayKey {
+
+    @field(Number)
+    day!: number;
+
+    @field(Number)
+    month!: number;
+
+    @field(Number)
+    year!: number;
+}
