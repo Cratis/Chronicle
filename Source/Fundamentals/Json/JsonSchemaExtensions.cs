4a2d8ea32478a9ff0847c3813147af91305f7740
Einar Ingebrigtsen <einari@me.com>
2022-10-30 12:24:19 +0100

    Up and running with conversion from Expando to Bson

--- Source/Fundamentals/Json/JsonSchemaExtensions.cs
+++ Source/Fundamentals/Json/JsonSchemaExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Aksio.Cratis.Properties;
 using NJsonSchema;
 
 namespace Aksio.Cratis.Json;
@@ -20,6 +21,33 @@ public static class JsonSchemaExtensions
         var properties = new List<JsonSchemaProperty>();
         CollectPropertiesFrom(schema, properties);
         return properties.DistinctBy(_ => _.Name).ToArray();
+    }
+
+    /// <summary>
+    /// Gets the schema for a property within the schema hierarchy based on a <see cref="PropertyPath"/>.
+    /// </summary>
+    /// <param name="schema"><see cref="JsonSchema"/> to get from.</param>
+    /// <param name="propertyPath"><see cref="PropertyPath"/> to get for.</param>
+    /// <returns>The actual <see cref="JsonSchema"/>.</returns>
+    public static JsonSchema GetSchemaForPropertyPath(this JsonSchema schema, PropertyPath propertyPath)
+    {
+        foreach (var segment in propertyPath.Segments)
+        {
+            if (schema.ActualProperties.ContainsKey(segment.Value))
+            {
+                var schemaProperty = schema.ActualProperties[segment.Value];
+                if (schemaProperty.IsArray)
+                {
+                    schema = schema.ActualProperties[segment.Value].Item.Reference;
+                }
+                else
+                {
+                    schema = schemaProperty.ActualTypeSchema;
+                }
+            }
+        }
+
+        return schema;
     }
 
     static void CollectPropertiesFrom(JsonSchema schema, List<JsonSchemaProperty> properties)
