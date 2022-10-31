c34ed86015efcf95ea0f099431da19a860ba7937
Einar Ingebrigtsen <einari@me.com>
2022-10-31 15:19:37 +0100

    Spec for converting to expando object

---
+++ Specifications/Extensions/MongoDB/for_ExpandoObjectConverter/given/an_expando_object_converter.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using Aksio.Cratis.Schemas;
+using NJsonSchema;
+using NJsonSchema.Generation;
+using NJsonSchemaGenerator = NJsonSchema.Generation.JsonSchemaGenerator;
+
+namespace Aksio.Cratis.Extensions.MongoDB.for_ExpandoObjectConverter.given;
+
+public class an_expando_object_converter : Specification
+{
+    protected ExpandoObjectConverter converter;
+    protected JsonSchema schema;
+
+    void Establish()
+    {
+        var settings = new JsonSchemaGeneratorSettings
+        {
+            SerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            },
+            ReflectionService = new ReflectionService(),
+        };
+        var generator = new NJsonSchemaGenerator(settings);
+        schema = generator.Generate(typeof(TargetType));
+        converter = new(new TypeFormats());
+    }
+}
