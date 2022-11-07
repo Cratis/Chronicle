692521efb3f1461dcb20be5803912b2d389a5779
Einar Ingebrigtsen <einari@me.com>
2022-11-07 16:05:03 +0100

    Use unknown schema type values

--- Source/Extensions/MongoDB/ExpandoObjectConverter.cs
+++ Source/Extensions/MongoDB/ExpandoObjectConverter.cs
@@ -36,7 +36,7 @@ public class ExpandoObjectConverter : IExpandoObjectConverter
             var schemaProperty = schemaProperties.SingleOrDefault(_ => _.Name == name);
             if (schemaProperty is null)
             {
-                ConvertUnknownSchemaTypeToBsonValue(keyValue.Value);
+                value = ConvertUnknownSchemaTypeToBsonValue(keyValue.Value);
             }
             else
             {
@@ -71,6 +71,7 @@ public class ExpandoObjectConverter : IExpandoObjectConverter
             {
                 value = ConvertFromBsonValue(element.Value, schemaProperty);
             }
+
             expandoObjectAsDictionary[name] = value;
         }
 
