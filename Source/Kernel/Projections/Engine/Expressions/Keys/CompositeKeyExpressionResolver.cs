4af8a2dffd4e551979ea0f58540e34c52f7a710c
Einar Ingebrigtsen <einari@me.com>
2022-11-07 13:40:16 +0100

    Composite keys will now resolve the actual nested properties

--- Source/Kernel/Projections/Engine/Expressions/Keys/CompositeKeyExpressionResolver.cs
+++ Source/Kernel/Projections/Engine/Expressions/Keys/CompositeKeyExpressionResolver.cs
@@ -46,8 +46,9 @@ public class CompositeKeyExpressionResolver : IKeyExpressionResolver
             {
                 throw new InvalidCompositeKeyPropertyMappingExpression(projection.Identifier, identifiedByProperty, _);
             }
+            var actualProperty = identifiedByProperty + keyValue[0];
 
-            var schemaProperty = projection.Model.Schema.GetSchemaPropertyForPropertyPath(identifiedByProperty)!;
+            var schemaProperty = projection.Model.Schema.GetSchemaPropertyForPropertyPath(actualProperty)!;
             schemaProperty ??= new JsonSchemaProperty
             {
                 Type = JsonObjectType.String
@@ -55,7 +56,7 @@ public class CompositeKeyExpressionResolver : IKeyExpressionResolver
 
             return new
             {
-                Property = new PropertyPath(keyValue[0]),
+                Property =  new PropertyPath(keyValue[0]),
                 KeyResolver = _resolvers.Resolve(schemaProperty, keyValue[1])
             };
         }).ToDictionary(_ => _.Property, _ => _.KeyResolver);
