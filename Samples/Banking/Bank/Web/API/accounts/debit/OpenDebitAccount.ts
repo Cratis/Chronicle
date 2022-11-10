fafe5c3fa00372ae229c8c69b69d35a744159efc
Einar Ingebrigtsen <einari@me.com>
2022-11-10 18:27:21 +0100

    Regen

--- Samples/Banking/Bank/Web/API/accounts/debit/OpenDebitAccount.ts
+++ Samples/Banking/Bank/Web/API/accounts/debit/OpenDebitAccount.ts
@@ -2,7 +2,7 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
-import { Command, CommandValidator, CommandPropertyValidators, useCommand, SetCommandValues } from '@aksio/cratis-applications-frontend/commands';
+import { Command, CommandValidator, CommandPropertyValidators, useCommand, SetCommandValues, ClearCommandValues } from '@aksio/cratis-applications-frontend/commands';
 import { Validator } from '@aksio/cratis-applications-frontend/validation';
 import { AccountDetails } from './AccountDetails';
 import Handlebars from 'handlebars';
@@ -58,7 +58,7 @@ export class OpenDebitAccount extends Command<IOpenDebitAccount> implements IOpe
         this.propertyChanged('details');
     }
 
-    static use(initialValues?: IOpenDebitAccount): [OpenDebitAccount, SetCommandValues<IOpenDebitAccount>] {
+    static use(initialValues?: IOpenDebitAccount): [OpenDebitAccount, SetCommandValues<IOpenDebitAccount>, ClearCommandValues] {
         return useCommand<OpenDebitAccount, IOpenDebitAccount>(OpenDebitAccount, initialValues);
     }
 }
