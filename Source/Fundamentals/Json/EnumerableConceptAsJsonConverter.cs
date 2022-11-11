da46de116d14880a958e53cad8328b9e7841cf5a
Einar Ingebrigtsen <einari@me.com>
2022-11-11 15:12:40 +0100

    Introducing a specific enumerable concept as JSON converter
    Note: For most circumstances it will use the ConceptAs converter,
    but not every time. So had to put this in explicitly.

---
+++ Source/Fundamentals/Json/EnumerableConceptAsJsonConverter.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Aksio.Cratis.Concepts;
+
+namespace Aksio.Cratis.Json;
+
+/// <summary>
+/// Represents a <see cref="JsonConverter{T}"/> for <see cref="ConceptAs{T}"/>.
+/// </summary>
+/// <typeparam name="TConcept">Underlying concept type.</typeparam>
+public class EnumerableConceptAsJsonConverter<TConcept> : JsonConverter<IEnumerable<TConcept>>
+{
+    readonly ConceptAsJsonConverter<TConcept> _conceptAsJsonConverter;
+
+    public EnumerableConceptAsJsonConverter()
+    {
+        _conceptAsJsonConverter = new();
+    }
+
+    /// <inheritdoc/>
+    public override IEnumerable<TConcept>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var items = new List<TConcept>();
+
+        if (reader.TokenType == JsonTokenType.StartArray)
+        {
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    break;
+                }
+                var converted = _conceptAsJsonConverter.Read(ref reader, typeof(TConcept), options);
+                if (converted is not null)
+                {
+                    items.Add(converted);
+                }
+            }
+        }
+
+        return items;
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, IEnumerable<TConcept> value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+        foreach (var item in value)
+        {
+            _conceptAsJsonConverter.Write(writer, item, options);
+        }
+        writer.WriteEndArray();
+    }
+}
