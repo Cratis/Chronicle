8f2c6b389d8b5f0e76549593711aa5777c153ea7
Einar Ingebrigtsen <einari@me.com>
2022-11-17 08:16:47 +0100

    Reorganizing specs and also adding specs for more supported types

--- Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties/SetValuesProjection.cs
+++ Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties/SetValuesProjection.cs
@@ -5,25 +5,28 @@ namespace Aksio.Cratis.Events.Projections.Scenarios.when_projecting_properties;
 
 public class SetValuesProjection : IProjectionFor<Model>
 {
-    public static string StringValue = "Forty two";
-    public static bool BoolValue = true;
-    public static int IntValue = 42;
-    public static double DoubleValue = 42.42;
-    public static StringConcept StringConceptValue = "Forty three";
-    public static BoolConcept BoolConceptValue = true;
-    public static IntConcept IntConceptValue = 43;
-    public static DoubleConcept DoubleConceptValue = 43.43;
-
     public ProjectionId Identifier => "152b1348-e612-4165-bc49-fcaba94e8183";
 
     public void Define(IProjectionBuilderFor<Model> builder) => builder
         .From<EmptyEvent>(_ => _
-            .Set(m => m.StringValue).ToValue(StringValue)
-            .Set(m => m.BoolValue).ToValue(BoolValue)
-            .Set(m => m.IntValue).ToValue(IntValue)
-            .Set(m => m.DoubleValue).ToValue(DoubleValue)
-            .Set(m => m.StringConceptValue).ToValue(StringConceptValue)
-            .Set(m => m.BoolConceptValue).ToValue(BoolConceptValue)
-            .Set(m => m.IntConceptValue).ToValue(IntConceptValue)
-            .Set(m => m.DoubleConceptValue).ToValue(DoubleConceptValue));
+            .Set(m => m.StringValue).ToValue(KnownValues.StringValue)
+            .Set(m => m.BoolValue).ToValue(KnownValues.BoolValue)
+            .Set(m => m.IntValue).ToValue(KnownValues.IntValue)
+            .Set(m => m.FloatValue).ToValue(KnownValues.FloatValue)
+            .Set(m => m.DoubleValue).ToValue(KnownValues.DoubleValue)
+            .Set(m => m.GuidValue).ToValue(KnownValues.GuidValue)
+            .Set(m => m.DateTimeValue).ToValue(KnownValues.DateTimeValue)
+            .Set(m => m.DateOnlyValue).ToValue(KnownValues.DateOnlyValue)
+            .Set(m => m.TimeOnlyValue).ToValue(KnownValues.TimeOnlyValue)
+            .Set(m => m.DateTimeOffsetValue).ToValue(KnownValues.DateTimeOffsetValue)
+            .Set(m => m.StringConceptValue).ToValue(KnownValues.StringConceptValue)
+            .Set(m => m.BoolConceptValue).ToValue(KnownValues.BoolConceptValue)
+            .Set(m => m.IntConceptValue).ToValue(KnownValues.IntConceptValue)
+            .Set(m => m.FloatConceptValue).ToValue(KnownValues.FloatConceptValue)
+            .Set(m => m.DoubleConceptValue).ToValue(KnownValues.DoubleConceptValue)
+            .Set(m => m.GuidConceptValue).ToValue(KnownValues.GuidConceptValue)
+            .Set(m => m.DateTimeConceptValue).ToValue(KnownValues.DateTimeConceptValue)
+            .Set(m => m.DateOnlyConceptValue).ToValue(KnownValues.DateOnlyConceptValue)
+            .Set(m => m.TimeOnlyConceptValue).ToValue(KnownValues.TimeOnlyConceptValue)
+            .Set(m => m.DateTimeOffsetConceptValue).ToValue(KnownValues.DateTimeOffsetConceptValue));
 }
