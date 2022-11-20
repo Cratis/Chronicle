0644e9fa5adeeb397bf5060ff8b1fa2634940c8f
Einar Ingebrigtsen <einari@me.com>
2022-11-20 09:16:27 +0100

    Adding specs for counting events

---
+++ Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties/CountingEventsProjection.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Events.Projections.Scenarios.when_projecting_properties;
+
+public class CountingEventsProjection : IProjectionFor<Model>
+{
+    public ProjectionId Identifier => "1796eea2-26e9-4fda-ad3c-d4aa27d00000";
+
+    public void Define(IProjectionBuilderFor<Model> builder) => builder
+        .From<EventWithPropertiesForAllSupportedTypes>(_ => _
+            .Count(m => m.IntValue));
+}
