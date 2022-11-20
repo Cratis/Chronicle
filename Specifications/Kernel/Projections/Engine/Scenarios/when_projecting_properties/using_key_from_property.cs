e84df8a8630c4e0ccef2a8b1eda1edfe9b75c1b2
Einar Ingebrigtsen <einari@me.com>
2022-11-20 09:35:33 +0100

    Reorganization - improved structure

--- Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties/using_key_from_property.cs
+++ Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties/using_key_from_property.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Aksio.Cratis.Events.Projections.Scenarios.Projections;
 using Aksio.Cratis.Specifications.Integration;
 
 namespace Aksio.Cratis.Events.Projections.Scenarios.when_projecting_properties;
