13b5ed9bcfd65341663508102d0cf8425097b3aa
Einar Ingebrigtsen <einari@me.com>
2022-11-20 09:35:33 +0100

    Reorganization - improved structure

--- Specifications/Kernel/Projections/Engine/Scenarios/Projections/SetPropertiesProjection.cs
+++ Specifications/Kernel/Projections/Engine/Scenarios/Projections/SetPropertiesProjection.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Aksio.Cratis.Events.Projections.Scenarios.when_projecting_properties;
+namespace Aksio.Cratis.Events.Projections.Scenarios.Projections;
 
 public class SetPropertiesProjection : IProjectionFor<Model>
 {
