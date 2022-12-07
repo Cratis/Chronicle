2da22ef080f91fe3d53da3c41aee78e71e7f40c7
Einar Ingebrigtsen <einari@me.com>
2022-12-07 15:24:58 +0100

    Specs for from every

---
+++ Specifications/Kernel/Projections/Engine/Scenarios/when_projecting_properties_from_every/with_event_of_first_event_type_applied.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Events.Projections.Scenarios.Projections;
+using Aksio.Cratis.Specifications.Integration;
+
+namespace Aksio.Cratis.Events.Projections.Scenarios.when_projecting_properties_from_every;
+
+public class with_event_of_first_event_type_applied : ProjectionSpecificationFor<Model>
+{
+    string event_source_id;
+    protected override IProjectionFor<Model> CreateProjection() => new FromEveryProjection();
+    ProjectionResult<Model> result;
+
+    async Task Because()
+    {
+        event_source_id = Guid.NewGuid().ToString();
+        await context.EventLog.Append(event_source_id, new EmptyEvent());
+        result = await context.GetById(event_source_id);
+    }
+
+    [Fact] void should_set_the_last_updated_property_to_occurred_for_event() => result.Model.LastUpdated.ShouldEqual(context.AppendedEvents.First().Context.Occurred);
+}
