c933eccdf94b9155c867ace63444032f3c0986e0
Einar Ingebrigtsen <einari@me.com>
2022-12-14 12:13:25 +0100

    Changing spec implementation for how to check result

--- Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_of_datetime_concept_with_value_as_string_as_universal.cs
+++ Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_of_datetime_concept_with_value_as_string_as_universal.cs
@@ -1,18 +1,16 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Globalization;
-
 namespace Aksio.Cratis.Concepts.for_ConceptFactory;
 
 public class when_creating_instance_of_datetime_concept_with_value_as_string_as_universal : Specification
 {
     DateTimeConcept result;
-    string now;
+    string expected;
 
-    void Establish() => now = "2022-12-14T08:45:46.4595800Z";
+    void Establish() => expected = "2022-12-14T08:45:46.4595800Z";
 
-    void Because() => result = ConceptFactory.CreateConceptInstance(typeof(DateTimeConcept), now) as DateTimeConcept;
+    void Because() => result = ConceptFactory.CreateConceptInstance(typeof(DateTimeConcept), expected) as DateTimeConcept;
 
-    [Fact] void should_be_the_value_of_the_datetime() => result.Value.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ").ShouldEqual(now);
+    [Fact] void should_be_the_value_of_the_datetime() => result.Value.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ").ShouldEqual(expected);
 }
