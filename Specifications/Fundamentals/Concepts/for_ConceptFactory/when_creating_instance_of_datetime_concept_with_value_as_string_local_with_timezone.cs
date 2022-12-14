9e0dd09ebbdb779b36cef869bdb79a5b10494c31
Einar Ingebrigtsen <einari@me.com>
2022-12-14 11:05:30 +0100

    Print culture to see why build is failing

--- Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_of_datetime_concept_with_value_as_string_local_with_timezone.cs
+++ Specifications/Fundamentals/Concepts/for_ConceptFactory/when_creating_instance_of_datetime_concept_with_value_as_string_local_with_timezone.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Globalization;
+
 namespace Aksio.Cratis.Concepts.for_ConceptFactory;
 
 public class when_creating_instance_of_datetime_concept_with_value_as_string_local_with_timezone : Specification
@@ -8,7 +10,12 @@ public class when_creating_instance_of_datetime_concept_with_value_as_string_loc
     DateTimeConcept result;
     string now;
 
-    void Establish() => now = "2022-12-14T09:45:46.4595800+01:00";
+    void Establish()
+    {
+        Console.WriteLine($"CUlture: {CultureInfo.CurrentCulture}");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        now = "2022-12-14T09:45:46.4595800+01:00";
+    }
 
     void Because() => result = ConceptFactory.CreateConceptInstance(typeof(DateTimeConcept), now) as DateTimeConcept;
 
