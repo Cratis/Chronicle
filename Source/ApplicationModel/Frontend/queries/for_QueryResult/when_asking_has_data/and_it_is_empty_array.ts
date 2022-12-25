af21691562799ecddf8b23f6a91230073203230d
Einar Ingebrigtsen <einari@me.com>
2022-12-25 09:18:03 +0100

    Fixing broken specs

--- Source/ApplicationModel/Frontend/queries/for_QueryResult/when_asking_has_data/and_it_is_empty_array.ts
+++ Source/ApplicationModel/Frontend/queries/for_QueryResult/when_asking_has_data/and_it_is_empty_array.ts
@@ -4,7 +4,10 @@
 import { QueryResult } from '../../QueryResult';
 
 describe("when asking has data and it is empty array", () => {
-    const queryResult = new QueryResult<any>([], true);
+    const queryResult = new QueryResult<any>({
+        validationErrors: [],
+        data: []
+    }, Object, true);
 
     it('should consider to not having data', () => queryResult.hasData.should.be.false);
 });
