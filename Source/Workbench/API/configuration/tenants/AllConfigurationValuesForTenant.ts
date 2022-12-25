fe3ac7b704c06e33884339a6237100f6101cee5a
Einar Ingebrigtsen <einari@me.com>
2022-12-25 08:52:15 +0100

    Added a REST API for working with Tenant configuration

---
+++ Source/Workbench/API/configuration/tenants/AllConfigurationValuesForTenant.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { QueryFor, QueryResultWithState, useQuery, PerformQuery } from '@aksio/cratis-applications-frontend/queries';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/configuration/tenants/{{tenantId}}');
+
+export interface AllConfigurationValuesForTenantArguments {
+    tenantId: string;
+}
+export class AllConfigurationValuesForTenant extends QueryFor<string[], AllConfigurationValuesForTenantArguments> {
+    readonly route: string = '/api/configuration/tenants/{{tenantId}}';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+    readonly defaultValue: string[] = [];
+
+    constructor() {
+        super(String, true);
+    }
+
+    get requestArguments(): string[] {
+        return [
+            'tenantId',
+        ];
+    }
+
+    static use(args?: AllConfigurationValuesForTenantArguments): [QueryResultWithState<string[]>, PerformQuery<AllConfigurationValuesForTenantArguments>] {
+        return useQuery<string[], AllConfigurationValuesForTenant, AllConfigurationValuesForTenantArguments>(AllConfigurationValuesForTenant, args);
+    }
+}
