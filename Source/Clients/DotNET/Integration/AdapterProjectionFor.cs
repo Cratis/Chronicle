13d3590cab3e5c43caef673223ed7ea3d9b05ec3
Einar Ingebrigtsen <einari@me.com>
2022-12-25 09:07:21 +0100

    Removing hard coded Development tenant. WIll now rely on execution context.

--- Source/Clients/DotNET/Integration/AdapterProjectionFor.cs
+++ Source/Clients/DotNET/Integration/AdapterProjectionFor.cs
@@ -19,6 +19,7 @@ public class AdapterProjectionFor<TModel> : IAdapterProjectionFor<TModel>
     readonly ProjectionDefinition _projectionDefinition;
     readonly IClusterClient _clusterClient;
     readonly JsonSerializerOptions _jsonSerializerOptions;
+    readonly IExecutionContextManager _executionContextManager;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AdapterProjectionFor{TModel}"/> class.
@@ -26,21 +27,27 @@ public class AdapterProjectionFor<TModel> : IAdapterProjectionFor<TModel>
     /// <param name="projectionDefinition">The <see cref="IProjection"/> to work with.</param>
     /// <param name="clusterClient">Orleans <see cref="IClusterClient"/>.</param>
     /// <param name="jsonSerializerOptions">The <see cref="JsonSerializerOptions"/> for serialization.</param>
+    /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for working with the execution context.</param>
     public AdapterProjectionFor(
         ProjectionDefinition projectionDefinition,
         IClusterClient clusterClient,
-        JsonSerializerOptions jsonSerializerOptions)
+        JsonSerializerOptions jsonSerializerOptions,
+        IExecutionContextManager executionContextManager)
     {
         _projectionDefinition = projectionDefinition;
         _clusterClient = clusterClient;
         _jsonSerializerOptions = jsonSerializerOptions;
+        _executionContextManager = executionContextManager;
     }
 
     /// <inheritdoc/>
     public async Task<AdapterProjectionResult<TModel>> GetById(ModelKey modelKey)
     {
-        // TODO: Fix so that this is not constant values. Multi-tenancy!!
-        var key = new ImmediateProjectionKey(ExecutionContextManager.GlobalMicroserviceId, TenantId.Development, Events.Store.EventSequenceId.Log, modelKey);
+        var key = new ImmediateProjectionKey(
+            _executionContextManager.Current.MicroserviceId,
+            _executionContextManager.Current.TenantId,
+            Events.Store.EventSequenceId.Log,
+            modelKey);
         var projection = _clusterClient.GetGrain<IImmediateProjection>(_projectionDefinition.Identifier, key);
 
         var result = await projection.GetModelInstance(_projectionDefinition);
