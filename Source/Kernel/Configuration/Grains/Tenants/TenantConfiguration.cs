baa20f953069a7c6d96920f87ac45c3240336d1b
Einar Ingebrigtsen <einari@me.com>
2022-12-28 08:32:12 +0100

    Revert "Completely removing tenant configuration - it was completely wrong"
    This reverts commit 09451da323dca3d8b31b90d6b2f5d9d67f7a07a1.

---
+++ Source/Kernel/Configuration/Grains/Tenants/TenantConfiguration.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Orleans;
+using Orleans.Providers;
+
+namespace Aksio.Cratis.Configuration.Grains.Tenants;
+
+/// <summary>
+/// Represents an implementation of <see cref="ITenantConfiguration"/>.
+/// </summary>
+[StorageProvider(ProviderName = TenantConfigurationState.StorageProvider)]
+public class TenantConfiguration : Grain<TenantConfigurationState>, ITenantConfiguration
+{
+    /// <inheritdoc/>
+    public async Task Set(string key, string value)
+    {
+        State[key] = value;
+        await WriteStateAsync();
+    }
+
+    /// <inheritdoc/>
+    public Task<TenantConfigurationState> All() => Task.FromResult(State);
+}
