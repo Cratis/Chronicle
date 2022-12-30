14a3b3915e26d2aadf1da2fb99483eda15b93101
Einar Ingebrigtsen <einari@me.com>
2022-12-30 15:51:03 +0100

    Support for enums represented as strings

--- Specifications/Fundamentals/Json/for_ExpandoObjectConverter/given/an_expando_object_converter.cs
+++ Specifications/Fundamentals/Json/for_ExpandoObjectConverter/given/an_expando_object_converter.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text.Json;
+using Aksio.Cratis.Compliance;
 using Aksio.Cratis.Schemas;
 using NJsonSchema;
-using NJsonSchema.Generation;
-using NJsonSchemaGenerator = NJsonSchema.Generation.JsonSchemaGenerator;
 
 namespace Aksio.Cratis.Json.for_ExpandoObjectConverter.given;
 
@@ -16,18 +14,12 @@ public class an_expando_object_converter : Specification
 
     void Establish()
     {
-        var settings = new JsonSchemaGeneratorSettings
-        {
-            SerializerOptions = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            },
-            ReflectionService = new ReflectionService(),
-        };
-        var typeFormats = new TypeFormats();
-        settings.SchemaProcessors.Add(new TypeFormatSchemaProcessor(typeFormats));
-        var generator = new NJsonSchemaGenerator(settings);
+        var generator = new JsonSchemaGenerator(
+            new ComplianceMetadataResolver(
+                new KnownInstancesOf<ICanProvideComplianceMetadataForType>(),
+                new KnownInstancesOf<ICanProvideComplianceMetadataForProperty>()));
+
         schema = generator.Generate(typeof(TargetType));
-        converter = new(typeFormats);
+        converter = new(new TypeFormats());
     }
 }
