31e2958f8cfca126bbf3a75d5e4d265a46187eff
Einar Ingebrigtsen <einari@me.com>
2023-01-05 09:08:24 +0100

    Moving to fundamentals

--- Source/ApplicationModel/CQRS/Commands/CommandActionFilter.cs
+++ Source/ApplicationModel/CQRS/Commands/CommandActionFilter.cs
@@ -1,9 +1,10 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Aksio.Cratis.Applications.Validation;
+using Aksio.Cratis.Commands;
 using Aksio.Cratis.Execution;
 using Aksio.Cratis.Strings;
+using Aksio.Cratis.Validation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
