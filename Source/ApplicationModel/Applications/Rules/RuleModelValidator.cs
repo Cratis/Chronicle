dbedba99ce597680601fd07e29569ed6ef574311
Einar Ingebrigtsen <einari@me.com>
2023-01-12 16:08:24 +0100

    ApplicationModel + Server compiling

--- Source/ApplicationModel/Applications/Rules/RuleModelValidator.cs
+++ Source/ApplicationModel/Applications/Rules/RuleModelValidator.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reflection;
-using Aksio.Cratis.Events.Projections;
+using Aksio.Cratis.Projections;
 using Aksio.Cratis.Reflection;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
