6200943834921000e8f795ad37465fd42b949d1c
Einar Ingebrigtsen <einari@me.com>
2023-01-12 10:41:59 +0100

    Hooking up with client lifecycle and sending correct command

--- Source/Clients/DotNET/Events/Outbox/OutboxProjectionsRegistrar.cs
+++ Source/Clients/DotNET/Events/Outbox/OutboxProjectionsRegistrar.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Text.Json;
+using Aksio.Cratis.Clients;
+using Aksio.Cratis.Events.Projections;
+using Aksio.Cratis.Events.Projections.Definitions;
+using Aksio.Cratis.Events.Projections.Json;
 using Aksio.Cratis.Events.Projections.Outbox;
 using Aksio.Cratis.Execution;
 using Aksio.Cratis.Schemas;
@@ -10,31 +14,39 @@ using Aksio.Cratis.Types;
 namespace Aksio.Cratis.Events.Outbox;
 
 /// <summary>
-/// Represents an implementation of <see cref="IOutboxProjectionsRegistrar"/>.
+/// Represents an implementation of <see cref="IParticipateInClientLifecycle"/> for handling registrations of outbox projections with the Kernel.
 /// </summary>
-public class OutboxProjectionsRegistrar : IOutboxProjectionsRegistrar
+public class OutboxProjectionsRegistrar : IParticipateInClientLifecycle
 {
+    readonly IClient _client;
     readonly IExecutionContextManager _executionContextManager;
     readonly IInstancesOf<IOutboxProjections> _outboxProjections;
+    readonly IJsonProjectionSerializer _projectionSerializer;
     readonly IEnumerable<OutboxProjectionsDefinition> _outboxProjectionsDefinitions;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OutboxProjectionsRegistrar"/> class.
     /// </summary>
+    /// <param name="client">The Cratis <see cref="IClient"/>.</param>
     /// <param name="eventTypes">Registered <see cref="IEventTypes"/>.</param>
     /// <param name="jsonSchemaGenerator"><see cref="IJsonSchemaGenerator"/> for generating schemas for projections.</param>
     /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for establishing execution context.</param>
     /// <param name="outboxProjections">All instances of <see cref="IOutboxProjections"/>.</param>
+    /// <param name="projectionSerializer"><see cref="IJsonProjectionSerializer"/> for serializing projections.</param>
     /// <param name="jsonSerializerOptions">The <see cref="JsonSerializerOptions"/> to use for any JSON serialization.</param>
     public OutboxProjectionsRegistrar(
+        IClient client,
         IEventTypes eventTypes,
         IJsonSchemaGenerator jsonSchemaGenerator,
         IExecutionContextManager executionContextManager,
         IInstancesOf<IOutboxProjections> outboxProjections,
+        IJsonProjectionSerializer projectionSerializer,
         JsonSerializerOptions jsonSerializerOptions)
     {
+        _client = client;
         _executionContextManager = executionContextManager;
         _outboxProjections = outboxProjections;
+        _projectionSerializer = projectionSerializer;
         _outboxProjectionsDefinitions = _outboxProjections.Select(projections =>
         {
             var builder = new OutboxProjectionsBuilder(eventTypes, jsonSchemaGenerator, projections.Identifier, jsonSerializerOptions);
@@ -44,28 +56,31 @@ public class OutboxProjectionsRegistrar : IOutboxProjectionsRegistrar
     }
 
     /// <inheritdoc/>
-    public Task DiscoverAndRegisterAll()
+    public async Task Connected()
     {
-        // TODO:
+        _executionContextManager.Establish(ExecutionContextManager.GlobalMicroserviceId);
 
-        return Task.CompletedTask;
+        var registrations = _outboxProjectionsDefinitions.SelectMany(_ => _.TargetEventTypeProjections.Values).Select(projection =>
+        {
+            var serializedPipeline = JsonSerializer.SerializeToNode(
+                new ProjectionPipelineDefinition(
+                    projection.Identifier,
+                    new[]
+                    {
+                        new ProjectionSinkDefinition(
+                                "06ec7e41-4424-4eb3-8dd0-defb45bc055e",
+                                WellKnownProjectionSinkTypes.Outbox)
+                    }))!;
 
-        // _executionContextManager.Establish(ExecutionContextManager.GlobalMicroserviceId);
-        // var projections = _clusterClient.GetGrain<Projections.Grains.IProjections>(ExecutionContextManager.GlobalMicroserviceId);
-        // foreach (var outboxProjectionsDefinition in _outboxProjectionsDefinitions)
-        // {
-        //     foreach (var (_, projectionDefinition) in outboxProjectionsDefinition.TargetEventTypeProjections)
-        //     {
-        //         var pipelineDefinition = new ProjectionPipelineDefinition(
-        //             projectionDefinition.Identifier,
-        //             new[]
-        //             {
-        //                 new ProjectionSinkDefinition(
-        //                     "06ec7e41-4424-4eb3-8dd0-defb45bc055e",
-        //                     WellKnownProjectionSinkTypes.Outbox)
-        //             });
-        //         await projections.Register(projectionDefinition, pipelineDefinition);
-        //     }
-        // }
+            return new ProjectionRegistration(
+                _projectionSerializer.Serialize(projection),
+                serializedPipeline);
+        });
+
+        var route = $"/api/events/store/{ExecutionContextManager.GlobalMicroserviceId}/projections";
+        await _client.PerformCommand(route, new RegisterProjections(registrations));
     }
+
+    /// <inheritdoc/>
+    public Task Disconnected() => Task.CompletedTask;
 }
