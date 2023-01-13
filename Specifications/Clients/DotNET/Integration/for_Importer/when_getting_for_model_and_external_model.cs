851d4203e744f27739a328c533b0ba8b2c2fe954
Einar Ingebrigtsen <einari@me.com>
2023-01-13 08:56:34 +0100

    Fixing usings

--- Specifications/Clients/DotNET/Integration/for_Importer/when_getting_for_model_and_external_model.cs
+++ Specifications/Clients/DotNET/Integration/for_Importer/when_getting_for_model_and_external_model.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Changes;
-using Aksio.Cratis.Events;
+using Aksio.Cratis.EventSequences;
 using AutoMapper;
 
 namespace Aksio.Cratis.Integration.for_Importer;
