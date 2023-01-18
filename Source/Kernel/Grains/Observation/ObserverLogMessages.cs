d16ea1f0d859ebcc74a72739bec48c9129cb47a2
Einar Ingebrigtsen <einari@me.com>
2023-01-18 10:19:23 +0100

    Adding logging for failed partition

--- Source/Kernel/Grains/Observation/ObserverLogMessages.cs
+++ Source/Kernel/Grains/Observation/ObserverLogMessages.cs
@@ -1,6 +1,10 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Aksio.Cratis.Events;
+using Aksio.Cratis.EventSequences;
+using Aksio.Cratis.Execution;
+using Aksio.Cratis.Observation;
 using Microsoft.Extensions.Logging;
 
 namespace Aksio.Cratis.Kernel.Grains.Observation;
@@ -11,41 +15,44 @@ namespace Aksio.Cratis.Kernel.Grains.Observation;
 internal static partial class ObserverLogMessages
 {
     [LoggerMessage(0, LogLevel.Information, "Subscribing observer {ObserverId} with subscriber type {SubscriberType} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void Subscribing(this ILogger<Observer> logger, Guid observerId, Type subscriberType, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void Subscribing(this ILogger<Observer> logger, ObserverId observerId, Type subscriberType, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(1, LogLevel.Information, "Unsubscribing observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void Unsubscribing(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void Unsubscribing(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(2, LogLevel.Information, "Rewinding observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void Rewinding(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void Rewinding(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(3, LogLevel.Information, "Catching up observer {ObserverId} up for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void CatchingUp(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void CatchingUp(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(4, LogLevel.Information, "Observer {ObserverId} is now active up for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void Active(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void Active(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(5, LogLevel.Information, "Replaying observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void Replaying(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void Replaying(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(6, LogLevel.Information, "Offset is at tail for observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void OffsetIsAtTail(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void OffsetIsAtTail(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(7, LogLevel.Debug, "Clearing out failed partitions for observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void ClearingFailedPartitions(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void ClearingFailedPartitions(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(8, LogLevel.Debug, "Clearing out recovering partitions for observer {ObserverId} for microservice '{MicroserviceId}' on sequence '{EventSequenceId}' for tenant '{TenantId}'")]
-    internal static partial void ClearingRecoveringPartitions(this ILogger<Observer> logger, Guid observerId, Guid microserviceId, Guid eventSequenceId, Guid tenantId);
+    internal static partial void ClearingRecoveringPartitions(this ILogger<Observer> logger, ObserverId observerId, MicroserviceId microserviceId, EventSequenceId eventSequenceId, TenantId tenantId);
 
     [LoggerMessage(9, LogLevel.Debug, "Activating observer {ObserverId} for sequence {EventSequenceId} for microservice '{MicroserviceId}' and tenant '{TenantId}' - observing source microservice '{SourceMicroserviceId}' and tenant '{SourceTenantId}'")]
-    internal static partial void Activating(this ILogger<Observer> logger, Guid observerId, Guid eventSequenceId, Guid microserviceId, Guid tenantId, Guid sourceMicroserviceId, Guid sourceTenantId);
+    internal static partial void Activating(this ILogger<Observer> logger, ObserverId observerId, EventSequenceId eventSequenceId, MicroserviceId microserviceId, TenantId tenantId, Guid sourceMicroserviceId, Guid sourceTenantId);
 
     [LoggerMessage(10, LogLevel.Trace, "Subscribing to stream for observer {ObserverId} for sequence {EventSequenceId} for microservice '{MicroserviceId}' and tenant '{TenantId}' - stream {StreamId} - namespace {StreamNamespace}")]
-    internal static partial void SubscribingToStream(this ILogger<Observer> logger, Guid observerId, Guid eventSequenceId, Guid microserviceId, Guid tenantId, Guid streamId, string streamNamespace);
+    internal static partial void SubscribingToStream(this ILogger<Observer> logger, ObserverId observerId, EventSequenceId eventSequenceId, MicroserviceId microserviceId, TenantId tenantId, Guid streamId, string streamNamespace);
 
     [LoggerMessage(11, LogLevel.Trace, "Event of type {EventTypeId} received for observer {ObserverId} from sequence {EventSequenceId} for microservice '{MicroserviceId}' and tenant '{TenantId}'")]
-    internal static partial void EventReceived(this ILogger<Observer> logger, Guid eventTypeId, Guid observerId, Guid eventSequenceId, Guid microserviceId, Guid tenantId);
+    internal static partial void EventReceived(this ILogger<Observer> logger, Guid eventTypeId, ObserverId observerId, EventSequenceId eventSequenceId, MicroserviceId microserviceId, TenantId tenantId);
 
     [LoggerMessage(12, LogLevel.Debug, "Deactivating observer {ObserverId} for sequence {EventSequenceId} for microservice '{MicroserviceId}' and tenant '{TenantId}' - observing source microservice '{SourceMicroserviceId}' and tenant '{SourceTenantId}'")]
-    internal static partial void Deactivating(this ILogger<Observer> logger, Guid observerId, Guid eventSequenceId, Guid microserviceId, Guid tenantId, Guid sourceMicroserviceId, Guid sourceTenantId);
+    internal static partial void Deactivating(this ILogger<Observer> logger, ObserverId observerId, EventSequenceId eventSequenceId, MicroserviceId microserviceId, TenantId tenantId, MicroserviceId sourceMicroserviceId, TenantId sourceTenantId);
+
+    [LoggerMessage(13, LogLevel.Warning, "Partition {Partition} failed for event with sequence number {EventSequenceNumber} observer {ObserverId} for sequence {EventSequenceId} for microservice '{MicroserviceId}' and tenant '{TenantId}' - observing source microservice '{SourceMicroserviceId}' and tenant '{SourceTenantId}'")]
+    internal static partial void PartitionFailed(this ILogger<Observer> logger, string partition, EventSequenceNumber eventSequenceNumber, ObserverId observerId, EventSequenceId eventSequenceId, MicroserviceId microserviceId, TenantId tenantId, MicroserviceId sourceMicroserviceId, TenantId sourceTenantId);
 }
