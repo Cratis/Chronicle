238a1de2ba3d4ac3c1e478803b85bc5d23dcb403
Einar Ingebrigtsen <einari@me.com>
2023-01-19 18:47:43 +0100

    Removing old proxies

--- Source/Workbench/API/events/store/sequence/ExpandoObject.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  **DO NOT EDIT** - This file is an automatically generated file.
- *--------------------------------------------------------------------------------------------*/
-
-import { field } from '@aksio/cratis-fundamentals';
-
-
-export class ExpandoObject {
-}
