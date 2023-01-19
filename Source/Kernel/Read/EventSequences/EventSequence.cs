fc676d7587e2b19ae1a176b5e8af2ad00d2e2e3f
Einar Ingebrigtsen <einari@me.com>
2023-01-19 18:59:50 +0100

    Moving event sequence related queries to Read

--- Source/Kernel/Read/EventSequences/EventSequence.cs
+++ Source/Kernel/Read/EventSequences/EventSequence.cs
@@ -3,14 +3,11 @@
 
 using Aksio.Cratis.DependencyInversion;
 using Aksio.Cratis.Execution;
+using Aksio.Cratis.Events;
 using Aksio.Cratis.EventSequences;
 using Microsoft.AspNetCore.Mvc;
-using Orleans;
-using IEventSequence = Aksio.Cratis.Kernel.Grains.EventSequences.IEventSequence;
 
-#pragma warning disable SA1600, IDE0060
-
-namespace Aksio.Cratis.Kernel.Domain.EventSequences;
+namespace Aksio.Cratis.Kernel.Read.EventSequences;
 
 /// <summary>
 /// Represents the API for working with the event log.
@@ -18,46 +15,42 @@ namespace Aksio.Cratis.Kernel.Domain.EventSequences;
 [Route("/api/events/store/{microserviceId}/{tenantId}/sequence/{eventSequenceId}")]
 public class EventSequence : Controller
 {
-    readonly IGrainFactory _grainFactory;
     readonly ProviderFor<IEventSequenceStorageProvider> _eventSequenceStorageProviderProvider;
     readonly IExecutionContextManager _executionContextManager;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EventSequence"/> class.
     /// </summary>
-    /// <param name="grainFactory"><see cref="IGrainFactory"/>.</param>
     /// <param name="eventSequenceStorageProviderProvider">Provider for <see cref="IEventSequenceStorageProvider"/>.</param>
     /// <param name="executionContextManager"><see cref="IExecutionContextManager"/>.</param>
     public EventSequence(
-        IGrainFactory grainFactory,
         ProviderFor<IEventSequenceStorageProvider> eventSequenceStorageProviderProvider,
         IExecutionContextManager executionContextManager)
     {
-        _grainFactory = grainFactory;
         _eventSequenceStorageProviderProvider = eventSequenceStorageProviderProvider;
         _executionContextManager = executionContextManager;
     }
 
     /// <summary>
-    /// Appends an event to the event log.
+    /// Get events for a specific event sequence in a microservice for a specific tenant.
     /// </summary>
-    /// <param name="microserviceId">The microservice to append for.</param>
-    /// <param name="eventSequenceId">The event sequence to append to.</param>
-    /// <param name="tenantId">The tenant to append to.</param>
-    /// <param name="eventToAppend">The payload with the details about the event to append.</param>
-    /// <returns>Awaitable task.</returns>
-    [HttpPost]
-    public async Task Append(
-        [FromRoute] MicroserviceId microserviceId,
+    /// <param name="eventSequenceId">Event sequence to get for.</param>
+    /// <param name="microserviceId">Microservice to get for.</param>
+    /// <param name="tenantId">Tenant to get for.</param>
+    /// <returns>A collection of <see cref="AppendedEvent"/>.</returns>
+    [HttpGet]
+    public async Task<IEnumerable<AppendedEvent>> FindFor(
         [FromRoute] EventSequenceId eventSequenceId,
-        [FromRoute] TenantId tenantId,
-        [FromBody] AppendEvent eventToAppend)
+        [FromQuery] MicroserviceId microserviceId,
+        [FromQuery] TenantId tenantId)
     {
+        var result = new List<AppendedEvent>();
         _executionContextManager.Establish(tenantId, CorrelationId.New(), microserviceId);
-        var eventLog = _grainFactory.GetGrain<IEventSequence>(eventSequenceId, keyExtension: new MicroserviceAndTenant(microserviceId, tenantId));
-        await eventLog.Append(
-            eventToAppend.EventSourceId,
-            eventToAppend.EventType,
-            eventToAppend.Content);
+        var cursor = await _eventSequenceStorageProviderProvider().GetFromSequenceNumber(eventSequenceId, EventSequenceNumber.First);
+        while (await cursor.MoveNext())
+        {
+            result.AddRange(cursor.Current);
+        }
+        return result;
     }
 }
