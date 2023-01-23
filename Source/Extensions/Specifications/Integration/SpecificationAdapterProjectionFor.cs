84f9acef888c1984d2923dc8496a07a269b0061d
Einar Ingebrigtsen <einari@me.com>
2023-01-23 19:29:28 +0100

    Fixing broken static code analysis rules

--- Source/Extensions/Specifications/Integration/SpecificationAdapterProjectionFor.cs
+++ Source/Extensions/Specifications/Integration/SpecificationAdapterProjectionFor.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Aksio.Cratis.Projections;
 using Aksio.Cratis.Integration;
+using Aksio.Cratis.Projections;
 
 namespace Aksio.Cratis.Specifications.Integration;
 
