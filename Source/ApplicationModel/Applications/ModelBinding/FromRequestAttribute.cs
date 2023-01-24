95fc8a72215d81e38618ecc84e0607e70ab836e7
Einar Ingebrigtsen <einari@me.com>
2023-01-24 11:12:51 +0100

    Adding support for [FromRequest] composition binding source

---
+++ Source/ApplicationModel/Applications/ModelBinding/FromRequestAttribute.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Aksio.Cratis.Applications.ModelBinding;
+
+/// <summary>
+/// Specifies that a parameter or property should be bound using the request body and allowing to be combined with route and query string using [FromRoute], [FromQuery] inside the object.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
+public class FromRequestAttribute : Attribute, IBindingSourceMetadata
+{
+    /// <inheritdoc/>
+    public BindingSource? BindingSource => new FromRequestBindingSource();
+}
