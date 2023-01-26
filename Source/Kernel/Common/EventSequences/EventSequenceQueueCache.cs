8a4b204f96762ded870e58bba3e95d1e4a09fccb
Einar Ingebrigtsen <einari@me.com>
2023-01-26 14:31:35 +0100

    Started on pulling in PoolQueueCache

--- Source/Kernel/Common/EventSequences/EventSequenceQueueCache.cs
+++ Source/Kernel/Common/EventSequences/EventSequenceQueueCache.cs
@@ -2,9 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.DependencyInversion;
-using Aksio.Cratis.Events;
 using Aksio.Cratis.EventSequences;
 using Aksio.Cratis.Execution;
+using Microsoft.Extensions.Logging;
+using Orleans.Providers.Streams.Common;
 using Orleans.Streams;
 
 namespace Aksio.Cratis.Kernel.EventSequences;
@@ -16,18 +17,43 @@ public class EventSequenceQueueCache : IQueueCache
 {
     readonly IExecutionContextManager _executionContextManager;
     readonly ProviderFor<IEventSequenceStorageProvider> _eventLogStorageProvider;
+    readonly ICacheMonitor _cacheMonitor;
+    readonly PooledQueueCache _cache;
+    readonly IEventSequenceCacheDataAdapter _dataAdapter;
+    readonly IEvictionStrategy _evictionStrategy;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EventSequenceQueueCache"/> class.
     /// </summary>
     /// <param name="executionContextManager"><see cref="IExecutionContextManager"/> for working with execution context.</param>
     /// <param name="eventLogStorageProvider"><see cref="IEventSequenceStorageProvider"/> for getting events from storage.</param>
+    /// <param name="cacheMonitor"></param>
+    /// <param name="cache"></param>
+    /// <param name="dataAdapter"></param>
+    /// <param name="evictionStrategyLogger"></param>
     public EventSequenceQueueCache(
         IExecutionContextManager executionContextManager,
-        ProviderFor<IEventSequenceStorageProvider> eventLogStorageProvider)
+        ProviderFor<IEventSequenceStorageProvider> eventLogStorageProvider,
+        ICacheMonitor cacheMonitor,
+        PooledQueueCache cache,
+        IEventSequenceCacheDataAdapter dataAdapter,
+        ILogger<IEvictionStrategy> evictionStrategyLogger)
     {
         _executionContextManager = executionContextManager;
         _eventLogStorageProvider = eventLogStorageProvider;
+        _cacheMonitor = cacheMonitor;
+        _cache = cache;
+        _dataAdapter = dataAdapter;
+        var timePurgePredicate = new TimePurgePredicate(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(30));
+        _evictionStrategy = new ChronologicalEvictionStrategy(evictionStrategyLogger, timePurgePredicate, cacheMonitor, TimeSpan.FromMinutes(5))
+        {
+            PurgeObservable = cache,
+            OnPurged = OnPurge
+        };
+    }
+
+    void OnPurge(CachedMessage? lastItemPurged, CachedMessage? newestItem)
+    {
     }
 
     /// <inheritdoc/>
@@ -51,6 +77,9 @@ public class EventSequenceQueueCache : IQueueCache
             return new EmptyEventSequenceQueueCacheCursor();
         }
 
+        return new Cursor(_cache, streamIdentity, token);
+
+        /*
         if (token is EventSequenceNumberTokenWithFilter tokenWithFilter)
         {
             if (tokenWithFilter.Partition == EventSourceId.Unspecified)
@@ -67,6 +96,7 @@ public class EventSequenceQueueCache : IQueueCache
         }
 
         return new EventSequenceQueueCacheCursor(_executionContextManager, (ulong)token.SequenceNumber, streamIdentity, _eventLogStorageProvider());
+        */
     }
 
     /// <inheritdoc/>
@@ -81,4 +111,48 @@ public class EventSequenceQueueCache : IQueueCache
         purgedItems = new List<IBatchContainer>();
         return true;
     }
+
+    class Cursor : IQueueCacheCursor
+    {
+        private readonly PooledQueueCache _cache;
+        private readonly object _cursor;
+        private IBatchContainer _current;
+
+        public Cursor(PooledQueueCache cache, IStreamIdentity streamId, StreamSequenceToken token)
+        {
+            _current = null!;
+            _cache = cache;
+            _cursor = cache.GetCursor(streamId, token);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IBatchContainer GetCurrent(out Exception exception)
+        {
+            exception = null!;
+            return _current;
+        }
+
+        public bool MoveNext()
+        {
+            if (!_cache.TryGetNextMessage(_cursor, out var next))
+            {
+                return false;
+            }
+
+            _current = next;
+            return true;
+        }
+
+        public void Refresh(StreamSequenceToken token)
+        {
+        }
+
+        public void RecordDeliveryFailure()
+        {
+        }
+    }
+
 }
