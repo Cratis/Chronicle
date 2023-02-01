08467284955417199350935e20f857f13ae4f835
Einar Ingebrigtsen <einari@me.com>
2023-02-01 12:18:49 +0100

    Adding log messages for client lifecycle events

---
+++ Source/Clients/DotNET/Integration/AdaptersClientLifecycleParticipantLogMessages.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace Aksio.Cratis.Integration;
+
+internal static partial class AdaptersClientLifecycleParticipantLogMessages
+{
+    [LoggerMessage(1, LogLevel.Information, "Registering adapters")]
+    internal static partial void RegisterAdapters(this ILogger<AdaptersClientLifecycleParticipant> logger);
+}
