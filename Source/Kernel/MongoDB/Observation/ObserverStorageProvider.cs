34775a39884fd22f902ce76a23bc42064499019f
Einar Ingebrigtsen <einari@me.com>
2023-02-02 19:55:45 +0100

    Making catchup storage provider only update the sequence number

--- Source/Kernel/MongoDB/Observation/ObserverStorageProvider.cs
+++ Source/Kernel/MongoDB/Observation/ObserverStorageProvider.cs
@@ -21,7 +21,10 @@ public class ObserverStorageProvider : IGrainStorage
     readonly IExecutionContextManager _executionContextManager;
     readonly ProviderFor<IEventStoreDatabase> _eventStoreDatabaseProvider;
 
-    IMongoCollection<ObserverState> Collection => _eventStoreDatabaseProvider().GetCollection<ObserverState>(CollectionNames.Observers);
+    /// <summary>
+    /// Gets the <ze cref="IMongoCollection{TDocument}"/> for <see cref="ObserverState"/>.
+    /// </summary>
+    protected IMongoCollection<ObserverState> Collection => _eventStoreDatabaseProvider().GetCollection<ObserverState>(CollectionNames.Observers);
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ObserverStorageProvider"/> class.
@@ -78,7 +81,13 @@ public class ObserverStorageProvider : IGrainStorage
             new ReplaceOptions { IsUpsert = true });
     }
 
-    string GetKeyFrom(ObserverKey key, ObserverId observerId) => key.SourceMicroserviceId is not null ?
+    /// <summary>
+    /// Get the key for the observer state.
+    /// </summary>
+    /// <param name="key"><see cref="ObserverKey"/> to get for.</param>
+    /// <param name="observerId"><see cref="ObserverId"/> to get for.</param>
+    /// <returns>The actual key.</returns>
+    protected string GetKeyFrom(ObserverKey key, ObserverId observerId) => key.SourceMicroserviceId is not null ?
         $"{key.EventSequenceId} : {observerId} : {key.SourceMicroserviceId}" :
         $"{key.EventSequenceId} : {observerId}";
 }
