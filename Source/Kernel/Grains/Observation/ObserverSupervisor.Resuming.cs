3373c7948dfed479f60dd4f0b892a4db35323324
Einar Ingebrigtsen <einari@me.com>
2023-02-02 13:18:31 +0100

    Renaming Observer to ObserverSupervisor

---
+++ Source/Kernel/Grains/Observation/ObserverSupervisor.Resuming.cs
@@ -0,0 +1,112 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Events;
+using Aksio.Cratis.Kernel.EventSequences;
+using Orleans.Streams;
+
+namespace Aksio.Cratis.Kernel.Grains.Observation;
+
+/// <summary>
+/// Partial Observer implementation focused on the reminder for failed partitions aspect.
+/// </summary>
+public partial class ObserverSupervisor
+{
+    /// <inheritdoc/>
+    public async Task TryResumePartition(EventSourceId eventSourceId)
+    {
+        if (State.IsDisconnected || !State.IsPartitionFailed(eventSourceId))
+        {
+            return;
+        }
+
+        var failedPartition = State.GetFailedPartition(eventSourceId);
+        if (State.IsRecoveringPartition(failedPartition.EventSourceId) &&
+            _streamSubscriptionsByEventSourceId.ContainsKey(eventSourceId))
+        {
+            return;
+        }
+
+        State.StartRecoveringPartition(eventSourceId);
+        await WriteStateAsync();
+
+        if (_streamSubscriptionsByEventSourceId.ContainsKey(eventSourceId))
+        {
+            await _streamSubscriptionsByEventSourceId[eventSourceId]!.UnsubscribeAsync();
+        }
+
+        var tailSequenceNumber = await EventSequenceStorageProvider.GetTailSequenceNumber(State.EventSequenceId, State.EventTypes, eventSourceId);
+        await SubscribeForResumingPartition(eventSourceId, failedPartition.SequenceNumber, tailSequenceNumber);
+    }
+
+    /// <summary>
+    /// Subscribe to stream for a specific eventsource id and sequence number. Typically used while resuming a partition.
+    /// </summary>
+    /// <param name="eventSourceId"><see cref="EventSourceId"/> to subscribe for.</param>
+    /// <param name="sequenceNumber"><see cref="EventSequenceNumber"/> to use as offset.</param>
+    /// <param name="tailSequenceNumber"><see cref="EventSequenceNumber"/> as the tail.</param>
+    /// <returns>Awaitable task.</returns>
+    public async Task SubscribeForResumingPartition(EventSourceId eventSourceId, EventSequenceNumber sequenceNumber, EventSequenceNumber tailSequenceNumber)
+    {
+        _logger.SubscribingToStream(_observerId, _eventSequenceId, _microserviceId, _tenantId, _stream!.Guid, _stream!.Namespace);
+        _streamSubscriptionsByEventSourceId[eventSourceId] = await _stream!.SubscribeAsync(
+            async (@event, _) => await HandleEventForRecoveringPartitionedObserver(@event, tailSequenceNumber),
+            new EventSequenceNumberToken(sequenceNumber),
+            EventTypesAndEventSourceIdFilter,
+            new EventTypesAndEventSourceId(State.EventTypes.ToArray(), eventSourceId));
+    }
+
+    static bool EventTypesAndEventSourceIdFilter(IStreamIdentity stream, object filterData, object item)
+    {
+        var appendedEvent = (item as AppendedEvent)!;
+        var eventTypesAndEventSourceId = (filterData as EventTypesAndEventSourceId)!;
+
+        var shouldIncludeEventType =
+            eventTypesAndEventSourceId.EventTypes.Any(_ => _.Id.Equals(appendedEvent.Metadata.Type.Id)) ||
+            eventTypesAndEventSourceId.EventTypes.Length == 0;
+
+        return
+            appendedEvent.Context.EventSourceId == eventTypesAndEventSourceId.EventSourceId &&
+            shouldIncludeEventType;
+    }
+
+    async Task HandleEventForRecoveringPartitionedObserver(AppendedEvent @event, EventSequenceNumber tailSequenceNumber)
+    {
+        await HandleEventForPartitionedObserver(@event);
+        if (State.IsPartitionFailed(@event.Context.EventSourceId))
+        {
+            await _streamSubscriptionsByEventSourceId[@event.Context.EventSourceId]!.UnsubscribeAsync();
+            _streamSubscriptionsByEventSourceId.Remove(@event.Context.EventSourceId);
+        }
+        else
+        {
+            var partitionRecovery = State.GetPartitionRecovery(@event.Context.EventSourceId);
+            partitionRecovery.SequenceNumber++;
+            await WriteStateAsync();
+
+            if (partitionRecovery.SequenceNumber >= tailSequenceNumber)
+            {
+                var actualTailSequenceNumber = await EventSequenceStorageProvider.GetTailSequenceNumber(State.EventSequenceId, State.EventTypes, @event.Context.EventSourceId);
+                if (actualTailSequenceNumber == tailSequenceNumber)
+                {
+                    State.PartitionRecovered(@event.Context.EventSourceId);
+                    await WriteStateAsync();
+
+                    await _streamSubscriptionsByEventSourceId[@event.Context.EventSourceId]!.UnsubscribeAsync();
+                    _streamSubscriptionsByEventSourceId.Remove(@event.Context.EventSourceId);
+                }
+            }
+        }
+    }
+
+    async Task TryResumeAnyFailedPartitions()
+    {
+        if (State.HasFailedPartitions)
+        {
+            foreach (var partition in State.FailedPartitions)
+            {
+                await TryResumePartition(partition.EventSourceId);
+            }
+        }
+    }
+}
---
+++ Specifications/Kernel/Grains/Observation/for_ObserverSupervisor/when_resuming/and_events_are_in_sequence.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Kernel.Grains.Observation.for_ObserverSupervisor.when_resuming;
+
+public class and_events_are_in_sequence : given.an_observer_and_two_event_types_and_one_event_in_sequence
+{
+    async Task Establish()
+    {
+        state.FailPartition(event_source_id, EventSequenceNumber.First, Array.Empty<string>(), string.Empty);
+        await observer.Subscribe<ObserverSubscriber>(event_types);
+    }
+
+    async Task Because()
+    {
+        await observer.TryResumePartition(event_source_id);
+        await observers[0].OnNextAsync(appended_event);
+    }
+
+    [Fact] void should_forward_event_to_observer_subscriber() => subscriber.Verify(_ => _.OnNext(appended_event), Once());
+    [Fact] void should_set_offset_to_next_event_sequence() => state_on_write.NextEventSequenceNumber.Value.ShouldEqual(appended_event.Metadata.SequenceNumber.Value + 1);
+    [Fact] void should_set_last_handled_to_event_sequence_number_from_the_event() => state_on_write.LastHandled.Value.ShouldEqual(appended_event.Metadata.SequenceNumber.Value);
+    [Fact] void should_unsubscribe_stream_when_finished() => subscription_handles[0].Verify(_ => _.UnsubscribeAsync(), Once());
+}
