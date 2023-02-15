b856ecd5abc032b0af62266e33783440f522d88d
Einar Ingebrigtsen <einari@me.com>
2023-02-15 14:45:51 +0100

    Adding observer id and key on subscription
    #752

--- Source/Kernel/Grains.Interfaces/Observation/ObserverSubscription.cs
+++ Source/Kernel/Grains.Interfaces/Observation/ObserverSubscription.cs
@@ -2,21 +2,24 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Events;
+using Aksio.Cratis.Observation;
 
 namespace Aksio.Cratis.Kernel.Grains.Observation;
 
 /// <summary>
 /// Represents a subscription to an observer.
 /// </summary>
-/// <param name="SubscriberType">Type that is subscribing.</param>
+/// <param name="ObserverId">The <see cref="ObserverId"/> that the subscription is for.</param>
+/// <param name="ObserverKey">The <see cref="ObserverKey"/> that the subscription is for.</param>
 /// <param name="EventTypes">Represents the event types for the subscription.</param>
+/// <param name="SubscriberType">Type that is subscribing.</param>
 /// <param name="Arguments">Any arguments it passed.</param>
-public record ObserverSubscription(Type SubscriberType, IEnumerable<EventType> EventTypes, object Arguments)
+public record ObserverSubscription(ObserverId ObserverId, ObserverKey ObserverKey, IEnumerable<EventType> EventTypes, Type SubscriberType, object Arguments)
 {
     /// <summary>
     /// Gets a subscription representing no subscription.
     /// </summary>
-    public static readonly ObserverSubscription Unsubscribed = new(typeof(IObserverSubscriber), Enumerable.Empty<EventType>(), null!);
+    public static readonly ObserverSubscription Unsubscribed = new(ObserverId.Unspecified, ObserverKey.NotSet, Enumerable.Empty<EventType>(), typeof(IObserverSubscriber), null!);
 
     /// <summary>
     /// Check whether or not the subscription is subscribed.
