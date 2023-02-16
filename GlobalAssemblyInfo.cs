52cc5fda45e1bc6e8d798dbc6971a98c50d7e77b
Einar Ingebrigtsen <einari@me.com>
2023-02-16 13:05:45 +0100

    Marking all assemblies as invariant culture
    #724

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
