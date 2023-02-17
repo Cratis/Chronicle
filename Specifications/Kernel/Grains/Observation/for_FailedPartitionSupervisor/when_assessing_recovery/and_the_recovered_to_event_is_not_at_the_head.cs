535b6edeee8833f57d2bf21d8dfab83c7b363ed5
Einar Ingebrigtsen <einari@me.com>
2023-02-17 15:05:56 +0100

    Formatting

--- Specifications/Kernel/Grains/Observation/for_FailedPartitionSupervisor/when_assessing_recovery/and_the_recovered_to_event_is_not_at_the_head.cs
+++ Specifications/Kernel/Grains/Observation/for_FailedPartitionSupervisor/when_assessing_recovery/and_the_recovered_to_event_is_not_at_the_head.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Aksio.Cratis.Kernel.Grains.Observation.for_ObserverSupervisor.for_FailedPartitionSupervisor.given;
 
 namespace Aksio.Cratis.Kernel.Grains.Observation.for_ObserverSupervisor.for_FailedPartitionSupervisor.when_assessing_recovery;
