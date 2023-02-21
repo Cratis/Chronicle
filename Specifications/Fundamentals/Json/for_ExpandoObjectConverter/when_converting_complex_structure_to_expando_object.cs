db6d7ede28ca862147071e68e3550d4153eca147
Einar Ingebrigtsen <einari@me.com>
2023-02-21 18:42:58 +0100

    Fixing support for primitive arrays (string, int) conversion

--- Specifications/Fundamentals/Json/for_ExpandoObjectConverter/when_converting_complex_structure_to_expando_object.cs
+++ Specifications/Fundamentals/Json/for_ExpandoObjectConverter/when_converting_complex_structure_to_expando_object.cs
@@ -19,7 +19,7 @@ public class when_converting_complex_structure_to_expando_object : given.an_expa
             ["intValue"] = 43,
             ["floatValue"] = 43.43,
             ["doubleValue"] = 43.43,
-            ["guidValue"] = Guid.Parse("251b9fbe-83d4-4306-9a5d-9d0e7d4dd456"),
+            ["guidValue"] = Guid.Parse("251b9fbe-83d4-4306-9a5d-9d0e7d4dd456")
         };
 
         child = new JsonObject
@@ -45,7 +45,9 @@ public class when_converting_complex_structure_to_expando_object : given.an_expa
             ["dateOnlyValue"] = DateTime.Parse("2022-10-31T14:51:32.8450000Z"),
             ["timeOnlyValue"] = DateTime.Parse("2022-10-31T14:51:32.8450000Z"),
             ["reference"] = reference,
-            ["children"] = new JsonArray(child)
+            ["children"] = new JsonArray(child),
+            ["stringArray"] = new JsonArray { "first", "second" },
+            ["intArray"] = new JsonArray { 42, 43 }
         };
     }
 
@@ -75,6 +77,10 @@ public class when_converting_complex_structure_to_expando_object : given.an_expa
     [Fact] void should_set_top_level_date_only_value_to_hold_correct_value() => ((DateOnly)result.dateOnlyValue).ShouldEqual(DateOnly.FromDateTime(source["dateOnlyValue"].GetValue<DateTime>()));
     [Fact] void should_set_top_level_time_only_value_to_be_of_date_time_offset_type() => ((object)result.timeOnlyValue).ShouldBeOfExactType<TimeOnly>();
     [Fact] void should_set_top_level_time_only_value_to_hold_correct_value() => ((TimeOnly)result.timeOnlyValue).ShouldEqual(TimeOnly.FromDateTime(source["timeOnlyValue"].GetValue<DateTime>()));
+    [Fact] void should_set_top_level_string_array_first_item() => ((string)result.stringArray[0]).ShouldEqual("first");
+    [Fact] void should_set_top_level_string_array_second_item() => ((string)result.stringArray[1]).ShouldEqual("second");
+    [Fact] void should_set_top_level_int_array_first_item() => ((int)result.intArray[0]).ShouldEqual(42);
+    [Fact] void should_set_top_level_int_array_second_item() => ((int)result.intArray[1]).ShouldEqual(43);
 
     [Fact] void should_reference_level_int_value_to_be_of_int_type() => ((object)result.reference.intValue).ShouldBeOfExactType<int>();
     [Fact] void should_reference_level_int_value_to_hold_correct_value() => ((int)result.reference.intValue).ShouldEqual(reference["intValue"].GetValue<int>());
