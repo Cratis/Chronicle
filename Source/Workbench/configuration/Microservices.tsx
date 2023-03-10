23c7affcf576aea39298774de04d1261cf1c10c6
Einar Ingebrigtsen <einari@me.com>
2023-03-10 19:05:54 +0100

    Changing to MUI datagrid

--- Source/Workbench/configuration/Microservices.tsx
+++ Source/Workbench/configuration/Microservices.tsx
@@ -1,34 +1,35 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { ScrollableDetailsList } from '@aksio/cratis-fluentui';
-import { IColumn } from '@fluentui/react';
+import { Box } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { AllMicroservices } from 'API/configuration/microservices/AllMicroservices';
 
-const columns: IColumn[] = [
+const columns: GridColDef[] = [
     {
-        key: 'id',
-        name: 'Id',
-        fieldName: 'id',
-        minWidth: 240,
-        maxWidth: 240
+        field: 'id',
+        headerName: 'Id',
+        width: 240,
     },
     {
-        key: 'name',
-        name: 'Name',
-        fieldName: 'name',
-        minWidth: 200
+        field: 'name',
+        headerName: 'Name',
+        width: 240,
     }
 ];
 
-
 export const Microservices = () => {
-
     const [allMicroservices] = AllMicroservices.use();
 
     return (
-        <ScrollableDetailsList
-            columns={columns}
-            items={allMicroservices.data} />
+        <Box sx={{ height: 400 }}>
+            <DataGrid
+                columns={columns}
+                filterMode="client"
+                sortingMode="client"
+                getRowId={row => row.id}
+                rows={allMicroservices.data}
+            />
+        </Box>
     );
 };
