d8cb4ca0864e7a369a1694959031a3639892eff1
Einar Ingebrigtsen <einari@me.com>
2023-03-16 16:46:23 +0100

    Fixing compiler errors

--- Source/Kernel/Grains/Configuration/Tenants/TenantConfiguration.cs
+++ Source/Kernel/Grains/Configuration/Tenants/TenantConfiguration.cs
@@ -30,5 +30,5 @@ public class TenantConfiguration : Grain<TenantConfigurationState>, ITenantConfi
     }
 
     /// <inheritdoc/>
-    public Task<TenantConfigurationState> All() => Task.FromResult(State);
+    public Task<IDictionary<string, string>> All() => Task.FromResult<IDictionary<string, string>>(new Dictionary<string, string>(State));
 }
