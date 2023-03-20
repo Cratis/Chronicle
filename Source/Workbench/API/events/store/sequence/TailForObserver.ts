95fc3daa6dfa0652bbb7f9ce6704f9ad0427ebdc
Einar Ingebrigtsen <einari@me.com>
2023-03-20 09:29:49 +0100

    Regen due to String -> string

---
+++ Source/Workbench/API/events/store/sequence/TailForObserver.ts
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+import { QueryFor, QueryResultWithState, useQuery, PerformQuery } from '@aksio/cratis-applications-frontend/queries';
+import Handlebars from 'handlebars';
+
+const routeTemplate = Handlebars.compile('/api/events/store/{{microserviceId}}/{{tenantId}}/sequence/{{eventSequenceId}}/tail-sequence-number/observer/{{observerId}}');
+
+export interface TailForObserverArguments {
+    eventSequenceId: string;
+    microserviceId: string;
+    tenantId: string;
+    observerId: string;
+}
+export class TailForObserver extends QueryFor<number, TailForObserverArguments> {
+    readonly route: string = '/api/events/store/{{microserviceId}}/{{tenantId}}/sequence/{{eventSequenceId}}/tail-sequence-number/observer/{{observerId}}';
+    readonly routeTemplate: Handlebars.TemplateDelegate = routeTemplate;
+    readonly defaultValue: number = {} as any;
+
+    constructor() {
+        super(Number, false);
+    }
+
+    get requestArguments(): string[] {
+        return [
+            'eventSequenceId',
+            'microserviceId',
+            'tenantId',
+            'observerId',
+        ];
+    }
+
+    static use(args?: TailForObserverArguments): [QueryResultWithState<number>, PerformQuery<TailForObserverArguments>] {
+        return useQuery<number, TailForObserver, TailForObserverArguments>(TailForObserver, args);
+    }
+}
