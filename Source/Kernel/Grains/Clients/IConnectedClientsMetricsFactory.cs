ef23423d2c2302b8a38e36ab48eb5c887306bd0f
Einar Ingebrigtsen <einari@me.com>
2023-04-19 20:05:39 +0200

    Connected clients metrics

---
+++ Source/Kernel/Grains/Clients/IConnectedClientsMetricsFactory.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Execution;
+
+namespace Aksio.Cratis.Kernel.Grains.Clients;
+
+/// <summary>
+/// Defines a factory for creating <see cref="IConnectedClientsMetrics"/>.
+/// </summary>
+public interface IConnectedClientsMetricsFactory
+{
+    /// <summary>
+    /// Create a new instance of <see cref="IConnectedClientsMetrics"/>.
+    /// </summary>
+    /// <param name="microserviceId"><see cref="MicroserviceId"/> to create for.</param>
+    /// <returns>A <see cref="IConnectedClientsMetrics"/>.</returns>
+    IConnectedClientsMetrics Create(MicroserviceId microserviceId);
+}
