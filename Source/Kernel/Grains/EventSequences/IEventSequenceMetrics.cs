22b7541e59ca676570b9fb0ae1eb5f358582d9e0
Einar Ingebrigtsen <einari@me.com>
2023-04-19 15:59:48 +0200

    Adding event sequence metrics

---
+++ Source/Kernel/Grains/EventSequences/IEventSequenceMetrics.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Kernel.Grains.EventSequences;
+
+/// <summary>
+/// Defines a system that can track metrics for event sequences.
+/// </summary>
+public interface IEventSequenceMetrics
+{
+    void AppendedEvent();
+}
