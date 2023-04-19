374b8f4e1f5ac4183089f9435e21f294d9b67707
Einar Ingebrigtsen <einari@me.com>
2023-04-19 15:59:39 +0200

    Added open telemetry and application insight support

--- Source/Kernel/Server/TelemetryConfigurationExtensions.cs
+++ Source/Kernel/Server/TelemetryConfigurationExtensions.cs
@@ -1,7 +1,10 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.Metrics;
 using Aksio.Cratis.Kernel.Orleans.Configuration;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
 
 namespace Orleans.Hosting;
 
@@ -17,15 +20,29 @@ public static class TelemetryConfigurationExtensions
     /// <returns>Builder for continuation.</returns>
     public static ISiloBuilder UseTelemetry(this ISiloBuilder builder)
     {
-        builder.ConfigureServices(_ =>
+        builder.ConfigureServices(services =>
         {
-            var telemetryConfig = _.GetTelemetryConfig();
+            var telemetryConfig = services.GetTelemetryConfig();
+            var meter = new Meter("Cratis.Kernel");
+            services.AddSingleton(meter);
 
             switch (telemetryConfig.Type)
             {
                 case TelemetryTypes.AppInsights:
                     var options = telemetryConfig.GetAppInsightsTelemetryOptions();
                     builder.AddApplicationInsightsTelemetryConsumer(options.Key);
+
+                    if (!string.IsNullOrEmpty(options.ConnectionString))
+                    {
+                        services
+                            .AddOpenTelemetry()
+                            .WithMetrics(metrics => metrics
+                                .AddMeter(meter.Name)
+                                .AddAspNetCoreInstrumentation()
+                                .AddHttpClientInstrumentation()
+                                .AddRuntimeInstrumentation()
+                                .AddAzureMonitorMetricExporter(exporter => exporter.ConnectionString = options.ConnectionString));
+                    }
                     break;
             }
         });
