fc75e2dc9de27f80272038ac0094d6585302333c
Einar Ingebrigtsen <einari@me.com>
2023-04-26 20:52:28 +0200

    Improved event types with schema to not have to roundtrip to server for every event

---
+++ Source/Kernel/Read/EventTypes/EventTypeWithSchemas.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+
+namespace Aksio.Cratis.Kernel.Read.EventTypes;
+
+/// <summary>
+/// Represents an event type with all its schemas.
+/// </summary>
+/// <param name="EventType">Event type.</param>
+/// <param name="Schemas">Collection of schemas.</param>
+/// <returns></returns>
+public record EventTypeWithSchemas(EventTypeInformation EventType, IEnumerable<JsonDocument> Schemas);
