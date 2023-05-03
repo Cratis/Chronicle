f54be87bf0abd7c08458298f9c736f4028c026cf
Einar Ingebrigtsen <einari@me.com>
2023-05-03 08:25:40 +0200

    Started moving fundamentals

--- Source/Node/Fundamentals/PropertyAccessor.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Aksio Insurtech. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-/**
- * The delegate for representing accessing a property
- */
-export type PropertyAccessor<T = any> = (instance: T) => any;
