aadb7c48b836f7fc1d68718747ce65a7b3290dbd
Einar Ingebrigtsen <einari@me.com>
2023-06-20 14:20:44 +0200

    Compiling and Running with Orleans 7

--- Source/Kernel/Orleans/Serialization/SerializationConfigurationExtensions.cs
+++ Source/Kernel/Orleans/Serialization/SerializationConfigurationExtensions.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json.Nodes;
+using Aksio.Json;
 using Orleans.Configuration;
 using Orleans.Hosting;
+using Orleans.Serialization;
 
 namespace Aksio.Cratis.Kernel.Orleans.Serialization;
 
@@ -18,6 +21,8 @@ public static class SerializationConfigurationExtensions
     /// <returns><see cref="ISiloBuilder"/> for continuation.</returns>
     public static ISiloBuilder ConfigureSerialization(this ISiloBuilder siloBuilder)
     {
+        siloBuilder.Services.AddSerializer(serializerBuilder => serializerBuilder.AddJsonSerializer(_ => _ == typeof(JsonObject) || (_.Namespace?.StartsWith("Aksio") ?? false), Globals.JsonSerializerOptions));
+
         //throw new NotImplementedException();
         // siloBuilder.Configure<SerializationProviderOptions>(options => options.SerializationProviders.Add(typeof(ExpandoObjectSerializer)));
         return siloBuilder;
