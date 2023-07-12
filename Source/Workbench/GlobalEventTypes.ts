1fcf02bcd82fb18ca0c9d73c564764ee0758cbc4
Einar Ingebrigtsen <einari@me.com>
2023-07-12 15:09:51 +0200

    Cleanup

--- Source/Workbench/GlobalEventTypes.ts
+++ Source/Workbench/GlobalEventTypes.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 const GlobalEventTypes = {
     redaction: 'fd5bdc58-a224-4e74-897c-fb28aed51c8b'
 };
 
-export default GlobalEventTypes; 
+export default GlobalEventTypes;
