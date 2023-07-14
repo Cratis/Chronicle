6cb7d6ecb8a8c27999ffa2416ec6b9ccb88b0281
Einar Ingebrigtsen <einari@me.com>
2023-07-14 09:53:58 +0200

    Started building a very basic sample

--- Documentation/get-started/basic.md
+++ Documentation/get-started/basic.md
@@ -1,1 +1,5 @@
 # Basic
+
+```shell
+dotnet new web
+```
---
+++ Samples/Basic/Program.cs
@@ -0,0 +1,41 @@
+﻿using Aksio.Cratis;
+using Aksio.Cratis.EventSequences;
+using Basic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.UseCratis();
+var app = builder.Build();
+app.UseCratis();
+
+app.MapGet("/", () =>
+{
+    var eventLog = app.Services.GetRequiredService<IEventLog>();
+    eventLog.Append("49b9727f-64da-4d5d-bb52-8a3fc77f6a81", new MyEvent());
+});
+
+
+// var client = ClientBuilder
+//     .SingleTenanted()
+//     .Build(builder.Services);
+
+
+// await client
+//     .EventStore
+//     .Sequences
+//     .EventLog
+//     .Append("49b9727f-64da-4d5d-bb52-8a3fc77f6a81", new MyEvent());
+
+// var client = ClientBuilder
+//     .MultiTenanted()
+//     .Build();
+
+// await client
+//     .EventStore
+//     .Sequences
+//     .ForTenant(TenantId.Development)
+//     .EventLog
+//     .Append(EventSourceId.New(), new MyEvent());
+
+app.Run();
