af9c9354c694282a473b3f8a98759651ca244ca5
Einar Ingebrigtsen <einari@me.com>
2023-07-18 13:08:41 +0200

    Working on the sample

--- Samples/Basic/Program.cs
+++ Samples/Basic/Program.cs
@@ -1,11 +1,15 @@
-﻿using Aksio.Cratis;
+﻿// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Aksio.Cratis.EventSequences;
 using Basic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.UseCratis();
+//builder.UseCratis(_ => _.MultiTenanted());
+builder.UseCratis(_ => _.MultiTenanted());
 var app = builder.Build();
 app.UseCratis();
 
@@ -14,28 +18,5 @@ app.MapGet("/", () =>
     var eventLog = app.Services.GetRequiredService<IEventLog>();
     eventLog.Append("49b9727f-64da-4d5d-bb52-8a3fc77f6a81", new MyEvent());
 });
-
-
-// var client = ClientBuilder
-//     .SingleTenanted()
-//     .Build(builder.Services);
-
-
-// await client
-//     .EventStore
-//     .Sequences
-//     .EventLog
-//     .Append("49b9727f-64da-4d5d-bb52-8a3fc77f6a81", new MyEvent());
-
-// var client = ClientBuilder
-//     .MultiTenanted()
-//     .Build();
-
-// await client
-//     .EventStore
-//     .Sequences
-//     .ForTenant(TenantId.Development)
-//     .EventLog
-//     .Append(EventSourceId.New(), new MyEvent());
 
 app.Run();
