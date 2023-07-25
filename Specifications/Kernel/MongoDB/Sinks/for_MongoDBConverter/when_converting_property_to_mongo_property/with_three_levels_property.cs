913832e7eeed77a037f1a4d145e3b11cce6126d8
Einar Ingebrigtsen <einari@me.com>
2023-07-25 12:30:49 +0200

    Started adding specs for the MongoDBConverter

---
+++ Specifications/Kernel/MongoDB/Sinks/for_MongoDBConverter/when_converting_property_to_mongo_property/with_three_levels_property.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Properties;
+
+namespace Aksio.Cratis.Kernel.MongoDB.Sinks.for_MongoDBConverter.when_converting_property_to_mongo_property;
+
+public class with_three_levels_property : given.a_mongodb_converter
+{
+    MongoDBProperty result;
+
+    void Because() => result = converter.ToMongoDBProperty(new PropertyPath("FirstLevel.SecondLevel.ThirdLevel"), ArrayIndexers.NoIndexers);
+
+    [Fact] void should_have_the_correct_property_name() => result.Property.ShouldEqual("firstLevel.secondLevel.thirdLevel");
+    [Fact] void should_not_have_any_array_filters() => result.ArrayFilters.ShouldBeEmpty();
+}
+
+
