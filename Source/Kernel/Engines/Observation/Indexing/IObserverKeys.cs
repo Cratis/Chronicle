0e0775e5708036e31df74dfb990e53aeb1d9084d
Einar Ingebrigtsen <einari@me.com>
2023-07-26 15:57:07 +0200

    Moving definition of observer keys and index into engine

--- Source/Kernel/Engines/Observation/Indexing/IObserverKeys.cs
+++ Source/Kernel/Engines/Observation/Indexing/IObserverKeys.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Aksio.Cratis.Kernel.Engines.Projections;
+using Aksio.Cratis.Kernel.Engines.Keys;
 
-namespace Aksio.Cratis.Kernel.Grains.Observation.Indexing;
+namespace Aksio.Cratis.Kernel.Engines.Observation.Indexing;
 
 /// <summary>
 /// Defines an async enumerable for working with keys.
