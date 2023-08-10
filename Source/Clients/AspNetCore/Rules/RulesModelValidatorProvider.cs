8769ed88e849477312e221bc29bdadbb6f7c7b9e
Einar Ingebrigtsen <einari@me.com>
2023-08-10 12:30:06 +0200

    Making it testable by introducing a constructor for injecting dependencies it uses

--- Source/Clients/AspNetCore/Rules/RulesModelValidatorProvider.cs
+++ Source/Clients/AspNetCore/Rules/RulesModelValidatorProvider.cs
@@ -13,8 +13,31 @@ namespace Aksio.Cratis.AspNetCore.Rules;
 public class RulesModelValidatorProvider : IModelValidatorProvider
 {
     IRules? _rules;
+    IServiceProvider? _serviceProvider;
 
     IRules Rules => _rules ??= GlobalInstances.ServiceProvider.GetRequiredService<IRules>();
+
+    IServiceProvider ServiceProvider => _serviceProvider ??= GlobalInstances.ServiceProvider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RulesModelValidatorProvider"/> class.
+    /// </summary>
+    public RulesModelValidatorProvider()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RulesModelValidatorProvider"/> class.
+    /// </summary>
+    /// <param name="rules"><see cref="IRules"/> to use.</param>
+    /// <param name="serviceProvider"><see cref="IServiceProvider"/> for getting instances.</param>
+    public RulesModelValidatorProvider(
+        IRules rules,
+        IServiceProvider serviceProvider)
+    {
+        _rules = rules;
+        _serviceProvider = serviceProvider;
+    }
 
     /// <inheritdoc/>
     public void CreateValidators(ModelValidatorProviderContext context)
@@ -22,7 +45,7 @@ public class RulesModelValidatorProvider : IModelValidatorProvider
         if (Rules.HasFor(context.ModelMetadata.ModelType))
         {
             var ruleTypes = Rules.GetFor(context.ModelMetadata.ModelType);
-            var rules = ruleTypes.Select(ruleType => (GlobalInstances.ServiceProvider.GetService(ruleType) as IRule)!).ToArray();
+            var rules = ruleTypes.Select(ruleType => (ServiceProvider.GetRequiredService(ruleType) as IRule)!).ToArray();
 
             var validator = new RuleModelValidator(rules, Rules);
 
