81cd6b79a3d24636989007b12f32120d3389f4f2
Einar Ingebrigtsen <einari@me.com>
2023-08-17 08:58:35 +0200

    Adding implicit operator conversions for string

--- Source/Kernel/Shared/Auditing/CausationType.cs
+++ Source/Kernel/Shared/Auditing/CausationType.cs
@@ -18,4 +18,16 @@ public record CausationType(string Name) : ConceptAs<string>(Name)
     /// Represents the unknown causation type.
     /// </summary>
     public static readonly CausationType Unknown = new("Unknown");
+
+    /// <summary>
+    /// Implicitly convert from <see cref="string"/> to <see cref="CausationType"/>.
+    /// </summary>
+    /// <param name="name">Name of causation type.</param>
+    public static implicit operator CausationType(string name) => new(name);
+
+    /// <summary>
+    /// Implicitly convert from <see cref="CausationType"/> to <see cref="string"/>.
+    /// </summary>
+    /// <param name="causationType">Causation type to convert from.</param>
+    public static implicit operator string(CausationType causationType) => causationType.Name;
 }
