77c1f1fa41b159d602624423acdf70cb1c990252
Einar Ingebrigtsen <einari@me.com>
2023-08-17 11:59:04 +0200

    Adding specs for ObserverHandler OnNext

---
+++ Specifications/Clients/DotNET/Observation/for_ObserverHandler/given/all_dependencies.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Auditing;
+using Aksio.Cratis.EventSequences;
+
+namespace Aksio.Cratis.Observation.for_ObserverHandler.given;
+
+public class all_dependencies : Specification
+{
+    protected ObserverId observer_id;
+    protected ObserverName observer_name;
+    protected EventSequenceId event_sequence_id;
+    protected Mock<IEventTypes> event_types;
+    protected Mock<IObserverInvoker> observer_invoker;
+    protected Mock<ICausationManager> causation_manager;
+    protected Mock<IEventSerializer> event_serializer;
+
+    void Establish()
+    {
+        observer_id = Guid.NewGuid();
+        observer_name = "My Observer";
+        event_sequence_id = Guid.NewGuid();
+        event_types = new();
+        observer_invoker = new();
+        causation_manager = new();
+        event_serializer = new();
+    }
+}
---
+++ Specifications/Clients/DotNET/Observation/for_ObserverHandler/given/an_observer_handler.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Observation.for_ObserverHandler.given;
+
+public class an_observer_handler : all_dependencies
+{
+    protected ObserverHandler handler;
+
+    void Establish() => handler = new(
+        observer_id,
+        observer_name,
+        event_sequence_id,
+        event_types.Object,
+        observer_invoker.Object,
+        causation_manager.Object,
+        event_serializer.Object);
+}
