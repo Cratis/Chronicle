3f6d4838c442af8dd6688a0013d648435f17ecff
Einar Ingebrigtsen <einari@me.com>
2023-08-21 14:17:36 +0200

    Adding specs for getting a chain from an identity - both single and behalf of

---
+++ Specifications/Kernel/MongoDB/Identities/for_MongoDBIdentityStore/when_getting_chain_for_single_identity/and_it_already_exists.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Aksio.Cratis.Identities;
+
+namespace Aksio.Cratis.Kernel.MongoDB.Identities.for_MongoDBIdentityStore.when_getting_chain_for_single_identity;
+
+public class and_it_already_exists : given.two_identities_registered
+{
+    Identity identity;
+    IEnumerable<IdentityId> identities;
+
+    void Establish() => identity = new Identity(first_identity_from_database.Subject, first_identity_from_database.Name, first_identity_from_database.UserName);
+
+    async Task Because() => identities = await store.GetChainFor(identity);
+
+    [Fact] void should_return_only_one_identity() => identities.Count().ShouldEqual(1);
+    [Fact] void should_not_insert_the_identity() => inserted_identities.Count.ShouldEqual(0);
+    [Fact] void should_return_the_correct_identity() => identities.First().ShouldEqual(first_identity);
+}
