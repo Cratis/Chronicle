8f0ae845110f67eb875accbf59edab2f9a4f5ef3
Einar Ingebrigtsen <einari@me.com>
2023-08-21 18:37:29 +0200

    Hooking up causation metadata and custom identity provider

--- Samples/Banking/Bank/Main/Program.cs
+++ Samples/Banking/Bank/Main/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Globalization;
+using Main;
 
 // Force invariant culture for the Kernel
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
@@ -12,6 +13,9 @@ CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
 var builder = WebApplication.CreateBuilder(args)
                 .UseCratis(_ => _
                     .MultiTenanted()
+                    .WithSoftwareVersion("1.0.0", "1234567890")
+                    .IdentifiedAs("Bank")
+                    .UseIdentityProvider<CustomIdentityProvider>()
                     .ForMicroservice("eaf02867-79b9-4967-be67-3e93cee7c601", "Bank"));
 builder.Host
         .UseMongoDB()
