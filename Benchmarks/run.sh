7d6687c75d670b510cf17e952143a0cce15f721b
Einar Ingebrigtsen <einari@me.com>
2023-09-05 10:40:27 +0200

    Forgot $ in front of PWD when echoing it :)

--- Benchmarks/run.sh
+++ Benchmarks/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kill -9 $(lsof -t -i :5500 -s TCP:LISTEN)
 #sudo
-echo PWD
+echo $PWD
 dotnet run -c Release -- @./run.rsp
 cp ./results/results/BenchmarkRun-joined*.json ./results/results.json
