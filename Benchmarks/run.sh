955b38c21a7bb85db360a9a0c22f661a069c1910
Einar Ingebrigtsen <einari@me.com>
2023-09-05 10:37:41 +0200

    Fixing paths for files used

--- Benchmarks/run.sh
+++ Benchmarks/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kill -9 $(lsof -t -i :5500 -s TCP:LISTEN)
 #sudo
-dotnet run -c Release -- @run.rsp
-cp results/results/BenchmarkRun-joined*.json results/results.json
+echo PWD
+dotnet run -c Release -- @./run.rsp
+cp ./results/results/BenchmarkRun-joined*.json ./results/results.json
