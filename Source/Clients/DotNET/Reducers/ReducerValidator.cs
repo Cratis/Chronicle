321a0aabab3c0b7a36d2b2997441179979cea910
Einar Ingebrigtsen <einari@me.com>
2023-09-05 10:21:43 +0200

    Removing comments that was a TODO list - moved to issues #947 and #958

--- Source/Clients/DotNET/Reducers/ReducerValidator.cs
+++ Source/Clients/DotNET/Reducers/ReducerValidator.cs
@@ -16,24 +16,5 @@ public class ReducerValidator : IReducerValidator
         TypeMustImplementReducer.ThrowIfTypeDoesNotImplementReducer(reducerType);
         TypeMustImplementOnlyOneReducer.ThrowIfTypeImplementsMoreThanOneReducer(reducerType);
         TypeMustBeAdornedWithReducerAttribute.ThrowIfReducerAttributeMissing(reducerType);
-
-        /*
-        Validation:
-        + Must implement IReducerFor<T>
-        + Shouldn't allow for multiple implementations of the IReducer interface for different read models
-        + Must have a `[Reducer("id")]` attribute
-
-        - If type is in nullable context, check if reducer methods use nullable for state, if not - error
-        - Unique reducer ids
-        - String keys need to match property names on the event / context
-        - Composite keys need to match property names on the event
-        - Types need to match between the different key definitions (can't have mixed key types)
-        - Can't have multiple key definitions for the same event type
-        - Should only allow one reduce method per event type
-
-        Compile time checks:
-        - Event type must be a valid event type
-        - Check if read model type is nullable - if not, error
-        */
     }
 }
