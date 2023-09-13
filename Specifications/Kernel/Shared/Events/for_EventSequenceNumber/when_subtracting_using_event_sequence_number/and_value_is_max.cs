1da4c7ed2db51fbb18afe21595f61cce3173a4d2
Einar Ingebrigtsen <einari@me.com>
2023-09-13 13:22:40 +0200

    Fixing formatting

--- Specifications/Kernel/Shared/Events/for_EventSequenceNumber/when_subtracting_using_event_sequence_number/and_value_is_max.cs
+++ Specifications/Kernel/Shared/Events/for_EventSequenceNumber/when_subtracting_using_event_sequence_number/and_value_is_max.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 namespace Aksio.Cratis.Events.for_EventSequenceNumber.when_subtracting_using_event_sequence_number;
 
 public class and_value_is_max : Specification
