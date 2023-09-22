5b44932b37dff7eed2cc6c1c6b2335d843dac7f9
Einar Ingebrigtsen <einari@me.com>
2023-09-22 13:42:16 +0200

    Specs for PartitionFailed

---
+++ Specifications/Kernel/Grains/Observation/for_Observer/when_failing_partition/and_partition_is_already_failed.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+using Aksio.Cratis.Kernel.Observation;
+
+namespace Aksio.Cratis.Kernel.Grains.Observation.for_Observer.when_failing_partition;
+
+public class and_partition_is_already_failed : given.an_observer
+{
+    const string event_source_id = "Something";
+    const string first_stack_trace = "First: This is the stack trace";
+    const string first_message = "First: Something went wrong";
+    const string second_stack_trace = "Second: This is the stack trace";
+    const string second_message = "Second: Something went wrong";
+
+
+    FailedPartition failed_partition;
+
+    void Establish()
+    {
+        failed_partition = new()
+        {
+            Partition = event_source_id,
+            Attempts = new[]
+            {
+                new FailedPartitionAttempt
+                {
+                    Occurred = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(1)),
+                    Tail = 42UL,
+                    Messages = new[] { first_message },
+                    StackTrace = first_stack_trace
+                }
+            }
+        };
+        failed_partitions_state.Add(failed_partition);
+    }
+
+    async Task Because() => await observer.PartitionFailed(event_source_id, 44UL, new[] { second_message }, second_stack_trace);
+
+    [Fact] void should_have_only_one_failed_partition() => failed_partitions_state.Partitions.Count().ShouldEqual(1);
+    [Fact] void should_have_two_attempts() => failed_partitions_state.Partitions.First().Attempts.Count().ShouldEqual(2);
+    [Fact] void should_have_the_correct_partition() => failed_partitions_state.Partitions.First().Partition.ShouldEqual((EventSourceId)event_source_id);
+    [Fact] void should_have_the_correct_tail_for_the_first_attempt() => failed_partitions_state.Partitions.First().Attempts.First().Tail.ShouldEqual((EventSequenceNumber)42UL);
+    [Fact] void should_have_the_correct_message_for_the_first_attempt() => failed_partitions_state.Partitions.First().Attempts.First().Messages.First().ShouldEqual(first_message);
+    [Fact] void should_have_the_correct_stack_trace_for_the_first_attempt() => failed_partitions_state.Partitions.First().Attempts.First().StackTrace.ShouldEqual(first_stack_trace);
+    [Fact] void should_have_the_correct_tail_for_the_second_attempt() => failed_partitions_state.Partitions.First().Attempts.ToArray()[1].Tail.ShouldEqual((EventSequenceNumber)44UL);
+    [Fact] void should_have_the_correct_message_for_the_second_attempt() => failed_partitions_state.Partitions.First().Attempts.ToArray()[1].Messages.First().ShouldEqual(second_message);
+    [Fact] void should_have_the_correct_stack_trace_for_the_second_attempt() => failed_partitions_state.Partitions.First().Attempts.ToArray()[1].StackTrace.ShouldEqual(second_stack_trace);
+}
---
+++ Specifications/Kernel/Grains/Observation/for_Observer/when_failing_partition/first_time.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Kernel.Grains.Observation.for_Observer.when_failing_partition;
+
+public class first_time : given.an_observer
+{
+    const string event_source_id = "Something";
+    const string stack_trace = "This is the stack trace";
+    const string message = "Something went wrong";
+
+    async Task Because() => await observer.PartitionFailed(event_source_id, 42UL, new[] { message }, stack_trace);
+
+    [Fact] void should_have_only_one_failed_partition() => failed_partitions_state.Partitions.Count().ShouldEqual(1);
+    [Fact] void should_have_the_correct_partition() => failed_partitions_state.Partitions.First().Partition.ShouldEqual((EventSourceId)event_source_id);
+    [Fact] void should_have_the_correct_tail() => failed_partitions_state.Partitions.First().Attempts.First().Tail.ShouldEqual((EventSequenceNumber)42UL);
+    [Fact] void should_have_the_correct_message() => failed_partitions_state.Partitions.First().Attempts.First().Messages.First().ShouldEqual(message);
+    [Fact] void should_have_the_correct_stack_trace() => failed_partitions_state.Partitions.First().Attempts.First().StackTrace.ShouldEqual(stack_trace);
+}
