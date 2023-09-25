17a720d30016f2655c66f5120f00ec02887dd794
Einar Ingebrigtsen <einari@me.com>
2023-09-25 20:03:44 +0200

    Setting default read models storage for unspecified / single tenant scenario

--- Source/Kernel/Common/Configuration/Storage.cs
+++ Source/Kernel/Common/Configuration/Storage.cs
@@ -42,6 +42,7 @@ public class Storage
         {
             Shared = new StorageTypes
             {
+                ["readModels"] = Cluster,
                 ["eventStore"] = Cluster,
             },
             Tenants = new()
