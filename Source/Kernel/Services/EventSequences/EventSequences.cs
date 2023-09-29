c33bbff510f4f53336f2a7b186d57266b6d8cb9f
Einar Ingebrigtsen <einari@me.com>
2023-09-30 01:28:35 +0200

    Adding the JsonSerializerOptions to deserialization of Json content

--- Source/Kernel/Services/EventSequences/EventSequences.cs
+++ Source/Kernel/Services/EventSequences/EventSequences.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using Aksio.Cratis.EventSequences;
 using Aksio.Cratis.Kernel.Contracts.EventSequences;
@@ -15,29 +16,33 @@ public class EventSequences : IEventSequences
 {
     readonly IGrainFactory _grainFactory;
     readonly IExecutionContextManager _executionContextManager;
+    readonly JsonSerializerOptions _jsonSerializerOptions;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="EventSequences"/> class.
     /// </summary>
     /// <param name="grainFactory"><see cref="IGrainFactory"/> to get grains with.</param>
     /// <param name="executionContextManager">The <see cref="IExecutionContextManager"/> for working with the execution context.</param>
+    /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/> for serialization.</param>
     public EventSequences(
         IGrainFactory grainFactory,
-        IExecutionContextManager executionContextManager)
+        IExecutionContextManager executionContextManager,
+        JsonSerializerOptions jsonSerializerOptions)
     {
         _grainFactory = grainFactory;
         _executionContextManager = executionContextManager;
+        _jsonSerializerOptions = jsonSerializerOptions;
     }
 
     /// <inheritdoc/>
     public async Task<AppendResponse> Append(AppendRequest request)
     {
-        _executionContextManager.Establish(request.TenantId, _executionContextManager.Current.CorrelationId, request.MicroserviceId);
+        _executionContextManager.Establish(request.TenantId, CorrelationId.New(), request.MicroserviceId);
         var eventSequence = GetEventSequence(request.MicroserviceId, request.EventSequenceId, request.TenantId);
         await eventSequence.Append(
             request.EventSourceId,
-            request.EventType,
-            JsonObject.Parse(request.Content)!.AsObject(),
+            new(request.EventType.Id, request.EventType.Generation),
+            JsonSerializer.Deserialize<JsonNode>(request.Content, _jsonSerializerOptions)!.AsObject(),
             request.Causation.ToKernel(),
             request.Identity.ToKernel(),
             request.ValidFrom);
