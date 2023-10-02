837da75fb6d5b8782b66319c0801bce3f890768b
Einar Ingebrigtsen <einari@me.com>
2023-10-02 19:58:52 +0200

    Remove old

--- Samples/Basic/Program.cs
+++ Samples/Basic/Program.cs
@@ -13,27 +13,3 @@ await eventStore.EventLog.Append(
         PersonId: new(Guid.NewGuid()),
         MaterialId: new(Guid.NewGuid()),
         Quantity: 1));
-
-/*
-var builder = WebApplication.CreateBuilder(args);
-//builder.UseCratis();
-builder.UseCratis(_ => _
-    .ForMicroservice("cd51c091-3bba-4608-87a8-93da1f88c4dd", "Basic Sample")
-    .MultiTenanted());
-
-builder.Services.AddTransient<CartReducer>();
-var app = builder.Build();
-
-app.MapGet("/add", () =>
-{
-    var eventLog = app.Services.GetRequiredService<IEventLog>();
-    eventLog.Append("299681c4-f100-4dea-bfea-633115349ed1", new ItemAddedToCart(
-        new(Guid.NewGuid()),
-        new(Guid.NewGuid()),
-        1));
-});
-
-app.UseCratis();
-
-app.Run();
-*/
