6604646c13ffffc7991550f189577958cf565e0f
Einar Ingebrigtsen <einari@me.com>
2023-10-03 10:57:29 +0200

    Adding support for adding to a property with a type given

---
+++ Specifications/Infrastructure/Properties/for_PropertyPath/when_adding_a_property_to_an_empty_property_with_a_dictionary_type.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Cratis.Properties.for_PropertyPath;
+
+public class when_adding_a_property_to_an_empty_property_with_a_dictionary_type : Specification
+{
+    const string left = "";
+    const string right = "something";
+
+    PropertyPath result;
+
+    void Because() => result = new PropertyPath(left).AddProperty(right, typeof(Dictionary<string, string>));
+
+    [Fact] void should_hold_only_property_added_on() => result.Path.ShouldEqual(right);
+}
