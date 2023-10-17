3a1d60bcf4ea15a7456886a7058d0b516e1ef05b
Einar Ingebrigtsen <einari@me.com>
2023-10-17 19:49:46 +0200

    Setting LogLevel to Debug

--- Source/Kernel/MongoDB/Schemas/MongoDBSchemaStoreLogMessages.cs
+++ Source/Kernel/MongoDB/Schemas/MongoDBSchemaStoreLogMessages.cs
@@ -7,9 +7,9 @@ namespace Aksio.Cratis.Events.MongoDB.Schemas;
 
 internal static partial class MongoDBSchemaStoreLogMessages
 {
-    [LoggerMessage(1, LogLevel.Information, "Populating event schemas for microservice '{MicroserviceId}' and tenant '{TenantId}'")]
+    [LoggerMessage(1, LogLevel.Debug, "Populating event schemas for microservice '{MicroserviceId}' and tenant '{TenantId}'")]
     internal static partial void Populating(this ILogger<MongoDBSchemaStore> logger, MicroserviceId microserviceId, TenantId tenantId);
 
-    [LoggerMessage(2, LogLevel.Information, "Registering event schema for '{Name}' (Id: {EventType} - Generation: {Generation}) for microservice '{MicroserviceId}' and tenant '{TenantId}'")]
+    [LoggerMessage(2, LogLevel.Debug, "Registering event schema for '{Name}' (Id: {EventType} - Generation: {Generation}) for microservice '{MicroserviceId}' and tenant '{TenantId}'")]
     internal static partial void Registering(this ILogger<MongoDBSchemaStore> logger, string name, EventTypeId eventType, EventGeneration generation, MicroserviceId microserviceId, TenantId tenantId);
 }
