b1257f55b7e9e9784fd52adb132915dddb34328b
Einar Ingebrigtsen <einari@me.com>
2023-10-18 15:01:51 +0200

    Removing unused using statement

--- Specifications/Kernel/Grains/Observation/for_ObserverWorker/given/an_observer_worker.cs
+++ Specifications/Kernel/Grains/Observation/for_ObserverWorker/given/an_observer_worker.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Text.Json;
-using Aksio.Cratis.EventSequences;
 using Microsoft.Extensions.Logging;
 using Orleans.Runtime;
 
