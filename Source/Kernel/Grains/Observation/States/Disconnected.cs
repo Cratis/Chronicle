733556a65d38333114ab1347f3c1b6d6225f2277
Einar Ingebrigtsen <einari@me.com>
2023-10-20 12:55:59 +0200

    Allowing disconnected to transition to routing state

--- Source/Kernel/Grains/Observation/States/Disconnected.cs
+++ Source/Kernel/Grains/Observation/States/Disconnected.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Immutable;
 using Aksio.Cratis.Kernel.Orleans.StateMachines;
 using Aksio.Cratis.Observation;
 
@@ -12,10 +13,16 @@ namespace Aksio.Cratis.Kernel.Grains.Observation.States;
 public class Disconnected : BaseObserverState
 {
     /// <inheritdoc/>
+    public override StateName Name => "Disconnected";
+
+    /// <inheritdoc/>
     public override ObserverRunningState RunningState => ObserverRunningState.Disconnected;
 
     /// <inheritdoc/>
-    public override StateName Name => "Disconnected";
+    protected override IImmutableList<Type> AllowedTransitions => new[]
+    {
+        typeof(Routing)
+    }.ToImmutableList();
 
     /// <inheritdoc/>
     public override Task<ObserverState> OnEnter(ObserverState state) => Task.FromResult(state);
