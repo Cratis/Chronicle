0ea5f01a0a3db649cf067741605901d7daeda424
Stig Salte Tidemandsen <tidemann@gmail.com>
2023-10-23 11:00:03 +0200

    added tailwind

--- Source/Workbench/src/Layout/Layout.tsx
+++ Source/Workbench/src/Layout/Layout.tsx
@@ -2,26 +2,30 @@ import { ReactNode } from 'react';
 import { Footer } from './Footer';
 import { Topbar } from './Topbar/Topbar';
 import { Sidebar } from './Sidebar';
-
+import css from './Layout.module.css';
 export interface LayoutProps {
     children: ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
-
-    // Layout module css classes file
-
     return (
         <>
-            <div >
-                <Topbar />
-                <div >
-                    <Sidebar />
-                </div>
-                <div>
-                    <div>{children}</div>
-                    <Footer />
-                </div>
+            <div className={css.layoutContainer}>
+                <aside>
+                    <Sidebar/>
+                </aside>
+                <main>
+                    <header>
+                        <Topbar/>
+                    </header>
+                    <h1 className="text-3xl font-bold underline">
+                        Hello world!
+                    </h1>
+                    {children}
+                </main>
+                <footer>
+                    <Footer/>
+                </footer>
             </div>
         </>
     );
