91128a0d52f603a7b6cc74fa23eec7b9dc962301
Einar Ingebrigtsen <einari@me.com>
2023-11-07 09:11:23 +0100

    If debugger attached to kernel - don't disconnect clients

--- Source/Kernel/Grains/Clients/ConnectedClients.cs
+++ Source/Kernel/Grains/Clients/ConnectedClients.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using Aksio.Cratis.Connections;
 using Aksio.Cratis.Kernel.Orleans.Observers;
 using Microsoft.Extensions.Logging;
@@ -126,6 +127,8 @@ public class ConnectedClients : Grain, IConnectedClients
 
     async Task ReviseConnectedClients(object state)
     {
+        if (Debugger.IsAttached) return;
+
         foreach (var connectedClient in _clients.ToArray())
         {
             if (connectedClient.IsRunningWithDebugger) continue;
