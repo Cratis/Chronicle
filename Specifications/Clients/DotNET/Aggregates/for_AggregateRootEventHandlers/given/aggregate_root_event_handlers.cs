9dc37a4c95b9a09372bdf4e60754da5df936e1cb
Einar Ingebrigtsen <einari@me.com>
2023-11-14 08:50:28 +0100

    Fixing after simplification

--- Specifications/Clients/DotNET/Aggregates/for_AggregateRootEventHandlers/given/aggregate_root_event_handlers.cs
+++ Specifications/Clients/DotNET/Aggregates/for_AggregateRootEventHandlers/given/aggregate_root_event_handlers.cs
@@ -6,17 +6,12 @@ namespace Aksio.Cratis.Aggregates.for_AggregateRootEventHandlers.given;
 public class aggregate_root_event_handlers : Specification
 {
     protected AggregateRootEventHandlers handlers;
-    protected Mock<IEventTypes> event_types;
     protected StatelessAggregateRoot aggregate_root;
 
     void Establish()
     {
-        event_types = new();
-        event_types.Setup(_ => _.GetEventTypeFor(typeof(FirstEventType))).Returns(FirstEventType.EventTypeId);
-        event_types.Setup(_ => _.GetEventTypeFor(typeof(SecondEventType))).Returns(SecondEventType.EventTypeId);
-
         aggregate_root = new();
 
-        handlers = new AggregateRootEventHandlers(typeof(StatelessAggregateRoot), event_types.Object);
+        handlers = new AggregateRootEventHandlers(typeof(StatelessAggregateRoot));
     }
 }
