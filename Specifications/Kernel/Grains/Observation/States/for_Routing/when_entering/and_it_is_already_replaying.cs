673f21b136f7fffe1abc0331dd84d7501678a7b5
Einar Ingebrigtsen <einari@me.com>
2023-11-17 19:50:41 +0100

    Fixing spec after introduction of ResumeReplay as a state

--- Specifications/Kernel/Grains/Observation/States/for_Routing/when_entering/and_it_is_already_replaying.cs
+++ Specifications/Kernel/Grains/Observation/States/for_Routing/when_entering/and_it_is_already_replaying.cs
@@ -12,5 +12,5 @@ public class and_it_is_already_replaying : given.a_routing_state
     async Task Because() => resulting_stored_state = await state.OnEnter(stored_state);
 
     [Fact] void should_only_perform_one_transition() => observer.Verify(_ => _.TransitionTo<IState<ObserverState>>(), Once());
-    [Fact] void should_transition_to_replay() => observer.Verify(_ => _.TransitionTo<Replay>(), Once());
+    [Fact] void should_transition_to_replay() => observer.Verify(_ => _.TransitionTo<ResumeReplay>(), Once());
 }
