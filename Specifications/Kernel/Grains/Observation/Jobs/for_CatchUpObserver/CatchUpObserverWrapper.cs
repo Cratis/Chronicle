e571ad8739ceac7106093c73393252ab7d5378ed
Einar Ingebrigtsen <einari@me.com>
2023-11-22 12:33:02 +0100

    Specs for CanResume()

--- Specifications/Kernel/Grains/Observation/Jobs/for_CatchUpObserver/CatchUpObserverWrapper.cs
+++ Specifications/Kernel/Grains/Observation/Jobs/for_CatchUpObserver/CatchUpObserverWrapper.cs
@@ -12,4 +12,6 @@ public class CatchUpObserverWrapper : CatchUpObserver
     public CatchUpObserverWrapper(IObserverKeyIndexes observerKeyIndexes) : base(observerKeyIndexes) { }
 
     public Task<IImmutableList<JobStepDetails>> WrappedPrepareSteps(CatchUpObserverRequest request) => PrepareSteps(request);
+    public Task<bool> WrappedCanResume() => CanResume();
+    public Task WrappedOnStepCompleted(JobStepId jobStepId, JobStepResult result) => OnStepCompleted(jobStepId, result);
 }
