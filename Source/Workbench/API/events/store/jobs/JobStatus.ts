f54c94a2543b4927d33948a98c7660eb73dd5625
Einar Ingebrigtsen <einari@me.com>
2023-11-22 08:24:12 +0100

    Regenerated

--- Source/Workbench/API/events/store/jobs/JobStatus.ts
+++ Source/Workbench/API/events/store/jobs/JobStatus.ts
@@ -2,7 +2,6 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
-<<<<<<<< HEAD:Source/Workbench/API/events/store/jobs/JobStatus.ts
 export enum JobStatus {
     none = 0,
     preparing = 1,
@@ -12,17 +11,4 @@ export enum JobStatus {
     completedWithFailures = 5,
     paused = 6,
     stopped = 7,
-========
-import { field } from '@aksio/fundamentals';
-
-import { EventType } from './EventType';
-
-export class EventToApply {
-
-    @field(EventType)
-    eventType!: EventType;
-
-    @field(Object)
-    content!: any;
->>>>>>>> main:Source/Workbench/API/events/store/projections/EventToApply.ts
 }
--- Source/Workbench/API/events/store/projections/EventToApply.ts
+++ Source/Workbench/API/events/store/projections/EventToApply.ts
@@ -2,17 +2,6 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
-<<<<<<<< HEAD:Source/Workbench/API/events/store/jobs/JobStatus.ts
-export enum JobStatus {
-    none = 0,
-    preparing = 1,
-    preparingSteps = 2,
-    running = 3,
-    completedSuccessfully = 4,
-    completedWithFailures = 5,
-    paused = 6,
-    stopped = 7,
-========
 import { field } from '@aksio/fundamentals';
 
 import { EventType } from './EventType';
@@ -24,5 +13,4 @@ export class EventToApply {
 
     @field(Object)
     content!: any;
->>>>>>>> main:Source/Workbench/API/events/store/projections/EventToApply.ts
 }
