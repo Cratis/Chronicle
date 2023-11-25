db79104875a346a31e8c76878694f1d67a1ec82c
Einar Ingebrigtsen <einari@me.com>
2023-11-25 13:58:47 +0100

    Basic hookup of showing observers

--- Source/Workbench/src/Components/Pages/EventStore/Observers/Observers.tsx
+++ Source/Workbench/src/Components/Pages/EventStore/Observers/Observers.tsx
@@ -1,7 +1,27 @@
-export const Observers = () => {
-    return <div>
-        <h1>
-            Observers
-        </h1>
-    </div>
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { withViewModel } from 'MVVM';
+import { ObserversViewModel } from './ObserversViewModel';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
+export interface IObserversProps {
+    something: string;
 }
+
+export const Observers = withViewModel<ObserversViewModel, IObserversProps>(ObserversViewModel, ({ viewModel, props }) => {
+    return (
+        <div className='p-4'>
+            <h1 className='text-3xl m-3'> Observers</h1>
+
+            <DataTable value={viewModel.observers}>
+                <Column field="observerId" header="Id" sortable/>
+                <Column field="name" header="Name" sortable/>
+                <Column field="type" header="ObserverType" sortable/>
+                <Column field="nextEventSequenceNumber" header="Next event sequence number" sortable/>
+                <Column field="runningState" header="State" sortable/>
+            </DataTable>
+        </div>
+    )
+});
