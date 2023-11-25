38d0fecce5446eea6b299a0236dd2a256b014c42
Einar Ingebrigtsen <einari@me.com>
2023-11-25 13:56:30 +0100

    Taking out test code

--- Source/Workbench/src/TestData/Test.tsx
+++
@@ -1,8 +0,0 @@
-import { useLoaderData } from 'react-router-dom';
-import { DefaultLayout } from '../Layout/Default/DefaultLayout';
-
-export function Test() {
-    const data = useLoaderData();
-
-    return <DefaultLayout> {JSON.stringify(data)}</DefaultLayout>;
-}
