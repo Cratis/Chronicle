a370862f1fc51840137382acc0433d73db1ccdd8
Einar Ingebrigtsen <einari@me.com>
2023-11-30 10:42:06 +0100

    Showing test capabilities

--- Specifications/Integration/Kernel/Test.cs
+++ Specifications/Integration/Kernel/Test.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -10,10 +11,10 @@ namespace Aksio.Cratis.Kernel;
 [Collection(GlobalCollection.Name)]
 public class Test : KernelTest
 {
+    int _eventLogCount;
+
     public Test(KernelFixture fixture) : base(fixture)
     {
-        // kernelFixture.EventStore.Database.GetCollection<BsonDocument>("event-log").Find(_ => true).ToList();
-
         fixture.Cluster.Changes
             .Subscribe(_ => Console.WriteLine("Got cluster change"));
 
@@ -21,20 +22,40 @@ public class Test : KernelTest
             .Subscribe(_ => Console.WriteLine("Got shared change"));
 
         fixture.EventStore.Changes
-            .Subscribe(_ => Console.WriteLine("Got event store change"));
+            .Subscribe(_ => Console.WriteLine($"Got event store change ({_.CollectionNamespace.CollectionName}) - {_.OperationType}"));
 
         fixture.SharedEventStore.Changes
              .Where(_ => _.OperationType == ChangeStreamOperationType.Insert)
-             .Subscribe(_ => Console.WriteLine("Got insert"));
+             .Subscribe(_ => Console.WriteLine($"Got insert into {_.CollectionNamespace.CollectionName}"));
+
+        var eventLogInserts = fixture.EventStore.Changes
+            .Where(_ =>
+                _.OperationType == ChangeStreamOperationType.Insert &&
+                _.CollectionNamespace.CollectionName == "event-log");
+
+        eventLogInserts.Subscribe(_ => Console.WriteLine("Got an insert into the event log"));
+        var eventLogCount = eventLogInserts.Count();
+        eventLogCount.Subscribe(_ =>
+        {
+            _eventLogCount = _;
+            Console.WriteLine($"Got event log count: {_}");
+        });
 
         var container = new TestClient(fixture, "Basic");
         container.Start().GetAwaiter().GetResult();
     }
 
     [Fact]
-    async Task DoStuff()
+    async Task should_have_one_event_inserted_in_event_log()
     {
-        // await Task.Delay(60000);
-        Assert.True(true);
+        //await Task.Delay(60000);
+        await Task.Delay(1000);
+
+        KernelFixture.EventStore.Complete();
+
+        _eventLogCount.ShouldEqual(1);
+
+        var count = await KernelFixture.EventStore.Database.GetCollection<BsonDocument>("event-log").CountDocumentsAsync(_ => true);
+        count.ShouldEqual(1);
     }
 }
