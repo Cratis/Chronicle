42d656ddc5d500448744068b504cef42877a4170
Einar Ingebrigtsen <einari@me.com>
2023-12-11 19:22:38 +0100

    Moving general type extension methods to the TypeExtensions

--- Source/Clients/DotNET/Reducers/ReducerExtensionMethods.cs
+++ Source/Clients/DotNET/Reducers/ReducerExtensionMethods.cs
@@ -4,6 +4,7 @@
 using System.Reflection;
 using Aksio.Cratis.Events;
 using Aksio.Cratis.Reducers.Validators;
+using Aksio.Cratis.Reflection;
 
 namespace Aksio.Cratis.Reducers;
 
@@ -72,20 +73,4 @@ public static class ReducerExtensionMethods
         var reducerInterface = interfaces.Single(_ => _.IsGenericType && _.GetGenericTypeDefinition() == typeof(IReducerFor<>));
         return reducerInterface.GetGenericArguments()[0];
     }
-
-    /// <summary>
-    /// Check whether or not a type is in a nullable context.
-    /// </summary>
-    /// <param name="type">Type to check.</param>
-    /// <returns>True if it is in a nullable context, false if not.</returns>
-    public static bool IsNullableContext(this Type type) =>
-        type.GetCustomAttributes().Any(_ => _.GetType().FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
-
-    /// <summary>
-    /// Check whether or not a member that implements <see cref="ICustomAttributeProvider"/> is a nullable reference type.
-    /// </summary>
-    /// <param name="member">Member that implements <see cref="ICustomAttributeProvider"/> to check.</param>
-    /// <returns>True if it nullable, false if not.</returns>
-    public static bool IsNullableReferenceType(this ICustomAttributeProvider member) =>
-        member.GetCustomAttributes(false).Any(_ => _.GetType().FullName == "System.Runtime.CompilerServices.NullableAttribute");
 }
--- Source/Infrastructure/Reflection/TypeExtensions.cs
+++ Source/Infrastructure/Reflection/TypeExtensions.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Reflection;
+
 namespace Aksio.Cratis.Reflection;
 
 /// <summary>
@@ -17,4 +19,20 @@ public static class TypeExtensions
         (type.Namespace?.StartsWith("System") ?? false) ?
             (type.FullName ?? type.Name) :
             $"{type.FullName ?? type.Name}, {type.Assembly.GetName().Name}";
+
+    /// <summary>
+    /// Check whether or not a type is in a nullable context.
+    /// </summary>
+    /// <param name="type">Type to check.</param>
+    /// <returns>True if it is in a nullable context, false if not.</returns>
+    public static bool IsNullableContext(this Type type) =>
+        type.GetCustomAttributes().Any(_ => _.GetType().FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
+
+    /// <summary>
+    /// Check whether or not a member that implements <see cref="ICustomAttributeProvider"/> is a nullable reference type.
+    /// </summary>
+    /// <param name="member">Member that implements <see cref="ICustomAttributeProvider"/> to check.</param>
+    /// <returns>True if it nullable, false if not.</returns>
+    public static bool IsNullableReferenceType(this ICustomAttributeProvider member) =>
+        member.GetCustomAttributes(false).Any(_ => _.GetType().FullName == "System.Runtime.CompilerServices.NullableAttribute");
 }
