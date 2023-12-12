3757d336c336c8ea01a5b74d0c5c97ac58f06d1c
Einar Ingebrigtsen <einari@me.com>
2023-12-12 16:27:08 +0100

    Making sure we at least have 1 level of parallelism

--- Source/Kernel/Server/CpuBoundWorkersExtensions.cs
+++ Source/Kernel/Server/CpuBoundWorkersExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Aksio.Cratis.Kernel.Grains.Workers;
+using Serilog;
 
 #pragma warning disable SA1600
 namespace Aksio.Cratis.Kernel.Server;
@@ -18,11 +19,16 @@ public static class CpuBoundWorkersExtensions
     /// <returns><see cref="ISiloBuilder"/> for continuation.</returns>
     public static IHostBuilder ConfigureCpuBoundWorkers(this IHostBuilder builder)
     {
+        Log.Logger.Information("Configuring Cpu bound workers");
+
         var maxLevelOfParallelism = Environment.ProcessorCount - 2;
-        if (maxLevelOfParallelism < 0)
+        if (maxLevelOfParallelism <= 0)
         {
             maxLevelOfParallelism = 1;
         }
+
+        Log.Logger.Information("Max level of parallelism for Cpu bound workers: {MaxLevelOfParallelism} - number of processor count {ProcessorCount}", maxLevelOfParallelism, Environment.ProcessorCount);
+
         builder.ConfigureServices((services) => services.AddSingleton(new LimitedConcurrencyLevelTaskScheduler(maxLevelOfParallelism)));
 
         return builder;
