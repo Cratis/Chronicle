b05b7f10bc9ef06fc2f434ee0e3689a405306565
Einar Ingebrigtsen <einari@me.com>
2024-01-06 10:27:49 +0100

    Getting rid of Configuration grain - not needed

--- Source/Kernel/Read/Configuration/Microservices.cs
+++ Source/Kernel/Read/Configuration/Microservices.cs
@@ -3,7 +3,6 @@
 
 using Aksio.Cratis.Configuration;
 using Aksio.Cratis.Kernel.Configuration;
-using Aksio.Cratis.Kernel.Grains.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using ApiMicroservice = Aksio.Cratis.Kernel.Read.Configuration.Microservices.Microservice;
 
@@ -16,23 +15,14 @@ namespace Aksio.Cratis.Kernel.Read.Configuration.Microservices;
 public class Microservices : ControllerBase
 {
     readonly KernelConfiguration _configuration;
-    readonly IGrainFactory _grainFactory;
-    readonly IExecutionContextManager _executionContextManager;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Microservices"/> class.
     /// </summary>
     /// <param name="configuration">The <see cref="KernelConfiguration"/>.</param>
-    /// <param name="grainFactory">Orleans <see cref="IGrainFactory"/>.</param>
-    /// <param name="executionContextManager">The <see cref="IExecutionContextManager"/> for working with the execution context.</param>
-    public Microservices(
-        KernelConfiguration configuration,
-        IGrainFactory grainFactory,
-        IExecutionContextManager executionContextManager)
+    public Microservices(KernelConfiguration configuration)
     {
         _configuration = configuration;
-        _grainFactory = grainFactory;
-        _executionContextManager = executionContextManager;
     }
 
     /// <summary>
@@ -48,10 +38,6 @@ public class Microservices : ControllerBase
     /// <param name="microserviceId"><see cref="MicroserviceId"/> for the microservice.</param>
     /// <returns>The <see cref="StorageForMicroservice"/>.</returns>
     [HttpGet("{microserviceId}/storage")]
-    public async Task<StorageForMicroservice> StorageConfigurationForMicroservice([FromRoute] MicroserviceId microserviceId)
-    {
-        _executionContextManager.Establish(microserviceId);
-        var grain = _grainFactory.GetGrain<IConfiguration>(Guid.Empty);
-        return await grain.GetStorage();
-    }
+    public Task<StorageForMicroservice> StorageConfigurationForMicroservice([FromRoute] MicroserviceId microserviceId) =>
+        Task.FromResult(_configuration.Storage.Microservices.Get(microserviceId));
 }
