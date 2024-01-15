d5412d14da2be630463a1a3ebfb599531671ab3e
Ahmed Waqar <ahmedwaqar@aksio.no>
2024-01-15 10:47:37 +0100

    confirm popup

--- Source/Workbench/src/Components/Pages/EventStore/Tenant/Sequences/Queries/Bookmark/components/EditableFolder.tsx
+++ Source/Workbench/src/Components/Pages/EventStore/Tenant/Sequences/Queries/Bookmark/components/EditableFolder.tsx
@@ -1,11 +1,12 @@
 /* Copyright (c) Aksio Insurtech. All rights reserved.
    Licensed under the MIT license. See LICENSE file in the project root for full license information. */
 
+import { ConfirmPopup } from 'primereact/confirmpopup';
 import { InputText } from 'primereact/inputtext';
 import { IBookmarkNode } from '../TestData';
 import { Button } from 'primereact/button';
 import css from '../Bookmark.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface EditableFolderProps {
     node: IBookmarkNode;
@@ -36,6 +37,10 @@ export const EditableFolder = (props: EditableFolderProps) => {
         handleInputChange,
         handleInputKeyDown,
     } = props;
+
+    const [isConfirmVisible, setIsConfirmVisible] = useState(false);
+    const [nodeToDelete, setNodeToDelete] = useState<string>();
+
     return (
         <>
             {editingNodeKey === node.key ? (
@@ -63,11 +68,29 @@ export const EditableFolder = (props: EditableFolderProps) => {
                             unstyled
                             icon='pi pi-trash'
                             className={css.deleteButton}
-                            onClick={() => deleteNode(node.key)}
+                            id={`delete-btn-${node.key}`}
+                            onClick={(e) => {
+                                setNodeToDelete(node.key);
+                                setIsConfirmVisible(true);
+                                e.persist(); // Needed to keep the event around for the ConfirmPopup
+                            }}
                         />
                     )}
                 </div>
             )}
+            <ConfirmPopup
+                target={
+                    document.getElementById(`delete-btn-${nodeToDelete}`) || undefined
+                }
+                visible={isConfirmVisible}
+                onHide={() => setIsConfirmVisible(false)}
+                message='Are you sure you want to delete this item?'
+                accept={() => {
+                    deleteNode(nodeToDelete!);
+                    setIsConfirmVisible(false);
+                }}
+                reject={() => setIsConfirmVisible(false)}
+            />
         </>
     );
 };
