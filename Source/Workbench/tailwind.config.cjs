31e1150660514823b4a5b22e69417a04b5008041
Einar Ingebrigtsen <einari@me.com>
2024-02-18 18:15:29 +0100

    Fixing startup time by reducing files for Tailwind

--- Source/Workbench/tailwind.config.cjs
+++ Source/Workbench/tailwind.config.cjs
@@ -6,8 +6,10 @@ module.exports = {
     content: {
         files: [
             "./index.html",
-            "./**/*.{js,ts,jsx,tsx}",
-            "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+            "./Components/**/*.tsx",
+            "./Features/**/*.tsx",
+            "./Layout/**/*.tsx",
+            "./node_modules/primereact/**/*.{js,ts,jsx,tsx}"
         ]
     },
     theme: {
