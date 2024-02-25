58c4646f6289bc72234cfdab5f213ea90e00ea74
Einar Ingebrigtsen <einari@me.com>
2024-02-25 11:01:47 +0100

    Start of making it possible to select event store easily

--- Source/Workbench/Layout/Default/TopBar/TopBar.tsx
+++ Source/Workbench/Layout/Default/TopBar/TopBar.tsx
@@ -4,13 +4,23 @@
 import { useLayoutContext } from '../context/LayoutContext';
 import { Button } from 'primereact/button';
 import css from './TopBar.module.css';
-import { FaBars } from 'react-icons/fa6';
-import { Logo } from "./Logo";
+import { FaBars, FaDatabase } from 'react-icons/fa6';
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { Profile } from "./Profile";
 import { Notifications } from './Notifications';
 import { Connection } from './Connection';
+import { useParams } from 'react-router-dom';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { useRef } from 'react';
+
+type Params = {
+    eventStoreId: string;
+};
+
 
 export const TopBar = () => {
+    const params = useParams() as Params;
+    const selectEventStorePanel = useRef<OverlayPanel>(null);
     const { toggleLeftSidebarOpen } = useLayoutContext();
 
     return (
@@ -19,24 +29,33 @@ export const TopBar = () => {
                 <div className={css.sidebarToggle}>
                     <Button
                         onClick={toggleLeftSidebarOpen}
-                        className='p-button-rounded p-button-text p-2'
-                    >
-                        <FaBars/>
+                        text
+                        rounded
+                        className='p-2'>
+                        <FaBars />
                     </Button>
                 </div>
                 <div className={'flex-1 flex align-center justify-center'}>
-                    <Logo/>
+                    <div className="flex mx-24 h-16 justify-evenly items-center cursor-pointer" onClick={(e) => selectEventStorePanel.current?.toggle(e)}>
+
+                        <FaDatabase className='text-2xl' />
+                        <div className='font-extrabold text-2xl m-2'>{params.eventStoreId}</div>
+                        <MdKeyboardArrowDown className='text-2xl' />
+                    </div>
                 </div>
+
+                <OverlayPanel ref={selectEventStorePanel}>
+                </OverlayPanel>
             </div>
             <div className="flex-1 flex items-center justify-end px-5 gap-6">
                 <div>
-                    <Connection/>
+                    <Connection />
                 </div>
                 <div>
-                    <Notifications/>
+                    <Notifications />
                 </div>
                 <div>
-                    <Profile/>
+                    <Profile />
                 </div>
             </div>
         </div>
