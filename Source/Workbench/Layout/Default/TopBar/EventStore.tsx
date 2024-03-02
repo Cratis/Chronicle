aeada535334ebaebc73660af52e42bbfcb227262
Einar Ingebrigtsen <einari@me.com>
2024-03-02 18:43:39 +0100

    Adding items list

--- Source/Workbench/Layout/Default/TopBar/EventStore.tsx
+++ Source/Workbench/Layout/Default/TopBar/EventStore.tsx
@@ -4,14 +4,19 @@
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { useRef } from 'react';
 import { ImDatabase } from "react-icons/im";
+import { ItemsList } from 'Components/ItemsList/ItemsList';
 
 export const EventStore = () => {
     const selectEventStorePanel = useRef<OverlayPanel>(null);
+
+    const eventStores = ['EventStore1', 'EventStore2', 'EventStore3'];
+
     return (<div className='flex-1 cursor-pointer' onClick={(e) => selectEventStorePanel.current?.toggle(e)}>
         <div className={'flex justify-end gap-3 '}>
             <ImDatabase size={25} />
 
             <OverlayPanel ref={selectEventStorePanel}>
+                <ItemsList<string> items={eventStores}/>
             </OverlayPanel>
 
         </div>
