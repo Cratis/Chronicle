6a64bb58cc97afad21874122549baa2d97e6ac80
Einar Ingebrigtsen <einari@me.com>
2024-04-28 19:22:57 +0200

    Started the transition to Vue

--- Source/Workbench/Strings.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import * as  translationEN from './Locales/en/translation.json';
-import * as  translationNO from './Locales/no/translation.json';
-
-export default translationEN;
