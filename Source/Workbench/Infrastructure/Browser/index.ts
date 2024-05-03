74572bcad01679f29d368625adaaa5b50d51eb48
Einar Ingebrigtsen <einari@me.com>
2024-05-03 09:51:08 +0200

    Making Browser a module

---
+++ Source/Workbench/Infrastructure/Browser/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export * from './INavigation';
+export * from './Navigation';
+export * from './ILocalStorage';
