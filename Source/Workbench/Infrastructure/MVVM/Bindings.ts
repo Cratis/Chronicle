8f7ae042449fb3ec8fd730f1e32e2ada40f7d8a5
Einar Ingebrigtsen <einari@me.com>
2024-05-03 09:51:29 +0200

    Hooking up browser bindings

--- Source/Workbench/Infrastructure/MVVM/Bindings.ts
+++ Source/Workbench/Infrastructure/MVVM/Bindings.ts
@@ -5,9 +5,12 @@ import { container } from 'tsyringe';
 import { Messenger } from './Messenger';
 import { Constructor } from 'Infrastructure';
 import { IMessenger } from './IMessenger';
+import { ILocalStorage, INavigation, Navigation } from 'Browser';
 
 export class Bindings {
     static initialize() {
         container.registerSingleton(IMessenger as Constructor<IMessenger>, Messenger);
+        container.registerSingleton(INavigation as Constructor<INavigation>, Navigation);
+        container.registerInstance(ILocalStorage as Constructor<ILocalStorage>, localStorage);
     }
 }
