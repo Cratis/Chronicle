c8e41387f7822a2168ceb9d74b514f62cc96ad19
Einar Ingebrigtsen <einari@me.com>
2024-06-23 09:10:56 +0200

    Remove config that is not needed

--- Samples/Orleans/Program.cs
+++ Samples/Orleans/Program.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Chronicle.Configuration;
 using Cratis.Json;
 using Cratis.MongoDB;
 
@@ -11,8 +10,6 @@ builder.UseMongoDB(_ => _
     .WithStaticServer("mongodb://localhost:27017")
     .WithStaticDatabaseName("orleans"));
 
-builder.Services.AddSingleton<IMongoDBClientFactory, MongoDBClientFactory>();
-builder.Services.AddSingleton(new Storage { Type = "mongodb" });
 builder.Host.UseDefaultServiceProvider(_ => _.ValidateOnBuild = false);
 
 builder.Services.AddSingleton(Globals.JsonSerializerOptions);
