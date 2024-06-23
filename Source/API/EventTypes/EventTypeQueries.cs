2704d07847930f6165f2ab2f0ad4a2f94c048450
Einar Ingebrigtsen <einari@me.com>
2024-06-23 08:07:11 +0200

    Adding missing using statement

--- Source/API/EventTypes/EventTypeQueries.cs
+++ Source/API/EventTypes/EventTypeQueries.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Text.Json;
+using Cratis.Chronicle;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cratis.API.EventTypes;
