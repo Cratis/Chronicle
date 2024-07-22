5504e1f6162b76bdd78a7f1286484a7af3dade75
Einar Ingebrigtsen <einari@me.com>
2024-07-22 09:21:11 +0200

    Enabling projection

--- Samples/Orleans/MyProjection.cs
+++ Samples/Orleans/MyProjection.cs
@@ -2,9 +2,9 @@ using Cratis.Chronicle.Projections;
 
 namespace Orleans;
 
-// public class MyProjection : IProjectionFor<MyModel>
-// {
-//     public void Define(IProjectionBuilderFor<MyModel> builder) => builder
-//         .From<MyFirstEvent>(_ => _
-//             .Set(m => m.Name).To(e => e.Name));
-// }
+public class MyProjection : IProjectionFor<MyModel>
+{
+    public void Define(IProjectionBuilderFor<MyModel> builder) => builder
+        .From<MyFirstEvent>(_ => _
+            .Set(m => m.Name).To(e => e.Name));
+}
