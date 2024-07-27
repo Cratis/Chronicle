e027c60dc62b9aa7040db5b6baf111d6095b4f08
Einar Ingebrigtsen <einari@me.com>
2024-07-27 11:18:06 +0200

    Adding support for regular event handlers on aggregate as well

--- Samples/Orleans/MyAggregateRoot.cs
+++ Samples/Orleans/MyAggregateRoot.cs
@@ -2,10 +2,15 @@ using Cratis.Chronicle.Orleans.Aggregates;
 
 namespace Orleans;
 
-public class MyAggregateRoot : AggregateRoot<MyAggregateRootState>, IMyAggregateRoot
+public class MyAggregateRoot : AggregateRoot, IMyAggregateRoot
 {
     public async Task DoStuff()
     {
         await Apply(new MyFirstEvent("Blah"));
+    }
+
+    public void On(MyFirstEvent @event)
+    {
+        Console.WriteLine($"Got an event with content '{@event}'");
     }
 }
--- Source/Clients/Orleans/Aggregates/AggregateRoot.cs
+++ Source/Clients/Orleans/Aggregates/AggregateRoot.cs
@@ -4,6 +4,7 @@
 using System.Collections.Immutable;
 using Cratis.Chronicle.Aggregates;
 using Cratis.Chronicle.Auditing;
+using Cratis.Chronicle.Events;
 using Cratis.Chronicle.EventSequences;
 using Cratis.Execution;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,12 +18,58 @@ namespace Cratis.Chronicle.Orleans.Aggregates;
 /// </summary>
 public class AggregateRoot : Grain, IAggregateRoot
 {
-    /// <inheritdoc/>
-    public Task Apply<T>(T @event)
-        where T : class => throw new NotImplementedException();
+    StatelessAggregateRootMutator? _mutator;
+    IAggregateRootContext? _context;
+    AggregateRootMutation? _mutation;
 
     /// <inheritdoc/>
-    Task<AggregateRootCommitResult> Chronicle.Aggregates.IAggregateRoot.Commit() => throw new NotImplementedException();
+    public override async Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        var eventStore = ServiceProvider.GetRequiredService<IEventStore>();
+        var eventLog = ServiceProvider.GetRequiredService<IEventLog>();
+        var eventSerializer = ServiceProvider.GetRequiredService<IEventSerializer>();
+
+        // TODO: Fix CorrelationId to be a real value from the current context
+        _context = new AggregateRootContext(
+            CorrelationId.New(),
+            this.GetPrimaryKeyString(),
+            eventLog,
+            this,
+            true);
+
+        var eventHandlersFactory = ServiceProvider.GetRequiredService<IAggregateRootEventHandlersFactory>();
+        var eventHandlers = eventHandlersFactory.GetFor(this);
+
+        _mutator = new StatelessAggregateRootMutator(
+            _context,
+            eventStore,
+            eventSerializer,
+            eventHandlers);
+        _mutation = new AggregateRootMutation(_context, _mutator, eventLog, ServiceProvider.GetRequiredService<ICausationManager>());
+
+        await _mutator.Rehydrate();
+
+        await OnActivate();
+    }
+
+    /// <inheritdoc/>
+    public Task Apply<T>(T @event)
+        where T : class
+    {
+        return _mutation?.Apply(@event) ?? Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public Task<AggregateRootCommitResult> Commit()
+    {
+        return _mutation?.Commit() ?? Task.FromResult(AggregateRootCommitResult.Failed(ImmutableList<object>.Empty));
+    }
+
+    /// <summary>
+    /// Called when the aggregate root is ready to be activated.
+    /// </summary>
+    /// <returns>Awaitable task.</returns>
+    protected virtual Task OnActivate() => Task.CompletedTask;
 }
 
 /// <summary>
