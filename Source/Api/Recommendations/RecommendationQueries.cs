ed9f51efea65ac4cff122d36d2129873fc6dd898
Einar Ingebrigtsen <einari@me.com>
2024-07-29 11:16:19 +0200

    "Fixing" API namespace based on proxy generator constraints

---
+++ Source/Api/Recommendations/RecommendationQueries.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reactive.Subjects;
+using Cratis.Chronicle;
+using Cratis.Chronicle.Recommendations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cratis.Api.Recommendations;
+
+/// <summary>
+/// Represents the API for working with recommendations.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="RecommendationQueries"/> class.
+/// </remarks>
+[Route("/api/events/store/{eventStore}/{namespace}/recommendations")]
+public class RecommendationQueries() : ControllerBase
+{
+    /// <summary>
+    /// Get all observers for an event store and namespace.
+    /// </summary>
+    /// <param name="eventStore"><see cref="EventStoreName"/> the recommendations are for.</param>
+    /// <param name="namespace"><see cref="EventStoreNamespaceName"/> the recommendations are for.</param>
+    /// <returns>Collection of <see cref="RecommendationInformation"/>.</returns>
+    [HttpGet]
+    public Task<IEnumerable<RecommendationInformation>> GetRecommendations(
+        [FromRoute] EventStoreName eventStore,
+        [FromRoute] EventStoreNamespaceName @namespace)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Get and observe all observers for an event store and namespace.
+    /// </summary>
+    /// <param name="eventStore"><see cref="EventStoreName"/> the recommendations are for.</param>
+    /// <param name="namespace"><see cref="EventStoreNamespaceName"/> the recommendations are for.</param>
+    /// <returns>An observable of a collection of <see cref="RecommendationInformation"/>.</returns>
+    [HttpGet("observe")]
+    public ISubject<IEnumerable<RecommendationInformation>> AllRecommendations(
+        [FromRoute] EventStoreName eventStore,
+        [FromRoute] EventStoreNamespaceName @namespace)
+    {
+        throw new NotImplementedException();
+    }
+}
