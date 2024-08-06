db4d2896d5ef5655a9fb5ade6cf93e1f6ee064ed
Einar Ingebrigtsen <einari@me.com>
2024-08-06 15:27:07 +0200

    Adding specs for attribute extensions

---
+++ Source/Clients/DotNET.Specs/Events/Constraints/for_UniqueAttributeExtensions/when_getting_constraint_message/and_attribute_does_not_have_message.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Events.Constraints.for_UniqueAttributeExtensions.when_getting_constraint_message;
+
+public class and_attribute_does_not_have_message : Specification
+{
+    string result;
+
+    void Because() => result = typeof(SomeType).GetConstraintMessage();
+
+    [Fact] void should_return_empty_message() => result.ShouldEqual(string.Empty);
+
+    [Unique]
+    record SomeType();
+}
---
+++ Source/Clients/DotNET.Specs/Events/Constraints/for_UniqueAttributeExtensions/when_getting_constraint_message/and_attribute_has_message.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Events.Constraints.for_UniqueAttributeExtensions.when_getting_constraint_message;
+
+public class and_attribute_has_message : Specification
+{
+    const string Message = "TheMessage";
+    string result;
+
+    void Because() => result = typeof(SomeType).GetConstraintMessage();
+
+    [Fact] void should_return_message() => result.ShouldEqual(Message);
+
+    [Unique(message: Message)]
+    record SomeType();
+}
