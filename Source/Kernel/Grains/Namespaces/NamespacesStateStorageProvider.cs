49f93ae0c78418634d5da752077e7ac1d0ba767c
Einar Ingebrigtsen <einari@me.com>
2024-08-13 19:37:34 +0200

    Fixing so that we always have the Default namespace in an event store

--- Source/Kernel/Grains/Namespaces/NamespacesStateStorageProvider.cs
+++ Source/Kernel/Grains/Namespaces/NamespacesStateStorageProvider.cs
@@ -27,8 +27,9 @@ public class NamespacesStateStorageProvider(IStorage storage) : IGrainStorage
         var namespaces = (await eventStore.Namespaces.GetAll()).Select(_ => new NamespaceState(_.Name, _.Created)).ToList();
         if (namespaces.Find(_ => _.Name == EventStoreNamespaceName.Default) == default)
         {
-            namespaces.Add(new NamespaceState(EventStoreNamespaceName.Default, DateTimeOffset.UtcNow));
-            await WriteStateAsync(stateName, grainId, grainState);
+            var @namespace = new NamespaceState(EventStoreNamespaceName.Default, DateTimeOffset.UtcNow);
+            namespaces.Add(@namespace);
+            await eventStore.Namespaces.Create(@namespace.Name, @namespace.Created);
         }
 
         actualGrainState.State = new NamespacesState
