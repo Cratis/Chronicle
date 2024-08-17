e20d97028f74a39dc66da820429c59801e774279
Einar Ingebrigtsen <einari@me.com>
2024-08-17 22:41:47 +0200

    Fixing test

--- Integration/Orleans.InProcess/for_EventSequence/when_appending/existing_sequence_number.cs
+++ Integration/Orleans.InProcess/for_EventSequence/when_appending/existing_sequence_number.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Concepts.Events;
+using Cratis.Chronicle.Events;
 using Cratis.Chronicle.Integration.Base;
 using Cratis.Chronicle.Storage.MongoDB;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Cratis.Chronicle.Integration.Orleans.InProcess.for_EventSequence.when_appending;
@@ -14,8 +16,8 @@ public class existing_sequence_number(existing_sequence_number.context context) 
     public class context(GlobalFixture globalFixture) : IntegrationSpecificationContext(globalFixture)
     {
         public Events.EventSourceId EventSourceId { get; } = "source";
-        public SomeEvent FirstEvent { get; private set; }
-        public SomeEvent SecondEvent { get; private set; }
+        public SomeEvent FirstEvent;
+        public SomeEvent SecondEvent;
 
         public override IEnumerable<Type> EventTypes => [typeof(SomeEvent)];
 
@@ -23,23 +25,26 @@ public class existing_sequence_number(existing_sequence_number.context context) 
         {
             FirstEvent = new SomeEvent("some value");
             SecondEvent = new SomeEvent("some other value");
-            await EventStore.EventLog.Append(EventSourceId, FirstEvent);
+            var database = EventStoreForNamespaceDatabase.Database;
+            var @event = new Event(
+                0,
+                CorrelationId.New(),
+                [],
+                [],
+                typeof(SomeEvent).GetEventType().Id.Value,
+                DateTimeOffset.UtcNow,
+                EventSourceId.Value,
+                new Dictionary<string, BsonDocument>() { { "1", FirstEvent.ToBsonDocument() } },
+                []);
+
+            await database.GetCollection<Event>(WellKnownCollectionNames.EventLog).InsertOneAsync(@event);
         }
 
-        async Task Because()
-        {
-            var database = EventStoreForNamespaceDatabase.Database;
-            var updateDef = Builders<Event>.Update.Set(_ => _.SequenceNumber, new EventSequenceNumber(1));
-            database.GetCollection<Event>(WellKnownCollectionNames.EventLog).UpdateOne(_ => _.SequenceNumber == 0, updateDef);
-            await EventStore.EventLog.Append(EventSourceId, SecondEvent);
-        }
+        async Task Because() => await EventStore.EventLog.Append(EventSourceId, SecondEvent);
     }
 
     [Fact] Task should_have_correct_next_sequence_number() => Context.ShouldHaveCorrectNextSequenceNumber(2);
-
     [Fact] Task should_have_correct_tail_sequence_number() => Context.ShouldHaveCorrectTailSequenceNumber(1);
-
     [Fact] Task should_have_the_first_event_stored() => Context.ShouldHaveStoredCorrectEvent<SomeEvent>(0, Context.EventSourceId.Value, (someEvent) => someEvent.Content.ShouldEqual(Context.FirstEvent.Content));
-
     [Fact] Task should_have_the_second_event_stored() => Context.ShouldHaveStoredCorrectEvent<SomeEvent>(1, Context.EventSourceId.Value, (someEvent) => someEvent.Content.ShouldEqual(Context.SecondEvent.Content));
 }
