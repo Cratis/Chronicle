df52501af0fc3bfb1a59173ab488429f0ef97c9d
Einar Ingebrigtsen <einari@me.com>
2024-08-18 13:38:28 +0200

    Tests for CompositeKey scenario

---
+++ Integration/Orleans.InProcess/Projections/Scenarios/when_projecting_properties/using_composite_key_from_property_and_context_property.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Integration.Base;
+using Cratis.Chronicle.Integration.Orleans.InProcess.Projections.Events;
+using Cratis.Chronicle.Integration.Orleans.InProcess.Projections.Models;
+using Cratis.Chronicle.Integration.Orleans.InProcess.Projections.ProjectionTypes;
+using MongoDB.Driver;
+using context = Cratis.Chronicle.Integration.Orleans.InProcess.Projections.Scenarios.when_projecting_properties.using_composite_key_from_property_and_context_property.context;
+
+namespace Cratis.Chronicle.Integration.Orleans.InProcess.Projections.Scenarios.when_projecting_properties;
+
+[Collection(GlobalCollection.Name)]
+public class using_composite_key_from_property_and_context_property(context context) : Given<context>(context)
+{
+    public class context(GlobalFixture globalFixture) : given.a_projection_and_events_appended_to_it<CompositeKeyFromPropertyAndContextPropertyProjection, ModelWithCompositeKey>(globalFixture)
+    {
+        public override IEnumerable<Type> EventTypes => [typeof(EventWithPropertiesForAllSupportedTypes)];
+        public CompositeKey ModelId;
+        public ModelWithCompositeKey Model;
+
+        void Establish()
+        {
+            ModelId = new(Guid.NewGuid().ToString(), 0);
+            EventsToAppend.Add(EventWithPropertiesForAllSupportedTypes.CreateWithRandomValues() with
+            {
+                StringValue = ModelId.First
+            });
+        }
+
+        async Task Because()
+        {
+            var result = await GlobalFixture.ReadModels.Database.GetCollection<ModelWithCompositeKey>().FindAsync(_ => _.Id == ModelId);
+            Model = await result.FirstOrDefaultAsync();
+        }
+    }
+
+    [Fact] void should_return_model() => Context.Model.ShouldNotBeNull();
+}
