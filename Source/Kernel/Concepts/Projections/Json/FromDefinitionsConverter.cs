bb6605dfb6a0c090b836488baf1257da5fbf1372
Einar Ingebrigtsen <einari@me.com>
2024-08-25 08:27:03 +0200

    Fixing XML comments

--- Source/Kernel/Concepts/Projections/Json/FromDefinitionsConverter.cs
+++ Source/Kernel/Concepts/Projections/Json/FromDefinitionsConverter.cs
@@ -4,13 +4,12 @@
 using System.Text.Json.Serialization;
 using Cratis.Chronicle.Concepts.Events;
 using Cratis.Chronicle.Concepts.Projections.Definitions;
-using Cratis.Chronicle.Properties;
 using Cratis.Json;
 
 namespace Cratis.Chronicle.Concepts.Projections.Json;
 
 /// <summary>
-/// Represents a <see cref="JsonConverter"/> that can convert to a dictionary of <see cref="PropertyPath"/> and <see cref="FromDefinition"/>.
+/// Represents a <see cref="JsonConverter"/> that can convert to a dictionary of <see cref="EventType"/> and <see cref="FromDefinition"/>.
 /// </summary>
 public class FromDefinitionsConverter : DictionaryJsonConverter<EventType, FromDefinition>
 {
