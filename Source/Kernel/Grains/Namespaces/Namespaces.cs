afdc01199ca141a413e1775aaddc6a1a289f24fb
Einar Ingebrigtsen <einari@me.com>
2024-08-31 17:52:56 +0200

    Adding support for ensuring a namespace

--- Source/Kernel/Grains/Namespaces/Namespaces.cs
+++ Source/Kernel/Grains/Namespaces/Namespaces.cs
@@ -25,6 +25,9 @@ public class Namespaces(
     readonly IBroadcastChannelProvider _namespaceAddedChannel = clusterClient.GetBroadcastChannelProvider(WellKnownBroadcastChannelNames.NamespaceAdded);
 
     /// <inheritdoc/>
+    public Task EnsureDefault() => Ensure(EventStoreNamespaceName.Default);
+
+    /// <inheritdoc/>
     public async Task Ensure(EventStoreNamespaceName @namespace)
     {
         if (State.Namespaces.Any(_ => _.Name == @namespace)) return;
