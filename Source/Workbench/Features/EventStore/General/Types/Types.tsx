01afc92718b9855df80d7244a60ee926468061af
Einar Ingebrigtsen <einari@me.com>
2024-09-01 09:50:51 +0200

    Adding the start of Types view

--- Source/Workbench/Features/EventStore/General/Types/Types.tsx
+++ Source/Workbench/Features/EventStore/General/Types/Types.tsx
@@ -2,7 +2,33 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Page } from 'Components/Common/Page';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import strings from 'Strings';
 
 export const Types = () => {
-    return <Page title='Types' />;
+
+    return (
+        <Page title={strings.eventStore.general.types.title} mainClassName={'overflow-hidden flex flex-col h-full'}>
+            <div className={'flex-1 overflow-hidden'}>
+                <DataTable
+                        value={[]}
+                        rows={100}
+                        paginator
+                        alwaysShowPaginator={false}
+                        scrollable
+                        scrollHeight={'flex'}
+                        selectionMode='single'
+                        onSelectionChange={(e) => {}}
+                        dataKey='observerId'
+                        filterDisplay='menu'
+                        emptyMessage='No types found'
+                    >
+                        <Column field='name' header={strings.eventStore.general.types.columns.name} sortable />
+                        <Column field='generation' header={strings.eventStore.general.types.columns.generation} sortable />
+                        <Column field='tombstone' header={strings.eventStore.general.types.columns.tombstone} sortable />
+                </DataTable>
+            </div>
+        </Page>
+    );
 };
