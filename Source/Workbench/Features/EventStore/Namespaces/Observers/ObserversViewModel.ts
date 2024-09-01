e7a17f36b1cfb52eec9ca63b1b48b9ce0b87b0b1
Einar Ingebrigtsen <einari@me.com>
2024-09-01 15:27:00 +0200

    Cleanup

--- Source/Workbench/Features/EventStore/Namespaces/Observers/ObserversViewModel.ts
+++ Source/Workbench/Features/EventStore/Namespaces/Observers/ObserversViewModel.ts
@@ -1,18 +1,15 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { inject, injectable } from 'tsyringe';
+import { injectable } from 'tsyringe';
 import { ObserverInformation } from 'Api/Concepts/Observation/ObserverInformation';
 import { INamespaces } from 'State/Namespaces';
-import * as Shared from 'Shared';
 import { Namespace } from 'Api/Namespaces';
 
 @injectable()
 export class ObserversViewModel {
 
-    constructor(
-        namespaces: INamespaces,
-        @inject('params') params: Shared.EventStoreAndNamespaceParams) {
+    constructor(namespaces: INamespaces) {
 
         this.currentNamespace = { name: '', description: '' };
 
