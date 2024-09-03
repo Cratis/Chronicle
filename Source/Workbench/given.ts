168cf9a5afb380465d423155074abf2ec0971e1e
Einar Ingebrigtsen <einari@me.com>
2024-09-03 17:13:21 +0200

    Adding a simple given() function for creating contexts for specs

---
+++ Source/Workbench/given.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { Constructor } from '@cratis/fundamentals';
+import { Suite } from 'mocha';
+
+export type ContextForSuite<TContext extends {}> = (this: Suite, context: TContext) => void;
+
+export function given<TContext extends {}>(contextType: Constructor<TContext>, callback: ContextForSuite<TContext>) {
+    return function (this: Suite) {
+        const context = new contextType(this);
+        callback.call(this, context);
+    };
+}
