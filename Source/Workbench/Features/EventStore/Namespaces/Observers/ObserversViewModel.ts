f449e39cd72acd4b99663b2c53107ca0798a70cc
Einar Ingebrigtsen <einari@me.com>
2024-09-03 17:16:16 +0200

    Showing confirmation dialogs

--- Source/Workbench/Features/EventStore/Namespaces/Observers/ObserversViewModel.ts
+++ Source/Workbench/Features/EventStore/Namespaces/Observers/ObserversViewModel.ts
@@ -3,9 +3,9 @@
 
 import { inject, injectable } from 'tsyringe';
 import { ObserverInformation } from 'Api/Concepts/Observation/ObserverInformation';
-import { INamespaces } from 'State/Namespaces';
 import { Namespace } from 'Api/Namespaces';
 import { Replay } from 'Api/Observation';
+import { INamespaces } from 'State/Namespaces';
 import { DialogButtons, IDialogs } from '@cratis/applications.react.mvvm/dialogs';
 import { DialogResult } from '@cratis/applications.react/dialogs';
 import * as Shared from 'Shared';
@@ -31,14 +31,18 @@ export class ObserversViewModel {
     async replay() {
         if (this.selectedObserver) {
             const observerId = this.selectedObserver.observerId;
-            const result = await this._dialogs.showStandard('Replay?', `Are you sure you want to replay ${observerId}?`, DialogButtons.YesNo);
+            const result = await this._dialogs.showConfirmation('Replay?', `Are you sure you want to replay ${observerId}?`, DialogButtons.YesNo);
             if (result == DialogResult.Yes) {
                 this._replay.eventStore = this._params.eventStore!;
                 this._replay.namespace = this.currentNamespace.name;
                 const commandResult = await this._replay.execute();
-                commandResult.onSuccess(() => {
-                    this._dialogs.showStandard('Replay', `Replay ${observerId} has started`, DialogButtons.Ok);
-                });
+                commandResult
+                    .onSuccess(() => {
+                        this._dialogs.showConfirmation('Replay', `Replay ${observerId} has started`, DialogButtons.Ok);
+                    })
+                    .onException((error) => {
+                        this._dialogs.showConfirmation('Replay', `Replay ${observerId} failed: ${error}`, DialogButtons.Ok);
+                    });
             }
         }
     }
