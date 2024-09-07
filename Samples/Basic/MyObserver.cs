1b4e3c817330ff9a0ea8bf0a0a8642deb236a75d
Einar Ingebrigtsen <einari@me.com>
2024-09-07 08:04:49 +0200

    Fixing compiler errors

--- Samples/Basic/MyObserver.cs
+++ Samples/Basic/MyObserver.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Chronicle.Events;
+using Cratis.Chronicle.Reactors;
+
 namespace Basic;
 
 public class MyObserver : IReactor
