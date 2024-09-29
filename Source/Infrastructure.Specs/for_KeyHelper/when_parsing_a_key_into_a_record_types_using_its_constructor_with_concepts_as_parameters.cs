40622e241ecf39e6fbc8ff88123accb70f858364
Einar Ingebrigtsen <einari@me.com>
2024-09-29 10:09:35 +0200

    Moved KeyHelper to infrastructure

---
+++ Source/Infrastructure.Specs/for_KeyHelper/when_parsing_a_key_into_a_record_types_using_its_constructor_with_concepts_as_parameters.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.for_KeyHelper;
+
+public class when_parsing_a_key_into_a_record_types_using_its_constructor_with_concepts_as_parameters : Specification
+{
+    record SomeConcept(string Value) : ConceptAs<string>(Value);
+
+    record Key(SomeConcept First, SomeConcept Second, SomeConcept Third);
+
+    string[] _parts;
+    string _key;
+    Key _result;
+
+    void Establish()
+    {
+        _parts = ["First", "Second", "Third"];
+        _key = KeyHelper.Combine(_parts);
+    }
+
+    void Because() => _result = KeyHelper.Parse<Key>(_key);
+
+    [Fact] public void should_create_a_key_from_the_parts() => _result.ShouldNotBeNull();
+    [Fact] public void should_have_the_first_part() => _result.First.Value.ShouldEqual(_parts[0]);
+    [Fact] public void should_have_the_second_part() => _result.Second.Value.ShouldEqual(_parts[1]);
+    [Fact] public void should_have_the_third_part() => _result.Third.Value.ShouldEqual(_parts[2]);
+}
