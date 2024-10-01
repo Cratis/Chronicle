6f176cd24831e99b4d65d99206b6e4bfad0f4e00
Einar Ingebrigtsen <einari@me.com>
2024-10-01 10:01:57 +0200

    Fixing linting errors

--- Source/Workbench/Components/Dialogs/ConfirmationDialog.tsx
+++ Source/Workbench/Components/Dialogs/ConfirmationDialog.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Dialog } from 'primereact/dialog'
+import { Dialog } from 'primereact/dialog';
 import { DialogButtons, ConfirmationDialogRequest, useDialogContext } from '@cratis/applications.react.mvvm/dialogs';
 import { DialogResult } from '@cratis/applications.react/dialogs';
 import { Button } from 'primereact/button';
