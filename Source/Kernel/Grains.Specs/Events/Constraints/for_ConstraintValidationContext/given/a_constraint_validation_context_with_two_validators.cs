c081df62353bf6f520ec43fe255017cdfc0542a6
Einar Ingebrigtsen <einari@me.com>
2024-11-20 19:03:55 +0100

    Adding specs

---
+++ Source/Kernel/Grains.Specs/Events/Constraints/for_ConstraintValidationContext/given/a_constraint_validation_context_with_two_validators.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Dynamic;
+using Cratis.Chronicle.Concepts.Events;
+
+namespace Cratis.Chronicle.Grains.Events.Constraints.for_ConstraintValidationContext.given;
+
+public class a_constraint_validation_context_with_two_validators : Specification
+{
+    protected IConstraintValidator _firstValidator;
+    protected IConstraintValidator _secondValidator;
+    protected ConstraintValidationContext context;
+    protected EventSourceId _eventSourceId;
+    protected EventType _eventType;
+    protected ExpandoObject _content;
+
+    void Establish()
+    {
+        _firstValidator = Substitute.For<IConstraintValidator>();
+        _secondValidator = Substitute.For<IConstraintValidator>();
+
+        _eventSourceId = EventSourceId.New();
+        _eventType = new("SomeEvent", 1);
+        _content = new();
+
+        context = new([_firstValidator, _secondValidator], _eventSourceId, _eventType, _content);
+    }
+}
---
+++ Source/Kernel/Grains.Specs/Events/Constraints/for_ConstraintValidationContext/when_validating/multiple_validators_and_one_has_violation.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.Events.Constraints;
+
+namespace Cratis.Chronicle.Grains.Events.Constraints.for_ConstraintValidationContext.when_validating;
+
+public class multiple_validators_and_one_has_violation : given.a_constraint_validation_context_with_two_validators
+{
+    ConstraintValidationResult _result;
+    ConstraintViolation _violation;
+
+
+    void Establish()
+    {
+        _firstValidator.Validate(Arg.Any<ConstraintValidationContext>()).Returns(new ConstraintValidationResult());
+
+        _violation = new(_eventType, 42, ConstraintType.Unknown, "The Constraint", "Something went wrong", []);
+        _secondValidator.Validate(Arg.Any<ConstraintValidationContext>()).Returns(ConstraintValidationResult.Failed([_violation]));
+    }
+
+    async Task Because() => _result = await context.Validate();
+
+    [Fact] void should_call_the_first_validator() => _firstValidator.Received(1).Validate(context);
+    [Fact] void should_call_the_second_validator() => _secondValidator.Received(1).Validate(context);
+    [Fact] void should_have_one_violation() => _result.Violations.ShouldContainOnly(_violation);
+}
