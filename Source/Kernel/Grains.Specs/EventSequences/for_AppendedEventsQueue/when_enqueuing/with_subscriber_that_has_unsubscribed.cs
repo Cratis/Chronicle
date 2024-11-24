08b185b85557a09d2ad0d66e64d8aaef536ddf50
Einar Ingebrigtsen <einari@me.com>
2024-11-24 07:55:49 +0100

    Adding spec to verify unsubscribe

---
+++ Source/Kernel/Grains.Specs/EventSequences/for_AppendedEventsQueue/when_enqueuing/with_subscriber_that_has_unsubscribed.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.Events;
+
+namespace Cratis.Chronicle.Grains.EventSequences.for_AppendedEventsQueue.when_enqueuing;
+
+public class with_subscriber_that_has_unsubscribed : given.a_single_subscriber_with_an_event_type
+{
+    AppendedEvent _appendedEvent;
+    EventSourceId _eventSourceId;
+
+    void Establish()
+    {
+        _appendedEvent = AppendedEvent.EmptyWithEventType(_eventType);
+        _eventSourceId = Guid.NewGuid();
+        _appendedEvent = _appendedEvent with { Context = EventContext.Empty with { EventSourceId = _eventSourceId } };
+
+        _queue.Unsubscribe(_observerKey);
+    }
+
+    async Task Because()
+    {
+        await _queue.Enqueue([_appendedEvent]);
+        await _queue.AwaitQueueDepletion();
+    }
+
+    [Fact] void should_not_call_handle_on_observer_twice() => _handledEvents.Count.ShouldEqual(0);
+}
