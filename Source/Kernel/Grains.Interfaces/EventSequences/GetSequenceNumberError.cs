05e216c7ab12e8fd94b206d79c6335aa6fd7f39b
woksin <sindrewilting@gmail.com>
2024-11-29 20:07:49 +0100

    Add missing files

---
+++ Source/Kernel/Grains.Interfaces/EventSequences/GetSequenceNumberError.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.Events;
+namespace Cratis.Chronicle.Grains.EventSequences;
+
+/// <summary>
+/// The errors that can occur when getting <see cref="EventSequenceNumber"/> from <see cref="IEventSequence"/>.
+/// </summary>
+public enum GetSequenceNumberError
+{
+    /// <summary>
+    /// The error when it's an unexpected error from storage layer.
+    /// </summary>
+    StorageError = 0,
+
+    /// <summary>
+    /// The error when the event sequence number matching the given filter was not wound.
+    /// </summary>
+    NotFound = 1
+}
