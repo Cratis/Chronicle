51d8fb1d89035747feefc001da13fe6e7d79a554
Einar Ingebrigtsen <einari@me.com>
2024-12-31 10:04:28 +0100

    Formatting

--- Source/Workbench/Web/global.d.ts
+++ Source/Workbench/Web/global.d.ts
@@ -5,8 +5,9 @@ declare module '*.css';
 declare module '*.svg' {
     const content: string;
     export default content;
-  }
+}
+
 declare module '*.png' {
     const content: string;
     export default content;
-  }
+}
