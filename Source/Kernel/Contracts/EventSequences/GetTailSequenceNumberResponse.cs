8817c21ce438ec16845a4e5f4cf33e5e6ef026c5
Einar Ingebrigtsen <einari@me.com>
2024-12-31 10:03:45 +0100

    Making it a contract

--- Source/Kernel/Contracts/EventSequences/GetTailSequenceNumberResponse.cs
+++ Source/Kernel/Contracts/EventSequences/GetTailSequenceNumberResponse.cs
@@ -1,15 +1,19 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using ProtoBuf;
+
 namespace Cratis.Chronicle.Contracts.EventSequences;
 
 /// <summary>
 /// Represents the response for getting the tail sequence number.
 /// </summary>
+[ProtoContract]
 public class GetTailSequenceNumberResponse
 {
     /// <summary>
     /// Gets or sets the sequence number.
     /// </summary>
+    [ProtoMember(1)]
     public ulong SequenceNumber { get; set; }
 }
