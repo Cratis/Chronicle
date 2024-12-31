c9355f6d66e752d824c8da59fd7b723dbb66eef0
Einar Ingebrigtsen <einari@me.com>
2024-12-31 10:04:06 +0100

    Overriding toLocaleString()

---
+++ Source/Workbench/Web/Utils/SerializableDateTimeOffsetExtensions.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { SerializableDateTimeOffset } from 'Api/Contracts/Primitives';
+
+SerializableDateTimeOffset.prototype.toLocaleString = function () {
+    const epochTicks = 621355968000000000; // Ticks at Unix epoch
+    const ticksPerMillisecond = 10000; // Ticks per millisecond
+    const milliseconds = (this.ticks - epochTicks) / ticksPerMillisecond;
+    const date = new Date(milliseconds);
+    const offsetInMinutes = this.offsetMinutes / 60000; // Offset is in milliseconds
+    const utcDate = new Date(date.getTime() + offsetInMinutes * 60000);
+    return utcDate.toLocaleString();
+};
