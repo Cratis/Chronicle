26553bccdc8085b2b3ff7cb414a08a17f79f2554
Einar Ingebrigtsen <einari@me.com>
2025-01-02 14:55:09 +0100

    Adding EventStore as argument

--- Source/Api/EventSequences/EventSequences.cs
+++ Source/Api/EventSequences/EventSequences.cs
@@ -6,13 +6,16 @@ namespace Cratis.Chronicle.Api.EventSequences;
 /// <summary>
 /// Represents the API for working with event logs.
 /// </summary>
-[Route("/api/event-store/sequences")]
+[Route("/api/event-store/{eventStore}/sequences")]
 public class EventSequences : ControllerBase
 {
     /// <summary>
     /// Gets all event sequences.
     /// </summary>
+    /// <param name="eventStore">The event store to get event sequences for.</param>
     /// <returns>Collection of names of event sequences.</returns>
     [HttpGet]
-    public Task<IEnumerable<string>> AllEventSequences() => Task.FromResult<IEnumerable<string>>(["Log"]);
+#pragma warning disable IDE0060 // Remove unused parameter
+    public Task<IEnumerable<string>> AllEventSequences([FromRoute] string eventStore) => Task.FromResult<IEnumerable<string>>(["Log"]);
+#pragma warning restore IDE0060 // Remove unused parameter
 }
