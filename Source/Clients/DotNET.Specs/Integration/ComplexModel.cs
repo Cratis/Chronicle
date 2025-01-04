e789a12786c22c41880ad64ebf7c01b709933fc4
Einar Ingebrigtsen <einari@me.com>
2025-01-04 11:53:15 +0100

    Removing Integrations - this is not really supported in the way it is now

--- Source/Clients/DotNET.Specs/Integration/ComplexModel.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Cratis.Chronicle.Integration;
-
-public record ComplexModel(int SomeInteger, string SomeString, Model Child);
--- Source/Clients/DotNET.Specs/Integration/for_AutoMapperExtensions/DestinationAsRecord.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Cratis.Chronicle.Integration.for_AutoMapperExtensions;
-
-public record DestinationAsRecord(int DestinationInteger, string DestinationString);
