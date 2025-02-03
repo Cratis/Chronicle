334392f99a9b6f2ecc966b8df9f4488ab1ca2974
woksin <sindrewilting@gmail.com>
2025-02-03 09:22:24 +0100

    Add Created timestamp to Job

--- Source/Kernel/Contracts/Jobs/Job.cs
+++ Source/Kernel/Contracts/Jobs/Job.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Chronicle.Contracts.Primitives;
 using ProtoBuf;
 
 namespace Cratis.Chronicle.Contracts.Jobs;
@@ -42,14 +43,20 @@ public class Job
     public JobStatus Status { get; set; }
 
     /// <summary>
+    /// Gets or sets when job was created.
+    /// </summary>
+    [ProtoMember(6)]
+    public SerializableDateTimeOffset Created { get; set; }
+
+    /// <summary>
     /// Gets or sets collection of status changes that happened to the job.
     /// </summary>
-    [ProtoMember(6, IsRequired = true)]
+    [ProtoMember(7, IsRequired = true)]
     public IList<JobStatusChanged> StatusChanges { get; set; } = [];
 
     /// <summary>
     /// Gets or sets the <see cref="JobProgress"/>.
     /// </summary>
-    [ProtoMember(7, IsRequired = true)]
+    [ProtoMember(8, IsRequired = true)]
     public JobProgress Progress { get; set; } = new();
 }
