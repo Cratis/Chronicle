fe4b39e26f8c8fbc6d534d1cd966c1a65cc8082a
woksin <sindrewilting@gmail.com>
2025-02-05 14:36:14 +0100

    Implement JobStateSerializer

--- Source/Kernel/Storage.MongoDB/Jobs/JobStateSerializer.cs
+++ Source/Kernel/Storage.MongoDB/Jobs/JobStateSerializer.cs
@@ -1,12 +1,11 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Applications.MongoDB;
 using Cratis.Chronicle.Concepts.Jobs;
 using Cratis.Chronicle.Storage.Jobs;
 using Cratis.Strings;
-using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 namespace Cratis.Chronicle.Storage.MongoDB.Jobs;
@@ -17,67 +16,50 @@ namespace Cratis.Chronicle.Storage.MongoDB.Jobs;
 /// <param name="jobTypes">The <see cref="IJobTypes"/>.</param>
 public class JobStateSerializer(IJobTypes jobTypes) : SerializerBase<JobState>
 {
+    /// <inheritdoc/>
     public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, JobState value)
-    {
-        var requestType = value.Request.GetType();
-        var requestSerializer = BsonSerializer.SerializerRegistry.GetSerializer(requestType);
-    }
+        => BsonSerializer.SerializerRegistry.GetSerializer<BsonDocument>().Serialize(context, args, value);
+
     /// <inheritdoc />
     public override JobState Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
-        var jobRequestElementName = nameof(JobState.Request).ToCamelCase();
-        var rawBsonDocument = context.Reader.ReadRawBsonDocument();
-        using var rawDocument = new RawBsonDocument(rawBsonDocument);
-        rawDocument.Remove(jobRequestElementName);
-        var bsonDocument = rawDocument.ToBsonDocument<BsonDocument>();
-        var jobTypeString = bsonDocument.GetValue(nameof(JobState.Type).ToCamelCase()).AsString;
-        var jobRequestType = jobTypes.GetRequestClrTypeFor(new(jobTypeString)).AsT0;
-        var request = (IJobRequest)BsonSerializer.Deserialize(
-            bsonDocument.GetElement(jobRequestElementName).ToBsonDocument(),
-            jobRequestType);
-        // bsonDocument.Remove(jobRequestElementName);
-        var jobState = BsonSerializer.Deserialize<JobState>(bsonDocument);
-        jobState.Request = request;
+        var (jobState, requestBookmark, endBookmark) = DeserializeJobStateExceptRequest(context);
+        context.Reader.ReturnToBookmark(requestBookmark);
+        var jobRequestClrType = jobTypes.GetRequestClrTypeFor(jobState.Type).Match(type => type, _ => throw new UnknownClrTypeForJobType(jobState.Type));
+        ValueType.GetProperty(nameof(JobState.Request))!
+            .SetValue(jobState, BsonSerializer.Deserialize(context.Reader, jobRequestClrType));
+
+        context.Reader.ReturnToBookmark(endBookmark);
+        context.Reader.ReadEndDocument();
         return jobState;
     }
-}
 
-public class JobRequestSerializer<TJobRequest>(IJobTypes jobTypes) : SerializerBase<TJobRequest>
-    where TJobRequest : IJobRequest
-{
-    /// <inheritdoc />
-    public override TJobRequest Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    static (JobState JobState, BsonReaderBookmark JobRequestBookmark, BsonReaderBookmark EndBookmark) DeserializeJobStateExceptRequest(BsonDeserializationContext context)
     {
-        var rawBsonDocument = context.Reader.ReadRawBsonDocument();
-        using var rawDocument = new RawBsonDocument(rawBsonDocument);
-        var bsonDocument = rawDocument.ToBsonDocument<BsonDocument>();
-        return BsonSerializer.Deserialize<TJobRequest>(bsonDocument);
-    }
-}
-
-/// <summary>
-/// Represents a <see cref="IBsonSerializationProvider"/> for concepts.
-/// </summary>
-public class JobRequestSerializationProvider(IServiceProvider serviceProvider) : IBsonSerializationProvider
-{
-    /// <summary>
-    /// Creates an instance of a serializer of the concept of the given type param T.
-    /// </summary>
-    /// <typeparam name="T">The Concept type.</typeparam>
-    /// <returns><see cref="ConceptSerializer{T}"/> for the specific type.</returns>
-    public static JobRequestSerializer<T> CreateSerializer<T>(IServiceProvider serviceProvider)
-        where T : class, IJobRequest
-        => ActivatorUtilities.CreateInstance<JobRequestSerializer<T>>(serviceProvider);
-
-    /// <inheritdoc/>
-    public IBsonSerializer GetSerializer(Type type)
-    {
-        if (type.IsAssignableTo(typeof(IJobRequest)))
+        var jobState = new JobState();
+        BsonReaderBookmark requestBookmark = default!;
+        context.Reader.ReadStartDocument();
+        var bsonCLassMap = BsonClassMap.LookupClassMap(typeof(JobState)).AllMemberMaps;
+        while (context.Reader.ReadBsonType() != BsonType.EndOfDocument)
         {
-            var createSerializerMethod = GetType().GetMethod(nameof(CreateSerializer))!.MakeGenericMethod(type);
-            return (createSerializerMethod.Invoke(null, [serviceProvider]) as IBsonSerializer)!;
+            var fieldName = context.Reader.ReadName();
+            var propertyName = fieldName.ToPascalCase();
+            if (propertyName == nameof(JobState.Request))
+            {
+                requestBookmark = context.Reader.GetBookmark();
+                context.Reader.SkipValue();
+                continue;
+            }
+            var memberMap = bsonCLassMap.FirstOrDefault(map => map.ElementName.Equals(fieldName));
+            if (memberMap == null)
+            {
+                context.Reader.SkipValue();
+                continue;
+            }
+            var jobStateProperty = typeof(JobState).GetProperty(memberMap.MemberName)!;
+            jobStateProperty.SetValue(jobState, BsonSerializer.Deserialize(context.Reader, memberMap.MemberType));
         }
-
-        return null!;
+        var endBookmark = context.Reader.GetBookmark();
+        return (jobState, requestBookmark, endBookmark);
     }
 }
