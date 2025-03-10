a1171b7ee402e39df21afae01ec1ac278cf4e9cc
Einar Ingebrigtsen <einari@me.com>
2025-03-10 11:41:15 +0100

    Fixing specs after API change

--- Source/Kernel/Grains.Specs/Projections/for_ProjectionReplayHandler/when_ending_replay/and_projection_and_context_exists.cs
+++ Source/Kernel/Grains.Specs/Projections/for_ProjectionReplayHandler/when_ending_replay/and_projection_and_context_exists.cs
@@ -20,5 +20,5 @@ public class and_projection_and_context_exists : given.a_projection_replay_handl
     Task Because() => _handler.EndReplayFor(_observerDetails);
 
     [Fact] void should_end_replay() => _projectionPipeline.Received(1).EndReplay(_replayContext);
-    [Fact] void should_signal_model_replayed() => _replayedModels.Received(1).Replayed(_replayContext);
+    [Fact] void should_signal_model_replayed() => _replayedModels.Received(1).Replayed(_observerDetails.Key.ObserverId, _replayContext);
 }
