9484b675a58b25e75411753fb3b265661a1ad6ad
Einar Ingebrigtsen <einari@me.com>
2025-03-12 16:05:17 +0100

    Fixing compiler warnings
    Co-authored-by: Sindre Alstad Wilting <woksin@users.noreply.github.com>

--- Integration/Orleans.InProcess/for_JobsManager/given/ITheJobStep.cs
+++ Integration/Orleans.InProcess/for_JobsManager/given/ITheJobStep.cs
@@ -1,5 +1,9 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Cratis.Chronicle.Grains.Jobs;
 using Orleans.Concurrency;
+
 namespace Cratis.Chronicle.Integration.Orleans.InProcess.for_JobsManager.given;
 
 public interface ITheJobStep : IJobStep<TheJobStepRequest, TheJobStepResult, TheJobStepState>
