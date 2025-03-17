3b3bd01094a181a0430b18288fe614d346a39f71
Einar Ingebrigtsen <einari@me.com>
2025-03-17 09:01:37 +0100

    Adding call context

--- Source/Kernel/Contracts/INamespaces.cs
+++ Source/Kernel/Contracts/INamespaces.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using ProtoBuf.Grpc;
 using ProtoBuf.Grpc.Configuration;
 
 namespace Cratis.Chronicle.Contracts;
@@ -31,7 +32,8 @@ public interface INamespaces
     /// Observe all available namespaces in an event store.
     /// </summary>
     /// <param name="request">A <see cref="GetNamespacesRequest"/> instance.</param>
+    /// <param name="context">The gRPC <see cref="CallContext"/>.</param>
     /// <returns>An observable of collection of strings representing the names of the namespaces.</returns>
     [Operation]
-    IObservable<IEnumerable<string>> ObserveNamespaces(GetNamespacesRequest request);
+    IObservable<IEnumerable<string>> ObserveNamespaces(GetNamespacesRequest request, CallContext context = default);
 }
