a0afdb11d4ffc49526cbb9cd20c03cb4924d2a8e
Einar Ingebrigtsen <einari@me.com>
2025-03-20 15:22:39 +0100

    Removing unused using

--- Source/Workbench/Embedded/WebServer.cs
+++ Source/Workbench/Embedded/WebServer.cs
@@ -6,7 +6,6 @@ using Cratis.Chronicle.Connections;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
