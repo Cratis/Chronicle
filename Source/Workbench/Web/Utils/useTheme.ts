8faee520fbf16cf5c853425429ffa6d0a8ff8192
Einar Ingebrigtsen <einari@me.com>
2025-03-25 16:09:37 +0100

    Fixing the path for themes that gets programatically set

--- Source/Workbench/Web/Utils/useTheme.ts
+++ Source/Workbench/Web/Utils/useTheme.ts
@@ -4,13 +4,13 @@
 import { useDarkMode } from "usehooks-ts";
 import { useLayoutEffect } from "react";
 
-export const useTheme = () => {
+export const useTheme = (basePath: string) => {
     const { toggle, enable, disable } = useDarkMode();
     const isDarkMode = true;
 
     useLayoutEffect(() => {
         const theme = isDarkMode ? 'dark' : 'light';
-        document.getElementById('theme')?.setAttribute('href', `/themes/${theme}.css`);
+        document.getElementById('theme')?.setAttribute('href', `${basePath}/themes/${theme}.css`);
         document.body.classList.toggle('dark', isDarkMode);
         document.body.classList.toggle('light', !isDarkMode);
     }, [isDarkMode]);
