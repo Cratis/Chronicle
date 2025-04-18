4af9c804a77189d84a21f6c9e3d9b3c9c10337bf
Einar Ingebrigtsen <einari@me.com>
2025-04-18 15:42:20 +0200

    Adding call filter for handling correlation Id cross cuttingly + a custom accessor for accessing it

---
+++ Source/Kernel/Setup/Execution/CorrelationIdOutgoingCallFilter.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Setup.Execution;
+
+/// <summary>
+/// Represents a filter for managing correlation IDs for outgoing calls.
+/// </summary>
+/// <param name="correlationIdAccessor">The <see cref="ICorrelationIdAccessor"/> to use.</param>
+public class CorrelationIdOutgoingCallFilter(ICorrelationIdAccessor correlationIdAccessor) : IOutgoingGrainCallFilter
+{
+    /// <inheritdoc/>
+    public async Task Invoke(IOutgoingGrainCallContext context)
+    {
+        RequestContext.Set(RequestContextKeys.CorrelationIdKey, correlationIdAccessor.Current.Value);
+        await context.Invoke();
+    }
+}
