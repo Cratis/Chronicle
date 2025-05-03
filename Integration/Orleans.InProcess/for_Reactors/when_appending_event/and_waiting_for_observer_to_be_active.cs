e0135e348b43cb20295ff5c12bcbf0361481813a
Einar <einari@me.com>
2025-05-03 09:31:26 +0200

    Started introduction of reactor extensions not dependent on Kernel grains

--- Integration/Orleans.InProcess/for_Reactors/when_appending_event/and_waiting_for_observer_to_be_active.cs
+++ Integration/Orleans.InProcess/for_Reactors/when_appending_event/and_waiting_for_observer_to_be_active.cs
@@ -1,18 +1,19 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Chronicle.Contracts.Observation;
 using Cratis.Chronicle.Events;
 using Cratis.Chronicle.Grains.Observation;
 using Cratis.Chronicle.Integration.Base;
+using Cratis.Chronicle.Observation;
+using Cratis.Chronicle.Reactors;
 using Cratis.Chronicle.Storage.MongoDB;
 using Cratis.Chronicle.Storage.MongoDB.Observation;
 using Cratis.Chronicle.Storage.Observation;
 using Humanizer;
 using context = Cratis.Chronicle.Integration.Orleans.InProcess.for_Reactors.when_appending_event.and_waiting_for_observer_to_be_active.context;
-using ObserverRunningState = Cratis.Chronicle.Concepts.Observation.ObserverRunningState;
 
 namespace Cratis.Chronicle.Integration.Orleans.InProcess.for_Reactors.when_appending_event;
+
 
 [Collection(GlobalCollection.Name)]
 public class and_waiting_for_observer_to_be_active(context context) : Given<context>(context)
@@ -23,8 +24,7 @@ public class and_waiting_for_observer_to_be_active(context context) : Given<cont
         public EventSourceId EventSourceId;
         public SomeEvent Event;
         public SomeReactor Reactor;
-        public IObserver ReactorObserver;
-        public ObserverState ReactorObserverState;
+        public ReactorState ReactorState;
         public Exception WaitingForObserverStateError;
         public IEnumerable<FailedPartition> FailedPartitions;
 
@@ -41,24 +41,18 @@ public class and_waiting_for_observer_to_be_active(context context) : Given<cont
         {
             EventSourceId = "some source";
             Event = new SomeEvent(42);
-            ReactorObserver = GetObserverForReactor<SomeReactor>();
         }
 
         async Task Because()
         {
-            await ReactorObserver.WaitTillActive();
+            await EventStore.Reactors.WaitTillActive<SomeReactor>();
             await EventStore.EventLog.Append(EventSourceId, Event);
             await Tcs.Task.WaitAsync(TimeSpanFactory.DefaultTimeout());
-            WaitingForObserverStateError = await Catch.Exception(async () => await ReactorObserver.WaitForState(ObserverRunningState.Active, TimeSpanFactory.DefaultTimeout()));
-            await ReactorObserver.WaitTillReachesEventSequenceNumber(EventSequenceNumber.First, TimeSpanFactory.DefaultTimeout());
-            ReactorObserverState = await ReactorObserver.GetState();
+            WaitingForObserverStateError = await Catch.Exception(async () => await EventStore.Reactors.WaitForState<SomeReactor>(ObserverRunningState.Active));
+            await EventStore.Reactors.WaitTillReachesEventSequenceNumber<SomeReactor>(EventSequenceNumber.First);
+            ReactorState = await EventStore.Reactors.GetState<SomeReactor>();
 
-            FailedPartitions = await EventStore.Connection.Services.FailedPartitions.GetFailedPartitions(new()
-            {
-                EventStore = EventStore.Name.Value,
-                Namespace = Concepts.EventStoreNamespaceName.Default,
-                ObserverId = ReactorObserverState.Id
-            });
+            FailedPartitions = await EventStore.FailedPartitions.GetFailedPartitionsFor(ReactorState.Id);
         }
     }
 
@@ -72,13 +66,13 @@ public class and_waiting_for_observer_to_be_active(context context) : Given<cont
     void should_not_fail_to_wait_for_observer_to_be_active_again() => Context.WaitingForObserverStateError.ShouldBeNull();
 
     [Fact]
-    void should_have_observer_state_be_active() => Context.ReactorObserverState.RunningState.ShouldEqual(ObserverRunningState.Active);
+    void should_have_observer_state_be_active() => Context.ReactorState.RunningState.ShouldEqual(ObserverRunningState.Active);
 
     [Fact]
-    void should_have_correct_observer_state_last_handled_event_sequence_number() => Context.ReactorObserverState.LastHandledEventSequenceNumber.Value.ShouldEqual(0ul);
+    void should_have_correct_observer_state_last_handled_event_sequence_number() => Context.ReactorState.LastHandledEventSequenceNumber.Value.ShouldEqual(0ul);
 
     [Fact]
-    void should_have_correct_observer_state_next_event_sequence_number() => Context.ReactorObserverState.NextEventSequenceNumber.Value.ShouldEqual(1ul);
+    void should_have_correct_observer_state_next_event_sequence_number() => Context.ReactorState.NextEventSequenceNumber.Value.ShouldEqual(1ul);
 
     [Fact]
     void should_not_have_failed_partitions() => Context.FailedPartitions.ShouldBeEmpty();
