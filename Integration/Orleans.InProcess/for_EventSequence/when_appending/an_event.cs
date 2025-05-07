d5eb018cdfb14963faf10528fc1c16aed2dee76f
Einar <einari@me.com>
2025-05-07 16:52:44 +0200

    Removing not needed namespace prefix

--- Integration/Orleans.InProcess/for_EventSequence/when_appending/an_event.cs
+++ Integration/Orleans.InProcess/for_EventSequence/when_appending/an_event.cs
@@ -11,7 +11,7 @@ public class an_event(context context) : Given<context>(context)
 {
     public class context(ChronicleFixture ChronicleFixture) : IntegrationSpecificationContext(ChronicleFixture)
     {
-        public Events.EventSourceId EventSourceId { get; } = "source";
+        public EventSourceId EventSourceId { get; } = "source";
         public SomeEvent Event { get; private set; }
 
         public override IEnumerable<Type> EventTypes => [typeof(SomeEvent)];
