5126802abfa13faa6510e2cdcf5c4f89cf88abc0
woksin <sindrewilting@gmail.com>
2025-05-26 14:50:25 +0200

    Setup integration test project

---
+++ Integration/Api/ChronicleCollection.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Integration.Api;
+
+/// <summary>
+/// Collection fixture for the Chronicle integration tests.
+/// </summary>
+[CollectionDefinition(Name)]
+public class ChronicleCollection : ICollectionFixture<ChronicleDevelopmentFixture>
+{
+    /// <summary>
+    /// Gets the name of the collection.
+    /// </summary>
+    public const string Name = "Global";
+}
--- Source/Api/InternalsVisibleTo.cs
+++ Source/Api/InternalsVisibleTo.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Cratis.Chronicle.Integration.Api;
-
-public class Startup;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Cratis.Chronicle.Integration.Api")]
