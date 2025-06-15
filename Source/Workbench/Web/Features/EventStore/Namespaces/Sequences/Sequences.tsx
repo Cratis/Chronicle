431c262be13d99de712bd986bf4eaaada1619c54
Einar <einari@me.com>
2025-06-15 19:18:02 +0200

    Started adding filtering

--- Source/Workbench/Web/Features/EventStore/Namespaces/Sequences/Sequences.tsx
+++ Source/Workbench/Web/Features/EventStore/Namespaces/Sequences/Sequences.tsx
@@ -6,9 +6,13 @@ import strings from 'Strings';
 import { AppendedEvents, AppendedEventsArguments } from 'Api/EventSequences';
 import { type EventStoreAndNamespaceParams } from 'Shared';
 import { useParams } from 'react-router-dom';
-import { Column } from 'primereact/column';
+import { Column, ColumnFilterElementTemplateOptions } from 'primereact/column';
 import { EventDetails } from './EventDetails';
 import { AppendedEvent } from 'Api/Events';
+import { AllEventTypes } from 'Api/EventTypes/AllEventTypes';
+import { MultiSelect } from 'primereact/multiselect';
+import { DataTableFilterMeta } from 'primereact/datatable';
+import { FilterMatchMode } from 'primereact/api';
 
 const occurred = (event: AppendedEvent) => {
     return event.context.occurred.toLocaleString();
@@ -22,6 +26,27 @@ export const Sequences = () => {
         eventSequenceId: 'event-log'
     };
 
+    const filters:DataTableFilterMeta = {
+        'metadata.type.id': { value: null, matchMode: FilterMatchMode.IN }
+    };
+
+    const [eventTypes] = AllEventTypes.use({
+        eventStore: params.eventStore!
+    });
+
+    const eventTypeFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
+        return (
+            <MultiSelect
+                value={options.value}
+                options={eventTypes.data}
+                onChange={(e) => options.filterCallback(e.value)}
+                optionLabel='id'
+                placeholder={strings.eventStore.namespaces.sequences.filters.placeholders.eventType}
+                maxSelectedLabels={1}
+                className='p-column-filter' />
+        );
+    };
+
     return (
         <DataPage
             title={strings.eventStore.namespaces.sequences.title}
@@ -29,10 +54,19 @@ export const Sequences = () => {
             queryArguments={queryArgs}
             emptyMessage={strings.eventStore.namespaces.sequences.empty}
             dataKey='metadata.sequenceNumber'
+            defaultFilters={filters}
+            globalFilterFields={['metadata.type.id']}
             detailsComponent={EventDetails}>
             <DataPage.Columns>
                 <Column field='metadata.sequenceNumber' header={strings.eventStore.namespaces.sequences.columns.sequenceNumber} />
-                <Column field='metadata.type.id' header={strings.eventStore.namespaces.sequences.columns.eventType} />
+                <Column field='metadata.type.id'
+                        header={strings.eventStore.namespaces.sequences.columns.eventType}
+                        showFilterMatchModes={false}
+                        filter
+                        filterMenuStyle={{ width: '14rem' }}
+                        filterField='metadata.type.id'
+                        filterElement={eventTypeFilterTemplate}
+                        filterPlaceholder='Event Types'/>
                 <Column field='context.occurred' header={strings.eventStore.namespaces.sequences.columns.occurred} body={occurred} />
             </DataPage.Columns>
         </DataPage>
