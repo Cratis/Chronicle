a315617f81889a785d2b1085feb061cb1f799aec
Einar <einari@me.com>
2025-06-19 14:31:53 +0200

    Xml comments

--- Source/Clients/Api/EventStores/AddEventStore.cs
+++ Source/Clients/Api/EventStores/AddEventStore.cs
@@ -3,4 +3,8 @@
 
 namespace Cratis.Chronicle.Api.EventStores;
 
+/// <summary>
+/// Represents a request to add an event store.
+/// </summary>
+/// <param name="Name">Name of the event store to add.</param>
 public record AddEventStore(string Name);
