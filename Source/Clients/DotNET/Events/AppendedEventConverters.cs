e3ae912b701d00569aeb306f50979e2475f28657
Einar <einari@me.com>
2025-07-10 15:53:50 +0200

    Removing methods that was marked obsolete, we don't offer these publicly

--- Source/Clients/DotNET/Events/AppendedEventConverters.cs
+++ Source/Clients/DotNET/Events/AppendedEventConverters.cs
@@ -3,9 +3,7 @@
 
 using System.Collections.Immutable;
 using System.Dynamic;
-using System.Text.Json;
 using System.Text.Json.Nodes;
-using Cratis.Json;
 
 namespace Cratis.Chronicle.Events;
 
@@ -28,19 +26,6 @@ internal static class AppendedEventConverters
     };
 
     /// <summary>
-    /// Convert to contract version of <see cref="AppendedEvent"/> (legacy method using JsonSerializer directly).
-    /// </summary>
-    /// <param name="event"><see cref="AppendedEvent"/> to convert.</param>
-    /// <returns>Converted contract version.</returns>
-    [Obsolete("Use ToContract(event, eventSerializer) instead to ensure proper event serialization")]
-    internal static Contracts.Events.AppendedEvent ToContract(this AppendedEvent @event) => new()
-    {
-        Metadata = @event.Metadata.ToContract(),
-        Context = @event.Context.ToContract(),
-        Content = JsonSerializer.Serialize(@event.Content, Globals.JsonSerializerOptions)
-    };
-
-    /// <summary>
     /// Convert to client version of <see cref="AppendedEvent"/>.
     /// </summary>
     /// <param name="event"><see cref="Contracts.Events.AppendedEvent"/> to convert.</param>
@@ -50,17 +35,6 @@ internal static class AppendedEventConverters
             @event.Metadata.ToClient(),
             @event.Context.ToClient(),
             await eventSerializer.Deserialize(typeof(ExpandoObject), JsonNode.Parse(@event.Content)!.AsObject()) as ExpandoObject ?? new ExpandoObject());
-
-    /// <summary>
-    /// Convert to client version of <see cref="AppendedEvent"/> (legacy method using JsonSerializer directly).
-    /// </summary>
-    /// <param name="event"><see cref="Contracts.Events.AppendedEvent"/> to convert.</param>
-    /// <returns>Converted Chronicle version.</returns>
-    [Obsolete("Use ToClient(event, eventSerializer) instead to ensure proper event deserialization")]
-    internal static AppendedEvent ToClient(this Contracts.Events.AppendedEvent @event) => new(
-            @event.Metadata.ToClient(),
-            @event.Context.ToClient(),
-            JsonSerializer.Deserialize<ExpandoObject>(@event.Content, Globals.JsonSerializerOptions)!);
 
     /// <summary>
     /// Convert to a client version of a collection of <see cref="Contracts.Events.AppendedEvent"/>.
@@ -74,13 +48,4 @@ internal static class AppendedEventConverters
         var convertedEvents = await Task.WhenAll(tasks);
         return convertedEvents.ToImmutableList();
     }
-
-    /// <summary>
-    /// Convert to a client version of a collection of <see cref="Contracts.Events.AppendedEvent"/> (legacy method using JsonSerializer directly).
-    /// </summary>
-    /// <param name="events">Collection of <see cref="Contracts.Events.AppendedEvent"/> to convert from.</param>
-    /// <returns>An immutable collection of <see cref="AppendedEvent"/>.</returns>
-    [Obsolete("Use ToClient(events, eventSerializer) instead to ensure proper event deserialization")]
-    internal static IImmutableList<AppendedEvent> ToClient(this IEnumerable<Contracts.Events.AppendedEvent> events) =>
-        events.Select(_ => _.ToClient()).ToImmutableList();
 }
