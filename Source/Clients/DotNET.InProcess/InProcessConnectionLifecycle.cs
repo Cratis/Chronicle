877bfc7bf72afd88464ada56d742578938aa2633
Einar <einari@me.com>
2025-07-11 14:17:24 +0200

    Removing not needed logging and simpifying

--- Source/Clients/DotNET.InProcess/InProcessConnectionLifecycle.cs
+++ Source/Clients/DotNET.InProcess/InProcessConnectionLifecycle.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Connections;
-using Microsoft.Extensions.Logging;
 
 namespace Cratis.Chronicle.InProcess;
 
@@ -10,11 +9,7 @@ namespace Cratis.Chronicle.InProcess;
 /// Represents an implementation of <see cref="IConnectionLifecycle"/> for in-process scenarios.
 /// This implementation does not trigger OnConnected/OnDisconnected events since there is no actual connection.
 /// </summary>
-/// <remarks>
-/// Initializes a new instance of the <see cref="InProcessConnectionLifecycle"/>.
-/// </remarks>
-/// <param name="logger">Logger for logging.</param>
-public class InProcessConnectionLifecycle(ILogger<InProcessConnectionLifecycle> logger) : IConnectionLifecycle
+public class InProcessConnectionLifecycle : IConnectionLifecycle
 {
     /// <summary>
     /// Adds or removes event handlers for when the connection is connected.
@@ -38,8 +33,6 @@ public class InProcessConnectionLifecycle(ILogger<InProcessConnectionLifecycle> 
     public Task Connected()
     {
         IsConnected = true;
-        logger.LogDebug("In-process connection marked as connected, but no OnConnected events will be triggered");
-        // Intentionally NOT triggering OnConnected events to avoid double registration
         return Task.CompletedTask;
     }
 
@@ -47,8 +40,6 @@ public class InProcessConnectionLifecycle(ILogger<InProcessConnectionLifecycle> 
     public Task Disconnected()
     {
         IsConnected = false;
-        logger.LogDebug("In-process connection marked as disconnected, but no OnDisconnected events will be triggered");
-        // Intentionally NOT triggering OnDisconnected events since in-process doesn't have real disconnections
         ConnectionId = ConnectionId.New();
         return Task.CompletedTask;
     }
