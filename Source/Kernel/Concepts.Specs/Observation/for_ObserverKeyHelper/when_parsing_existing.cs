6cdbd9527a4417f89060a9eb0e0c25dafc29c1fe
Einar <einari@me.com>
2025-07-15 17:53:15 +0200

    Fixing static broken code analysis rules

--- Source/Kernel/Concepts.Specs/Observation/for_ObserverKeyHelper/when_parsing_existing.cs
+++ Source/Kernel/Concepts.Specs/Observation/for_ObserverKeyHelper/when_parsing_existing.cs
@@ -7,18 +7,18 @@ namespace Cratis.Chronicle.Grains.Observation.for_ObserverKeyHelper;
 
 public class when_parsing_existing : Specification
 {
-    const string observer_id = "1a07d613-278a-4b5d-ae87-eaf0890f00e4";
-    const string event_store = "7a28c4fa-cfd4-405e-9873-753bab4fd2e3";
-    const string @namespace = "0c2adff8-7ac6-4998-bcec-c58c18d45f8f";
-    const string event_sequence_id = "c7b1abce-9a6a-43aa-89e8-7ee6e154bdf7";
-    string combined = $"{observer_id}{KeyHelper.Separator}{event_store}{KeyHelper.Separator}{@namespace}{KeyHelper.Separator}{event_sequence_id}";
+    const string ObserverId = "1a07d613-278a-4b5d-ae87-eaf0890f00e4";
+    const string EventStore = "7a28c4fa-cfd4-405e-9873-753bab4fd2e3";
+    const string Namespace = "0c2adff8-7ac6-4998-bcec-c58c18d45f8f";
+    const string EventSequenceId = "c7b1abce-9a6a-43aa-89e8-7ee6e154bdf7";
+    string _combined = $"{ObserverId}{KeyHelper.Separator}{EventStore}{KeyHelper.Separator}{Namespace}{KeyHelper.Separator}{EventSequenceId}";
 
-    ObserverKey result;
+    ObserverKey _result;
 
-    void Because() => result = ObserverKey.Parse(combined);
+    void Because() => _result = ObserverKey.Parse(_combined);
 
-    [Fact] void should_hold_correct_observer_id() => result.ObserverId.ToString().ShouldEqual(observer_id);
-    [Fact] void should_hold_correct_event_store() => result.EventStore.ToString().ShouldEqual(event_store);
-    [Fact] void should_hold_correct_namespace() => result.Namespace.ToString().ShouldEqual(@namespace);
-    [Fact] void should_hold_correct_event_sequence_id() => result.EventSequenceId.ToString().ShouldEqual(event_sequence_id);
+    [Fact] void should_hold_correct_observer_id() => _result.ObserverId.ToString().ShouldEqual(ObserverId);
+    [Fact] void should_hold_correct_event_store() => _result.EventStore.ToString().ShouldEqual(EventStore);
+    [Fact] void should_hold_correct_namespace() => _result.Namespace.ToString().ShouldEqual(Namespace);
+    [Fact] void should_hold_correct_event_sequence_id() => _result.EventSequenceId.ToString().ShouldEqual(EventSequenceId);
 }
--- Source/Kernel/Concepts.Specs/Observation/for_PartitionedObserverKey/when_parsing_existing.cs
+++ Source/Kernel/Concepts.Specs/Observation/for_PartitionedObserverKey/when_parsing_existing.cs
@@ -7,18 +7,18 @@ namespace Cratis.Chronicle.Grains.Observation.for_PartitionedObserverKey;
 
 public class when_parsing_existing : Specification
 {
-    const string @namespace = "0c2adff8-7ac6-4998-bcec-c58c18d45f8f";
-    const string event_store = "7a28c4fa-cfd4-405e-9873-753bab4fd2e3";
-    const string event_sequence_id = "c7b1abce-9a6a-43aa-89e8-7ee6e154bdf7";
-    const string event_source_id = "181d06d6-2b0a-49cc-802d-734e3fab2a9b";
-    string combined = $"{event_store}{KeyHelper.Separator}{@namespace}{KeyHelper.Separator}{event_sequence_id}{KeyHelper.Separator}{event_source_id}";
+    const string EventStore = "7a28c4fa-cfd4-405e-9873-753bab4fd2e3";
+    const string Namespace = "0c2adff8-7ac6-4998-bcec-c58c18d45f8f";
+    const string EventSequenceId = "c7b1abce-9a6a-43aa-89e8-7ee6e154bdf7";
+    const string EventSourceId = "181d06d6-2b0a-49cc-802d-734e3fab2a9b";
+    string _combined = $"{EventStore}{KeyHelper.Separator}{Namespace}{KeyHelper.Separator}{EventSequenceId}{KeyHelper.Separator}{EventSourceId}";
 
-    PartitionedObserverKey result;
+    PartitionedObserverKey _result;
 
-    void Because() => result = PartitionedObserverKey.Parse(combined);
+    void Because() => _result = PartitionedObserverKey.Parse(_combined);
 
-    [Fact] void should_hold_correct_microservice_id() => result.EventStore.ToString().ShouldEqual(event_store);
-    [Fact] void should_hold_correct_tenant_id() => result.Namespace.ToString().ShouldEqual(@namespace);
-    [Fact] void should_hold_correct_event_sequence_id() => result.EventSequenceId.ToString().ShouldEqual(event_sequence_id);
-    [Fact] void should_hold_correct_event_source_id() => result.EventSourceId.ToString().ShouldEqual(event_source_id);
+    [Fact] void should_hold_correct_microservice_id() => _result.EventStore.ToString().ShouldEqual(EventStore);
+    [Fact] void should_hold_correct_tenant_id() => _result.Namespace.ToString().ShouldEqual(Namespace);
+    [Fact] void should_hold_correct_event_sequence_id() => _result.EventSequenceId.ToString().ShouldEqual(EventSequenceId);
+    [Fact] void should_hold_correct_event_source_id() => _result.EventSourceId.ToString().ShouldEqual(EventSourceId);
 }
