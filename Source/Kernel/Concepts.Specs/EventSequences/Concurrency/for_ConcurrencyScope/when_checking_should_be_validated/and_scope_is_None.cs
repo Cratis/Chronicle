187bf0bbf471495de6019018eec126ccc5a21dd4
Einar <einari@me.com>
2025-07-15 20:02:37 +0200

    Gone

--- Source/Kernel/Concepts.Specs/EventSequences/Concurrency/for_ConcurrencyScope/when_checking_should_be_validated/and_scope_is_None.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Cratis.Chronicle.Concepts.EventSequences.Concurrency.for_ConcurrencyScope.when_checking_should_be_validated;
-
-public class and_scope_is_none : Specification
-{
-    [Fact] void should_not_validate() => ConcurrencyScope.None.ShouldBeValidated.ShouldBeFalse();
-}
