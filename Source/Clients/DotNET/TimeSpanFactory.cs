ecf07ac6f7675b3e711360985c3ee16bafa634d6
Einar <einari@me.com>
2025-07-20 08:39:50 +0200

    Revering default timeout

--- Source/Clients/DotNET/TimeSpanFactory.cs
+++ Source/Clients/DotNET/TimeSpanFactory.cs
@@ -20,7 +20,7 @@ public static class TimeSpanFactory
         {
             return Timeout.InfiniteTimeSpan;
         }
-        return TimeSpan.FromSeconds(10);
+        return TimeSpan.FromSeconds(5);
     }
 
     /// <summary>
