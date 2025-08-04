443db8cc65adca36441f4b038d0d7f92b19cb27c
Einar <einari@me.com>
2025-08-04 19:11:49 +0200

    Moving Reactor to its own file and fixing signature

--- Source/Kernel/Grains/Namespaces/NamespaceAdded.cs
+++ Source/Kernel/Grains/Namespaces/NamespaceAdded.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Concepts;
-using Cratis.Chronicle.Grains.Observation.Reactors.Kernel;
 
 namespace Cratis.Chronicle.Grains.Namespaces;
 
@@ -12,18 +11,3 @@ namespace Cratis.Chronicle.Grains.Namespaces;
 /// <param name="EventStore">Event store that the namespace was added to.</param>
 /// <param name="Namespace">Namespace that was added.</param>
 public record NamespaceAdded(EventStoreName EventStore, EventStoreNamespaceName Namespace);
-
-
-/// <summary>
-///
-/// </summary>
-public class NamespacesReactor : Reactor
-{
-
-    public Task Added(NamespaceAdded message)
-    {
-        // Handle the namespace added event
-        // This could involve registering the namespace with reactors or other components
-        return Task.CompletedTask;
-    }
-}
