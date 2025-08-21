8733911107867cf08609733131c31cc65b112a18
Einar <einari@me.com>
2025-08-21 14:50:14 +0200

    Fixing configuration to use the ChronicleAspNetCoreOptions rather than the ChronicleOptions

--- Source/Clients/XUnit.Integration/ChronicleWebApplicationFactory.cs
+++ Source/Clients/XUnit.Integration/ChronicleWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,10 +22,10 @@ public abstract class ChronicleWebApplicationFactory<TStartup>(IChronicleSetupFi
     {
         builder
             .UseContentRoot(contentRoot)
-            .ConfigureServices(services => services.Configure<ChronicleOptions>(options =>
-            {
-                options.ArtifactsProvider = fixture;
-                options.Url = "chronicle://localhost:35001";
-            }));
+            .ConfigureServices(services => services.PostConfigure<ChronicleAspNetCoreOptions>(options =>
+                {
+                    options.ArtifactsProvider = fixture;
+                    options.Url = "chronicle://localhost:35001";
+                }));
     }
 }
