5df527edc2772a2d852e516380b81803977f9567
Einar <einari@me.com>
2025-08-21 14:13:40 +0200

    Taking out WebApplicationFactory disposal but leaving GC collect and waiting for pending finalizers

--- Source/Clients/XUnit.Integration/ChronicleClientFixture.cs
+++ Source/Clients/XUnit.Integration/ChronicleClientFixture.cs
@@ -216,12 +216,8 @@ public abstract class ChronicleClientFixture<TChronicleFixture> : IDisposable, I
         }
 
         await ChronicleFixture.RemoveAllDatabases();
-        _ = Task.Run(async () =>
-        {
-            await (_webApplicationFactory?.DisposeAsync() ?? ValueTask.CompletedTask);
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-        });
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
     }
 
     /// <summary>
