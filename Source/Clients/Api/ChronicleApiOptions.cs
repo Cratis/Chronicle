ca541efa2ad0c8da9511eb0b3cb8d5259f5c0d6f
Einar <einari@me.com>
2025-08-22 14:01:32 +0200

    Use ChronicleOptions for configuring URL instead

--- Source/Clients/Api/ChronicleApiOptions.cs
+++ Source/Clients/Api/ChronicleApiOptions.cs
@@ -14,14 +14,4 @@ public class ChronicleApiOptions
     /// Gets the port for the REST API.
     /// </summary>
     public int ApiPort { get; init; } = 8080;
-
-    /// <summary>
-    /// Gets the <see cref="ChronicleUrl"/> to use.
-    /// </summary>
-    public ChronicleUrl ChronicleUrl { get; init; } = ChronicleUrl.Default;
-
-    /// <summary>
-    /// Gets the timeout for connecting in seconds.
-    /// </summary>
-    public int ConnectTimeout { get; init; } = 5;
 }
