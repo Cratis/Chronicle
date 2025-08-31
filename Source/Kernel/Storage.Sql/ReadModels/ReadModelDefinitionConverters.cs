37e47119b954a7daf049495f8bad6e1adbe9b7fc
Einar <einari@me.com>
2025-08-31 19:09:49 +0200

    Fixing Xml comments

--- Source/Kernel/Storage.Sql/ReadModels/ReadModelDefinitionConverters.cs
+++ Source/Kernel/Storage.Sql/ReadModels/ReadModelDefinitionConverters.cs
@@ -1,23 +1,21 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Chronicle.Concepts.Events;
-using Cratis.Chronicle.Concepts.EventTypes;
 using Cratis.Chronicle.Concepts.ReadModels;
 using NJsonSchema;
 
 namespace Cratis.Chronicle.Storage.Sql.ReadModels;
 
 /// <summary>
-/// Converter methods for working with <see cref="EventTypeSchema"/> converting to and from SQL representations.
+/// Converter methods for working with <see cref="ReadModelDefinition"/> converting to and from SQL representations.
 /// </summary>
 public static class ReadModelDefinitionConverters
 {
     /// <summary>
-    /// Convert to a <see cref="EventType">SQL</see> representation.
+    /// Convert to a <see cref="ReadModel">SQL</see> representation.
     /// </summary>
     /// <param name="definition"><see cref="ReadModelDefinition"/> to convert.</param>
-    /// <returns>Converted <see cref="EventType"/>.</returns>
+    /// <returns>Converted <see cref="ReadModel"/>.</returns>
     public static ReadModel ToSql(this ReadModelDefinition definition) =>
         new()
         {
@@ -27,10 +25,10 @@ public static class ReadModelDefinitionConverters
         };
 
     /// <summary>
-    /// Convert to <see cref="EventTypeSchema"/> from <see cref="EventType"/>.
+    /// Convert to <see cref="ReadModelDefinition"/> from <see cref="ReadModel"/>.
     /// </summary>
-    /// <param name="schema"><see cref="EventType"/> to convert from.</param>
-    /// <returns>Converted <see cref="EventTypeSchema"/>.</returns>
+    /// <param name="schema"><see cref="ReadModel"/> to convert from.</param>
+    /// <returns>Converted <see cref="ReadModelDefinition"/>.</returns>
     public static ReadModelDefinition ToKernel(this ReadModel schema) =>
         new(
             schema.Id,
