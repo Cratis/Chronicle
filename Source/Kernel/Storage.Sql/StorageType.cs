ea83ab642a7afe7802bb4e658b4dd1f9f40846b5
Einar <einari@me.com>
2025-08-31 09:00:27 +0200

    Fix namespace

--- Source/Kernel/Storage.Sql/StorageType.cs
+++ Source/Kernel/Storage.Sql/StorageType.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Cratis.Chronicle.Setup;
+namespace Cratis.Chronicle.Storage.Sql;
 
 /// <summary>
 /// Storage types supported by Chronicle SQL.
