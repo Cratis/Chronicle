50f80c6984627bcb95315775ee0b73733273d06d
woksin <sindrewilting@gmail.com>
2025-09-02 21:27:08 +0200

    Concepts for Webhooks observer

---
+++ Source/Kernel/Concepts/Observation/Webhooks/WebhookDefinition.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.EventSequences;
+
+namespace Cratis.Chronicle.Concepts.Observation.Webhooks;
+
+/// <summary>
+/// Represents the registration of a single client webhook observer.
+/// </summary>
+/// <param name="Identifier"><see cref="WebhookId"/> of the webhook.</param>
+/// <param name="Owner">The owner of the webhook.</param>
+/// <param name="EventSequenceId">The <see cref="EventSequenceId"/> the webhook is for.</param>
+/// <param name="EventTypes">The type of events the observer is interested in.</param>
+/// <param name="IsReplayable">Whether the webhook supports replay scenarios.</param>
+public record WebhookDefinition(
+    WebhookId Identifier,
+    WebhookOwner Owner,
+    EventSequenceId EventSequenceId,
+    IEnumerable<EventTypeWithKeyExpression> EventTypes,
+    bool IsReplayable = true);
---
+++ Source/Kernel/Concepts/Observation/Webhooks/WebhookKey.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.EventSequences;
+
+namespace Cratis.Chronicle.Concepts.Observation.Webhooks;
+
+/// <summary>
+/// Represents the compound key for a webhook.
+/// </summary>
+/// <param name="ReactorId">The webhook identifier.</param>
+/// <param name="EventStore">The event store.</param>
+/// <param name="Namespace">The namespace.</param>
+/// <param name="EventSequenceId">The event sequence.</param>
+public record WebhookKey(WebhookId ReactorId, EventStoreName EventStore, EventStoreNamespaceName Namespace, EventSequenceId EventSequenceId)
+{
+    /// <summary>
+    /// Implicitly convert from <see cref="WebhookKey"/> to string.
+    /// </summary>
+    /// <param name="key"><see cref="WebhookKey"/> to convert from.</param>
+    public static implicit operator string(WebhookKey key) => key.ToString();
+
+    /// <inheritdoc/>
+    public override string ToString() => KeyHelper.Combine(ReactorId, EventStore, Namespace, EventSequenceId);
+
+    /// <summary>
+    /// Parse a key into its components.
+    /// </summary>
+    /// <param name="key">Key to parse.</param>
+    /// <returns>Parsed <see cref="WebhookKey"/> instance.</returns>
+    public static WebhookKey Parse(string key) => KeyHelper.Parse<WebhookKey>(key);
+}
