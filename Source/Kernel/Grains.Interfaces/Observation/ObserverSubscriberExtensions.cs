1501ea671822b8369988b20ae0e66e247f7fb01c
woksin <sindrewilting@gmail.com>
2025-09-03 21:19:16 +0200

    Add GetKeys extension method to IObserverSubscriber

---
+++ Source/Kernel/Grains.Interfaces/Observation/ObserverSubscriberExtensions.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Concepts.Observation;
+
+namespace Cratis.Chronicle.Grains.Observation;
+
+/// <summary>
+/// Extension methods for <see cref="IObserverSubscriber"/>.
+/// </summary>
+public static class ObserverSubscriberExtensions
+{
+    /// <summary>
+    /// Gets the keys for the subscriber.
+    /// </summary>
+    /// <param name="subscriber">The <see cref="IObserverSubscriber"/>.</param>
+    /// <returns><see cref="ObserverKey"/> and <see cref="ObserverSubscriberKey"/>.</returns>
+    public static (ObserverKey ObserverKey, ObserverSubscriberKey ObserverSubscriberKey) GetKeys(
+        this IObserverSubscriber subscriber)
+    {
+        var subscriberKey = ObserverSubscriberKey.Parse(subscriber.GetPrimaryKeyString());
+        var observerKey = new ObserverKey(subscriberKey.ObserverId, subscriberKey.EventStore, subscriberKey.Namespace, subscriberKey.EventSequenceId);
+        return (observerKey, subscriberKey);
+    }
+}
