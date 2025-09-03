27f1923fb041ff54f931036fc5f8985f8633eb80
Einar <einari@me.com>
2025-09-03 11:39:53 +0200

    Adding support for all different supported databases

--- Source/Kernel/Storage.Sql/DbContextFactory.cs
+++ Source/Kernel/Storage.Sql/DbContextFactory.cs
@@ -16,8 +16,32 @@ public class DbContextFactory<TDbContext> : IDesignTimeDbContextFactory<TDbConte
     /// <inheritdoc/>
     public TDbContext CreateDbContext(string[] args)
     {
+        var provider = GetArg(args, "--provider")
+            ?? Environment.GetEnvironmentVariable("EF_PROVIDER")
+            ?? "sqlserver";
+
         var builder = new DbContextOptionsBuilder<TDbContext>();
+        switch (provider.ToLowerInvariant())
+        {
+            case "sqlserver":
+                builder.UseSqlServer("Server=(local);Database=Dummy;Trusted_Connection=True;", x => x.MigrationsAssembly(typeof(TDbContext).Assembly.FullName));
+                break;
+            case "npgsql":
+            case "postgresql":
+                builder.UseNpgsql("Host=localhost;Database=dummy;Username=x;Password=y", x => x.MigrationsAssembly(typeof(TDbContext).Assembly.FullName));
+                break;
+            case "sqlite":
+                builder.UseSqlite("Data Source=:memory:", x => x.MigrationsAssembly(typeof(TDbContext).Assembly.FullName));
+                break;
+            default:
+                throw new InvalidOperationException($"Unknown provider '{provider}'.");
+        }
         builder.UseSqlite("Data Source=chronicle.db", x => x.MigrationsAssembly(typeof(TDbContext).Assembly.FullName));
         return (Activator.CreateInstance(typeof(TDbContext), builder.Options) as TDbContext)!;
+    }
+    static string? GetArg(string[] args, string name)
+    {
+        var i = Array.IndexOf(args, name);
+        return i >= 0 && i + 1 < args.Length ? args[i + 1] : null;
     }
 }
