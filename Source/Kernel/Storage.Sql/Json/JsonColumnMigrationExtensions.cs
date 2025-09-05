c9097111a901d41938d7354d16e139b597247828
Einar <einari@me.com>
2025-09-05 16:31:57 +0200

    Moving into Json namespace

--- Source/Kernel/Storage.Sql/Json/JsonColumnMigrationExtensions.cs
+++ Source/Kernel/Storage.Sql/Json/JsonColumnMigrationExtensions.cs
@@ -5,24 +5,34 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 
-namespace Cratis.Chronicle.Storage.Sql;
+namespace Cratis.Chronicle.Storage.Sql.Json;
 
+/// <summary>
+/// Extension methods for working with Json columns.
+/// </summary>
 public static class JsonColumnMigrationExtensions
 {
-    public static OperationBuilder<AddColumnOperation> JsonColumn<TProperty>(this ColumnsBuilder cb, MigrationBuilder mb)
-    {
-        // if (mb.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
-        // {
-        //     return cb.Column<TProperty>("jsonb", nullable: false);
-        // }
-        // else if (mb.ActiveProvider == "Microsoft.EntityFrameworkCore.SqlServer")
-        // {
-        //     return cb.Column<TProperty>("nvarchar(max)", nullable: false);
-        // }
+    /// <summary>
+    /// Adds a Json column to the specified table.
+    /// </summary>
+    /// <param name="cb">Columns builder.</param>
+    /// <param name="mb">Migration builder.</param>
+    /// <typeparam name="TProperty">Type of property.</typeparam>
+    /// <returns>Operation builder for the column.</returns>
+    public static OperationBuilder<AddColumnOperation> JsonColumn<TProperty>(this ColumnsBuilder cb, MigrationBuilder mb) =>
+        mb.ActiveProvider switch
+        {
+            "Npgsql.EntityFrameworkCore.PostgreSQL" => cb.Column<TProperty>("jsonb", nullable: false),
+            "Microsoft.EntityFrameworkCore.SqlServer" => cb.Column<TProperty>("nvarchar(max)", nullable: false),
+            _ => cb.Column<TProperty>("text", nullable: false)
+        };
 
-        return cb.Column<TProperty>("text", nullable: false);
-    }
-
+    /// <summary>
+    /// Add logic to the database to ensure the specified column is a valid Json column.
+    /// </summary>
+    /// <param name="mb">Migration builder.</param>
+    /// <param name="table">Table name.</param>
+    /// <param name="column">Column name.</param>
     public static void EnsureJsonColumn(this MigrationBuilder mb, string table, string column)
     {
         if (mb.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
@@ -50,4 +60,3 @@ public static class JsonColumnMigrationExtensions
         }
     }
 }
-
