b686bdd4e32019670a67fde7b3ac192e0644ecee
woksin <sindrewilting@gmail.com>
2025-09-20 22:22:06 +0200

    Add `GrainFactoryExtension` and simplify `GetProjectionDefinitions`
    - Introduced `GrainFactoryExtension` for accessing `IRecommendationsManager` with `EventStoreAndNamespace`.
    - Converted `GetProjectionDefinitions` method to expression-bodied for clarity and simplicity.

--- Source/Kernel/Grains/Projections/ProjectionsManager.cs
+++ Source/Kernel/Grains/Projections/ProjectionsManager.cs
@@ -52,10 +52,7 @@ public class ProjectionsManager(
     }
 
     /// <inheritdoc/>
-    public Task<IEnumerable<ProjectionDefinition>> GetProjectionDefinitions()
-    {
-        return Task.FromResult(State.Projections);
-    }
+    public Task<IEnumerable<ProjectionDefinition>> GetProjectionDefinitions() => Task.FromResult(State.Projections);
 
     /// <inheritdoc/>
     public Task OnSubscribed(IBroadcastChannelSubscription streamSubscription)
