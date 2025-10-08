5e1869d659d45c40c043d2eaef3a713aef06de59
woksin <sindrewilting@gmail.com>
2025-10-08 08:04:47 +0200

    Introduce core models for webhooks
    - Added `WebhookId`, `WebhookTarget`, `WebhookTargetUrl`, and `AuthenticationType` to represent webhooks and their attributes.

---
+++ Source/Clients/DotNET/Webhooks/WebhookTarget.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Webhooks;
+
+/// <summary>
+/// Represents the target of a webhook.
+/// </summary>
+/// <param name="Url">The <see cref="WebhookTargetUrl"/>.</param>
+/// <param name="Authentication">The <see cref="AuthenticationType"/>.</param>
+/// <param name="Username">The optional username.</param>
+/// <param name="Password">The optional password.</param>
+/// <param name="BearerToken">The optional bearer token.</param>
+/// <param name="Headers">The headers.</param>
+public record WebhookTarget(
+    WebhookTargetUrl Url,
+    AuthenticationType Authentication,
+    string? Username,
+    string? Password,
+    string? BearerToken,
+    IReadOnlyDictionary<string, string> Headers);
