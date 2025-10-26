836f9f888132b8a8278cff5ebba69a3cb1c834e7
Einar <einari@me.com>
2025-10-26 09:44:38 +0100

    Hooking up so that we get a unique database per event store

--- Source/Kernel/Storage.Sql/Database.cs
+++ Source/Kernel/Storage.Sql/Database.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
 using Cratis.Applications.EntityFrameworkCore;
 using Cratis.Chronicle.Concepts;
 using Cratis.Chronicle.Configuration;
@@ -44,12 +46,59 @@ public class Database(IOptions<ChronicleOptions> options) : IDatabase
         if (!_eventStoreDbContexts.Value.TryGetValue(eventStore.Value, out var dbContext))
         {
             var builder = new DbContextOptionsBuilder<EventStoreDbContext>();
-            builder.UseDatabaseFromConnectionString(options.Value.Storage.ConnectionDetails);
+            var connectionString = GetConnectionStringForEventStore(eventStore);
+            builder.UseDatabaseFromConnectionString(connectionString);
             dbContext = new EventStoreDbContext(builder.Options);
             await dbContext.Database.MigrateAsync();
             _eventStoreDbContexts.Value[eventStore.Value] = dbContext;
         }
 
         return new DbContextScope<EventStoreDbContext>(dbContext, () => _eventStoreDbContexts.Value?.Remove(eventStore.Value));
+    }
+
+    string GetConnectionStringForEventStore(EventStoreName eventStore)
+    {
+        var databaseType = options.Value.Storage.ConnectionDetails.GetDatabaseType();
+        if (databaseType == DatabaseType.Sqlite)
+        {
+            return ReplaceFilename(eventStore);
+        }
+
+        return options.Value.Storage.ConnectionDetails;
+    }
+
+    string ReplaceFilename(EventStoreName eventStore)
+    {
+        if (TryReplaceFilename(eventStore, "Data Source", out var dataSource))
+        {
+            return dataSource;
+        }
+        if (TryReplaceFilename(eventStore, "Filename", out var filename))
+        {
+            return filename;
+        }
+
+        return options.Value.Storage.ConnectionDetails;
+    }
+
+    bool TryReplaceFilename(EventStoreName eventStore, string keyToReplace, [NotNullWhen(true)] out string? connectionString)
+    {
+        var builder = new DbConnectionStringBuilder
+        {
+            ConnectionString = options.Value.Storage.ConnectionDetails
+        };
+
+        if (builder.TryGetValue(keyToReplace, out var dataSource))
+        {
+            var originalFilename = dataSource.ToString()!;
+            var directory = Path.GetDirectoryName(originalFilename) ?? string.Empty;
+            var newFilename = $"{Path.GetFileNameWithoutExtension(originalFilename)}_{eventStore.Value}{Path.GetExtension(originalFilename)}";
+            builder[keyToReplace] = Path.Combine(directory, newFilename);
+            connectionString = builder.ConnectionString!;
+            return true;
+        }
+
+        connectionString = null!;
+        return false;
     }
 }
