83808c77601881353d7dbc6f4ff404a5ea7fd64c
Einar <einari@me.com>
2025-11-03 09:27:22 +0100

    Moving magic strings for well known expressions into WellKnownExpressions

--- Source/Clients/DotNET/Reactors/Reactors.cs
+++ Source/Clients/DotNET/Reactors/Reactors.cs
@@ -227,7 +227,7 @@ public class Reactors : IReactors
             {
                 ReactorId = handler.Id,
                 EventSequenceId = handler.EventSequenceId,
-                EventTypes = handler.EventTypes.Select(et => new EventTypeWithKeyExpression { EventType = et.ToContract(), Key = "$eventSourceId" }).ToArray()
+                EventTypes = handler.EventTypes.Select(et => new EventTypeWithKeyExpression { EventType = et.ToContract(), Key = WellKnownExpressions.EventSourceId }).ToArray()
             }
         };
 
---
+++ Source/Kernel/Concepts/WellKnownExpressions.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Concepts;
+
+/// <summary>
+/// Known expressions for projections.
+/// </summary>
+public static class WellKnownExpressions
+{
+    /// <summary>
+    /// The event source identifier expression.
+    /// </summary>
+    public const string EventSourceId = "$eventSourceId";
+}
