84952915f4233c2ba5bff5283914f19c63277e64
Einar <einari@me.com>
2025-11-04 12:21:24 +0100

    Initializing single cluster mode if not already initialized

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+FLAG_FILE="/data/db/.initialized"
+LOG_FILE="/var/log/mongodb/mongo.log"
+mkdir -p /var/log/mongodb
+touch "$LOG_FILE"
+chown -R mongodb:mongodb /var/log/mongodb
+
+if [ ! -f "$FLAG_FILE" ]; then
+    echo "Initializing MongoDB as a single node cluster"
+
+    mkdir -p /data/db /data/configdb
+    chown -R mongodb:mongodb /data/db /data/configdb
+
+    mongod --logpath /var/log/mongodb/initdb.log --replSet "rs0" --bind_ip 0.0.0.0 --fork
+    mongosh --eval 'rs.initiate({_id: "rs0", members: [{ _id: 0, host: "localhost:27017"}]})'
+    mongod --shutdown
+    touch "$FLAG_FILE"
+fi
+
 /usr/bin/mongod --replSet "rs0" --bind_ip 0.0.0.0 > /dev/null &
 ./Cratis.Chronicle.Server &
 
