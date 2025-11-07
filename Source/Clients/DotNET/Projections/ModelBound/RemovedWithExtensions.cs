8e1e43b954fd24e66027e3c9ef401958dcf41909
Einar <einari@me.com>
2025-11-07 15:46:09 +0100

    Fixing uniqueness for events in the build definitions

--- Source/Clients/DotNET/Projections/ModelBound/RemovedWithExtensions.cs
+++ Source/Clients/DotNET/Projections/ModelBound/RemovedWithExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Contracts.Projections;
-using Cratis.Chronicle.Events;
 using EventType = Cratis.Chronicle.Contracts.Events.EventType;
 
 namespace Cratis.Chronicle.Projections.ModelBound;
@@ -16,16 +15,16 @@ static class RemovedWithExtensions
     /// Processes a RemovedWith attribute and adds the removed with definition to the target dictionary.
     /// </summary>
     /// <param name="targetRemovedWith">The target RemovedWith dictionary to add the definition to.</param>
-    /// <param name="eventTypes">The event types registry for resolving event type information.</param>
+    /// <param name="getOrCreateEventType">Function to get or create a cached EventType instance.</param>
     /// <param name="attr">The RemovedWith attribute to process.</param>
     /// <param name="eventType">The event type that triggers removal.</param>
     internal static void ProcessRemovedWithAttribute(
         this IDictionary<EventType, RemovedWithDefinition> targetRemovedWith,
-        IEventTypes eventTypes,
+        Func<Type, EventType> getOrCreateEventType,
         Attribute attr,
         Type eventType)
     {
-        var eventTypeId = eventTypes.GetEventTypeFor(eventType).ToContract();
+        var eventTypeId = getOrCreateEventType(eventType);
         var keyProperty = attr.GetType().GetProperty(nameof(RemovedWithAttribute<object>.Key));
         var parentKeyProperty = attr.GetType().GetProperty(nameof(RemovedWithAttribute<object>.ParentKey));
 
@@ -43,16 +42,16 @@ static class RemovedWithExtensions
     /// Processes a RemovedWithJoin attribute and adds the removed with join definition to the target dictionary.
     /// </summary>
     /// <param name="targetRemovedWithJoin">The target RemovedWithJoin dictionary to add the definition to.</param>
-    /// <param name="eventTypes">The event types registry for resolving event type information.</param>
+    /// <param name="getOrCreateEventType">Function to get or create a cached EventType instance.</param>
     /// <param name="attr">The RemovedWithJoin attribute to process.</param>
     /// <param name="eventType">The event type that triggers removal.</param>
     internal static void ProcessRemovedWithJoinAttribute(
         this IDictionary<EventType, RemovedWithJoinDefinition> targetRemovedWithJoin,
-        IEventTypes eventTypes,
+        Func<Type, EventType> getOrCreateEventType,
         Attribute attr,
         Type eventType)
     {
-        var eventTypeId = eventTypes.GetEventTypeFor(eventType).ToContract();
+        var eventTypeId = getOrCreateEventType(eventType);
         var keyProperty = attr.GetType().GetProperty(nameof(RemovedWithJoinAttribute<object>.Key));
 
         var key = keyProperty?.GetValue(attr) as string ?? WellKnownExpressions.EventSourceId;
