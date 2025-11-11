333782212f46bdb1fe6d4f9966001b36d20eb5ab
Einar <einari@me.com>
2025-11-11 15:13:52 +0100

    Fixing so that the returned EventSourceId is honored

--- Source/Clients/ApplicationModel/Commands/EventsCommandResponseValueHandler.cs
+++ Source/Clients/ApplicationModel/Commands/EventsCommandResponseValueHandler.cs
@@ -18,7 +18,7 @@ public class EventsCommandResponseValueHandler(IEventLog eventLog, IEventTypes e
     public bool CanHandle(CommandContext commandContext, object value) =>
         (value is IEnumerable<object> objects) &&
         objects.All(o => eventTypes.HasFor(o.GetType())) &&
-        commandContext.Values.TryGetValue(WellKnownCommandContextKeys.EventSourceId, out var id) && id is EventSourceId;
+        commandContext.HasEventSourceId();
 
     /// <inheritdoc/>
     public async Task<CommandResult> Handle(CommandContext commandContext, object value)
