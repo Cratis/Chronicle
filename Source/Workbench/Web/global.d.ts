4b0b8aa32d8ef7227768055456500c8b9cb46eaf
Einar <einari@me.com>
2025-11-29 09:17:16 +0100

    Fixing most squigglies - errors - in specs

--- Source/Workbench/Web/global.d.ts
+++ Source/Workbench/Web/global.d.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/// <reference types="chai" />
+
 declare module '*.css';
 declare module '*.svg' {
     const content: string;
@@ -10,4 +12,13 @@ declare module '*.svg' {
 declare module '*.png' {
     const content: string;
     export default content;
+}
+
+// Extend Chai's Assertion interface for TypeScript support
+declare global {
+    namespace Chai {
+        interface Assertion {
+            called: Assertion;
+        }
+    }
 }
