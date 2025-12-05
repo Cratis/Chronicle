fb6d6c4cd10c27fcc2eee6dd07fe054c3017b243
Einar <einari@me.com>
2025-12-05 14:00:37 +0100

    Leveraging the MongoDB representation of EventType

--- Source/Kernel/Storage.MongoDB/Projections/EventType.cs
+++ Source/Kernel/Storage.MongoDB/Projections/EventType.cs
@@ -10,4 +10,36 @@ namespace Cratis.Chronicle.Storage.MongoDB.Projections;
 /// </summary>
 /// <param name="Id">The <see cref="EventTypeId"/>.</param>
 /// <param name="Generation">The generation of the event type.</param>
-public record EventType(EventTypeId Id, EventTypeGeneration Generation);
+public record EventType(EventTypeId Id, EventTypeGeneration Generation)
+{
+    /// <summary>
+    /// Represents an unknown event type.
+    /// </summary>
+    public static readonly EventType Unknown = new(EventTypeId.Unknown, EventTypeGeneration.First);
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"{Id.Value}+{Generation.Value}";
+    }
+
+    /// <summary>
+    /// Parse from a string representation of event type to actual type.
+    /// </summary>
+    /// <param name="input">String representation.</param>
+    /// <returns>Parsed <see cref="EventType"/>.</returns>
+    /// <remarks>
+    /// The expected format is guid+generation.
+    /// Ex: aa7faa25-afc1-48d1-8558-716581c0e916+1.
+    /// </remarks>
+    public static EventType Parse(string input)
+    {
+        var segments = input.Split('+');
+        if (segments.Length == 1)
+        {
+            return new(segments[0], EventTypeGeneration.First);
+        }
+
+        return new(segments[0], uint.Parse(segments[1]));
+    }
+}
--- Source/Kernel/Storage.MongoDB/Projections/ProjectionFuture.cs
+++ Source/Kernel/Storage.MongoDB/Projections/ProjectionFuture.cs
@@ -3,7 +3,6 @@
 
 using Cratis.Chronicle.Concepts.Projections;
 using Cratis.Chronicle.Properties;
-using Cratis.Chronicle.Storage.Projections;
 
 namespace Cratis.Chronicle.Storage.MongoDB.Projections;
 
