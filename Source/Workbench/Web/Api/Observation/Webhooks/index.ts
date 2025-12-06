621c87130bafd8933c4f5b7063d1c0e3a3b69554
woksin <sindrewilting@gmail.com>
2025-12-06 15:17:17 +0100

    Webhook proxies

---
+++ Source/Workbench/Web/Api/Observation/Webhooks/index.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  **DO NOT EDIT** - This file is an automatically generated file.
+ *--------------------------------------------------------------------------------------------*/
+
+/* eslint-disable sort-imports */
+// eslint-disable-next-line header/header
+export * from './AllWebhooks';
+export * from './BasicAuthorization';
+export * from './BearerTokenAuthorization';
+export * from './GetAllWebhooks';
+export * from './OAuthAuthorization';
+export * from './RegisterWebhook';
+export * from './WebhookDefinition';
+export * from './WebhookTarget';
