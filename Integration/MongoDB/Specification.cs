d16dd98d3f99f651969114dbdf19ada74eb2a0dc
Einar <einari@me.com>
2025-12-09 14:02:43 +0100

    Removing unused usings

--- Integration/MongoDB/Specification.cs
+++ Integration/MongoDB/Specification.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Cratis.Chronicle.XUnit.Integration;
-
 namespace Cratis.Chronicle.MongoDB.Integration;
 
-public abstract class Specification(ChronicleInProcessFixture fixture) : XUnit.Integration.Specification<ChronicleInProcessFixture>(fixture);
+public abstract class Specification(ChronicleInProcessFixture fixture) : Specification<ChronicleInProcessFixture>(fixture);
