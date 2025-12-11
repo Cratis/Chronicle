00edb5b9aca172ed94c22ad7f157ac11bea23345
Einar <einari@me.com>
2025-12-11 12:36:16 +0100

    Removing unused using

--- Integration/DotNET.InProcess/Projections/Scenarios/when_projecting_with_deferred_parent_resolution/Events/RootUpdated.cs
+++ Integration/DotNET.InProcess/Projections/Scenarios/when_projecting_with_deferred_parent_resolution/Events/RootUpdated.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Events;
-using Cratis.Chronicle.InProcess.Integration.Projections.Scenarios.when_projecting_with_deferred_parent_resolution.Concepts;
 
 namespace Cratis.Chronicle.InProcess.Integration.Projections.Scenarios.when_projecting_with_deferred_parent_resolution.Events;
 
