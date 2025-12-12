d12b7505a754dd26d288890e274c2b516e3d6761
Einar <einari@me.com>
2025-12-12 09:15:23 +0100

    Adding specs to verify that we don't get "ghost" properties that belong to children or grand children

---
+++ Source/Infrastructure.Specs/Json/for_ExpandoObjectConverter/GrandChild.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Chronicle.Json.for_ExpandoObjectConverter;
+
+public record GrandChild(
+    Guid HubId,
+    string Name);
