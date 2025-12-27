301bf056e9dd31b0beb5afdc0d3efb3668475c56
Einar <einari@me.com>
2025-12-27 14:27:35 +0100

    Regenerated

--- Source/Workbench/Web/Api/Events/index.ts
+++ Source/Workbench/Web/Api/Events/index.ts
@@ -2,5 +2,7 @@ export * from './AppendedEvent';
 export * from './CreateEventType';
 export * from './EventContext';
 export * from './EventType';
+export * from './EventTypeOwner';
 export * from './EventTypeRegistration';
+export * from './EventTypeSource';
 export * from './Register';
