a071a97254112900971962ebe91b34fb615294a3
Einar <einari@me.com>
2026-01-02 22:36:39 +0100

    regen

--- Source/Workbench/Web/Api/Projections/index.ts
+++ Source/Workbench/Web/Api/Projections/index.ts
@@ -2,6 +2,6 @@ export * from './AllProjections';
 export * from './AllProjectionsWithDsl';
 export * from './PreviewProjection';
 export * from './Projection';
+export * from './ProjectionDefinitionSyntaxError';
 export * from './ProjectionPreview';
 export * from './ProjectionWithDsl';
-export * from './ProjectionDefinitionSyntaxError';
