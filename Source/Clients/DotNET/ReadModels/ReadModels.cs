147cfca9e6a307770e558d6cd4b8490eddedfab8
Einar <einari@me.com>
2026-01-05 19:47:38 +0100

    Switching to Attribute.IsDefined

--- Source/Clients/DotNET/ReadModels/ReadModels.cs
+++ Source/Clients/DotNET/ReadModels/ReadModels.cs
@@ -100,7 +100,7 @@ public class ReadModels(
                 ? namingPolicy.GetPropertyName(property.Name)
                 : $"{prefix}.{namingPolicy.GetPropertyName(property.Name)}";
 
-            if (property.GetCustomAttribute<IndexAttribute>() is not null)
+            if (Attribute.IsDefined(property, typeof(IndexAttribute)))
             {
                 indexes.Add(new IndexDefinition { PropertyPath = propertyPath });
             }
