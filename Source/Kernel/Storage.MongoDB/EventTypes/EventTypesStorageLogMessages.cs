641fd77d479bd65901e3bfb7c892413da8f726c6
Einar <einari@me.com>
2026-01-05 19:01:46 +0100

    Removing log message Id, not needed

--- Source/Kernel/Storage.MongoDB/EventTypes/EventTypesStorageLogMessages.cs
+++ Source/Kernel/Storage.MongoDB/EventTypes/EventTypesStorageLogMessages.cs
@@ -9,9 +9,9 @@ namespace Cratis.Chronicle.Storage.MongoDB.Events.EventTypes;
 
 internal static partial class EventTypesStorageLogMessages
 {
-    [LoggerMessage(1, LogLevel.Debug, "Populating event schemas for event store '{EventStore}'")]
+    [LoggerMessage(LogLevel.Debug, "Populating event schemas for event store '{EventStore}'")]
     internal static partial void Populating(this ILogger<EventTypesStorage> logger, EventStoreName eventStore);
 
-    [LoggerMessage(2, LogLevel.Debug, "Registering event schema for (Id: {EventType} - Generation: {Generation}) for event store '{EventStore}'")]
+    [LoggerMessage(LogLevel.Debug, "Registering event schema for (Id: {EventType} - Generation: {Generation}) for event store '{EventStore}'")]
     internal static partial void Registering(this ILogger<EventTypesStorage> logger, EventTypeId eventType, EventTypeGeneration generation, EventStoreName eventStore);
 }
