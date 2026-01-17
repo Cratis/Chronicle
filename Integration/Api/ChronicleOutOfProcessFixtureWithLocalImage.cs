a3e1a7e5f77456bed696095c764c7c1a4ff0ac77
Einar <einari@me.com>
2026-01-17 19:06:01 +0100

    Making everything use HTTPS

--- Integration/Api/ChronicleOutOfProcessFixtureWithLocalImage.cs
+++ Integration/Api/ChronicleOutOfProcessFixtureWithLocalImage.cs
@@ -10,7 +10,30 @@ namespace Cratis.Chronicle.Integration.Api;
 /// </summary>
 public class ChronicleOutOfProcessFixtureWithLocalImage : ChronicleOutOfProcessFixture
 {
+    const string CertificatePassword = "TestPassword123";
+
+    /// <summary>
+    /// Gets the path to the certificate file.
+    /// </summary>
+    public static string CertificatePath { get; }
+
+    /// <summary>
+    /// Gets the certificate password.
+    /// </summary>
+    public static string CertPassword => CertificatePassword;
+
+    static ChronicleOutOfProcessFixtureWithLocalImage()
+    {
+        var certDir = Path.Combine(Path.GetTempPath(), "chronicle-test-certs");
+        CertificatePath = Path.Combine(certDir, "chronicle-test.pfx");
+        TestCertificateGenerator.GenerateAndSaveCertificate(CertificatePath, CertificatePassword);
+    }
+
     /// <inheritdoc/>
     protected override ContainerBuilder ConfigureImage(ContainerBuilder builder) =>
-        builder.WithImage(Environment.GetEnvironmentVariable("CRATIS_CHRONICLE_LOCAL_IMAGE") ?? "cratis/chronicle:local-development");
+        builder
+            .WithImage(Environment.GetEnvironmentVariable("CRATIS_CHRONICLE_LOCAL_IMAGE") ?? "cratis/chronicle:local-development")
+            .WithBindMount(CertificatePath, "/app/certs/chronicle.pfx")
+            .WithEnvironment("Cratis__Chronicle__Tls__CertificatePath", "/app/certs/chronicle.pfx")
+            .WithEnvironment("Cratis__Chronicle__Tls__CertificatePassword", CertificatePassword);
 }
--- Integration/Api/given/an_http_client.cs
+++ Integration/Api/given/an_http_client.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Cratis.Chronicle.Integration.Api.given;
 
 public class an_http_client(ChronicleOutOfProcessFixtureWithLocalImage fixture) : Specification(fixture)
@@ -9,9 +12,33 @@ public class an_http_client(ChronicleOutOfProcessFixtureWithLocalImage fixture) 
 
     void Establish()
     {
-        Client = CreateClient(new()
+        var handler = new HttpClientHandler();
+        var certificate = X509CertificateLoader.LoadPkcs12FromFile(
+            ChronicleOutOfProcessFixtureWithLocalImage.CertificatePath,
+            ChronicleOutOfProcessFixtureWithLocalImage.CertPassword);
+        handler.ClientCertificates.Add(certificate);
+#pragma warning disable MA0039 // Do not write your own certificate validation method
+        handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
         {
-            BaseAddress = new("http://localhost:8080")
-        });
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            if (cert is not null && certificate is not null)
+            {
+                return cert.GetCertHashString() == certificate.GetCertHashString();
+            }
+
+            return sslPolicyErrors == SslPolicyErrors.RemoteCertificateNameMismatch;
+        };
+#pragma warning restore MA0039 // Do not write your own certificate validation method
+
+        Client = CreateClient(
+            new()
+            {
+                BaseAddress = new("https://localhost:8080")
+            },
+            handler);
     }
 }
