ef8b8b14ba575a3bece5bbe8fc6b51f382ed198c
Einar <einari@me.com>
2026-01-18 08:03:45 +0100

    Removing unused using

--- Integration/Api/ChronicleOutOfProcessFixtureWithLocalImage.cs
+++ Integration/Api/ChronicleOutOfProcessFixtureWithLocalImage.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Net.Http.Json;
 using System.Text.Json;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
