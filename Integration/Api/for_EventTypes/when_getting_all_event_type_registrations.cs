2e891c5b885c3dd76ec20cfb76f75ec0af4708ab
Einar <einari@me.com>
2026-01-18 08:13:18 +0100

    Adding a "synchronous" endpoint for checking event type registrations and getting specs to pass

--- Integration/Api/for_EventTypes/when_getting_all_event_type_registrations.cs
+++ Integration/Api/for_EventTypes/when_getting_all_event_type_registrations.cs
@@ -1,8 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
+using Cratis.Arc.Queries;
 using Cratis.Chronicle.Api.Events;
 using context = Cratis.Chronicle.Integration.Api.for_EventTypes.when_getting_all_event_type_registrations.context;
 
@@ -13,8 +12,8 @@ public class when_getting_all_event_type_registrations(context context) : Given<
 {
     public class context(ChronicleOutOfProcessFixtureWithLocalImage fixture) : given.an_http_client(fixture)
     {
-        public ISubject<IEnumerable<EventTypeRegistration>> Observable;
-        public IEnumerable<EventTypeRegistration> Result;
+        public QueryResult Result;
+        public IEnumerable<EventTypeRegistration> Data;
 
         async Task Establish()
         {
@@ -35,16 +34,18 @@ public class when_getting_all_event_type_registrations(context context) : Given<
 
         async Task Because()
         {
-            Observable = Client.Observe<IEnumerable<EventTypeRegistration>>("/api/event-store/test-store-registrations/types/schemas");
-            Result = await Observable.FirstAsync().Timeout(TimeSpan.FromSeconds(10));
+            Result = await Client.ExecuteQuery<IEnumerable<EventTypeRegistration>>("/api/event-store/test-store-registrations/types/registrations");
+            Data = Result.Data as IEnumerable<EventTypeRegistration>;
         }
     }
 
-    [Fact] void should_return_registered_event_types() => Context.Result.Count().ShouldBeGreaterThanOrEqual(2);
+    [Fact] void should_succeed_query() => Context.Result.IsSuccess.ShouldBeTrue();
 
-    [Fact] void should_include_first_event_type() => Context.Result.ShouldContain(_ => _.Type.Id == "26f57829-c3a0-45ca-b1fc-2e05e6e54b8e");
+    [Fact] void should_return_registered_event_types() => Context.Data.Count().ShouldBeGreaterThanOrEqual(2);
 
-    [Fact] void should_include_second_event_type() => Context.Result.ShouldContain(_ => _.Type.Id == "8f7b4a3c-d2e1-4f9a-b8c7-1d6e3f2a4b5c");
+    [Fact] void should_include_first_event_type() => Context.Data.ShouldContain(_ => _.Type.Id == "26f57829-c3a0-45ca-b1fc-2e05e6e54b8e");
 
-    [Fact] void should_have_schema_for_first_event_type() => Context.Result.First(_ => _.Type.Id == "26f57829-c3a0-45ca-b1fc-2e05e6e54b8e").Schema.ShouldNotBeNull();
+    [Fact] void should_include_second_event_type() => Context.Data.ShouldContain(_ => _.Type.Id == "8f7b4a3c-d2e1-4f9a-b8c7-1d6e3f2a4b5c");
+
+    [Fact] void should_have_schema_for_first_event_type() => Context.Data.First(_ => _.Type.Id == "26f57829-c3a0-45ca-b1fc-2e05e6e54b8e").Schema.ShouldNotBeNull();
 }
