ed30f5ba7e2dee410c8ecdced2e753c51e211f01
Einar <einari@me.com>
2026-01-26 13:33:21 +0100

    Temporary solution for refreshing after adding

--- Source/Workbench/Web/Features/EventStore/General/ReadModelTypes/ReadModelTypes.tsx
+++ Source/Workbench/Web/Features/EventStore/General/ReadModelTypes/ReadModelTypes.tsx
@@ -6,11 +6,12 @@ import strings from 'Strings';
 import { AllReadModelDefinitions, ReadModelDefinition, ReadModelOwner, ReadModelSource } from 'Api/ReadModelTypes';
 import { type EventStoreAndNamespaceParams } from 'Shared';
 import { useParams } from 'react-router-dom';
-import { useDialog } from '@cratis/arc.react/dialogs';
+import { useDialog, DialogResult } from '@cratis/arc.react/dialogs';
 import { AddReadModelDialog } from './Add/AddReadModelDialog';
 import { DataPage, MenuItem } from 'Components';
 import { ReadModelDetails } from './ReadModelDetails';
 import * as faIcons from 'react-icons/fa6';
+import { useState } from 'react';
 
 const renderSource = (readModel: ReadModelDefinition) => {
     switch (readModel.source) {
@@ -35,10 +36,20 @@ const renderOwner = (readModel: ReadModelDefinition) => {
 export const ReadModelTypes = () => {
     const params = useParams<EventStoreAndNamespaceParams>();
     const [AddReadModelWrapper, showAddReadModel] = useDialog(AddReadModelDialog);
+    // TODO: This is a workaround to force refresh after save. Should be replaced with WebSocket-based updates.
+    const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+    const handleAddReadModel = async () => {
+        const [result] = await showAddReadModel();
+        if (result === DialogResult.Ok) {
+            setTimeout(() => setRefreshTrigger(prev => prev + 1), 200);
+        }
+    };
 
     return (
         <>
             <DataPage
+                key={refreshTrigger}
                 title={strings.eventStore.general.readModels.title}
                 query={AllReadModelDefinitions}
                 queryArguments={{ eventStore: params.eventStore! }}
@@ -51,7 +62,7 @@ export const ReadModelTypes = () => {
                         id='create'
                         label={strings.eventStore.general.readModels.actions.create}
                         icon={faIcons.FaPlus}
-                        command={() => showAddReadModel()} />
+                        command={handleAddReadModel} />
                 </DataPage.MenuItems>
 
                 <DataPage.Columns>
