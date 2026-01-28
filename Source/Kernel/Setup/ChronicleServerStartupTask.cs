7539700dbcb050e3be0be0f9e7ebcc5153305248
Einar <einari@me.com>
2026-01-28 10:40:28 +0100

    Moving EventTypes out of Kernel namespace + adding a way to mark if an Event Type belonging to the Kernel should be in all EventStores or not

--- Source/Kernel/Setup/ChronicleServerStartupTask.cs
+++ Source/Kernel/Setup/ChronicleServerStartupTask.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Chronicle.Concepts;
-using Cratis.Chronicle.Grains.EventTypes.Kernel;
+using Cratis.Chronicle.Grains.EventTypes;
 using Cratis.Chronicle.Grains.Jobs;
 using Cratis.Chronicle.Grains.Namespaces;
 using Cratis.Chronicle.Grains.Observation.Reactors.Kernel;
