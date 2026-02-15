2f98c289db864a8a1df33daadb883567e8573ada
Einar <einari@me.com>
2026-02-15 11:20:05 +0100

    Fixing namespace / type references

--- Source/Kernel/Core.Specs/Projections/Engine/for_ProjectionsManager/when_registering/with_multiple_projection_definitions.cs
+++ Source/Kernel/Core.Specs/Projections/Engine/for_ProjectionsManager/when_registering/with_multiple_projection_definitions.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Cratis.Chronicle.Projections.for_ProjectionsManager.when_registering;
+namespace Cratis.Chronicle.Projections.Engine.for_ProjectionsManager.when_registering;
 
 public class with_multiple_projection_definitions : given.a_projections_manager
 {
