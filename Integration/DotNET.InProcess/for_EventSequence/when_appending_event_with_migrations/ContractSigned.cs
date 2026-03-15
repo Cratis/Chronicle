bb69c9d700cca05899394e8d2b382203f8198116
Einar <einari@me.com>
2026-03-15 11:47:51 +0100

    Add integration spec for default value migration on event append

---
+++ Integration/DotNET.InProcess/for_EventSequence/when_appending_event_with_migrations/ContractSigned.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Chronicle.Events;
+
+namespace Cratis.Chronicle.InProcess.Integration.for_EventSequence.when_appending_event_with_migrations;
+
+/// <summary>
+/// Generation 1 of <see cref="ContractSigned"/>. Has only a contract identifier.
+/// Generation 2 adds a <see cref="Status"/> property, defaulting to <c>pending</c> for events
+/// stored before the property existed.
+/// </summary>
+[EventType]
+public record ContractSigned(string ContractId);
