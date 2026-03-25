5eb294e2c07b268e026f8605f34ee9273b47c8ae
Einar <einari@me.com>
2026-03-25 14:42:20 +0100

    Implementing SinkFactory

--- Source/Kernel/Storage.MongoDB/Sinks/SinkFactory.cs
+++ Source/Kernel/Storage.MongoDB/Sinks/SinkFactory.cs
@@ -19,22 +19,12 @@ namespace Cratis.Chronicle.Storage.MongoDB.Sinks;
 public class SinkFactory(
     IDatabase database,
     ITypeFormats typeFormats,
-    IExpandoObjectConverter expandoObjectConverter)
-
-    // : ISinkFactory
+    IExpandoObjectConverter expandoObjectConverter) : ISinkFactory
 {
-    /// <summary>
-    /// Gets the type ID for this sink factory.
-    /// </summary>
+    /// <inheritdoc/>
     public SinkTypeId TypeId => WellKnownSinkTypes.MongoDB;
 
-    /// <summary>
-    /// Creates a sink for the specified event store, namespace, and read model.
-    /// </summary>
-    /// <param name="eventStore">The event store name.</param>
-    /// <param name="namespace">The namespace name.</param>
-    /// <param name="readModel">The read model definition.</param>
-    /// <returns>A sink instance.</returns>
+    /// <inheritdoc/>
     public ISink CreateFor(EventStoreName eventStore, EventStoreNamespaceName @namespace, ReadModelDefinition readModel)
     {
         var mongoDBConverter = new MongoDBConverter(
