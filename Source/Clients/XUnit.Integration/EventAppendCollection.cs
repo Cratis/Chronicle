ad470aef2254d5fc379e5ba22b006201fceac28c
woksin <sindrewilting@gmail.com>
2026-04-04 08:23:39 +0200

    fix: Remove unused using directive for System.Linq in EventAppendCollection

--- Source/Clients/XUnit.Integration/EventAppendCollection.cs
+++ Source/Clients/XUnit.Integration/EventAppendCollection.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Immutable;
-using System.Linq;
 using Cratis.Chronicle.Auditing;
 using Cratis.Chronicle.Events;
 using Cratis.Chronicle.EventSequences;
