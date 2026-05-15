50055a1a395a43090a31ce81611985c5091d70a3
Einar <einari@me.com>
2026-05-15 02:09:12 +0200

    Add SQL migrations for Tags, Filters, and Source columns
    - v15_1_0 for ReactorDefinitions: add Tags (TEXT) and Filters (TEXT) columns
    - v15_1_0 for ReducerDefinitions: add Tags (TEXT) and Filters (TEXT) columns
    - v15_2_0 for EventTypes: add Source (INTEGER) column for EventTypeSource enum
    
    All use raw SQL (ALTER TABLE ... ADD COLUMN) to avoid Arc extension methods
    that generate junction tables for collection types instead of scalar columns.

---
+++ Source/Kernel/Storage.Sql/EventStores/EventTypes/Migrations/v15_2_0.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cratis.Chronicle.Storage.Sql.EventStores.EventTypes.Migrations;
+
+#nullable disable
+#pragma warning disable SA1600, SA1402, MA0048
+
+[DbContext(typeof(EventStoreDbContext))]
+[Migration($"ES-{WellKnownTableNames.EventTypes}-{nameof(v15_2_0)}")]
+public class v15_2_0 : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql($"ALTER TABLE \"{WellKnownTableNames.EventTypes}\" ADD COLUMN \"Source\" INTEGER");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
---
+++ Source/Kernel/Storage.Sql/EventStores/Reducers/Migrations/v15_1_0.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cratis.Chronicle.Storage.Sql.EventStores.Reducers.Migrations;
+
+#nullable disable
+#pragma warning disable SA1600, SA1402, MA0048
+
+[DbContext(typeof(EventStoreDbContext))]
+[Migration($"ES-{WellKnownTableNames.ReducerDefinitions}-{nameof(v15_1_0)}")]
+public class v15_1_0 : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql($"ALTER TABLE \"{WellKnownTableNames.ReducerDefinitions}\" ADD COLUMN \"Tags\" TEXT");
+        migrationBuilder.Sql($"ALTER TABLE \"{WellKnownTableNames.ReducerDefinitions}\" ADD COLUMN \"Filters\" TEXT");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
