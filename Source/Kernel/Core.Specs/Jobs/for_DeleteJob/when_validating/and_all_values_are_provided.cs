5dea459723bbc279c4d2b051c383eca8cca33540
Einar <einari@me.com>
2026-08-27 13:33:04 +0200

    Add IStorage/IGrainFactory to Jobs specs for the existence checks
    StopJob, DeleteJob, and ResumeJob carry EventStore as EventStoreName
    now and each go through their own command-level job-existence check,
    so their specs need a substituted IStorage and IGrainFactory (mocking
    GetGrain<IJobsManager> and its GetAllJobs()) or validator construction
    throws before the rule under test even runs.

---
+++ Source/Kernel/Core.Specs/Jobs/for_DeleteJob/when_validating/and_all_values_are_provided.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+using Cratis.Arc.Commands;
+using Cratis.Arc.Testing.Commands;
+using Cratis.Chronicle.Concepts;
+using Cratis.Chronicle.Storage;
+using Cratis.Chronicle.Storage.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cratis.Chronicle.Jobs.for_DeleteJob.when_validating;
+
+public class and_all_values_are_provided : Specification
+{
+    readonly CommandScenario<DeleteJob> _scenario = new();
+    readonly Concepts.Jobs.JobId _jobId = Concepts.Jobs.JobId.New();
+    CommandResult _result;
+
+    void Establish()
+    {
+        var storage = Substitute.For<IStorage>();
+        storage.HasEventStore(Arg.Any<EventStoreName>()).Returns(true);
+        _scenario.Services.AddSingleton(storage);
+
+        var jobsManager = Substitute.For<IJobsManager>();
+        jobsManager.GetAllJobs().Returns(new List<JobState> { new() { Id = _jobId } }.ToImmutableList());
+        var grainFactory = Substitute.For<IGrainFactory>();
+        grainFactory.GetGrain<IJobsManager>(Arg.Any<long>(), Arg.Any<string>()).Returns(jobsManager);
+        _scenario.Services.AddSingleton(grainFactory);
+    }
+
+    async Task Because() => _result = await _scenario.Validate(new DeleteJob("some-event-store", "some-namespace", _jobId.Value));
+
+    [Fact] void should_be_valid() => _result.ShouldBeValid();
+}
