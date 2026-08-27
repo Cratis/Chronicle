3b2a016f67c3819b3d30a5abb006887c082aa6f4
Einar <einari@me.com>
2026-08-27 15:40:37 +0200

    Add IStorage to ReadModelDefinitions specs for the event-store check
    CreateReadModel and UpdateReadModelDefinition carry EventStore as
    EventStoreName now, so their existing specs need a substituted
    IStorage or EventStoreNameValidator's construction throws before the
    rule under test even runs.

---
+++ Source/Kernel/Core.Specs/ReadModelDefinitions/for_UpdateReadModelDefinition/when_validating/and_an_index_is_empty.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Arc.Commands;
+using Cratis.Arc.Testing.Commands;
+using Cratis.Chronicle.Concepts;
+using Cratis.Chronicle.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cratis.Chronicle.ReadModelDefinitions.for_UpdateReadModelDefinition.when_validating;
+
+public class and_an_index_is_empty : Specification
+{
+    readonly CommandScenario<UpdateReadModelDefinition> _scenario = new();
+    CommandResult _result;
+
+    void Establish()
+    {
+        var storage = Substitute.For<IStorage>();
+        storage.HasEventStore(Arg.Any<EventStoreName>()).Returns(true);
+        _scenario.Services.AddSingleton(storage);
+    }
+
+    async Task Because() => _result = await _scenario.Validate(new UpdateReadModelDefinition("some-event-store", "some-identifier", "some-container", 1, "{}", [string.Empty]));
+
+    [Fact] void should_not_be_successful() => _result.ShouldNotBeSuccessful();
+    [Fact] void should_have_validation_errors() => _result.ShouldHaveValidationErrors();
+}
---
+++ Source/Kernel/Core.Specs/ReadModelDefinitions/for_UpdateReadModelDefinition/when_validating/and_required_values_are_missing.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Arc.Commands;
+using Cratis.Arc.Testing.Commands;
+using Cratis.Chronicle.Concepts;
+using Cratis.Chronicle.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cratis.Chronicle.ReadModelDefinitions.for_UpdateReadModelDefinition.when_validating;
+
+public class and_required_values_are_missing : Specification
+{
+    readonly CommandScenario<UpdateReadModelDefinition> _scenario = new();
+    CommandResult _result;
+
+    void Establish()
+    {
+        var storage = Substitute.For<IStorage>();
+        storage.HasEventStore(Arg.Any<EventStoreName>()).Returns(true);
+        _scenario.Services.AddSingleton(storage);
+    }
+
+    async Task Because() => _result = await _scenario.Validate(new UpdateReadModelDefinition(string.Empty, string.Empty, string.Empty, 1, string.Empty, null!));
+
+    [Fact] void should_not_be_successful() => _result.ShouldNotBeSuccessful();
+    [Fact] void should_have_validation_errors() => _result.ShouldHaveValidationErrors();
+}
