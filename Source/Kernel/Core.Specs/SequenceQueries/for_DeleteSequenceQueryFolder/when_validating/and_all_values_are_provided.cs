a981b8af9d0f5dd8cbdda87335a563137b6383e5
Einar <einari@me.com>
2026-08-27 15:35:16 +0200

    Add IStorage to SequenceQueries specs for the event-store check
    SaveSequenceQuery, DeleteSequenceQuery, SaveSequenceQueryFolder, and
    DeleteSequenceQueryFolder carry EventStore as EventStoreName now, so
    their existing specs need a substituted IStorage or
    EventStoreNameValidator's construction throws before the rule under
    test even runs.

---
+++ Source/Kernel/Core.Specs/SequenceQueries/for_DeleteSequenceQueryFolder/when_validating/and_all_values_are_provided.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Arc.Commands;
+using Cratis.Arc.Testing.Commands;
+using Cratis.Chronicle.Concepts;
+using Cratis.Chronicle.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cratis.Chronicle.SequenceQueries.for_DeleteSequenceQueryFolder.when_validating;
+
+public class and_all_values_are_provided : Specification
+{
+    readonly CommandScenario<DeleteSequenceQueryFolder> _scenario = new();
+    CommandResult _result;
+
+    void Establish()
+    {
+        var storage = Substitute.For<IStorage>();
+        storage.HasEventStore(Arg.Any<EventStoreName>()).Returns(true);
+        _scenario.Services.AddSingleton(storage);
+    }
+
+    async Task Because() => _result = await _scenario.Validate(new DeleteSequenceQueryFolder("some-event-store", "some-folder"));
+
+    [Fact] void should_be_valid() => _result.ShouldBeValid();
+}
