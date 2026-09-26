ca46c04da6e1d9a9e125383c6a899a3e64c823f7
woksin <sindrewilting@gmail.com>
2026-09-26 13:09:29 +0200

    Revert "Merge remote-tracking branch 'origin/fix/derived-database-name-truncation' into release/consolidated-fixes"
    This reverts commit 1f2a8fa5fbd4b28ff8908b73627286d61ab444ef, reversing
    changes made to a9f899657e271efb5b6e03b151dd69584735be51.

--- Source/Kernel/Storage.Sql/Database.cs
+++ Source/Kernel/Storage.Sql/Database.cs
@@ -18,7 +18,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 #if DEVELOPMENT
 using System.Collections.Frozen;
@@ -45,9 +44,8 @@ namespace Cratis.Chronicle.Storage.Sql;
 /// <param name="eventSequenceMigrator">The <see cref="IEventSequenceMigrator"/> for managing event sequence table migrations.</param>
 /// <param name="uniqueConstraintMigrator">The <see cref="IUniqueConstraintMigrator"/> for managing unique constraint table migrations.</param>
 /// <param name="readModelMigrator">The <see cref="IReadModelMigrator"/> for managing read model table migrations.</param>
-/// <param name="logger">The <see cref="ILogger{TCategoryName}"/>.</param>
 [IgnoreConvention]
-public class Database(IServiceProvider serviceProvider, IOptions<ChronicleOptions> options, IEventSequenceMigrator eventSequenceMigrator, IUniqueConstraintMigrator uniqueConstraintMigrator, IReadModelMigrator readModelMigrator, ILogger<Database> logger) : IDatabase
+public class Database(IServiceProvider serviceProvider, IOptions<ChronicleOptions> options, IEventSequenceMigrator eventSequenceMigrator, IUniqueConstraintMigrator uniqueConstraintMigrator, IReadModelMigrator readModelMigrator) : IDatabase
 {
     static readonly System.Collections.Concurrent.ConcurrentDictionary<string, SemaphoreSlim> _migrationLocks = new();
 
@@ -714,28 +712,20 @@ public class Database(IServiceProvider serviceProvider, IOptions<ChronicleOption
         return databaseType switch
         {
             DatabaseType.Sqlite => ReplaceFilename(suffix),
-            DatabaseType.PostgreSql => AppendToDatabaseName("Database", suffix, databaseType),
-            DatabaseType.SqlServer => AppendToDatabaseName(GetSqlServerDatabaseKey(), suffix, databaseType),
+            DatabaseType.PostgreSql => AppendToDatabaseName("Database", suffix),
+            DatabaseType.SqlServer => AppendToDatabaseName(GetSqlServerDatabaseKey(), suffix),
             _ => connectionString,
         };
     }
 
-    string AppendToDatabaseName(string key, string suffix, DatabaseType databaseType)
+    string AppendToDatabaseName(string key, string suffix)
     {
         var builder = new DbConnectionStringBuilder { ConnectionString = options.Value.Storage.ConnectionDetails };
         if (!builder.TryGetValue(key, out var current))
         {
             return options.Value.Storage.ConnectionDetails;
         }
-
-        var logicalName = $"{current}{suffix}";
-        var physicalName = DerivedDatabaseName.WithinBudget(logicalName, DerivedDatabaseName.MaxBytesFor(databaseType));
-        if (physicalName != logicalName)
-        {
-            logger.DerivedDatabaseNameTruncated(logicalName, physicalName);
-        }
-
-        builder[key] = physicalName;
+        builder[key] = $"{current}{suffix}";
         return builder.ConnectionString;
     }
 
